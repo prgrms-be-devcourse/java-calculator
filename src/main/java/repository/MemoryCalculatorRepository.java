56cc6c8ba5da914cf8b1c9fb25aec23ee1f11b3f
dbslzld15 <dbslzld15@naver.com>
2022-03-22 16:27:57 +0900

    계산식 인메모리 저장 기능 구현

---
+++ src/main/java/repository/MemoryCalculatorRepository.java
@@ -0,0 +1,22 @@
+package repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MemoryCalculatorRepository implements CalculatorRepository{
+    private static Map<Long, String> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public void save(String expression) {
+        store.put(sequence, expression);
+        sequence++;
+    }
+
+    @Override
+    public List<String> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
