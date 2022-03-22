d7b0ae7f1defce159dfe3d27e5896e451f80d1fe
dbslzld15 <dbslzld15@naver.com>
2022-03-22 16:30:17 +0900

    계산식 인메모리 저장 기능 구현

--- src/main/java/repository/CalculatorRepository.java
+++ src/main/java/repository/CalculatorRepository.java
@@ -1,4 +1,9 @@
 package repository;
 
-public class CalculatorRepository {
+import java.util.List;
+
+public interface CalculatorRepository {
+
+    void save(String expression);
+    List<String> findAll();
 }
