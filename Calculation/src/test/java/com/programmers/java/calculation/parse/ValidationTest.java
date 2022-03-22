ab3276a8517c9f5144a77ce0a05faeeceb5554ac
Hyunggeun447 <bopi447@naver.com>
2022-03-23 03:12:12 +0900

    Validation test

--- Calculation/src/test/java/com/programmers/java/calculation/parse/ValidationTest.java
+++ Calculation/src/test/java/com/programmers/java/calculation/parse/ValidationTest.java
@@ -1,26 +1,67 @@
 package com.programmers.java.calculation.parse;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 class ValidationTest {
 
-    Validation validation = new ValidationImpl();
+    ValidationOp validation = new ValidationImpl();
 
     @Test
-    public void validation() throws Exception {
+    public void validate() throws Exception {
 
         String[] input1 = {"24", "+", "12", "/", "5", "-", "3", "*", "2"};
         String[] input2 = {"24", "+", "12", "/", "+", "5", "-", "3", "*", "2"};
+        String[] input3 = {"24", "+", "12", "/", "-", "5", "-", "3", "*", "2"};
+        String[] input4 = {"24", "+", "12", "/", "*", "5", "-", "3", "*", "2"};
         boolean result1 = validation.validate(input1);
         boolean result2 = validation.validate(input2);
+        boolean result3 = validation.validate(input3);
+        boolean result4 = validation.validate(input4);
 
-        Assertions.assertThat(result1).isTrue();
-        Assertions.assertThat(result2).isFalse();
+        assertThat(result1).isTrue();
+        assertThat(result2).isFalse();
+        assertThat(result3).isFalse();
+        assertThat(result4).isFalse();
+    }
 
+    @Test
+    public void validateFirstAndLastOp() throws Exception {
+        String[] input1 = {"24", "+", "12", "/", "5", "-", "3", "*", "2"};
+        String[] input2 = {"24", "+", "12", "/", "5", "-", "3", "*", "2","-"};
+        String[] input3 = {"24", "+", "12", "/", "5", "-", "3", "*", "2","+"};
+        String[] input4 = {"24", "+", "12", "/", "5", "-", "3", "*", "2","*"};
+        String[] input5 = {"24", "+", "12", "/", "5", "-", "3", "*", "2","/"};
 
+        boolean result1 = validation.validateFirstAndLastOp(input1);
+        boolean result2 = validation.validateFirstAndLastOp(input2);
+        boolean result3 = validation.validateFirstAndLastOp(input3);
+        boolean result4 = validation.validateFirstAndLastOp(input4);
+        boolean result5 = validation.validateFirstAndLastOp(input5);
+
+        assertThat(result1).isTrue();
+        assertThat(result2).isFalse();
+        assertThat(result3).isFalse();
+        assertThat(result4).isFalse();
+        assertThat(result5).isFalse();
 
     }
+
+    @Test
+    public void validateString() throws Exception {
+
+        ValidationString validation2 = new ValidationImpl();
+        String input1 = "1+2+4+5+1";
+        String input2 = "1a+2+4+5+1";
+        boolean result1 = validation2.validateString(input1);
+        boolean result2 = validation2.validateString(input2);
+
+        assertThat(result1).isTrue();
+        assertThat(result2).isFalse();
+
+    }
+
 
 }
