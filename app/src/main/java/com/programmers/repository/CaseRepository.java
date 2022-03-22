474e9816463ba55fcc482abe249e0df967d42c2a
dldydgns530@gmail.com <dldydgns530@gmail.com>
2022-03-22 17:19:53 +0900

    Repository 인터페이스 작성

---
+++ app/src/main/java/com/programmers/repository/CaseRepository.java
@@ -0,0 +1,11 @@
+package com.programmers.repository;
+
+import com.programmers.entity.CaseData;
+
+public interface CaseRepository {
+    void save(CaseData caseData);
+
+    CaseData findById(Long memberId);
+
+    void allStoreValue();
+}
