c618dc1c0d4f5b6b1524b8d7b58b8a6ec1525767
dbslzld15 <dbslzld15@naver.com>
2022-03-22 15:49:28 +0900

    프로젝트 생성

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,12 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void 더하기_계산을_한다() {
+        //given
+        //when
+        //then
+    }
+}
