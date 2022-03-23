8c01e2efc2fddd00b23034b329b41f9ebb0a2005
Hyunggeun447 <bopi447@naver.com>
2022-03-23 19:18:21 +0900

    validation + calcutate

---
+++ Calculation/src/main/java/com/programmers/java/calculation/CalTotal.java
@@ -0,0 +1,40 @@
+package com.programmers.java.calculation;
+
+import com.programmers.java.calculation.calculate.Calculate;
+import com.programmers.java.calculation.parse.Parsing;
+import com.programmers.java.calculation.parse.ValidationOp;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class CalTotal {
+
+    private final Parsing parsing;
+    private final ValidationOp validation;
+    private final Calculate calculate;
+
+    public Double calcalateTotal(String input) {
+        String result1 = parsing.removeSpase(input);
+        boolean validateString = validation.validateString(result1);
+        boolean validateContOp = validation.validateContOp(result1);
+        boolean validateFirstOp = validation.validateFirstOp(result1);
+        boolean validateLastOp = validation.validateLastOp(result1);
+
+        if (validateTotal(validateString, validateContOp, validateFirstOp, validateLastOp)) {
+            List<String> resultList = parsing.makeArray(result1);
+            Double cal = calculate.cal(resultList);
+            return cal;
+        } else {
+
+            return null;
+        }
+    }
+
+
+    private boolean validateTotal(boolean validateString, boolean validateContOp, boolean validateFirstOp, boolean validateLastOp) {
+        return validateString && validateContOp && validateFirstOp && validateLastOp;
+    }
+
+
+}
---
+++ Calculation/src/main/java/com/programmers/java/calculation/calculate/CalculateBasicImpl.java
@@ -0,0 +1,48 @@
+package com.programmers.java.calculation.calculate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.*;
+
+public class CalculateBasicImpl implements Calculate {
+
+    @Override
+    public Double cal(List<String> input) {
+        List<String> operatorMulAndDiv = new ArrayList<>(Arrays.asList("*", "/"));
+        List<String> operatorPlusAndMinus = new ArrayList<>(Arrays.asList("+", "-"));
+        List<Double> nums = new ArrayList<>();
+        int temp = 1;
+
+        for (int i = 0; i < input.size(); i++) {
+            if (operatorPlusAndMinus.contains(input.get(i))) {
+                if (Objects.equals(input.get(i), "-")) {
+                    temp = -1;
+                } else {
+                    temp = 1;
+                }
+            } else if (operatorMulAndDiv.contains(input.get(i))) {
+                int lastIndex = nums.size() - 1;
+                double mulDivResult;
+                if (Objects.equals(input.get(i), "*")) {
+                    mulDivResult = nums.get(lastIndex) * Double.parseDouble(input.get(i+1));
+                } else {
+                    if (Integer.parseInt(input.get(i+1)) == 0) {
+                        return null;
+                    }
+                    mulDivResult = nums.get(lastIndex) / Double.parseDouble(input.get(i+1));
+                }
+                nums.set(lastIndex, mulDivResult);
+                i++;
+            } else {
+                Double result = temp * Double.parseDouble(input.get(i));
+                nums.add(result);
+            }
+        }
+
+        Double result = (double) 0;
+        for (Double num : nums) {
+            result += num;
+        }
+        return result;
+    }
+}
