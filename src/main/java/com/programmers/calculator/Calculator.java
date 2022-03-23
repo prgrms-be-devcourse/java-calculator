7e652b489309566b47c15d136fba7b1ce4eccd1e
pppp0722 <poj0722@naver.com>
2022-03-24 03:46:06 +0900

    계산기능 추가

--- src/main/java/com/programmers/calculator/Calculator.java
+++ src/main/java/com/programmers/calculator/Calculator.java
@@ -1,44 +1,77 @@
 package com.programmers.calculator;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.regex.Pattern;
 
 public class Calculator {
-    void play() {
-        Scanner sc = new Scanner(System.in);
-        while(true) {
-            System.out.println("1. 조회\n2. 계산\n3. 종료\n");
-            System.out.print("선택: ");
+    static Map<String, Integer> expressionResult = new LinkedHashMap<>();
 
-            String s = sc.nextLine();
-            System.out.println();
-            boolean close = false;
-            switch(s) {
-                case "1" :
-                    System.out.println("조회");
-                    break;
-                case "2" :
-                    System.out.println("계산");
-                    break;
-                case "3" :
-                    close = true;
-                    break;
-                default :
-                    System.out.println("잘못된 입력");
-                    break;
-            }
-
-            if(close) break;
-            System.out.println();
+    static void calc(String expression) {
+        List<String> postfix = infixToPostfix(expression);
+        if(postfix == null) {
+            System.out.println("잘못된 수식");
+            return;
         }
 
-        sc.close();
+        Stack<String> stack = new Stack<>();
+        for(String s : postfix) {
+            if(Pattern.matches(Regex.num, s)) {
+                stack.push(s);
+            } else {
+                int num2 = Integer.parseInt(stack.pop());
+                int num1 = Integer.parseInt(stack.pop());
+                stack.push(s.equals("+") ? Integer.toString(num1 + num2) :
+                           s.equals("-") ? Integer.toString(num1 - num2) :
+                           s.equals("*") ? Integer.toString(num1 * num2) :
+                                           Integer.toString(num1 / num2));
+            }
+        }
+
+        int answer = Integer.parseInt(stack.pop());
+        expressionResult.put(expression, answer);
+
+        System.out.println(answer);
     }
 
-    int multiply(int a, int b) {
-        return a * b;
+    static void show() {
+
     }
 
-    int multiply(int a, int b, int c) {
-        return a * b * c;
+    static List<String> infixToPostfix(String infix) {
+        List<String> postfix = new ArrayList<>();
+        Stack<String> stack = new Stack<>();
+        String[] numsNSymbols = infix.split(" ");
+        for(String s : numsNSymbols) {
+            if (Pattern.matches(Regex.num, s)) {
+                postfix.add(s);
+            } else if (s.equals("(")) {
+                stack.push(s);
+            } else if (s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/")) {
+                if (stack.isEmpty()) {
+                    stack.push(s);
+                } else {
+                    if (Priority.getPriority(stack.peek()) >= Priority.getPriority(s)) {
+                        postfix.add(stack.pop());
+                        stack.push(s);
+                    } else {
+                        stack.push(s);
+                    }
+                }
+            } else if (s.equals(")")) {
+                while(true) {
+                    String tmp = stack.pop();
+                    if(tmp.equals("(")) break;
+                    postfix.add(tmp);
+                }
+            } else {
+                return null;
+            }
+        }
+
+        while(!stack.isEmpty()) {
+            postfix.add(stack.pop());
+        }
+
+        return postfix;
     }
 }
