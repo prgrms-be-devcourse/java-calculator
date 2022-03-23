79d0f806ca1af3d03a162ec01df46533df29847b
nnagman <ckdgh930314@naver.com>
2022-03-23 14:02:55 +0900

    사칙연산 테스트 및 코드 작성

---
+++ src/main/java/com/programmers/calculator/service/CalcExpression.java
@@ -0,0 +1,23 @@
+package com.programmers.calculator.service;
+
+public class CalcExpression {
+    public Double calcPlus(int a, int b) {
+        return (double) a + b;
+    }
+
+    public Double calcMulti(int a, int b) {
+        return (double) a * b;
+    }
+
+    public Double calcDivi(int a, int b) {
+        if (a == 0 || b == 0) {
+            System.out.println("0으로 나눌 수 없습니다.");
+            return (double) 0;
+        }
+        return (double) a / b;
+    }
+
+    public Double calcMinus(int a, int b) {
+        return (double) a - b;
+    }
+}
---
+++ src/test/java/com/programmers/calculator/service/CalcExpressionTest.java
@@ -0,0 +1,80 @@
+package com.programmers.calculator.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalcExpressionTest {
+    CalcExpression ex = new CalcExpression();
+
+    @Test
+    @DisplayName("더하기 연산의 경우")
+    void calcPlusTest() {
+        // given
+        int a = 1;
+        int b = 1;
+
+        // when
+        Double result = ex.calcPlus(a, b);
+
+        // then
+        assertEquals(a + b, result);
+    }
+
+    @Test
+    @DisplayName("곱셈 연산의 경우")
+    void calcMultiTest() {
+        // given
+        int a = 2;
+        int b = 3;
+
+        // when
+        Double result = ex.calcMulti(a, b);
+
+        // then
+        assertEquals(a * b, result);
+    }
+
+    @Test
+    @DisplayName("나눗셈 연산의 경우")
+    void calcDiviTest() {
+        // given
+        int a = 2;
+        int b = 3;
+
+        // when
+        Double result = ex.calcDivi(a, b);
+
+        // then
+        assertEquals((double) a / b, result);
+    }
+
+    @Test
+    @DisplayName("뺄셈 연산의 경우")
+    void calcMinusTest() {
+        // given
+        int a = 2;
+        int b = 3;
+
+        // when
+        Double result = ex.calcMinus(a, b);
+
+        // then
+        assertEquals(a - b, result);
+    }
+
+    @Test
+    @DisplayName("0으로 나눌 경우")
+    void calcDiviByZeroTest() {
+        // given
+        int a = 2;
+        int b = 0;
+
+        // when
+        Double result = ex.calcDivi(a, b);
+
+        // then
+        assertEquals(0, result);
+    }
+}
