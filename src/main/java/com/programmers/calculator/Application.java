511a8c301b43f4d4e32675c0d1893eb0ba6a6b2e
pppp0722 <poj0722@naver.com>
2022-03-23 18:38:50 +0900

    Application.java 추가

---
+++ src/main/java/com/programmers/calculator/Application.java
@@ -0,0 +1,8 @@
+package com.programmers.calculator;
+
+public class Application {
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        c.play();
+    }
+}
