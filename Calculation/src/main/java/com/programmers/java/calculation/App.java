97a27b94ff27c188237408cd6fd3e0b01d30ca32
Hyunggeun447 <bopi447@naver.com>
2022-03-23 19:21:35 +0900

    Main 클래스 설정

--- Calculation/src/main/java/com/programmers/java/calculation/App.java
+++ Calculation/src/main/java/com/programmers/java/calculation/App.java
@@ -1,11 +1,15 @@
 package com.programmers.java.calculation;
 
 
-import java.util.ArrayList;
+import com.programmers.java.calculation.repository.RepositoryImpl;
 
 public class App {
-
     public static void main(String[] args) {
+        Console console = new Console();
+        CalculationService calculationService = new CalculationService(console, console, new RepositoryImpl());
+
+        calculationService.run();
+
 
     }
 }
