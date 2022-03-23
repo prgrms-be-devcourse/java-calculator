7cd3d1c8295a4334b581fe8fe6224a6e79389baf
Hyunggeun447 <bopi447@naver.com>
2022-03-23 19:19:18 +0900

    Console 설정

--- Calculation/src/main/java/com/programmers/java/calculation/io/Input.java
+++ Calculation/src/main/java/com/programmers/java/calculation/io/Input.java
@@ -1,5 +1,6 @@
 package com.programmers.java.calculation.io;
 
 public interface Input {
+    String input(String s);
 
 }
---
+++ Calculation/src/main/java/com/programmers/java/calculation/io/Output.java
@@ -0,0 +1,10 @@
+package com.programmers.java.calculation.io;
+
+public interface Output {
+
+    void wrongInput();
+
+    void correct(Object answer);
+
+
+}
