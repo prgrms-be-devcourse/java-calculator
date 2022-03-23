579f5937039deb2d30281b7c90e896dc7bfcaacd
pppp0722 <poj0722@naver.com>
2022-03-24 03:44:05 +0900

    Service.java 추가

--- src/main/java/com/programmers/calculator/Application.java
+++ src/main/java/com/programmers/calculator/Application.java
@@ -2,7 +2,7 @@ package com.programmers.calculator;
 
 public class Application {
     public static void main(String[] args) {
-        Calculator c = new Calculator();
-        c.play();
+        Service s = new Service();
+        s.play();
     }
 }
