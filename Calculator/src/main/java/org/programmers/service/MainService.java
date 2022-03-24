045d0ce0446729e11d930b321c0224ac10c385c7
UJ15 <dbwogml15@naver.com>
2022-03-25 02:18:33 +0900

    화면 생성 + 입력 검사 클래스 추가

--- Calculator/src/main/java/org/programmers/service/MainService.java
+++ Calculator/src/main/java/org/programmers/service/MainService.java
@@ -4,8 +4,35 @@ import java.util.Scanner;
 
 public class MainService {
     CalculateService calculateService = new CalculateService();
+    Validation validation = new Validation();
 
-    public void runCalculator(){
+    public void runCalculator() {
+        Scanner sc = new Scanner(System.in);
 
+        while (true) {
+            int select;
+
+            System.out.println("1. 조회\n2. 계산\n");
+            System.out.print("선택 : ");
+
+            select = sc.nextInt();
+            sc.nextLine();
+            System.out.println();
+
+            if (select == 1) {
+                calculateService.find();
+            } else if (select == 2) {
+                String inputEx = sc.nextLine();
+                try {
+                    double result = calculateService.calculateSave(inputEx);
+                    System.out.println(result + "\n");
+                } catch (Exception e) {
+                    System.out.println("다시 입력하세요");
+                }
+
+            } else {
+                System.out.println("그런 선택은 없어요");
+            }
+        }
     }
 }
---
+++ Calculator/src/main/java/org/programmers/service/Validation.java
@@ -0,0 +1,8 @@
+package org.programmers.service;
+
+public class Validation {
+
+    public boolean check(String inputEx){
+        return true;
+    }
+}
