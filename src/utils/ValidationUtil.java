7cdabc4f6d429f3382761234ceb18e5612c8cd92
taesan <pawer02023@gmail.com>
2022-03-25 02:13:40 +0900

    chore: Util클래스 위치 변경
    - Util클래스 위치 변경

---
+++ src/utils/ValidationUtil.java
@@ -0,0 +1,21 @@
+package utils;
+
+public class ValidationUtil {
+
+    public static boolean isNumberWithWhite(char c) {
+        if (isNumber(c))
+            return true;
+        else if (Character.isWhitespace(c))
+            return true;
+        return false;
+    }
+
+    public static boolean isNumber(char c) {
+        return (c >= '0' && c <= '9');
+    }
+
+    public static boolean isOperator(char c) {
+        return (c == '-' || c == '+' || c == '*' || c == '/');
+    }
+
+}
