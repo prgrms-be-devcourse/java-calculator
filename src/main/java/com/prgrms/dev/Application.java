dfa8e87c86a34c51819ec7bcc3b7fa80513271d3
midas <midas117@knou.ac.kr>
2022-03-26 01:42:19 +0900

    feat: Application(main 클래스) 적용 완료

--- src/main/java/com/prgrms/dev/Application.java
+++ src/main/java/com/prgrms/dev/Application.java
@@ -1,7 +1,13 @@
 package com.prgrms.dev;
 
+import com.prgrms.dev.calculator.Calculator;
+import com.prgrms.dev.calculator.io.Operator;
+
 public class Application {
   public static void main(String[] args) {
+    Operator operator = new SimpleOperator();
+    Console console = new Console();
 
+    new Calculator(operator, console, console).run();
   }
 }
