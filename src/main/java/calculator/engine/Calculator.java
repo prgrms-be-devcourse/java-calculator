8e33bd4b7fa727af6653acc0f925894b20a8c170
Seonghyeon Kim <goshk1995@gmail.com>
2022-03-26 18:35:48 +0900

    계산기 객체(계산, 조회, 시작, 종료)

---
+++ src/main/java/calculator/engine/Calculator.java
@@ -0,0 +1,120 @@
+package calculator.engine;
+
+
+import calculator.repository.CalculatorRepository;
+import calculator.repository.ICalculatorRepository;
+
+import java.util.List;
+import java.util.Stack;
+
+public class Calculator implements Runnable {
+    private final ICalculatorRepository repository;
+    private final Console console;
+    private final Postfix postfix;
+
+    public Calculator() {
+        this.repository = new CalculatorRepository();
+        this.postfix = new Postfix();
+        this.console = new Console();
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            String input = console.runMessage();
+            doAction(input);
+        }
+    }
+
+    private void doAction(String input) {
+        switch(input) {
+            case "1":
+                console.print(search());
+                break;
+            case "2":
+                console.print(calculate(console.input()));
+                break;
+            case "3":
+                exit();
+                break;
+            default:
+                doAction(console.errorMessage());
+                break;
+        }
+    }
+
+    public String search() {
+        return repository.search();
+    }
+
+    public String calculate(String formula) {
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+        try {
+            List<String> strings = postfix.makeToPostfix(formula);
+            for(String s : strings) {
+                switch(s) {
+                    case "+":
+                        if (stack.size() >= 2) {
+                            Integer a = stack.pop();
+                            Integer b = stack.pop();
+                            stack.push(a + b);
+                        }
+                        break;
+                    case "-":
+                        if (stack.size() >= 2) {
+                            Integer a = stack.pop();
+                            Integer b = stack.pop();
+                            stack.push(b - a);
+                        }
+                        break;
+                    case "*":
+                        if (stack.size() >= 2) {
+                            Integer a = stack.pop();
+                            Integer b = stack.pop();
+                            stack.push(a * b);
+                        }
+                        break;
+                    case "/":
+                        if (stack.size() >= 2) {
+                            Integer a = stack.pop();
+                            Integer b = stack.pop();
+
+                            try {
+                                if(a == 0) {
+                                    throw new Exception("0으로 값을 나눌 수 없습니다.");
+                                }
+
+                                stack.push(b / a);
+                            } catch(Exception e) {
+                                console.print(e.getMessage());
+                                calculate(console.input());
+                            }
+                        }
+                        break;
+                    case "(":
+                    case ")":
+                        break;
+                    default:
+                        stack.push(Integer.parseInt(s));
+                        break;
+                }
+            }
+        } catch (IllegalAccessException e) {
+            console.print(e.getMessage());
+            calculate(console.input());
+        }
+
+        String answer = String.valueOf(stack.get(0));
+        repository.save(formula, answer);
+
+        sb.append(formula).append("\n").append(answer).append("\n");
+
+        return sb.toString();
+    }
+
+    public void exit() {
+        console.exitMessage();
+        System.exit(0);
+    }
+}
