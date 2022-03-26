9760b029ae7f25c09e4ba777893c8c2da5ba1768
yuminhwan <ymh7532@icloud.com>
2022-03-26 23:18:53 +0900

    feat(Calculator): 후위표기식을 가지고 계산하는 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,11 +1,66 @@
 package calculator;
 
+import java.util.List;
+import java.util.Stack;
+
 import calculator.domain.OperatorType;
+import calculator.utils.ExpressionParser;
 
 public class Calculator {
+
+    private static final String ERROR_INPUT_FORM = "[ERROR] 정상적인 입력이 아닙니다. 다시 입력해주세요.";
+
+    public Double calculateExpression(List<String> expression) {
+        Stack<Double> stack = new Stack<>();
+
+        for (String value : expression) {
+            if (isNumber(value)) {
+                stack.add(Double.valueOf(value));
+                continue;
+            }
+
+            if (isOperator(value)) {
+                validateStackPop(stack);
+
+                double secondNumber = stack.pop();
+                double firstNumber = stack.pop();
+                stack.add(calculate(value, firstNumber, secondNumber));
+            }
+        }
+
+        validateStackSize(stack);
+
+        return roundNumberTwoDecimal(stack.pop());
+    }
+
+    private void validateStackSize(Stack<Double> stack) {
+        if (stack.size() > 1) {
+            throw new IllegalArgumentException(ERROR_INPUT_FORM);
+        }
+    }
+
+    private void validateStackPop(Stack<Double> stack) {
+        if (stack.isEmpty() || stack.size() < 2) {
+            throw new IllegalArgumentException(ERROR_INPUT_FORM);
+        }
+    }
+
+    private double roundNumberTwoDecimal(double result) {
+        return Math.round(result * 100) / 100.0;
+    }
 
     public double calculate(String operator, double firstNumber, double secondNumber) {
         return OperatorType.from(operator)
             .calculate(firstNumber, secondNumber);
+    }
+
+    private boolean isOperator(String inputString) {
+        return ExpressionParser.OPERATOR.matcher(inputString)
+            .matches();
+    }
+
+    private boolean isNumber(String inputString) {
+        return ExpressionParser.NUMBER.matcher(inputString)
+            .matches();
     }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -2,19 +2,59 @@ package calculator;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class CalculatorTest {
 
     Calculator calculator;
 
+    private static Stream<Arguments> provideNormalExpression() { // argument source method
+        return Stream.of(
+            Arguments.of(Arrays.asList("1", "2", "*"), "2"),
+            Arguments.of(Arrays.asList("4", "5", "2", "/", "*"), "10"),
+            Arguments.of(Arrays.asList("1", "2", "3", "*", "+", "4", "5", "/", "-"), "6.20")
+        );
+    }
+
+    private static Stream<Arguments> provideWrongExpression() { // argument source method
+        return Stream.of(
+            Arguments.of(Arrays.asList("+", "1")),
+            Arguments.of(Arrays.asList("4", "5", "2", "/", "*", "3")),
+            Arguments.of(Arrays.asList("1", "2", "3", "*", "+", "4", "5", "/", "-", "/")),
+            Arguments.of(Arrays.asList("0", "1", "/", "*", "+", "4", "5", "/", "-", "/"))
+        );
+    }
+
     @BeforeEach
     void setup() {
         calculator = new Calculator();
+    }
+
+    @DisplayName("정상적인 후위표기식을 주면 계산한다.")
+    @ParameterizedTest
+    @MethodSource("provideNormalExpression")
+    void calculate_expression(List<String> input, double expected) {
+        double result = calculator.calculateExpression(input);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("잘못된 후위표기식을 주면 에러를 발생한다.")
+    @ParameterizedTest
+    @MethodSource("provideWrongExpression")
+    void calculate_expression(List<String> input) {
+        assertThatThrownBy(() -> calculator.calculateExpression(input))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("[ERROR]");
     }
 
     @DisplayName("연산자를 주면 사칙연산을 실시한다.")
