be414a010063703956cdf5530ff6192a9eeefccd
San Kim <waterfog9580@gmail.com>
2022-03-26 20:01:45 +0900

    후위표기식 변환기 구현

---
+++ src/main/java/com/waterfogsw/converter/Converter.java
@@ -0,0 +1,7 @@
+package com.waterfogsw.converter;
+
+import java.util.List;
+
+public interface Converter {
+    List<String> convert(List<String> infixExpr);
+}
---
+++ src/test/java/com/waterfogsw/converter/ConverterTest.java
@@ -0,0 +1,57 @@
+package com.waterfogsw.converter;
+
+import com.waterfogsw.AppConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class ConverterTest {
+    private Converter converter;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        converter = appConfig.converter();
+    }
+
+    @Test
+    public void 후위표기식_변환_테스트_1() throws Exception {
+        // given
+        List<String> infix = new ArrayList<>(Arrays.asList("1", "+", "2", "+", "3"));
+
+        // when
+        List<String> postfix = converter.convert(infix);
+
+        // then
+        Assertions.assertThat(postfix).isEqualTo(Arrays.asList("1", "2", "+", "3", "+"));
+    }
+
+    @Test
+    public void 후위표기식_변환_테스트_2() throws Exception {
+        // given
+        List<String> infix = new ArrayList<>(Arrays.asList("1", "-", "2", "-", "3"));
+
+        // when
+        List<String> postfix = converter.convert(infix);
+
+        // then
+        Assertions.assertThat(postfix).isEqualTo(Arrays.asList("1", "2", "-", "3", "-"));
+    }
+
+    @Test
+    public void 후위표기식_변환_테스트_3() throws Exception {
+        // given
+        List<String> infix = new ArrayList<>(Arrays.asList("2", "/", "(", "2", "+", "2", ")"));
+
+        // when
+        List<String> postfix = converter.convert(infix);
+
+        // then
+        Assertions.assertThat(postfix).isEqualTo(Arrays.asList("2", "2", "2", "+", "/"));
+    }
+
+}
