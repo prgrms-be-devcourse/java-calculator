9b7ea5930b3ce3055644ed05b88d2a3ecbbbe840
Seonghyeon Kim <goshk1995@gmail.com>
2022-03-26 18:33:47 +0900

    입출력 구현 콘솔 객체

---
+++ src/main/java/calculator/engine/Console.java
@@ -0,0 +1,43 @@
+package calculator.engine;
+
+
+import calculator.io.Input;
+import calculator.io.Output;
+
+import java.util.Scanner;
+
+public class Console implements Input, Output {
+    private final Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public String input() {
+        return scanner.nextLine();
+    }
+
+    @Override
+    public String runMessage() {
+        System.out.println("1. 조회");
+        System.out.println("2. 계산");
+        System.out.println("3. 종료");
+        String input = input();
+        System.out.println("선택 : " + input);
+
+        return input;
+    }
+
+    @Override
+    public void print(String str) {
+        System.out.println(str);
+    }
+
+    @Override
+    public String errorMessage() {
+        System.out.println("입력값을 다시 확인해주세요.\n");
+        return runMessage();
+    }
+
+    @Override
+    public void exitMessage() {
+        System.out.println("계산기를 종료합니다.");
+    }
+}
