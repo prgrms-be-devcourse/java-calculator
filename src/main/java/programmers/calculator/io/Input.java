6b9995211da16cac514d206c94f35f20af775ed5
ordilov <ordilov@gmail.com>
2022-03-26 13:23:35 +0900

    feat: 콘솔을 추가하라

---
+++ src/main/java/programmers/calculator/io/Input.java
@@ -0,0 +1,6 @@
+package programmers.calculator.io;
+
+public interface Input {
+
+  String read();
+}
---
+++ src/main/java/programmers/calculator/io/Output.java
@@ -0,0 +1,8 @@
+package programmers.calculator.io;
+
+public interface Output {
+
+  void printLine(String message);
+
+  void printMenu();
+}
