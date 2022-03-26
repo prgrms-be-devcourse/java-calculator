5672b6922434beb233bd9fb5a0b39934c230434c
San Kim <waterfog9580@gmail.com>
2022-03-26 20:03:01 +0900

    토큰화 구현 및 테스트

---
+++ src/main/java/com/waterfogsw/parser/Parser.java
@@ -0,0 +1,7 @@
+package com.waterfogsw.parser;
+
+import java.util.List;
+
+public interface Parser {
+    List<String> parse(String exprStr);
+}
---
+++ src/test/java/com/waterfogsw/parser/ParserTest.java
@@ -0,0 +1,31 @@
+package com.waterfogsw.parser;
+
+import com.waterfogsw.AppConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+class ParserTest {
+    private Parser parser;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        parser = appConfig.parser();
+    }
+
+    @Test
+    public void 문자열_파싱_테스트1() throws Exception {
+        // given
+        String exprStr = "2 + 2 + 2";
+
+        // when
+        List<String> exprList = parser.parse(exprStr);
+
+        // then
+        Assertions.assertThat(exprList).isEqualTo(Arrays.asList("2", "+", "2", "+", "2"));
+    }
+}
