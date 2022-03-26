f3b71859e73617d66477c06504de396cd1e6f2b0
yuminhwan <ymh7532@icloud.com>
2022-03-26 20:12:19 +0900

    feat: 중위 표기식을 후위 표기식으로 전화하는 기능 구현

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,9 +1,52 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class Calculator {
+
+    public static final String DELIMITER = " ";
 
     public double calculate(String operator, double firstNumber, double secondNumber) {
         return OperatorType.from(operator)
             .calculate(firstNumber, secondNumber);
+    }
+
+    public List<String> parsePostfix(String infix) {
+        Stack<String> converter = new Stack<>();
+        List<String> result = new ArrayList<>();
+
+        String[] inputStrings = split(infix);
+
+        for (String inputString : inputStrings) {
+            if (inputString.matches("\\d+(.\\d+)?")) {
+                result.add(inputString);
+            } else if (inputString.matches("[+-/*]")) {
+                if (converter.isEmpty()) {
+                    converter.add(inputString);
+                } else {
+                    while (!converter.isEmpty() && OperatorType.from(converter.peek()).getValue() >= OperatorType.from(
+                        inputString).getValue()) {
+                        result.add(converter.pop());
+                    }
+                    converter.add(inputString);
+                }
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        while (!converter.isEmpty()) {
+            result.add(converter.pop());
+        }
+        return result;
+    }
+
+    private String[] split(String inputString) {
+        if (inputString == null || inputString.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 정상적인 입력이 아닙니다. 다시 입력해주세요.");
+        }
+        return inputString.split(DELIMITER);
     }
 }
--- src/main/java/calculator/OperatorType.java
+++ src/main/java/calculator/OperatorType.java
@@ -5,22 +5,24 @@ import java.util.function.BinaryOperator;
 
 public enum OperatorType {
 
-    PLUS("+", Double::sum),
-    MINUS("-", (firstNumber, secondNumber) -> firstNumber - secondNumber),
-    MULTIPLY("*", (firstNumber, secondNumber) -> firstNumber * secondNumber),
+    PLUS("+", Double::sum, 1),
+    MINUS("-", (firstNumber, secondNumber) -> firstNumber - secondNumber, 1),
+    MULTIPLY("*", (firstNumber, secondNumber) -> firstNumber * secondNumber, 2),
     DIVIDE("/", (firstNumber, secondNumber) -> {
         if (secondNumber == 0) {
             throw new IllegalArgumentException("[ERROR] 0으로 나눌 수 없습니다. 다시 입력해주세요.");
         }
         return firstNumber / secondNumber;
-    });
+    }, 3);
 
     private final String operator;
     private final BinaryOperator<Double> expression;
+    private final int value;
 
-    OperatorType(String operator, BinaryOperator<Double> expression) {
+    OperatorType(String operator, BinaryOperator<Double> expression, int value) {
         this.operator = operator;
         this.expression = expression;
+        this.value = value;
     }
 
     public static OperatorType from(String operator) {
@@ -28,6 +30,10 @@ public enum OperatorType {
             .filter(type -> type.operator.equals(operator))
             .findFirst()
             .orElseThrow(() -> new IllegalArgumentException("[ERROR] 연산자가 아닙니다. 다시 입력해주세요."));
+    }
+
+    public int getValue() {
+        return value;
     }
 
     public double calculate(double firstNumber, double secondNumber) {
