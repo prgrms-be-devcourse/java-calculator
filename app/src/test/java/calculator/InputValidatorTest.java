e6565709b55702be976723aa0e386dc66f554596
dojinyou <djyou128@gmail.com>
2022-03-28 04:52:41 +0900

    Test(calculator): StringCalculator 테스트 작성 #2

--- app/src/test/java/calculator/InputValidatorTest.java
+++ app/src/test/java/calculator/InputValidatorTest.java
@@ -1,45 +1,26 @@
 package calculator;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class InputValidatorTest {
+    static private InputValidator inputValidator;
+    @BeforeAll
+    static void setUp() {
+        inputValidator = new InputValidator();
+    }
+
     @Test
     @DisplayName("입력값_검증_테스트")
     void valid() {
-        assertThat(InputValidator.valid(null)).isFalse();
-        assertThat(InputValidator.valid(new String[]{})).isFalse();
-        assertThat(InputValidator.valid(new String[]{"1"})).isFalse();
-        assertThat(InputValidator.valid(new String[]{"1","+"})).isFalse();
-        assertThat(InputValidator.valid(new String[]{"1","+","2"})).isTrue();
-        assertThat(InputValidator.valid(new String[]{"1","+","2","3"})).isFalse();
-    }
-
-    @Test
-    @DisplayName("숫자_검증_테스트")
-    void isNumber() {
-        assertThat(InputValidator.isNumber(null)).isFalse();
-        assertThat(InputValidator.isNumber("")).isFalse();
-        assertThat(InputValidator.isNumber(" ")).isFalse();
-        assertThat(InputValidator.isNumber("1")).isTrue();
-        assertThat(InputValidator.isNumber("1.0")).isTrue();
-        assertThat(InputValidator.isNumber("-1.0")).isTrue();
-        assertThat(InputValidator.isNumber("-")).isFalse();
-    }
-
-    @Test
-    @DisplayName("연산자_검증_테스트")
-    void isOperator() {
-        assertThat(InputValidator.isOperator(null)).isFalse();
-        assertThat(InputValidator.isOperator("")).isFalse();
-        assertThat(InputValidator.isOperator(" ")).isFalse();
-        assertThat(InputValidator.isOperator("+")).isTrue();
-        assertThat(InputValidator.isOperator("-")).isTrue();
-        assertThat(InputValidator.isOperator("*")).isTrue();
-        assertThat(InputValidator.isOperator("/")).isTrue();
-        assertThat(InputValidator.isOperator("/+")).isFalse();
-        assertThat(InputValidator.isOperator("1")).isFalse();
+        assertThat(inputValidator.valid(null)).isFalse();
+        assertThat(inputValidator.valid(new String[]{})).isFalse();
+        assertThat(inputValidator.valid(new String[]{"1"})).isFalse();
+        assertThat(inputValidator.valid(new String[]{"1","+"})).isFalse();
+        assertThat(inputValidator.valid(new String[]{"1","+","2"})).isTrue();
+        assertThat(inputValidator.valid(new String[]{"1","+","2","3"})).isFalse();
     }
 }
---
+++ app/src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,27 @@
+package calculator;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class StringCalculatorTest {
+    static private StringCalculator stringCalculator;
+    @BeforeAll
+    static void setUp(){
+        stringCalculator = new StringCalculator();
+    }
+    @Test
+    @DisplayName("문자열_계산기_계산_테스트")
+    void calculate() throws Exception {
+        assertThatThrownBy(()->stringCalculator.calculate(null)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()->stringCalculator.calculate("")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()->stringCalculator.calculate("1 + 2 / 0")).isInstanceOf(IllegalArgumentException.class);
+        assertThat(stringCalculator.calculate("1 + 2")).isEqualTo(3.0);
+        assertThat(stringCalculator.calculate("1 * 2")).isEqualTo(2.0);
+        assertThat(stringCalculator.calculate("1 + 2 / 2")).isEqualTo(2.0);
+        assertThat(stringCalculator.calculate("1 / 2 / 2")).isEqualTo(0.25);
+    }
+}
