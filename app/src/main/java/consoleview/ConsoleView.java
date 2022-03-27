8c03c864e0bbf6e28559b8445d242fa167298001
dojinyou <djyou128@gmail.com>
2022-03-28 06:05:31 +0900

    Refact(consoleview): consoleview refactoring

--- app/src/main/java/consoleview/ConsoleView.java
+++ app/src/main/java/consoleview/ConsoleView.java
@@ -1,22 +1,46 @@
 package consoleview;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class ConsoleView {
-    private final String mainMessage = "1. 조회\n2. 계산\n\n선택 : ";
+    private final String mainMessage = "1. 조회\n2. 계산\n3. 종료\n\n선택 : ";
+    private final String newline = "\n";
+    private final String calculatorMessage = "계산식 : ";
+    private final String errorMessage = "잘못된 입력입니다.\n";
+    private Scanner sc;
+
+    public ConsoleView() {
+        sc = new Scanner(System.in);
+    }
+
     public void display() {
         this.display(mainMessage);
     }
+
     public void display(String message) {
         System.out.print(message);
     }
 
     public String getUserInput() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String userInput = br.readLine();
-        this.display("\n");
-        return userInput;
+        return sc.nextLine();
+    }
+
+    public void displayBlankLine() {
+        this.display(newline);
+    }
+
+    public void displayCalculator() {
+        this.displayBlankLine();
+        this.display(calculatorMessage);
+    }
+
+    public void displayErrorMessage() {
+        this.display(errorMessage);
+        this.displayBlankLine();
+    }
+
+    public void close() {
+        sc.close();
     }
 }
