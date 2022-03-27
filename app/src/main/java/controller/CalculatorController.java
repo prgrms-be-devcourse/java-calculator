6abdb95485c3a41fbece0bcdaae0f2330a22bc55
dojinyou <djyou128@gmail.com>
2022-03-28 06:48:01 +0900

    Refact(consoleview): closing message 추가 #4

--- app/src/main/java/controller/CalculatorController.java
+++ app/src/main/java/controller/CalculatorController.java
@@ -2,16 +2,19 @@ package controller;
 
 import calculator.StringCalculator;
 import consoleview.ConsoleView;
+import database.Database;
 
 import java.io.IOException;
 
 public class CalculatorController {
     ConsoleView consoleView;
     StringCalculator stringCalculator;
+    Database db;
 
     public CalculatorController() {
         this.consoleView = new ConsoleView();
         this.stringCalculator = new StringCalculator();
+        this.db = new Database();
     }
 
     public String displayAndGetUserInput() throws IOException {
@@ -25,13 +28,19 @@ public class CalculatorController {
 
         String userInput = consoleView.getUserInput();
         double result = this.stringCalculator.calculate(userInput);
-
+        db.add(userInput, result);
         consoleView.display(Double.toString(result));
         consoleView.displayBlankLine();
         consoleView.displayBlankLine();
     }
 
     public void getHistory() {
+        consoleView.displayBlankLine();
+        String[] results = db.findAll();
+        for (String prevFormula:results){
+            consoleView.display(prevFormula);
+        }
+        consoleView.displayBlankLine();
     }
 
     public void run() {
@@ -39,7 +48,8 @@ public class CalculatorController {
             try {
                 int selectedNumber = Integer.parseInt(this.displayAndGetUserInput());
                 if (selectedNumber == 3) {
-                    this.consoleView.close();
+                    consoleView.displayCloseMessage();
+                    consoleView.close();
                     break;
                 }
                 assignUserInput(selectedNumber);
