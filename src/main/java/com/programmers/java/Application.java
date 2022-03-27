7a2768102e49edd85a56cd5919aea658e25da75e
HYEBIN,PARK <hyeqls3742@gmail.com>
2022-03-27 17:02:45 +0900

    App !

--- src/main/java/com/programmers/java/Application.java
+++ src/main/java/com/programmers/java/Application.java
@@ -1,12 +1,7 @@
 package com.programmers.java;
-
-import java.util.StringTokenizer;
-
 public class Application {
     public static void main(String[] args){
-        Calculator c = new Calculator();
-        System.out.println("3 + 2 + 1 * 2");
-        System.out.println(c.PostFixForm(new StringTokenizer("3 + 2 + 1 * 2")));
-        System.out.println(c.Calc(c.PostFixForm(new StringTokenizer("3 + 2 + 1 * 2"))));
+        Console console = new Console();
+        new Calculator(console,console).run();
     }
 }
