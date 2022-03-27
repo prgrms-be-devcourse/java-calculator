52b4adb0c4ad83569a1de7a92383b4be0e03f7fb
dojinyou <djyou128@gmail.com>
2022-03-27 23:49:03 +0900

    Feat(calculator): 나ë누기 기능 구현 #2

--- app/src/main/java/calculator/Calculator.java
+++ app/src/main/java/calculator/Calculator.java
@@ -12,4 +12,9 @@ public class Calculator {
     public static double multiply(double operand1, double operand2) {
         return operand1*operand2;
     }
+
+    public static double divied(double operand1, double operand2){
+        if(operand2 == 0.0) { throw new IllegalArgumentException(); }
+        return operand1/operand2;
+    }
 }
