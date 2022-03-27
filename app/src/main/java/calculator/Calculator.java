53250b13a4ac45b411f9c585f0f840029e94ccd6
dojinyou <djyou128@gmail.com>
2022-03-28 04:29:12 +0900

    Feat(calculator): calculator static 제거

--- app/src/main/java/calculator/Calculator.java
+++ app/src/main/java/calculator/Calculator.java
@@ -1,19 +1,19 @@
 package calculator;
 
 public class Calculator {
-    public static double plus(double operand1, double operand2) {
+    public double plus(double operand1, double operand2) {
         return operand1+operand2;
     }
 
-    public static double minus(double operand1, double operand2) {
+    public double minus(double operand1, double operand2) {
         return operand1-operand2;
     }
 
-    public static double multiply(double operand1, double operand2) {
+    public double multiply(double operand1, double operand2) {
         return operand1*operand2;
     }
 
-    public static double divied(double operand1, double operand2){
+    public double divied(double operand1, double operand2){
         if(operand2 == 0.0) { throw new IllegalArgumentException(); }
         return operand1/operand2;
     }
