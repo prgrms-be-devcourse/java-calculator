7b4a707ff7dce060794cb117b8e76ad0e36ab030
dojinyou <djyou128@gmail.com>
2022-03-28 05:21:06 +0900

    Feat(consoleview): 유저입력기능 구현 #3

--- app/src/main/java/consoleview/ConsoleView.java
+++ app/src/main/java/consoleview/ConsoleView.java
@@ -1,11 +1,22 @@
 package consoleview;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleView {
     private final String mainMessage = "1. 조회\n2. 계산\n\n선택 : ";
     public void display() {
         this.display(mainMessage);
     }
     public void display(String message) {
-        System.out.println(message);
+        System.out.print(message);
+    }
+
+    public String getUserInput() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String userInput = br.readLine();
+        this.display("\n");
+        return userInput;
     }
 }
