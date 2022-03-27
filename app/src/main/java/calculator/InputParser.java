1c3a3b1119bdd2a419d6861f7bcbf7be57bf9d5f
dojinyou <djyou128@gmail.com>
2022-03-28 04:53:37 +0900

    Feat(calculator): StringCalculator 구현

--- app/src/main/java/calculator/InputParser.java
+++ app/src/main/java/calculator/InputParser.java
@@ -3,13 +3,20 @@ package calculator;
 import java.util.Arrays;
 
 public class InputParser {
-    private String seperator = " ";
+    private InputValidator inputValidator;
+    private final String seperator;
+
+    InputParser() {
+        this.inputValidator = new InputValidator();
+        this.seperator = " ";
+    }
+
     public String[] parse(String inputFormula) {
         if(inputFormula == null) throw new IllegalArgumentException();
-        String[] splitedInputFormula = inputFormula.split(seperator);
+        String[] splittedInputFormula = inputFormula.split(seperator);
 
-        if(!InputValidator.valid(splitedInputFormula)) throw new IllegalArgumentException();
+        if(!inputValidator.valid(splittedInputFormula)) throw new IllegalArgumentException();
 
-        return PostfixConvertor.convert(splitedInputFormula);
+        return PostfixConvertor.convert(splittedInputFormula);
     }
 }
