6336dc82a79bc6de3502eacb24d75ff5341fc5e5
dojinyou <djyou128@gmail.com>
2022-03-28 04:31:03 +0900

    Test(calculator): 입력 변환기 객체 생성 추가 #2

--- app/src/test/java/calculator/InputParserTest.java
+++ app/src/test/java/calculator/InputParserTest.java
@@ -1,18 +1,24 @@
 package calculator;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class InputParserTest {
+    static InputParser inputParser;
+    @BeforeAll
+    static void setUp() {
+        inputParser = new InputParser();
+    }
     @Test
     @DisplayName("입력 변환기 테스트")
     void parse(){
-        assertThatThrownBy(()->InputParser.parse(null)).isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(()->InputParser.parse("")).isInstanceOf(IllegalArgumentException.class);
-        assertThat(InputParser.parse("1 + 2")).isEqualTo(new String[] {"1", "2","+"});
-        assertThat(InputParser.parse("1 + 2 * 3")).isEqualTo(new String[] {"1", "2", "3", "*", "+"});
-        assertThat(InputParser.parse("1 * 2 + 3 / 0")).isEqualTo(new String[] {"1", "2", "*", "3", "0", "/", "+"});
+        assertThatThrownBy(()->inputParser.parse(null)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()->inputParser.parse("")).isInstanceOf(IllegalArgumentException.class);
+        assertThat(inputParser.parse("1 + 2")).isEqualTo(new String[] {"1", "2","+"});
+        assertThat(inputParser.parse("1 + 2 * 3")).isEqualTo(new String[] {"1", "2", "3", "*", "+"});
+        assertThat(inputParser.parse("1 * 2 + 3 / 0")).isEqualTo(new String[] {"1", "2", "*", "3", "0", "/", "+"});
     }
 }
