1e67bea8b2b7aa1fdfb8e9ecb6a1c2cf6055733c
HYEBIN,PARK <hyeqls3742@gmail.com>
2022-03-27 17:00:46 +0900

    Calculator 실행 부분 구현

---
+++ src/main/java/com/programmers/java/Calculator.java
@@ -0,0 +1,75 @@
+package com.programmers.java;
+
+import com.programmers.java.engine.io.Input;
+import com.programmers.java.engine.io.Output;
+import com.programmers.java.engine.model.PostFixFormula;
+import com.programmers.java.engine.model.ValidFormula;
+import com.programmers.java.engine.repository.FormulaRepo;
+import com.programmers.java.engine.service.CalcService;
+import com.programmers.java.engine.service.Function;
+import com.programmers.java.engine.service.PostFixService;
+import com.programmers.java.engine.service.ValidationService;
+
+import java.util.Optional;
+
+public class Calculator implements Runnable, Function {
+    ValidationService validService = new ValidationService();
+    PostFixService postFixService = new PostFixService();
+    FormulaRepo formulaRepo = new FormulaRepo();
+    CalcService calcService = new CalcService();
+
+    private final int INQUERY = 1;
+    private final int CALCULATE = 2;
+
+    private Input input;
+    private Output output;
+
+    public Calculator(Input input, Output output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    @Override
+    public void run() {
+
+        while (true) {
+            String inputString = input.returnInput("1. 조회" + "\n2. 계산");
+            if (inputString.isEmpty())
+                break;
+
+            int selectNum = parse(inputString);
+            if (selectNum == INQUERY) {
+                input.inquireInput("선택 : " + selectNum);
+                if (formulaRepo.size() < 1)
+                    output.errorEmptyRepo();
+                else
+                    formulaRepo.inquire();
+            } else if (selectNum == CALCULATE) {
+                String inputFormula = input.returnInput("선택 : " + selectNum +", 식을 입력해주세요 !");
+                Optional<ValidFormula> validFormula = validService.Validation(inputFormula);
+                if (validFormula.isEmpty()) {
+                    output.inputError();
+                } else {
+                    PostFixFormula postFixFormula = new PostFixFormula(postFixService.makePostFixFormula(validFormula.get()));
+                    Long result = calcService.calculate(postFixFormula.getFormula());
+                    output.printCalcResult(result);
+                    formulaRepo.save(inputFormula, result);
+                }
+            } else {
+                output.inputError();
+            }
+        }
+    }
+
+    private int parse(String inputString) {
+        if (inputString.length() > 1 || !isStrDigit(inputString)) {
+            return -1;
+        } else if (Integer.parseInt(inputString) == 1) {
+            return 1;
+        } else if (Integer.parseInt(inputString) == 2) {
+            return 2;
+        } else {
+            return -1;
+        }
+    }
+}
