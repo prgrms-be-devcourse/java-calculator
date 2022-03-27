a5f90bf3be812f003de6d55efdabb821ab2fd8d2
dhkstnaos <dhkstnaos@gmail.com>
2022-03-27 18:16:11 +0900

    Add Option 예외 처리

--- calculator/src/test/java/OptionTest.java
+++ calculator/src/test/java/OptionTest.java
@@ -2,6 +2,8 @@ import org.junit.jupiter.api.Test;
 
 import org.assertj.core.api.Assertions;
 
+import java.util.NoSuchElementException;
+
 class OptionTest {
 
     @Test
@@ -15,12 +17,19 @@ class OptionTest {
     }
 
     @Test
-    void 비정상_입력() {
+    void _12가_아닌_숫자를_입력() {
         //given
         String input = "3";
-        //when
-        Option option = Option.find(input);
         //then
-        Assertions.assertThat(option).isEqualByComparingTo(Option.CALCULATE);
+        Assertions.assertThatIllegalArgumentException().isThrownBy(() -> Option.find(input));
+
+    }
+
+    @Test
+    void _숫자가_아닌_문자를_입력() {
+        //given
+        String input = "a";
+        //then
+        Assertions.assertThatIllegalArgumentException().isThrownBy(() -> Option.find(input));
     }
 }
