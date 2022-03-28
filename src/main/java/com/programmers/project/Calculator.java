b7be3c417e806a330b8f99c3b0eca17e7c3d72e8
suy2on <tnwjdrms@naver.com>
2022-03-28 19:30:34 +0900

    # feat : 입력수식 공백추가로 바꿈

--- src/main/java/com/programmers/project/Calculator.java
+++ src/main/java/com/programmers/project/Calculator.java
@@ -4,20 +4,19 @@ import com.programmers.project.io.Console;
 import com.programmers.project.io.Input;
 
 import java.time.temporal.ChronoField;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
+import java.util.*;
 
 
 public class Calculator {
 
     private Console console = new Console();
+    private List<String> operator = new ArrayList<>(Arrays.asList("+","-","/","*")); // 연산자
 
     public String menu() {
         return console.menu();
     }
 
-    public String priority(String s) {
+    public Queue<String> priority(String s) { // 후위연산식으로 변환
         HashMap<String, Integer> pr = new HashMap<>(); // 우선순위 저장
         pr.put("+", 1);
         pr.put("-", 1);
@@ -25,29 +24,29 @@ public class Calculator {
         pr.put("/", 2);
 
         Stack<String> stack = new Stack<>();
-        int start = 0;
-        StringBuffer result = new StringBuffer();
+        String[] parts = s.split(" ");
 
-        for (int i = 0; i < s.length(); i++) {
-            if (!Character.isDigit(s.charAt(i))) {
-                String num = s.substring(start, i);
-                result.append(num);
+        Queue<String> result = new LinkedList<>();
 
-                if (!stack.isEmpty() && pr.get(stack.peek()) >= pr.get(s.substring(i, i + 1))) { // 스택에 있는 수가 우선순위가 높거나 같을 때
-                    result.append(stack.pop());
-                }
-                stack.push(s.substring(i, i + 1));
-                start = i+1;
+        for (int i = 0; i < parts.length; i++) {
+            if (i%2 == 0) {  // 숫자라면
+                result.offer(parts[i]);
             }
-        }
 
-        stack.push(s.substring(start,s.length()));  // 마지막 숫자 넣어주기 
+            else { // 연산자라면
+                if (!stack.isEmpty() && pr.get(stack.peek()) >= pr.get(parts[i])) { // 스택에 있는 수가 우선순위가 높거나 같을 때
+                    result.offer(stack.pop()); // 큐에 넣기
+                }
+                stack.push(parts[i]);  // 스택에 넣기
+            }
+
+        }
 
         while (!stack.isEmpty()) {
-            result.append(stack.pop());
+            result.offer(stack.pop());
         }
 
-        return result.toString();
+        return result;
     }
 
 
