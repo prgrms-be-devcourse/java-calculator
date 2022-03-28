20f3150d84e3551767ef276c5ae4f106616698fe
dbslzld15 <dbslzld15@naver.com>
2022-03-28 21:20:46 +0900

    수식 검증 예외처리 테스트 코드 구현

--- src/test/java/service/ExpressionServiceTest.java
+++ src/test/java/service/ExpressionServiceTest.java
@@ -88,21 +88,33 @@ class ExpressionServiceTest {
     @Test
     public void 빈공백이_들어오면_오류가_발생한다(){
         //given
+        String expression = "";
         //when
         //then
+        assertThatThrownBy(() -> expressionService.calculateExpression(expression))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("연산자와 숫자가 제대로 입력되지 않았습니다.");
     }
 
     @Test
     public void 연산자와_피연산자의_순서가_맞지않을때_오류가_발생한다(){
         //given
+        String expression = "* 1 - 2";
         //when
         //then
+        assertThatThrownBy(() -> expressionService.calculateExpression(expression))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("연산자와 숫자가 제대로 입력되지 않았습니다.");
     }
 
     @Test
     public void 연산자_혹은_숫자이외에_다른문자가_입력되면_오류가_발생한다(){
         //given
+        String expression = "a 1 - 2";
         //when
         //then
+        assertThatThrownBy(() -> expressionService.calculateExpression(expression))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("연산자 혹은 숫자만 입력해주세요");
     }
 }
