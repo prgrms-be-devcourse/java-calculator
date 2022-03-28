2e07c428125e061f76c96cddbbbe804c0d14ffb3
dbslzld15 <dbslzld15@naver.com>
2022-03-28 12:15:09 +0900

    사칙연산 성공 테스트 케이스 구현

---
+++ src/test/java/service/ExpressionServiceTest.java
@@ -0,0 +1,97 @@
+package service;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import repository.ExpressionRepository;
+import repository.MemoryExpressionRepository;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ExpressionServiceTest {
+    ExpressionRepository expressionRepository = new MemoryExpressionRepository();
+    ExpressionService expressionService = new ExpressionService(expressionRepository);
+
+    @Test
+    public void 덧셈계산_성공() {
+        //given
+        String expression = "1 + 2";
+        //when
+        double resultNum = expressionService.calculateExpression(expression);
+        //then
+        assertThat(resultNum).isEqualTo(3);
+    }
+
+    @Test
+    public void 뺄셈계산_성공() {
+        //given
+        String expression = "1 - 2";
+        //when
+        double resultNum = expressionService.calculateExpression(expression);
+        //then
+        assertThat(resultNum).isEqualTo(-1);
+    }
+
+    @Test
+    public void 곱셈계산_성공() {
+        //given
+        String expression = "1 * 2";
+        //when
+        double resultNum = expressionService.calculateExpression(expression);
+        //then
+        assertThat(resultNum).isEqualTo(2);
+    }
+
+    @Test
+    public void 나눗셈계산_성공() {
+        //given
+        String expression = "4 / 2";
+        //when
+        double resultNum = expressionService.calculateExpression(expression);
+        //then
+        assertThat(resultNum).isEqualTo(2);
+    }
+
+    @Test
+    public void 우선순위_계산_성공() {
+        //given
+        List<String> expressions = Arrays.asList("4 + 9 / 3 - 1 + 2 * 10", "20 / 4 + 9 - 10 * 5 / 2");
+        //when
+        List<Double> resultNums = expressions
+                .stream()
+                .map(exp -> expressionService.calculateExpression(exp))
+                .collect(Collectors.toList());
+        //then
+        assertThat(resultNums).containsExactly((double)26,(double)-11);
+    }
+
+    @Test
+    public void 사칙연산_수행후_계산된_이력을_찾기_성공(){
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    public void 빈공백이_들어오면_오류가_발생한다(){
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    public void 연산자와_피연산자의_순서가_맞지않을때_오류가_발생한다(){
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    public void 연산자_혹은_숫자이외에_다른문자가_입력되면_오류가_발생한다(){
+        //given
+        //when
+        //then
+    }
+}
