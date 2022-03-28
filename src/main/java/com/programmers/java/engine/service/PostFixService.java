e76be813b44efd572a069315925124983541a79e
HYEBIN,PARK <hyeqls3742@gmail.com>
2022-03-29 03:01:10 +0900

    fix : 후위표기법 구현 로직 전면 수정

--- src/main/java/com/programmers/java/engine/service/PostFixService.java
+++ src/main/java/com/programmers/java/engine/service/PostFixService.java
@@ -1,32 +1,43 @@
 package com.programmers.java.engine.service;
 
-import com.programmers.java.engine.model.ValidFormula;
+import com.programmers.java.engine.model.Formula;
 
 import java.util.*;
 
 public class PostFixService {
-    static StringBuilder sb;
-    static Queue<String> operatorStack;
-
-    public String[] makePostFixFormula(ValidFormula validFormula) {
-        operatorStack = new LinkedList<>();
-        sb = new StringBuilder();
-
+    public String[] makePostFixFormula(Formula validFormula) {
+        Stack<String> operatorStack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < validFormula.getFormula().length; i++) {
             String str = validFormula.getFormula()[i];
 
-            if (i % 2 == 0) sb.append(str).append(" ");
-
-            else if (str.equals("*") || str.equals("/")) {
-                sb.append(validFormula.getFormula()[i + 1]).append(" ");
+            if (i % 2 == 0) {
                 sb.append(str).append(" ");
-                i += 1;
             } else {
-                operatorStack.add(str);
+                if (operatorStack.isEmpty()) {
+                    operatorStack.push(str);
+                } else if (OperatorPriority(str) >= OperatorPriority(operatorStack.peek())) {
+                    while (!operatorStack.isEmpty() && OperatorPriority(str) >= OperatorPriority(operatorStack.peek())) {
+                        sb.append(operatorStack.pop()).append(" ");
+                    }
+                    operatorStack.push(str);
+                } else {
+                    operatorStack.push(str);
+                }
             }
         }
-        sb.append("^").append(" ");
-        while (!operatorStack.isEmpty()) sb.append(operatorStack.poll()).append(" ");
+        while (!operatorStack.isEmpty()) {
+            sb.append(operatorStack.pop()).append(" ");
+        }
         return sb.toString().split(" ");
+    }
+
+    public int OperatorPriority(String operator) {
+        int priority = 0;
+        switch (operator) {
+            case "+", "-" -> priority = 2;
+            case "*", "/" -> priority = 1;
+        }
+        return priority;
     }
 }
