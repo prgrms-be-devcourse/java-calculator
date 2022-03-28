8989e73571ae83d9a074763326a6712557e92c05
moosongsong <songe08@gmail.com>
2022-03-28 16:13:16 +0900

    feat: Formula의 equals() hashcode() 구현
    미구현 되어 있던 부분을 구현하였습니다.

--- src/main/java/com/programmers/calculator/vo/Formula.java
+++ src/main/java/com/programmers/calculator/vo/Formula.java
@@ -1,6 +1,7 @@
 package com.programmers.calculator.vo;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Formula {
     private final String[] originFormula;
@@ -12,9 +13,24 @@ public class Formula {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Formula formula = (Formula) o;
+        return Arrays.equals(originFormula, formula.originFormula) && result.equals(formula.result);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = Objects.hash(result);
+        hash = 31 * hash + Arrays.hashCode(originFormula);
+        return hash;
+    }
+
+    @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
-        Arrays.stream(originFormula).forEach(token->{
+        Arrays.stream(originFormula).forEach(token -> {
             builder.append(token).append(' ');
         });
         return builder + "= " + result;
