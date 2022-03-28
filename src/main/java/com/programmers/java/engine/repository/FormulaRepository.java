a40d9fa0d3896a0fc2f26d8368abb4b09bb8ca39
HYEBIN,PARK <hyeqls3742@gmail.com>
2022-03-29 00:24:05 +0900

    MemoryRepository interface 구현체 함수를 구현했습니다.

--- src/main/java/com/programmers/java/engine/repository/FormulaRepository.java
+++ src/main/java/com/programmers/java/engine/repository/FormulaRepository.java
@@ -6,18 +6,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Getter
-public class FormulaRepo {
+public class FormulaRepository implements MemoryRepository {
+
     private final Map<String, Long> history = new HashMap<>();
 
+    @Override
     public void save(String formula, Long result) {
         history.put(formula, result);
     }
 
+    @Override
     public void inquire() {
         history.forEach((form, result) -> System.out.println(form + " = " + result));
         System.out.println("END !");
     }
 
+    @Override
     public int size() {
         return history.size();
     }
