f05ff66640ef896970b0947e656d3e2139d04d47
kimziou77 <kimziou77@naver.com>
2022-03-28 17:25:51 +0900

    (feat) history api 추가 : isExist,  getResult

--- calculator-project/src/test/java/HistoryTest.java
+++ calculator-project/src/test/java/HistoryTest.java
@@ -1,4 +1,3 @@
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -11,7 +10,7 @@ public class HistoryTest {
     @Test
     void testSave() {
         String operation = "2+3-4";
-        int number = 1;
+        double number = 1;
         history.save(operation, number);
         int historyNum = history.count();
         Assertions.assertEquals(historyNum, 1);
@@ -19,19 +18,24 @@ public class HistoryTest {
 
     @Test
     void testListAll() {
-        List<String> expect = Arrays.asList("27/3-2 = 1","234-3 = 1","9345+1 = 1");
-
+        List<String> expect = Arrays.asList("27/3-2 = 1.0","234-3 = 1.0","9345+1 = 1.0");
         historySave("27/3-2");
         historySave("234-3");
         historySave("9345+1");
         List<String> result = history.getList();
-//        result.forEach(System.out::println);
         Assertions.assertLinesMatch(result, expect);
     }
 
-    void historySave(String operation) {
-        int number = 1;
-        history.save(operation, number);
+    @Test
+    void testIsExist(){
+        String operation = "27-3";
+        historySave(operation);
+        Assertions.assertEquals(history.isExist(operation),true);
     }
 
+    /* helper method */
+    void historySave(String operation) {
+        double number = 1;
+        history.save(operation, number);
+    }
 }
