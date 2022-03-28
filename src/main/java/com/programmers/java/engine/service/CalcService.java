79969cca89d0d049bdb5e6639d4db237f5bad401
HYEBIN,PARK <hyeqls3742@gmail.com>
2022-03-29 03:01:37 +0900

    fix : 후위표기법 구현 로직 수정에 따라 계산 로직 전면 수정

--- src/main/java/com/programmers/java/engine/service/CalcService.java
+++ src/main/java/com/programmers/java/engine/service/CalcService.java
@@ -8,31 +8,17 @@ import java.util.Stack;
 public class CalcService {
     public Long calculate(String[] postFixFormula) {
         Stack<Long> s = new Stack<>();
-        Stack<Long> s2 = new Stack<>();
-
-        int idx = 0;
-        for (int i = 0; !postFixFormula[i].equals("^"); i++) {
+        for (int i = 0; i < postFixFormula.length; i++) {
             String str = postFixFormula[i];
-            if (Function.isStrDigit(str))
-                s.push(Long.parseLong(str));
-            else {
+            if (!Function.isStrDigit(str)) {
                 Long b = s.pop();
                 Long a = s.pop();
-                s = calcArithmetic(a, b, str, s);
+                calcArithmetic(a, b, str, s);
+            } else {
+                s.push(Long.parseLong(str));
             }
-            idx = i;
         }
-
-        while (!s.isEmpty()) {
-            s2.push(s.pop());
-        }
-
-        for (int i = idx + 2; i < postFixFormula.length; i++) {
-            Long a = s2.pop();
-            Long b = s2.pop();
-            s2 = calcArithmetic(a, b, postFixFormula[i], s2);
-        }
-        return s2.pop();
+        return s.pop();
     }
 
     public Stack<Long> calcArithmetic(Long a, Long b, String str, Stack<Long> s) {
