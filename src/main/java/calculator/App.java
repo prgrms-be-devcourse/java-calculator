96dbf75615ef788e1184f8ce0a5b323917a0cb7d
beomsic <qjatjr29@khu.ac.kr>
2022-03-28 23:49:08 +0900

    gradle 설정

---
+++ src/main/java/calculator/App.java
@@ -0,0 +1,18 @@
+package calculator;
+
+import calculator.calculate.Calculation;
+import calculator.calculate.CalculationImpl;
+import calculator.repository.Repository;
+import calculator.repository.RepositoryImpl;
+
+
+import java.io.IOException;
+
+/**
+ * entry point
+ */
+public class App {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
