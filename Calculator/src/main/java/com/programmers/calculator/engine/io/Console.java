569b3aa45b7df580e911cbc7ebc70ddbb323742f
ChoiYeonho0903 <903yh@naver.com>
2022-03-30 02:30:57 +0900

    입출력 구현

---
+++ Calculator/src/main/java/com/programmers/calculator/engine/io/Console.java
@@ -0,0 +1,40 @@
+package com.programmers.calculator.engine.io;
+
+import com.programmers.calculator.engine.repository.CalculatorRepository;
+import com.programmers.calculator.engine.repository.MemoryCalculatorRepository;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Console implements Input, Output  {
+
+    private final Scanner scanner = new Scanner(System.in);
+    private CalculatorRepository repository = new MemoryCalculatorRepository();
+
+    @Override
+    public String optionInput()  {
+        System.out.println("1. 조회");
+        System.out.print("2. 계산\n\n");
+        System.out.print("선택 : ");
+        String str = scanner.nextLine();
+        System.out.println();
+        return str;
+    }
+
+    @Override
+    public String calculationInput() {
+        String str = scanner.nextLine();
+        return str;
+    }
+
+    @Override
+    public void inputError() {
+
+    }
+
+    @Override
+    public void lookUp(List<String> list) {
+        list.stream().forEach(System.out::println);
+        System.out.println();
+    }
+}
---
+++ Calculator/src/main/java/com/programmers/calculator/engine/io/Output.java
@@ -0,0 +1,8 @@
+package com.programmers.calculator.engine.io;
+
+import java.util.List;
+
+public interface Output {
+    void lookUp(List<String> list);
+    void inputError();
+}
