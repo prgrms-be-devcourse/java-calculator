2d3f667e41a2f7a6a009a8e051c6801b291075ff
jeonghaemin <jhm0828@gmail.com>
2022-03-30 01:50:42 +0900

    Calculator 클래스 주석 작성

--- src/main/java/com/caculator/Calculator.java
+++ src/main/java/com/caculator/Calculator.java
@@ -44,10 +44,18 @@ public class Calculator {
         }
     }
 
+    /**
+     * 잘못된 메뉴를 선택했을 때 호출 되어 에러 메시지를 출력한다.
+     */
     private void commandError() {
         output.printError(INCORRECT_COMMAND_ERROR_MESSAGE);
     }
 
+    /**
+     * - 올바른 수식인 경우 -> 인자로 받은 수식을 계산 후 repository 에 저장하고 결과를 출력한다.
+     * - 올바른 수식이 아닌 경우 -> 에러 메시지를 출력한다.
+     * @param expression : 수식
+     */
     private void executeCalculationCommand(String expression) {
         try {
             int result = executeCalculator(expression);
@@ -60,6 +68,11 @@ public class Calculator {
         }
     }
 
+    /**
+     * 계산 이력을 출력한다.
+     * - 계산 이력이 있는 경우 -> 계산 이력을 출력
+     * - 계산 이력이 없는 경우 -> 계산 이력이 없다는 메시지 출력
+     */
     private void executeHistoryCommand(List<Result> history) {
         if (isEmptyHistory(history)) {
             output.printError(EMPTY_HISTORY_MESSAGE);
@@ -68,15 +81,30 @@ public class Calculator {
         output.printHistory(history);
     }
 
+    /**
+     * 계산 이력이 없으면 true 를 반환한다.
+     */
     private boolean isEmptyHistory(List<Result> history) {
         return history.size() == 0;
     }
 
+    /**
+     * 계산기를 실행한다.
+     * @param expression : 중위 표기법 수식
+     * @return : 계산 결과 정수
+     * @throws IllegalArgumentException, ArithmeticException : convert(), calculatePostfixExpression()에서 던져진 예외
+     */
     public int executeCalculator(String expression) throws IllegalArgumentException, ArithmeticException {
         List<String> postfix = PostfixConverter.convert(expression);
         return calculatePostfixExpression(postfix);
     }
 
+    /**
+     후위 표기식을 입력 받아 계산 후 결과를 반환한다.
+     * @param expressions : 후위 표기식
+     * @return 연산 결과
+     * @throws IllegalArgumentException, ArithmeticException : calculate()에서 던져진 예외
+     */
     private int calculatePostfixExpression(List<String> expressions) throws IllegalArgumentException, ArithmeticException {
         Stack<Integer> stack = new Stack<>();
 
@@ -93,6 +121,11 @@ public class Calculator {
         return stack.pop();
     }
 
+    /**
+     * n1,n2를 operator 연산한 결과를 반환한다.
+     * @throws ArithmeticException : 0으로 나누는 연산, 연산 결과에 오버플로 발생시 던져진다.
+     * @throws IllegalArgumentException : operator 가 연산자(+, -. *, /)가 가 아닌 경우 던져진다.
+     */
     private int calculate(int n1, int n2, String operator) throws ArithmeticException, IllegalArgumentException {
         switch (operator) {
             case "+":
