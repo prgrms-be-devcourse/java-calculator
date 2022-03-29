6c43ec1b520be60dde427e6c47acc1e4baf50d22
dojinyou <djyou128@gmail.com>
2022-03-29 23:26:21 +0900

    Refact(calculator): operation 함수 분리

--- app/src/main/java/calculator/StringCalculator.java
+++ app/src/main/java/calculator/StringCalculator.java
@@ -20,18 +20,23 @@ public class StringCalculator {
             if (OPERATOR.isOperator(element)) {
                 double operand2 = stack.pop();
                 double operand1 = stack.pop();
-                if (OPERATOR.PLUS.getValue().equals(element))
-                    stack.push(cal.plus(operand1, operand2));
-                if (OPERATOR.MINUS.getValue().equals(element))
-                    stack.push(cal.minus(operand1, operand2));
-                if (OPERATOR.MULTIPLY.getValue().equals(element))
-                    stack.push(cal.multiply(operand1, operand2));
-                if (OPERATOR.DIVIED.getValue().equals(element))
-                    stack.push(cal.divied(operand1, operand2));
+                stack.push(operation(operand1,operand2,element));
                 continue;
             }
             stack.push(Double.parseDouble(element));
         }
         return stack.pop();
+    }
+
+    private double operation(double operand1, double operand2, String operator) {
+        if (OPERATOR.PLUS.getValue().equals(operator))
+            return cal.plus(operand1, operand2);
+        if (OPERATOR.MINUS.getValue().equals(operator))
+            return cal.minus(operand1, operand2);
+        if (OPERATOR.MULTIPLY.getValue().equals(operator))
+            return cal.multiply(operand1, operand2);
+        if (OPERATOR.DIVIED.getValue().equals(operator))
+            return cal.divied(operand1, operand2);
+        throw new IllegalArgumentException();
     }
 }
