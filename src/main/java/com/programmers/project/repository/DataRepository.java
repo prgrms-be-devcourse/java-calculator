dbbee6cbb56f235769cc5ca77cfbf4650bfddab7
suy2on <tnwjdrms@naver.com>
2022-03-29 19:07:40 +0900

    # feat : data저장소 인터페이스 추가

---
+++ src/main/java/com/programmers/project/repository/DataRepository.java
@@ -0,0 +1,11 @@
+package com.programmers.project.repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public interface DataRepository {
+    List<String> show();
+    void add(String str);
+
+}
