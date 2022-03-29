0f904f80cd501ed3d8201f33b69c0e78a55f8f23
unnokid <kimki0609@gmail.com>
2022-03-30 00:07:39 +0900

    Add Console

---
+++ src/main/calculator/engine/Console.java
@@ -0,0 +1,31 @@
+package main.calculator.engine;
+
+import main.calculator.engine.io.Input;
+import main.calculator.engine.io.Output;
+
+import java.util.Scanner;
+
+public class Console implements Output, Input {
+    private final Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public String input(String s) {
+        System.out.println(s);
+        return scanner.nextLine();
+    }
+
+    @Override
+    public void menu() {
+        System.out.println("1.조회 \n2.계산 \n3.종료");
+    }
+
+    @Override
+    public void inputError() {
+        System.out.println("잘못 입력하셨습니다.");
+    }
+
+    @Override
+    public void quit() {
+        System.out.println("종료합니다.");
+    }
+}
