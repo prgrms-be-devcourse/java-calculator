3e7a649702fdb0292aadfc917114ca26f2299248
sdardew <sdardew@gmail.com>
2022-03-29 11:26:05 +0900

    feat: main 함수 구현

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -1,7 +1,15 @@
 package calculator;
 
-public class Main {
-    public static void main(String[] args) {
+import calculator.calculate.Calculator;
+import calculator.calculate.CalculatorImpl;
+import calculator.console.Console;
+import calculator.repository.ResultRepositoryImpl;
 
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Calculator calculator = new CalculatorImpl(new Console(), ResultRepositoryImpl.getInstance());
+        calculator.run();
     }
 }
