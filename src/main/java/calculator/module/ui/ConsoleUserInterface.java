92612faf26b7fcb2582a430e3c7838f988d7bf88
blessing333 <>
2022-03-29 13:45:00 +0900

    콘솔 기반 입출력 기능 구현

---
+++ src/main/java/calculator/module/ui/ConsoleUserInterface.java
@@ -0,0 +1,23 @@
+package calculator.module.ui;
+
+import java.util.Scanner;
+
+public class ConsoleUserInterface implements UserInterface{
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public String inputString() {
+        return scanner.nextLine();
+    }
+
+    @Override
+    public int inputNumber() {
+        return scanner.nextInt();
+    }
+
+    @Override
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+}
