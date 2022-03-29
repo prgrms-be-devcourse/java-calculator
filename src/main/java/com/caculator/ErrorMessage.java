4383ff794d4629c0662a256fd7015d0e2a57e7ce
jeonghaemin <jhm0828@gmail.com>
2022-03-30 00:25:29 +0900

    ErrMsg 인터페이스 수정
    - 인터페이스 이름과 상수 이름의 줄임말 제거

---
+++ src/main/java/com/caculator/ErrorMessage.java
@@ -0,0 +1,8 @@
+package com.caculator;
+
+public interface ErrorMessage {
+    String DIVIDE_ZERO_ERROR_MESSAGE = "0으로 나누는 연산은 할 수 없습니다";
+    String INCORRECT_COMMAND_ERROR_MESSAGE = "올바른 메뉴를 입력해주세요.";
+    String INCORRECT_EXPRESSION_ERROR_MESSAGE = "올바른 수식을 입력해주세요";
+    String EMPTY_HISTORY_MESSAGE = "계산 기록이 없습니다.";
+}
