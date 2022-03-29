2bea6bcdd41e2131178ccedea49d693b3153aee4
unnokid <kimki0609@gmail.com>
2022-03-30 00:23:55 +0900

    Update Calculator to run by value of `target`

--- src/main/calculator/Calculator.java
+++ src/main/calculator/Calculator.java
@@ -7,7 +7,6 @@ public class Calculator implements Runnable{
 
     private Input input;
     private Output output;
-    boolean quit = false;
     public Calculator(Input input, Output output) {
         this.input = input;
         this.output = output;
@@ -17,23 +16,25 @@ public class Calculator implements Runnable{
     public void run() {
 
         while(true){
+
             //메뉴출력
             output.menu();
             int target = Integer.parseInt(input.input("선택 :"));
-            //1번이 보여주기, 2번이 계산
-            switch (target){
-                case 1: break;
-                case 2: break;
-                case 3: quit = true; break;
-                default: output.inputError(); break;
-            }
 
-            if(quit){
+            //1번이 보여주기, 2번이 계산
+            if(target == 1){
+                //TODO: 지금까지 했던 기록 list 보여주기
+                continue;
+            }
+            if(target == 2){
+                //TODO: operator 만들기
+                continue;
+            }
+            if(target == 3){
                 output.quit();
                 break;
             }
+            output.inputError();
         }
-
-
     }
 }
