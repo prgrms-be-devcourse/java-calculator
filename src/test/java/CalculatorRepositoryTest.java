22fbe1c33b36714f19132db4fb9a01abfb96ad2f
ㅎㅈ <hgkimmme9@gmail.com>
2022-03-31 00:54:34 +0900

    Test 코드 구현

---
+++ src/test/java/CalculatorRepositoryTest.java
@@ -0,0 +1,38 @@
+import engine.repository.CalculatorRepository;
+import lombok.AllArgsConstructor;
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@AllArgsConstructor
+public class CalculatorRepositoryTest {
+
+    private CalculatorRepository repository;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        repository = appConfig.calculatorRepository();
+    }
+
+    @Test
+    public void saveFormulaTest() throws Exception {
+
+        //given
+        repository.save("3.9 * 9 / 3.0 + 2.02 = 13.72");
+        repository.save("1 / 3.0 = 0.33");
+
+
+        //when
+        List<String> formula = repository.findAllValues();
+
+        //then
+        assertEquals(formula.size(), 2);
+    }
+
+
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,48 @@
+import engine.Calculator;
+import engine.io.OutputString;
+import lombok.AllArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        this.calculator = appConfig.calculator();
+    }
+
+
+    @Test
+    public void calculateTest() throws Exception {
+
+        //given
+        String input = "2 + 3 * 8 / 2";
+        String valueExpected="14.00";
+
+        //when
+        String result=calculator.cal(input);
+
+        //then
+        assertEquals(valueExpected, result);
+
+    }
+
+
+    /* IllegalArgumentException test */
+    @Test
+    public void exceptionThrownTest() throws Exception {
+
+        String input = "8 ) 0";
+
+        Exception exception = assertThrows(IllegalArgumentException.class,
+                () -> {
+                    String result = calculator.cal(input);
+                });
+    }
+
+}
