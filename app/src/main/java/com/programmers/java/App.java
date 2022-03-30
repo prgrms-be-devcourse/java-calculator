1b37986a838e1e8beace7efbf8dfb36e0b5559a5
Juhan <juhan211@naver.com>
2022-03-31 00:44:39 +0900

    App java에 Calculator 객체 생성후 실행(run())

--- app/src/main/java/com/programmers/java/App.java
+++ app/src/main/java/com/programmers/java/App.java
@@ -3,12 +3,21 @@
  */
 package com.programmers.java;
 
+import com.programmers.java.calculator.Calculator;
+import com.programmers.java.calculator.Console;
+import com.programmers.java.calculator.engine.Input;
+import com.programmers.java.calculator.engine.Output;
+
+import java.util.ArrayList;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        Input input = new Console();
+        Output output = new Console();
+        new Calculator(input,output,new ArrayList<>()).run();
     }
 }
