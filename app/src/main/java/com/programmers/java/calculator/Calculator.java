df7b549719873ce8e501c49219bbb17d8a2331c8
Juhan <juhan211@naver.com>
2022-03-30 23:43:31 +0900

    Calculator 클래스 정의 Runnable 인터페이스 상속후 run() 함수 재정의

--- app/src/main/java/com/programmers/java/calculator/Calculator.java
+++ app/src/main/java/com/programmers/java/calculator/Calculator.java
@@ -1,4 +1,42 @@
 package com.programmers.java.calculator;
 
-public class Calculator {
+import com.programmers.java.calculator.engine.Input;
+import com.programmers.java.calculator.engine.Output;
+import com.programmers.java.calculator.model.Problem;
+import lombok.AllArgsConstructor;
+
+import java.util.EmptyStackException;
+import java.util.List;
+@AllArgsConstructor
+public class Calculator implements Runnable{
+    private Input input;
+    private Output output;
+    private List<Problem> problems;
+
+    @Override
+    public void run() {
+        while (true){
+            int option = input.selectOption();
+            System.out.println();
+            switch (option){
+                case 2:
+                    String question = input.inputProblem();
+                    try {
+                        Problem problem = makeProblem(question);
+                        problems.add(problem);
+                        output.showAnswer(problem);
+                    } catch (EmptyStackException e){
+                        output.inputError();
+                    }
+                    break;
+                case 1:
+                    output.showAllProblemNAnswer(problems);
+                    break;
+            }
+        }
+    }
+
+    public Problem makeProblem(String input){
+        return new Problem().makeQuestionArray(input).calculate();
+    }
 }
