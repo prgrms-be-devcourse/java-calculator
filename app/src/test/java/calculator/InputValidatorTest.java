ed98f1534db4fc6a494d94fe14d24339820cc8be
DojinYou <djyou128@gmail.com>
2022-03-30 13:30:15 +0900

    Test(InputValidation): ParameterizedTest 적용

--- app/src/test/java/calculator/InputValidatorTest.java
+++ app/src/test/java/calculator/InputValidatorTest.java
@@ -1,8 +1,10 @@
-package calculator;
+package javacalculator.calculator;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,14 +16,19 @@ public class InputValidatorTest {
         inputValidator = new InputValidator();
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("입력값_검증_테스트")
-    void valid() {
+    @CsvSource({"1,false", "1 +,false", "1 + 2 3,false", "1 + 2,true"})
+    void valid(String formula, boolean expect) {
+        String[] splitedFormula = formula.split(" ");
+        assertThat(inputValidator.valid(splitedFormula)).isEqualTo(expect);
+    }
+
+    @Test
+    @DisplayName("입력값_검증_테스트 - null or empty")
+    void validWithNullOrEmpty() {
         assertThat(inputValidator.valid(null)).isFalse();
         assertThat(inputValidator.valid(new String[]{})).isFalse();
-        assertThat(inputValidator.valid(new String[]{"1"})).isFalse();
-        assertThat(inputValidator.valid(new String[]{"1", "+"})).isFalse();
-        assertThat(inputValidator.valid(new String[]{"1", "+", "2"})).isTrue();
-        assertThat(inputValidator.valid(new String[]{"1", "+", "2", "3"})).isFalse();
     }
+
 }
