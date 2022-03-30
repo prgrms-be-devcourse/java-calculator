c58298600934bfe2a753172a3ed5e38182125e00
San Kim <waterfog9580@gmail.com>
2022-03-30 18:59:07 +0900

    run 메소드 depth를 menuHandle메서드로 분리

--- src/main/java/com/waterfogsw/Calculator.java
+++ src/main/java/com/waterfogsw/Calculator.java
@@ -2,6 +2,8 @@ package com.waterfogsw;
 
 import com.waterfogsw.domain.Calculation;
 import com.waterfogsw.exception.DoubleOverflow;
+import com.waterfogsw.exception.InvalidFormulaInput;
+import com.waterfogsw.exception.InvalidMenuInput;
 import com.waterfogsw.io.Input;
 import com.waterfogsw.io.MenuType;
 import com.waterfogsw.io.Output;
@@ -9,6 +11,7 @@ import com.waterfogsw.service.CalculationService;
 import com.waterfogsw.service.HistoryService;
 import lombok.AllArgsConstructor;
 
+import java.io.IOException;
 import java.util.List;
 
 @AllArgsConstructor
@@ -26,23 +29,7 @@ public class Calculator implements Runnable {
             System.out.println("2. 계산\n");
             try {
                 int menu = input.inputMenu("선택 : ");
-                switch (menu) {
-                    case MenuType.LOOKUP:
-                        // 1. 조회 메뉴 입력
-                        List<Calculation> histories = historyService.findAll();
-                        output.printHistories(histories);
-                        break;
-                    case MenuType.CALCULATE:
-                        // 2. 계산 메뉴 입력
-                        String expr = input.inputFormula("계산식 : ");
-                        String result = calculationService.getResult(expr);
-
-                        output.printResult(expr, result);
-                        historyService.save(expr, result);
-                        break;
-                    default:
-                        throw new Exception();
-                }
+                menuHandle(menu);
             } catch (ArithmeticException e) {
                 output.divZeroError();
             } catch (DoubleOverflow e) {
@@ -52,4 +39,23 @@ public class Calculator implements Runnable {
             }
         }
     }
+
+    private void menuHandle(int menu) throws Exception {
+        switch (menu) {
+            case MenuType.LOOKUP -> {
+                // 1. 조회 메뉴 입력
+                List<Calculation> histories = historyService.findAll();
+                output.printHistories(histories);
+            }
+            case MenuType.CALCULATE -> {
+                // 2. 계산 메뉴 입력
+                String expr = input.inputFormula("계산식 : ");
+                String result = calculationService.getResult(expr);
+                output.printResult(expr, result);
+                historyService.save(expr, result);
+            }
+            default -> throw new InvalidMenuInput();
+        }
+    }
+
 }
