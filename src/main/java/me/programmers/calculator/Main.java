d0bbb02099e8c6d9abc2011dde74ea88afccf8ab
Yanjoo <hanjub3@gmail.com>
2022-03-31 03:41:00 +0900

    Main 클래스

---
+++ src/main/java/me/programmers/calculator/Main.java
@@ -0,0 +1,68 @@
+package me.programmers.calculator;
+
+import me.programmers.calculator.engine.Calculator;
+import me.programmers.calculator.engine.excpetion.Validator;
+import me.programmers.calculator.engine.io.Console;
+import me.programmers.calculator.engine.io.Input;
+import me.programmers.calculator.engine.io.Output;
+import me.programmers.calculator.engine.model.History;
+
+import java.util.Map;
+
+public class Main {
+    private final String HISTORY = "1";
+    private final String CALCULATE = "2";
+
+    public static void main(String[] args) {
+        Console console = new Console();
+        new Main().run(console, console);
+    }
+
+    public void run(Input input, Output output) {
+        History history = new History();
+        Calculator calculator = new Calculator();
+        Validator validator = new Validator();
+
+        while (true) {
+            String prompt = showMenu(output, input);
+
+            switch (prompt) {
+                case HISTORY:
+                    showHistory(output, history);
+                    break;
+                case CALCULATE:
+                    calculateProblem(input, output, history, calculator, validator);
+                    break;
+                default:
+                    output.output("숫자를 잘못 입력했습니다.\n");
+                    break;
+            }
+        }
+    }
+
+    private void calculateProblem(Input input, Output output, History history, Calculator calculator, Validator validator) {
+        try {
+            String problemString = input.problemInput();
+            validator.problemValidate(problemString);
+            long result = calculator.calculate(problemString);
+            history.saveMemory(problemString, result);
+            output.output(result + "\n");
+        } catch (Exception e) {
+            output.output(e.getMessage() + "\n");
+        }
+    }
+
+    private void showHistory(Output output, History history) {
+        Map<Integer, String> memory = history.getMemory();
+        for (int key = 1; key <= memory.size(); key++) {
+            output.output(memory.get(key));
+        }
+        output.output("\n");
+    }
+
+    private String showMenu(Output output, Input input) {
+        output.output("1. 조회");
+        output.output("2. 계산");
+        return input.input("선택: ");
+    }
+}
