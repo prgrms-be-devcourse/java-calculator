62ad9deac6448685a69b357ad654257863c04c06
ㅎㅈ <hgkimmme9@gmail.com>
2022-03-31 01:21:24 +0900

    App, AppConfig

---
+++ src/main/java/App.java
@@ -0,0 +1,10 @@
+import engine.Calculator;
+import engine.repository.CalculatorRepository;
+
+public class App {
+    public static void main(String[] args) {
+        new AppConfig().calculator().run();
+    }
+}
+
+
