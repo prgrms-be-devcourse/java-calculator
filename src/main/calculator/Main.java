af2fe875470d39b9c67a0a9aa417833d514404e9
unnokid <kimki0609@gmail.com>
2022-03-30 12:35:41 +0900

    Change Console to Input and Output

--- src/main/calculator/Main.java
+++ src/main/calculator/Main.java
@@ -1,13 +1,17 @@
 package main.calculator;
 
+import main.calculator.engine.io.Input;
+import main.calculator.engine.io.Output;
 import main.calculator.engine.model.CalculationRepository;
 import main.calculator.engine.model.MemoryCalculationRepository;
 
 public class Main {
     public static void main(String[] args) {
 
-        Console console = new Console();
+        Input input = new Console();
+        Output output = new Console();
         CalculationRepository calculationRepository = new MemoryCalculationRepository();
-        new Calculator(console,console,calculationRepository).run();
+        Operator operator = new Operator();
+        new Calculator(input, output, calculationRepository, operator).run();
     }
 }
