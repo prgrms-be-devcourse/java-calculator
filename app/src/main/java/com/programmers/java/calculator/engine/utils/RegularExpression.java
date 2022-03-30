1ec6f617c9be6f811204791ddc62f9f861118671
ichoi <ichoi@student.42seoul.kr>
2022-03-30 15:49:44 +0900

    feat:정규 표현식을 추가하라

---
+++ app/src/main/java/com/programmers/java/calculator/engine/utils/RegularExpression.java
@@ -0,0 +1,21 @@
+package com.programmers.java.calculator.engine.utils;
+
+import java.util.regex.Pattern;
+
+public class RegularExpression {
+    public static boolean isSum (String str) {
+        return Pattern.matches("[\\+\\-]", str);
+    }
+
+    public static boolean isMulti (String str) {
+        return Pattern.matches("[\\*\\/]", str);
+    }
+
+    public static boolean isNumeric (String str) {
+        return Pattern.matches("^[0-9]*$", str);
+    }
+
+    public static boolean isOperator (String str) {
+        return Pattern.matches("[\\+\\-\\*\\/]", str);
+    }
+}
