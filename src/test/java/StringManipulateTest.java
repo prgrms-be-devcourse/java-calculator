db57036d2a62a6852e889fe35a8d7648374f175d
DESKTOP-5836PEC\tiger <tiger980424@naver.com>
2022-03-30 11:35:50 +0900

    빈 IntegerStack 관련 Test 작성 및 Test 완료

--- src/test/java/StringManipulateTest.java
+++ src/test/java/StringManipulateTest.java
@@ -7,16 +7,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class StringManipulateTest {
 
     @Test
-    public void getStackOfString(String string){
+    public void testIntegerStack(){
+        String string = "1 + 2 * 3";
         StringManipulator manipulator = new StringManipulator(string);
         Stack<Integer> integerStack = manipulator.getIntegerStack();
-        Stack<Character> operatorStack = manipulator.getOperatorStack();
 
-        assertEquals(getTestIntegerStack(string),integerStack);
-        assertEquals(getTestOperatorStack(string), operatorStack);
+        assertEquals(getRightIntegerStack(string),integerStack);
     }
 
-    public Stack<Integer> getTestIntegerStack(String string){
+    @Test
+    public void testEmptyIntegerStack(){
+        StringManipulator manipulator = new StringManipulator("");
+        Stack<Integer> integerStack = manipulator.getIntegerStack();
+
+        assertEquals(new Stack<Integer>(),integerStack);
+    }
+
+    public Stack<Integer> getRightIntegerStack(String string){
         Stack<Integer> integerStack = new Stack<>();
         for (int i = 0; i<string.length(); i++){
             char nowChar = string.charAt(i);
@@ -26,15 +33,5 @@ public class StringManipulateTest {
         }
         return integerStack;
     }
-
-    public Stack<Character> getTestOperatorStack(String string){
-        Stack<Character> operatorStack = new Stack<>();
-        for (int i = 0; i<string.length(); i++){
-            char nowChar = string.charAt(i);
-            if ('0' > nowChar || nowChar >'9'){
-                operatorStack.push(nowChar);
-            }
-        }
-        return operatorStack;
-    }
+    
 }
