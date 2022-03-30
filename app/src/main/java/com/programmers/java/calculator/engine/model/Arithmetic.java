6aeebf031948527d7e1422891d0c2ef27187fec9
ichoi <ichoi@student.42seoul.kr>
2022-03-30 15:45:45 +0900

    feat:Arithmetic 모델을 추가하라

---
+++ app/src/main/java/com/programmers/java/calculator/engine/model/Arithmetic.java
@@ -0,0 +1,12 @@
+package com.programmers.java.calculator.engine.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
+@Getter
+public class Arithmetic {
+    private String[] arithmetic;
+}
