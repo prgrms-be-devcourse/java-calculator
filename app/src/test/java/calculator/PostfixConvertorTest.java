289d0d8d7273a61a889b5d950099fa1de85676ac
DojinYou <djyou128@gmail.com>
2022-03-30 13:31:03 +0900

    Test(postfix convert): ParameterizedTest 적용

--- app/src/test/java/calculator/PostfixConvertorTest.java
+++ app/src/test/java/calculator/PostfixConvertorTest.java
@@ -1,19 +1,27 @@
-package calculator;
+package javacalculator.calculator;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PostfixConvertorTest {
+    @ParameterizedTest
+    @DisplayName("후위 표기법 변환 기능 테스트-정상입력")
+    @CsvSource({"1 + 2,1 2 +", "1 + 2 * 3,1 2 3 * +", "1 * 2 + 3,1 2 * 3 +"})
+    void convert(String infix, String postfix) {
+        String[] splitedInfix = infix.split(" ");
+        String[] splitedPostfix = postfix.split(" ");
+        assertThat(PostfixConvertor.convert(splitedInfix)).isEqualTo(splitedPostfix);
+    }
+
     @Test
-    @DisplayName("후위표기법_변환_기능_테스트")
-    void convert() {
+    @DisplayName("후위표기법_변환_기능_테스트-nullOrEmpty")
+    void convertWithNullOrEmpty() {
         assertThatThrownBy(() -> PostfixConvertor.convert(null)).isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> PostfixConvertor.convert(new String[]{})).isInstanceOf(IllegalArgumentException.class);
-        assertThat(PostfixConvertor.convert(new String[]{"1", "+", "2"})).isEqualTo(new String[]{"1", "2", "+"});
-        assertThat(PostfixConvertor.convert(new String[]{"1", "+", "2", "*", "3"})).isEqualTo(new String[]{"1", "2", "3", "*", "+"});
-        assertThat(PostfixConvertor.convert(new String[]{"1", "*", "2", "+", "3"})).isEqualTo(new String[]{"1", "2", "*", "3", "+"});
     }
 }
