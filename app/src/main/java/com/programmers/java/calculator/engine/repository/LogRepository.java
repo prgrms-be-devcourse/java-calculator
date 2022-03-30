80f8d0ed42639733844146087b0fd36c3a3a9604
ichoi <ichoi@student.42seoul.kr>
2022-03-30 15:47:14 +0900

    feat:저장소를 추가하라

---
+++ app/src/main/java/com/programmers/java/calculator/engine/repository/LogRepository.java
@@ -0,0 +1,27 @@
+package com.programmers.java.calculator.engine.repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LogRepository implements Repository{
+
+    private final Map<Long, String> map = new HashMap<>();
+    Long index = -1L;
+
+    @Override
+    public void save(String log) {
+        map.put(++index, log);
+    }
+
+    @Override
+    public void output() {
+        if (index == -1L) {
+            System.out.println("계산 이력이 없습니다.\n");
+        } else {
+            for (Long l = 0L; l <= index; l++) {
+                System.out.println(map.get(l));
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ app/src/main/java/com/programmers/java/calculator/engine/repository/Repository.java
@@ -0,0 +1,7 @@
+package com.programmers.java.calculator.engine.repository;
+
+public interface Repository {
+    void save(String str);
+
+    void output();
+}
