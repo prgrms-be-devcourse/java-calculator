0220f7bbcb8f7765003b0b9d59dd552116965601
DojinYou <djyou128@gmail.com>
2022-03-30 13:46:48 +0900

    Fefact(inputvalidtor): 조건절 method 분리

--- app/src/main/java/calculator/InputValidator.java
+++ app/src/main/java/calculator/InputValidator.java
@@ -1,18 +1,30 @@
-package calculator;
+package javacalculator.calculator;
 
 public class InputValidator {
     public boolean valid(String[] splittedInputFormula) {
-        if (splittedInputFormula == null) return false;
-        if (splittedInputFormula.length < 3 || splittedInputFormula.length % 2 == 0) return false;
+        if (
+                splittedInputFormula != null &&
+                isValidLength(splittedInputFormula) &&
+                isValidElements(splittedInputFormula)
+        ) return true;
+        return false;
+    }
+
+    private boolean isValidElements(String[] splittedInputFormula) {
         for (int i = 0; i < splittedInputFormula.length; i++) {
-            if (i % 2 == 0 && !isNumber(splittedInputFormula[i])) return false;
-            if (i % 2 == 1 && !isOperator(splittedInputFormula[i])) return false;
+            String element = splittedInputFormula[i];
+            if (i % 2 == 0 && !isNumber(element)) return false;
+            if (i % 2 == 1 && !isOperator(element)) return false;
         }
         return true;
     }
 
+    private boolean isValidLength(String[] splittedInputFormula) {
+        return splittedInputFormula.length >= 3 && splittedInputFormula.length % 2 != 0;
+    }
+
     private boolean isOperator(String operator) {
-        return OPERATOR.isOperator(operator);
+        return Operator.isTextOperator(operator);
     }
 
     private boolean isNumber(String stringNumber) {
