67eb32f477392743c78b0bd40e6705a8934f1e47
ichoi <ichoi@student.42seoul.kr>
2022-03-30 15:48:21 +0900

    feat:유효성 검사를 추가하라

---
+++ app/src/main/java/com/programmers/java/calculator/engine/Validation.java
@@ -0,0 +1,29 @@
+package com.programmers.java.calculator.engine;
+
+import com.programmers.java.calculator.engine.model.Arithmetic;
+import com.programmers.java.calculator.engine.utils.RegularExpression;
+
+import java.util.Optional;
+
+public class Validation {
+    public Optional<Arithmetic> checkValid(String input) {
+        if (input == null) return Optional.empty();
+
+        String[] splitTokens = input.split("\\s");
+//        StringTokenizer stringTokenizer = new StringTokenizer(input);
+        boolean flag = true;
+
+        for (String splitToken : splitTokens) {
+            if (flag) {
+                flag = false;
+                if (!RegularExpression.isNumeric(splitToken)) return Optional.empty();
+            } else {
+                flag = true;
+                if (!RegularExpression.isOperator(splitToken)) return Optional.empty();
+            }
+        }
+        if(flag)return Optional.empty();
+
+        return Optional.of(new Arithmetic(splitTokens));
+}
+}
