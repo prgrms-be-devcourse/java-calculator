b47321a641e3a8c6c759968ddd7f10e1f48b7baf
DojinYou <djyou128@gmail.com>
2022-03-30 14:45:05 +0900

    Refact(inputparser): 객체 변수 상수 처리 추가

--- app/src/main/java/calculator/InputParser.java
+++ app/src/main/java/calculator/InputParser.java
@@ -3,7 +3,7 @@ package calculator;
 import java.util.Arrays;
 
 public class InputParser {
-    private InputValidator inputValidator;
+    private final InputValidator inputValidator;
     private final String seperator;
 
     InputParser() {
