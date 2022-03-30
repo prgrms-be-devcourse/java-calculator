21d594b7ac5f96072f00119dcdc7a6787c1bf244
jeonghaemin <jhm0828@gmail.com>
2022-03-30 22:57:15 +0900

    Operator 주석 작성

--- src/main/java/com/caculator/Operator.java
+++ src/main/java/com/caculator/Operator.java
@@ -17,20 +17,36 @@ public enum Operator {
         this.function = function;
     }
 
+    /**
+     * num1, num2 를 대상으로 operator 연산을 하여 결과를 반환한다.
+     * @throws IllegalArgumentException : getOperator() 에서 operator 가 연산자가 아닌 경우 던져진다.
+     * @throws ArithmeticException : 나누기 연산에서 0으로 나누는 연산이 실행되는 경우 던져진다.
+     */
     public static long calculate(String operator, long num1, long num2) throws IllegalArgumentException, ArithmeticException {
         return getOperator(operator).function.applyAsLong(num1, num2);
     }
 
-    private static Operator getOperator(String s) {
+    /**
+     * s 와 일치하는 연산자 enum 값을 반환한다.
+     * @throws IllegalArgumentException : s 가 연산자가 아닌 경우 던져진다.
+     */
+    private static Operator getOperator(String s)  throws IllegalArgumentException {
         return Arrays.stream(Operator.values()).filter(operator -> operator.operator.equals(s))
                 .findFirst().orElseThrow(IllegalArgumentException::new);
     }
 
+    /**
+     * s 가 연산자(+, -, *, /)이면 true 를 반환한다.
+     */
     public static boolean isOperator(String s) {
         return Arrays.stream(Operator.values())
                 .anyMatch(operator -> operator.operator.equals(s));
     }
 
+    /**
+     * operator 연산자의 우선 순위를 반환한다.
+     * @throws IllegalArgumentException : operator 가 연산자가 아닌 경우 던져진다.
+     */
     public static int getPriority(String operator) throws IllegalArgumentException {
         if (operator.equals("*") || operator.equals("/")) return 2;
         if (operator.equals("+") || operator.equals("-")) return 1;
