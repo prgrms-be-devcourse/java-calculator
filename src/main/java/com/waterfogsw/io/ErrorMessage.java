21b31428794acd5ee475056bbe6dbbfceb2712c7
San Kim <waterfog9580@gmail.com>
2022-03-31 13:28:58 +0900

    ErrorMessage 접근제어자 추가

--- src/main/java/com/waterfogsw/io/ErrorMessage.java
+++ src/main/java/com/waterfogsw/io/ErrorMessage.java
@@ -5,7 +5,7 @@ public class ErrorMessage {
         //유틸리티 클래스
     }
 
-    static final String DIV_BY_ZERO = "0으로 나눌수 없습니다.\n";
-    static final String WRONG_INPUT = "잘못된 입력입니다.\n";
-    static final String DATA_OVERFLOW = "계산할 수 있는 데이터 범위를 초과하였습니다.\n";
+    public static final String DIV_BY_ZERO = "0으로 나눌수 없습니다.\n";
+    public static final String WRONG_INPUT = "잘못된 입력입니다.\n";
+    public static final String DATA_OVERFLOW = "계산할 수 있는 데이터 범위를 초과하였습니다.\n";
 }
