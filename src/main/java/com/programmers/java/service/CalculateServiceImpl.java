fcd5e1c9d43f23b6a99fdc15b22b01c5d1daa61e
ecvheo1 <ecvheo@naver.com>
2022-03-31 14:16:00 +0900

    fix : 분기문 내의 반복문을 private 메소드로 분리

--- src/main/java/com/programmers/java/service/CalculateServiceImpl.java
+++ src/main/java/com/programmers/java/service/CalculateServiceImpl.java
@@ -16,7 +16,7 @@ public class CalculateServiceImpl implements CalculateService {
     }
 
     /**
-     * 계산식(중위표기식)을 파라미터로 받고 후위표기식으로 변환 후 계산
+     * 계산식(중위표기식)을 파라미터로 받고 후위표기식으로 변환 후 계산하는 메서드
      * @param formula
      * @return 계산 결과
      */
@@ -29,7 +29,7 @@ public class CalculateServiceImpl implements CalculateService {
         Stack<Integer> numStack = new Stack<>();
         for (String token : postfixFormulaList) {
 
-            if (!token.equals("MINUS_SIGN") && token.matches(REGEX)) {
+            if (!token.equals(MINUS_SIGN) && token.matches(REGEX)) {
                 numStack.push(Integer.parseInt(token));
                 continue;
             }
@@ -45,9 +45,9 @@ public class CalculateServiceImpl implements CalculateService {
     }
 
     /**
-     * 중위 표기식 -> 후위 표기식 변환
-     * @param infixFormulaList
-     * @return 후위표기식
+     * 중위 표기식 -> 후위 표기식 변환 메서드
+     * @param infixFormulaList : 중위표기식 토큰 리스트
+     * @return 후위표기식 토큰 리스트
      */
     @Override
     public List<String> convertInfixToPostFix(List<String> infixFormulaList) {
@@ -58,9 +58,9 @@ public class CalculateServiceImpl implements CalculateService {
             if (i % 2 == 0) {
                 postFixFormulaList.add(infixFormulaList.get(i));
             } else {
-                while (!opStack.isEmpty() && Operator.comparePriority(opStack.peek(), infixFormulaList.get(i)) >= 0) {
-                    postFixFormulaList.add(opStack.pop());
-                }
+                String infixOperator = infixFormulaList.get(i);
+                AddOperatorToPostFixFormulaList(opStack, postFixFormulaList, infixOperator);
+
                 opStack.push(infixFormulaList.get(i));
             }
         }
@@ -70,5 +70,17 @@ public class CalculateServiceImpl implements CalculateService {
         }
 
         return postFixFormulaList;
+    }
+
+    /**
+     * 스택에 담겨 있는 연산자와 후위표기식 연산자의 우선순위를 비교하여 중위표기식 리스트에 추가하는 메서드
+     * @param opStack : 연산자를 담는 스택
+     * @param postFixFormulaList : 후위표기식 토큰 리스트
+     * @param infixOperator : 중위표기식 연산자 토큰
+     */
+    private void AddOperatorToPostFixFormulaList(Stack<String> opStack, List<String> postFixFormulaList, String infixOperator) {
+        while (!opStack.isEmpty() && Operator.comparePriority(opStack.peek(), infixOperator) >= 0) {
+            postFixFormulaList.add(opStack.pop());
+        }
     }
 }
