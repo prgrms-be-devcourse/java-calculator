d56002053af98fe3306c0163280516f59fb56d35
jeonghaemin <jhm0828@gmail.com>
2022-03-31 23:10:57 +0900

    ErrorMessgae 인터페이스에서 클래스로 변경

--- src/main/java/com/caculator/ErrorMessage.java
+++ src/main/java/com/caculator/ErrorMessage.java
@@ -1,8 +1,12 @@
 package com.caculator;
 
-public interface ErrorMessage {
-    String DIVIDE_ZERO_ERROR_MESSAGE = "0으로 나누는 연산은 할 수 없습니다";
-    String INCORRECT_COMMAND_ERROR_MESSAGE = "올바른 메뉴를 입력해주세요.";
-    String INCORRECT_EXPRESSION_ERROR_MESSAGE = "올바른 수식을 입력해주세요";
-    String EMPTY_HISTORY_MESSAGE = "계산 기록이 없습니다.";
+public final class ErrorMessage {
+
+    // 인스턴스 생성을 막기 위한 생성자
+    private ErrorMessage() {}
+
+    public static String DIVIDE_ZERO_ERROR_MESSAGE = "0으로 나누는 연산은 할 수 없습니다";
+    public static String INCORRECT_COMMAND_ERROR_MESSAGE = "올바른 메뉴를 입력해주세요.";
+    public static String INCORRECT_EXPRESSION_ERROR_MESSAGE = "올바른 수식을 입력해주세요";
+    public static String EMPTY_HISTORY_MESSAGE = "계산 기록이 없습니다.";
 }
