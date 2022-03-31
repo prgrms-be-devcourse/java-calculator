2285826898b0cd765d0200fc5dfcb60245c58e26
pppp0722 <poj0722@naver.com>
2022-03-31 22:25:45 +0900

    fix - Priority => Operator로 rename 후 연산자인지 확인하는 메소드 추가

--- src/main/java/com/programmers/calculator/engine/Operator.java
+++ src/main/java/com/programmers/calculator/engine/Operator.java
@@ -3,8 +3,7 @@ package com.programmers.calculator.engine;
 import java.util.HashMap;
 import java.util.Map;
 
-// 연산자 우선순위
-public class Priority {
+public class Operator {
     private static final Map<String, Integer> priority = new HashMap<String, Integer>() {
         {
             put("*", 3);
@@ -15,7 +14,20 @@ public class Priority {
         }
     };
 
+    // 연산자 우선순위
     public static int getPriority(String symbol) {
         return priority.get(symbol);
+    }
+
+    // 연산자 인지
+    public static boolean isOperator(String symbol) {
+        boolean ret = false;
+
+        if(symbol.equals("+") || symbol.equals("-") ||
+                symbol.equals("*") || symbol.equals("/")) {
+            ret = true;
+        }
+
+        return ret;
     }
 }
