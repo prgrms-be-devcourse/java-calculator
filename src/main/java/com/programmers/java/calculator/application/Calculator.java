5b8237f3447628181c11e4266ad94fd2d83d37d5
NownS <kimjuicy29@gmail.com>
2022-04-01 08:16:01 +0900

    인터페이스를 통한 계산기 application 구현

---
+++ src/main/java/com/programmers/java/calculator/application/Calculator.java
@@ -0,0 +1,44 @@
+package com.programmers.java.calculator.application;
+
+import com.programmers.java.calculator.io.Input;
+import com.programmers.java.calculator.io.Output;
+import com.programmers.java.calculator.repository.CalculatorRepository;
+import com.programmers.java.calculator.utility.Operator;
+
+import java.util.List;
+
+public class Calculator {
+    private final CalculatorRepository repository;
+    private final Input input;
+    private final Output output;
+    private static final int HISTORY = 1, CALCULATE = 2;
+    private final Operator operator;
+
+    public Calculator(CalculatorRepository repository, Input input, Output output, Operator operator){
+        this.repository = repository;
+        this.input = input;
+        this.output = output;
+        this.operator = operator;
+    }
+
+    public void run(){
+        int cmd = 0;
+        while (cmd != -1) {
+            cmd = input.inputCmd();
+
+            switch (cmd) {
+                case HISTORY:
+                    List<String> history = repository.findAll();
+                    output.outputHistory(history);
+                    break;
+
+                case CALCULATE:
+                    String exp = input.inputExp();
+                    double result = operator.calculate(exp);
+                    repository.save(exp + " = " + result);
+                    output.outputResult(result);
+                    break;
+            }
+        }
+    }
+}
