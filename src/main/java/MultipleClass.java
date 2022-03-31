74dcc4fd7c6a4d5f091c93160cf66fcb2cd44a2b
DESKTOP-5836PEC\tiger <tiger980424@naver.com>
2022-04-01 01:20:13 +0900

    곱하기 class 구현 완료 및 테스트 완료

---
+++ src/main/java/MultipleClass.java
@@ -0,0 +1,10 @@
+public class MultipleClass extends Operator {
+    public MultipleClass(){
+        super('*');
+    }
+
+    @Override
+    public float getResult(float a, float b) {
+        return a*b;
+    }
+}
