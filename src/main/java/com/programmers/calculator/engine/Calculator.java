e0cd1a8d1e51947ea8892e51bc8b40e93edc5aa8
pppp0722 <poj0722@naver.com>
2022-03-31 22:23:16 +0900

    fix - 중첩 괄호 오류 수정

--- src/main/java/com/programmers/calculator/engine/Calculator.java
+++ src/main/java/com/programmers/calculator/engine/Calculator.java
@@ -1,6 +1,6 @@
 package com.programmers.calculator.engine;
 
-import com.programmers.calculator.engine.exception.Exception;
+import com.programmers.calculator.engine.exception.Validator;
 import com.programmers.calculator.engine.io.Input;
 import com.programmers.calculator.engine.io.Output;
 import com.programmers.calculator.engine.repository.ExpressionAndResult;
@@ -31,7 +31,12 @@ public class Calculator implements Runnable{
                     break;
                 case "2" :
                     String expression = input.input("수식 입력 : ");
-                    calc(expression);
+                    Optional<Integer> result = calc(expression);
+                    if(!result.isPresent()) { // 예외 처리
+                        output.inputError();
+                    } else {
+                        output.printResult(result.get()); // 결과 출력
+                    }
                     break;
                 case "3" :
                     close = true;
@@ -42,24 +47,24 @@ public class Calculator implements Runnable{
             }
 
             if(close) break;
+
             output.printEmpty();
         }
     }
 
     // 연산식을 후위표기법으로 변환 -> Optional null체크 -> 후위표기법 계산하고 출력 -> map에 연산식, 결과 저장
-    public void calc(String expression) {
+    public Optional<Integer> calc(String expression) {
         Optional<List<String>> postfix = infixToPostfix(expression);
 
         // 예외 출력
         if(!postfix.isPresent()) {
-            output.inputError();
-            return;
+            return Optional.empty();
         }
 
         // 후위표기법 연산
         Stack<String> stack = new Stack<>();
         for(String s : postfix.get()) {
-            if(Pattern.matches(Regex.getNumRegex(), s)) {
+            if(Pattern.matches(Regex.getNumRegex(), s) && !s.equals("-")) {
                 stack.push(s);
             } else {
                 int num2 = Integer.parseInt(stack.pop());
@@ -72,40 +77,41 @@ public class Calculator implements Runnable{
         }
 
         int result = Integer.parseInt(stack.pop());
-        output.printResult(result); // 결과 출력
         expressionAndResult.put(expression, result); // map에 저장
+        return Optional.of(result);
     }
 
     // 예외 체크 후 중위표기법 => 후위표기법 변환 (수식이 잘못 입력된 경우 Optional.empty() 리턴)
     public Optional<List<String>> infixToPostfix(String infix) {
         List<String> postfix = new ArrayList<>();
         Stack<String> stack = new Stack<>();
-        String[] numsNSymbols = infix.split(" ");
+        String[] numsNSymbols = Arrays.stream(infix.split(" ")) // " "로 split & 공백 제거
+                .filter(s -> !s.equals(" "))
+                .toArray(String[]::new);
 
         // 예외 체크
-        if(!Exception.check(numsNSymbols)) {
+        if(!Validator.check(numsNSymbols)) {
             return Optional.empty();
         }
 
         // 중위 표기법 => 후위표기법 변환
         for(String s : numsNSymbols) {
-            if (Pattern.matches(Regex.getNumRegex(), s)) {
+            if (Pattern.matches(Regex.getNumRegex(), s) && !s.equals("-")) {
                 postfix.add(s);
             } else if (s.equals("(")) {
                 stack.push(s);
-            } else if (s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/")) {
-                if (!stack.isEmpty() && Priority.getPriority(stack.peek()) >= Priority.getPriority(s)) {
+            } else if (Operator.isOperator(s)) {
+                if (!stack.isEmpty() && Operator.getPriority(stack.peek()) >= Operator.getPriority(s)) {
                     postfix.add(stack.pop());
                     stack.push(s);
                 } else {
                     stack.push(s);
                 }
             } else if (s.equals(")")) {
-                while (true) {
-                    String tmp = stack.pop();
-                    if (tmp.equals("(")) break;
-                    postfix.add(tmp);
+                while (!stack.isEmpty() && !stack.peek().equals("(")) {
+                    postfix.add(stack.pop());
                 }
+                stack.pop();
             }
         }
 
