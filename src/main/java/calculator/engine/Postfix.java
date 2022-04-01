097119f0f7b09cb408612abf31e888363f26d7ca
Seonghyeon Kim <goshk1995@gmail.com>
2022-04-02 01:51:57 +0900

    괄호 처리 리팩터링

--- src/main/java/calculator/engine/Postfix.java
+++ src/main/java/calculator/engine/Postfix.java
@@ -7,14 +7,16 @@ import java.util.regex.Pattern;
 
 public class Postfix {
     public Postfix() {}
+    static Stack<String> stack;
+    static List<String> arr;
 
     public List<String> makeToPostfix(String formula) throws IllegalAccessException {
         if(!isCorrectFormula(formula)) {
             throw new IllegalArgumentException();
         }
 
-        List<String> arr = new ArrayList<>();
-        Stack<String> stack = new Stack<>();
+        arr = new ArrayList<>();
+        stack = new Stack<>();
 
         String[] splitFormula = formula.trim().replaceAll(" +", " ").split(" ");
 
@@ -31,25 +33,9 @@ public class Postfix {
                     break;
                 default:
                     if (s.contains("(")) {
-                        for (int j = 0; j < s.length(); j++) {
-                            if (s.charAt(j) != '(') {
-                                arr.add(s.substring(j));
-                                break;
-                            }
-
-                            stack.push("(");
-                        }
+                        openingParenthesis(s);
                     } else if (s.contains(")")) {
-                        int idx = s.indexOf(")");
-                        arr.add(s.substring(0, idx));
-                        int count = s.substring(idx).length();
-                        for(int i = 0; i < count; i++) {
-                            while (!stack.isEmpty() && !stack.peek().equals("(")) {
-                                arr.add(stack.pop());
-                            }
-
-                            stack.pop();
-                        }
+                        closingParenthesis(s);
                     } else {
                         arr.add(s);
                     }
@@ -63,6 +49,29 @@ public class Postfix {
         }
 
         return arr;
+    }
+
+    private void openingParenthesis(String s) {
+        for (int j = 0; j < s.length(); j++) {
+            if (s.charAt(j) != '(') {
+                arr.add(s.substring(j));
+                break;
+            }
+
+            stack.push("(");
+        }
+    }
+    private void closingParenthesis(String s) {
+        int idx = s.indexOf(")");
+        arr.add(s.substring(0, idx));
+        int count = s.substring(idx).length();
+        for(int i = 0; i < count; i++) {
+            while (!stack.isEmpty() && !stack.peek().equals("(")) {
+                arr.add(stack.pop());
+            }
+
+            stack.pop();
+        }
     }
 
     public boolean isCorrectFormula(String formula) {
