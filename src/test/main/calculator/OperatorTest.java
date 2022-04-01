63797a08c4141140d3f4b7f4968c947db40af556
unnokid <kimki0609@gmail.com>
2022-04-01 21:43:40 +0900

    Add `plus()`, `minus()`, `multiply`, `divide()` Test in OperatorTest

--- src/test/main/calculator/OperatorTest.java
+++ src/test/main/calculator/OperatorTest.java
@@ -1,7 +1,51 @@
 package main.calculator;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class OperatorTest {
+    Operator operator = new Operator();
 
+    @Test
+    @DisplayName("덧셈 성공")
+    void plus() {
+        String input = "1 + 3";
+        double answer = 1 + 3;
+        double result = Double.parseDouble(operator.calculate(input));
+
+        assertThat(result).isEqualTo(answer);
+    }
+
+    @Test
+    @DisplayName("뺄셈 성공")
+    void minus() {
+        String input = "3 - 1";
+        double answer = 3 - 1;
+        double result = Double.parseDouble(operator.calculate(input));
+
+        assertThat(result).isEqualTo(answer);
+    }
+
+    @Test
+    @DisplayName("곱셉 성공")
+    void multiply() {
+        String input = "3 * 2";
+        double answer = 3 * 2;
+        double result = Double.parseDouble(operator.calculate(input));
+
+        assertThat(result).isEqualTo(answer);
+    }
+
+    @Test
+    @DisplayName("나눗셈 성공")
+    void divide() {
+        String input = "4 / 2";
+        double answer = 4 / 2;
+        double result = Double.parseDouble(operator.calculate(input));
+
+        assertThat(result).isEqualTo(answer);
+    }
 
 }
