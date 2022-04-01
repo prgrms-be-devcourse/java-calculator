13498db35fb7760409fad39a51070d1fe258d492
HunkiKim <gnsrl76@naver.com>
2022-04-01 12:41:47 +0900

    refacotr : 리포지토리 추상,구체 클래스로 변경
    나중에 확장 가능성을 위해 추상,구체 클래스로 변경

--- src/main/java/com/kimhunki/java/calculator/db/ResultRepository.java
+++ src/main/java/com/kimhunki/java/calculator/db/ResultRepository.java
@@ -1,19 +1,10 @@
 package com.kimhunki.java.calculator.db;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
 import java.util.List;
 
-@AllArgsConstructor
-@Getter
-public class ResultRepository
+public interface ResultRepository
 {
-    private List<String> resultList;
+    void addResult(String result);
 
-    void addResult(String result)
-    {
-        resultList.add(result);
-    }
-
+    List<String> getRepository();
 }
