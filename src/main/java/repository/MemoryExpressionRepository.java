ba656e01d1130808470ad92e587059f7284a267e
dbslzld15 <dbslzld15@naver.com>
2022-04-02 00:41:28 +0900

    저장 로직 map 대신 list로 수정

--- src/main/java/repository/MemoryExpressionRepository.java
+++ src/main/java/repository/MemoryExpressionRepository.java
@@ -3,22 +3,18 @@ package repository;
 import model.Expression;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class MemoryExpressionRepository implements ExpressionRepository {
-    private static Map<Long, Expression> store = new HashMap<>();
-    private static long sequence = 0L;
+    private static final List<Expression> store = new ArrayList<>();
 
     @Override
     public void save(Expression expression) {
-        store.put(sequence, expression);
-        sequence++;
+        store.add(expression);
     }
 
     @Override
     public List<Expression> findAll() {
-        return new ArrayList<>(store.values());
+        return store;
     }
 }
