dc3b734ee8e8f1af49478e5b1f19c2441633db60
HunkiKim <gnsrl76@naver.com>
2022-04-01 13:03:13 +0900

    feat : 계산 전략 추가
    실제로으로 계산을 수행해주는 클래스

---
+++ src/main/java/com/kimhunki/java/calculator/strategy/CalculatorStrategy.java
@@ -0,0 +1,15 @@
+package com.kimhunki.java.calculator.strategy;
+
+import com.kimhunki.java.calculator.db.MemoryResultRepository;
+
+import java.util.List;
+
+public interface CalculatorStrategy
+{
+
+
+
+
+
+    String calculate(List<String> expressionList);
+}
