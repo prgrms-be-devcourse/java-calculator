ed0d08309cab3e54f97c5319402ca9873fd83fae
Yanjoo <hanjub3@gmail.com>
2022-04-01 17:24:09 +0900

    fourOperations í 메소드 분리

--- src/main/java/me/programmers/calculator/engine/Calculator.java
+++ src/main/java/me/programmers/calculator/engine/Calculator.java
@@ -5,49 +5,45 @@ import java.util.Stack;
 
 public class Calculator {
 
-    private final String PLUS = "+";
-    private final String MINUS = "-";
-    private final String MUL = "*";
-    private final String DIV = "/";
-
     public long calculateFormula(String problem) {
         List<String> posteriorProblem = Posterior.convertToPosterior(problem);  // 후위표기식 변환 1+2*3 -> 123*+
         return fourArithmeticOperations(posteriorProblem);
     }
 
     // 후위표기식 계산
-    private long fourArithmeticOperations(List<String> problem) {
+    private long fourArithmeticOperations(List<String> postOrderProblem) {
         Stack<Long> calcStack = new Stack<>();
-        for (String s : problem) {
-            long res = 0;
+        for (String s : postOrderProblem) {
+            long sum = 0;
             if (Operator.isOperator(s.charAt(0))) {
+                Operator op = Operator.getOperator(s.charAt(0));
                 long b = calcStack.pop();
                 long a = calcStack.pop();
-                switch (s){
-                    case PLUS:
-                        res = a + b;
-                        break;
-                    case MINUS:
-                        res = a - b;
-                        break;
-                    case MUL:
-                        res = a * b;
-                        break;
-                    case DIV:
-                        if (b == 0)
-                            throw new ArithmeticException("0으로 나눌 수 없습니다.");
-                        res = a / b;
-                        break;
-                    default:
-                        throw new ArithmeticException("연산자를 잘못 입력했습니다.");
-                }
+                sum = doOperation(a, b, op);
             } else {
-                res = Long.parseLong(s);
+                sum = Long.parseLong(s);
             }
-            calcStack.push(res);
+            calcStack.push(sum);
         }
 
         return calcStack.pop();
+    }
+
+    private long doOperation(long a, long b, Operator op) {
+        switch (op){
+            case PLUS:
+                return a + b;
+            case MINUS:
+                return a - b;
+            case MUL:
+                return a * b;
+            case DIV:
+                if (b == 0)
+                    throw new ArithmeticException("0으로 나눌 수 없습니다.");
+                return a / b;
+            default:
+                throw new ArithmeticException("연산자를 잘못 입력했습니다.");
+        }
     }
 
 }
