e3b325d1f6aecc3adfe9edf590df090adc04751d
will kim <78planet@gmail.com>
2022-04-01 17:19:42 +0900

    completed calculator

--- src/cal/calculator/Calculator.java
+++ src/cal/calculator/Calculator.java
@@ -1,45 +1,37 @@
+package src.cal.calculator;
+
+import java.util.Scanner;
 import java.util.Stack;
 
 public class Calculator {
 
     public enum Operator{ADD, SUBTRACT, MULTIPLY, DIVIDE, BLANK}
+    private final Scanner gSc = new Scanner(System.in);
 
-//    public static void main(String[] args){
-//        String expression = "2+2*8";
-//        Calculator calc = new Calculator();
-//        System.out.println(calc.compute(expression));
-//    }
+    public String nextLine() {
+        return gSc.nextLine();
+    }
 
     public double compute(String sequence){
         Stack<Double> numberStack = new Stack<Double>();
         Stack<Operator> operatorStack = new Stack<Operator>();
         for(int i = 0; i < sequence.length(); i++){
-//            System.out.println("---------------------------------------------");
             try{
-                // number: operation 사이의 넘버
                 int number = parseNumber(sequence, i);
-//                System.out.printf("number: %d, i: %d\n", number, i);
                 numberStack.push((double)number);
 
-                // i에 number의 자리수 만큼 더함. 23 이면 2개 더함.
                 i += Integer.toString(number).length();
                 if(i >= sequence.length()){
                     break;
                 }
 
-//                System.out.printf("parse operator parameter: %s, %d\n", sequence, i);
                 Operator op = parseOperator(sequence, i);
-
-//                System.out.println("Call Collaps TOP 1");
                 collapseTop(numberStack, operatorStack, op);
                 operatorStack.push(op);
             } catch(NumberFormatException ex){
                 return Integer.MIN_VALUE;
             }
-//            System.out.println("---------------------------------------------");
-
         }
-//        System.out.println("Call Collaps TOP 2");
         collapseTop(numberStack, operatorStack, Operator.BLANK);
         if(numberStack.size() == 1 && operatorStack.size() == 0){
             return numberStack.pop();
@@ -47,18 +39,12 @@ public class Calculator {
         return 0;
     }
 
-
     private void collapseTop(Stack<Double> numberStack, Stack<Operator> operatorStack, Operator futureTop){
         while(numberStack.size() >= 2 && operatorStack.size() >= 1){
-//            System.out.println("NumberStack: "  + numberStack);
-//            System.out.println("OperatorStack: "  + operatorStack);
-//            System.out.println("FutureTop:" + futureTop);
-//            System.out.println("operatorStack.peek():" + operatorStack.peek());
             if(priorityOfOperator(futureTop) <= priorityOfOperator(operatorStack.peek())){
                 double second = numberStack.pop();
                 double first = numberStack.pop();
                 Operator op = operatorStack.pop();
-//                System.out.println("op = " + op);
                 double result = applyOp(first, op, second);
                 numberStack.push(result);
             } else{
@@ -99,10 +85,7 @@ public class Calculator {
     }
 
     private Operator parseOperator(String sequence, int offset){
-//        System.out.println("--------------start parseOperator------------------");
         if(offset < sequence.length()){
-//            System.out.println("offset: " + offset);
-//            System.out.println("sequence.length(): "+ sequence.length());
             char op = sequence.charAt(offset);
             switch (op){
                 case '+': return Operator.ADD;
