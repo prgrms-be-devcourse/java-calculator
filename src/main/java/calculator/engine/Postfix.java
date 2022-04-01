8978261abb91a213f843f570a4735dec2d0e9576
Seonghyeon Kim <goshk1995@gmail.com>
2022-04-02 01:03:47 +0900

    후위식 변경 메서드 개선 & 음수 처리 가능

--- src/main/java/calculator/engine/Postfix.java
+++ src/main/java/calculator/engine/Postfix.java
@@ -9,17 +9,16 @@ public class Postfix {
     public Postfix() {}
 
     public List<String> makeToPostfix(String formula) throws IllegalAccessException {
-        List<String> arr = new ArrayList<>();
-
         if(!isCorrectFormula(formula)) {
-            throw new IllegalArgumentException("올바른 식을 입력해 주세요.");
+            throw new IllegalArgumentException();
         }
 
+        List<String> arr = new ArrayList<>();
         Stack<String> stack = new Stack<>();
-        String[] str = formula.replaceAll(" ", "").split("");
 
-        for(int i = 0; i < str.length; i++) {
-            String s = str[i];
+        String[] splitFormula = formula.trim().replaceAll(" +", " ").split(" ");
+
+        for (String s : splitFormula) {
             switch(s) {
                 case "+":
                 case "-":
@@ -30,42 +29,39 @@ public class Postfix {
                     }
                     stack.add(s);
                     break;
-                case "(":
-                    stack.add(s);
-                    break;
-                case ")":
-                    while(!stack.isEmpty() && !stack.peek().equals("(")) {
-                        arr.add(stack.pop());
-                    }
-                    stack.pop();
-                    break;
-                case " ":
-                    break;
                 default:
-                    String temp = s;
-                    for(int j = i; j < str.length - 1; j++) {
-                        int n = str[j + 1].charAt(0) - '0';
-                        if(n >= 0 && n <= 9) {
-                            temp += String.valueOf(n);
-                            i = j + 1;
-                        } else {
-                            break;
+                    if (s.contains("(")) {
+                        for (int j = 0; j < s.length(); j++) {
+                            if (s.charAt(j) != '(') {
+                                arr.add(s.substring(j));
+                                break;
+                            }
+
+                            stack.push("(");
                         }
+                    } else if (s.contains(")")) {
+                        while (!stack.isEmpty() && !stack.peek().equals("(")) {
+                            arr.add(stack.pop());
+                        }
+
+                        stack.pop();
+                    } else {
+                        arr.add(s);
                     }
-                    arr.add(temp);
+
                     break;
             }
         }
 
-        while(!stack.isEmpty()) {
+        while(!stack.empty()) {
             arr.add(stack.pop());
         }
 
         return arr;
     }
 
-    private boolean isCorrectFormula(String formula) {
-        return Pattern.matches("^[0-9()*-/+]*$", formula.replaceAll(" ", ""));
+    public boolean isCorrectFormula(String formula) {
+        return Pattern.matches("^[0-9()*-/+]*$", formula.trim().replaceAll(" ", ""));
     }
 
     public int priority(String operator) {
