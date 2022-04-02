f687f81a36803de574acdef14a2f90bf3758f558
yuminhwan <ymh7532@icloud.com>
2022-04-02 21:04:36 +0900

    refactor: Parser 테스트 케이스 추가

--- src/test/java/calculator/utils/PostfixParserTest.java
+++ src/test/java/calculator/utils/PostfixParserTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class PostfixParserTest {
 
@@ -21,6 +22,15 @@ class PostfixParserTest {
             Arguments.of("1 * 3 + 5 / 2", Arrays.asList("1", "3", "*", "5", "2", "/", "+")),
             Arguments.of("1.5 * 3.5 + 5.5 / 2.012", Arrays.asList("1.5", "3.5", "*", "5.5", "2.012", "/", "+")),
             Arguments.of("-1.5 * 3.5 + -5.5 / 2.012", Arrays.asList("-1.5", "3.5", "*", "-5.5", "2.012", "/", "+"))
+        );
+    }
+
+    private static Stream<Arguments> provideWrongExpression() { // argument source method
+        return Stream.of(
+            Arguments.of("1 + 3 / 2 ㅁ", Arrays.asList("1", "3", "2", "/", "+")),
+            Arguments.of("1 / 5 // 3 ", Arrays.asList("1", "5", "/", "3", "/")),
+            Arguments.of("1 * 3 ++ 5 / 2", Arrays.asList("1", "3", "*", "5", "2", "/", "+")),
+            Arguments.of("1.5 * 3.5 + 5.5 / 2.012ㅇㄴㅁ", Arrays.asList("1.5", "3.5", "*", "5.5", "2.012", "/", "+"))
         );
     }
 
@@ -35,6 +45,16 @@ class PostfixParserTest {
     @ParameterizedTest
     @NullAndEmptySource
     void parse_blankOrNull_throwException(String input) {
+        assertThatThrownBy(() -> PostfixParser.parse(input))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("[ERROR] 정상적인 입력이 아닙니다. 다시 입력해주세요.");
+    }
+
+    @DisplayName("입력된 값이 잘못된 식이면 예외를 던진다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1 + 3 / 2 ㅁ", "1 / 5 // 3 ", "1 * 3 ++ 5 / 2", "1.5 * 3.5 + 5.5 / 2.012ㅇㄴㅁ", "1+2+3",
+        "1 + ! + 3"})
+    void parse_WrongInput_ThrowException(String input) {
         assertThatThrownBy(() -> PostfixParser.parse(input))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage("[ERROR] 정상적인 입력이 아닙니다. 다시 입력해주세요.");
