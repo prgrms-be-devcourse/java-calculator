1eed177df5dd5f10affc317e91be3496b536a269
Yoonoh <xkakak142@naver.com>
2022-04-02 16:20:51 +0900

    refactor: Enum 적용 및 소수 판별 함수 역할 분리

--- src/main/java/com/programmers/java/calculator/arithmetic/Validator.java
+++ src/main/java/com/programmers/java/calculator/arithmetic/Validator.java
@@ -1,31 +1,30 @@
 package com.programmers.java.calculator.arithmetic;
 
-import java.util.Map;
-
 public class Validator {
-    private final String ADD = "+";
-    private final String SUB = "-";
-    private final String MUL = "*";
-    private final String DIV = "/";
-
-    private final Map<String, Integer> priorityOfOpCode = Map.of(
-            ADD,1,
-            SUB,1,
-            MUL,2,
-            DIV,2
-    );
 
     public Integer getPriority(String op) {
-        return priorityOfOpCode.get(op);
+        return OperationCode.getPriority(op);
     }
 
     public boolean isOperator(String op) {
-        return op.equals(ADD) || op.equals(SUB) || op.equals(MUL) || op.equals(DIV);
+        return OperationCode.isOperator(op);
     }
 
     public boolean isDecimal(String num) {
-        String[] res = num.split("\\.");
-        if (res.length == 1) return false;
-        return !res[1].equals("0");
+        String[] res = splitIntegerAndRealNumbers(num);
+        if (isInteger(res)) return false;
+        return !isZeroBelowDecimalPoint(res);
+    }
+
+    private String[] splitIntegerAndRealNumbers(String num){
+        return num.split("\\.");
+    }
+
+    private boolean isInteger(String[] num) {
+        return num.length == 1;
+    }
+
+    private boolean isZeroBelowDecimalPoint(String[] num) {
+        return num[1].equals("0");
     }
 }
