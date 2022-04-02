afb7ac3a9726625a2fa861c69b291b306281d8e7
Yoonoh <xkakak142@naver.com>
2022-04-02 17:06:42 +0900

    refactor: stack -> deque로 변경

--- src/main/java/com/programmers/java/calculator/arithmetic/Operator.java
+++ src/main/java/com/programmers/java/calculator/arithmetic/Operator.java
@@ -1,19 +1,57 @@
 package com.programmers.java.calculator.arithmetic;
 
-import java.util.ArrayList;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
 import java.util.Stack;
 
 public class Operator {
     private final Validator validator = new Validator();
 
-    public String getResult(ArrayList<String> postfix) {
-        Stack<String> stack = new Stack<>();
+//    public String getResult(List<String> postfix) {
+//        Stack<String> stack = new Stack<>();
+//
+//        for (int i = 0; i < postfix.size(); i++) {
+//            stack.push(postfix.get(i));
+//            if (validator.isOperator(postfix.get(i))){
+//                stack.pop();
+//                Double n2 = Double.parseDouble(stack.pop()); // 역순으로 뽑아냄
+//                Double n1 = Double.parseDouble(stack.pop());
+//
+//                String calculationResult = switch (postfix.get(i)){
+//                    case "+" -> Double.toString(n1 + n2);
+//                    case "-" -> Double.toString(n1 - n2);
+//                    case "*" -> Double.toString(n1 * n2);
+//                    case "/" -> Double.toString(n1 / n2);
+//                    default -> "";
+//                };
+//                stack.push(calculationResult);
+//            }
+//        }
+//
+//        String res = stack.get(0);
+//        if (validator.isDecimal(res)) // 실수
+//            return res;
+//
+//        if (res.contains("."))
+//            return res.substring(0, res.indexOf(".")); // 정수인데 . 포함된 경우
+//
+//        return res; // 그냥 정수
+//    }
+
+    public String getResult(List<String> postfix) {
+        Deque<String> stack = new ArrayDeque<>();
+
         for (int i = 0; i < postfix.size(); i++) {
-            stack.push(postfix.get(i));
+            stack.addLast(postfix.get(i));
             if (validator.isOperator(postfix.get(i))){
-                stack.pop();
-                Double n2 = Double.parseDouble(stack.pop()); // 역순으로 뽑아냄
-                Double n1 = Double.parseDouble(stack.pop());
+                stack.removeLast();
+
+                Double n2 = Double.parseDouble(stack.getLast()); // 역순으로 뽑아냄
+                stack.removeLast();
+
+                Double n1 = Double.parseDouble(stack.getLast());
+                stack.removeLast();
 
                 String calculationResult = switch (postfix.get(i)){
                     case "+" -> Double.toString(n1 + n2);
@@ -22,11 +60,11 @@ public class Operator {
                     case "/" -> Double.toString(n1 / n2);
                     default -> "";
                 };
-                stack.push(calculationResult);
+                stack.addLast(calculationResult);
             }
         }
 
-        String res = stack.get(0);
+        String res = stack.getFirst();
         if (validator.isDecimal(res)) // 실수
             return res;
 
--- src/main/java/com/programmers/java/calculator/arithmetic/Parser.java
+++ src/main/java/com/programmers/java/calculator/arithmetic/Parser.java
@@ -1,35 +1,66 @@
 package com.programmers.java.calculator.arithmetic;
 
-import java.util.ArrayList;
-import java.util.EnumSet;
-import java.util.Stack;
+import java.util.*;
 
 public class Parser {
     private final Validator validator = new Validator();
 
-    public ArrayList<String> parseToToken(String expression) {
-        String[] result = expression.split("\\s+");
-        Stack<String> stack = new Stack<>();
-        ArrayList<String> token = new ArrayList<>();
+//    public ArrayList<String> parseToToken(String expression) {
+//        String[] result = expression.split("\\s+");
+//        Stack<String> stack = new Stack<>();
+//        ArrayList<String> token = new ArrayList<>();
+//
+//        for (int i = 0; i < result.length; i++) {
+//            if (validator.isOperator(result[i])) {
+//                if (stack.isEmpty())
+//                    stack.push(result[i]);
+//                else {
+//                    if (validator.getPriority(stack.peek()) >= validator.getPriority(result[i]))
+//                        token.add(stack.pop());
+//                    stack.push(result[i]);
+//                }
+//            } else
+//                token.add(result[i]);
+//        }
+//
+//        while (!stack.isEmpty()){
+//            token.add(stack.pop());
+//        }
+//
+//        return token;
+//    }
 
-        for (int i = 0; i < result.length; i++) {
-            if (validator.isOperator(result[i])) {
+    public List<String> parseToToken(String expression) {
+        String[] element = splitBySpace(expression);
+        Deque<String> stack = new ArrayDeque<>();
+        List<String> token = new ArrayList<>();
+
+        for (int i = 0; i < element.length; i++) {
+            if (validator.isOperator(element[i])) {
                 if (stack.isEmpty())
-                    stack.push(result[i]);
+                    stack.addLast(element[i]);
                 else {
-                    if (validator.getPriority(stack.peek()) >= validator.getPriority(result[i]))
-                        token.add(stack.pop());
-                    stack.push(result[i]);
+                    if (validator.getPriority(stack.getLast()) >= validator.getPriority(element[i])) {
+                        token.add(stack.getLast());
+                        stack.removeLast();
+                    }
+                    stack.addLast(element[i]);
                 }
-            } else
-                token.add(result[i]);
+            } else {
+                token.add(element[i]);
+            }
         }
 
         while (!stack.isEmpty()){
-            token.add(stack.pop());
+            token.add(stack.getLast());
+            stack.removeLast();
         }
 
         return token;
+    }
+
+    private String[] splitBySpace(String expression){
+        return expression.split("\\s+");
     }
 
 }
