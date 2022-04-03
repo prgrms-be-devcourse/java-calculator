fd19a3b15dd379ed1bcf4d6446eb68b7099339d1
dbslzld15 <dbslzld15@naver.com>
2022-04-03 14:25:10 +0900

    연산 수행 시 Expression 객체를 사용하도록 테스트 변경

--- src/test/java/model/CalculatorTest.java
+++ src/test/java/model/CalculatorTest.java
@@ -13,7 +13,8 @@ class CalculatorTest {
     @Test
     public void 덧셈계산_성공() {
         //given
-        String expression = "1 + 2";
+        String stringExpression = "1 + 2";
+        Expression expression = new Expression(stringExpression);
         //when
         double resultNum = Calculator.calculate(expression);
         //then
@@ -23,7 +24,8 @@ class CalculatorTest {
     @Test
     public void 뺄셈계산_성공() {
         //given
-        String expression = "1 - 2";
+        String stringExpression = "1 - 2";
+        Expression expression = new Expression(stringExpression);
         //when
         double resultNum = Calculator.calculate(expression);
         //then
@@ -33,7 +35,8 @@ class CalculatorTest {
     @Test
     public void 곱셈계산_성공() {
         //given
-        String expression = "1 * 2";
+        String stringExpression = "1 * 2";
+        Expression expression = new Expression(stringExpression);
         //when
         double resultNum = Calculator.calculate(expression);
         //then
@@ -43,7 +46,8 @@ class CalculatorTest {
     @Test
     public void 나눗셈계산_성공() {
         //given
-        String expression = "4 / 2";
+        String stringExpression = "4 / 2";
+        Expression expression = new Expression(stringExpression);
         //when
         double resultNum = Calculator.calculate(expression);
         //then
@@ -53,7 +57,11 @@ class CalculatorTest {
     @Test
     public void 우선순위_계산_성공() {
         //given
-        List<String> expressions = Arrays.asList("4 + 9 / 3 - 1 + 2 * 10", "20 / 4 + 9 - 10 * 5 / 2");
+        List<String> stringExpression = Arrays.asList("4 + 9 / 3 - 1 + 2 * 10", "20 / 4 + 9 - 10 * 5 / 2");
+        List<Expression> expressions = stringExpression
+                .stream()
+                .map(Expression::new)
+                .collect(Collectors.toList());
         //when
         List<Double> resultNums = expressions
                 .stream()
@@ -66,10 +74,10 @@ class CalculatorTest {
     @Test
     public void 빈공백이_들어오면_오류가_발생한다(){
         //given
-        String expression = "";
+        String stringExpression = "";
         //when
         //then
-        assertThatThrownBy(() -> Calculator.calculate(expression))
+        assertThatThrownBy(() -> new Expression(stringExpression))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("연산자와 숫자가 제대로 입력되지 않았습니다.");
     }
@@ -77,10 +85,10 @@ class CalculatorTest {
     @Test
     public void 연산자와_피연산자의_순서가_맞지않을때_오류가_발생한다(){
         //given
-        String expression = "* 1 - 2";
+        String stringExpression = "* 1 - 2";
         //when
         //then
-        assertThatThrownBy(() -> Calculator.calculate(expression))
+        assertThatThrownBy(() -> new Expression(stringExpression))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("연산자와 숫자가 제대로 입력되지 않았습니다.");
     }
@@ -88,10 +96,10 @@ class CalculatorTest {
     @Test
     public void 연산자_혹은_숫자이외에_다른문자가_입력되면_오류가_발생한다(){
         //given
-        String expression = "a 1 - 2";
+        String stringExpression = "a 1 - 2";
         //when
         //then
-        assertThatThrownBy(() -> Calculator.calculate(expression))
+        assertThatThrownBy(() -> new Expression(stringExpression))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("연산자 혹은 숫자만 입력해주세요");
     }
