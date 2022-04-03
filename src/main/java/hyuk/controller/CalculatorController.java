e9c1ccd92c3f1ff13bde3d2036ba00c21e408f1f
hyuk0309 <bahngeh@naver.com>
2022-04-03 18:26:33 +0900

    refactor : CalculatorController 모든 메서드 depth 1로 수정

--- src/main/java/hyuk/controller/CalculatorController.java
+++ src/main/java/hyuk/controller/CalculatorController.java
@@ -29,46 +29,52 @@ public class CalculatorController {
 
     public void run() {
         while (true) {
-            outputView.printMenu();
-            try {
-                Menu menu = Menu.of(inputView.selectMenu());
-                outputView.printEmptySpace();
-
-                if (menu.isPrint()) {
-
-                    int dataSize = repository.getRecordsSize();
-                    List<Record> records = new ArrayList<>();
-                    for (long i = 1; i <= dataSize; ++i) {
-                        records.add(repository.findById(i));
-                    }
-
-                    outputView.printRecords(new RecordsDTO(records));
-                    continue;
-                }
-                calculate();
-            } catch (Exception e) {
-                System.out.println(e.getMessage() + "\n");
-                continue;
-            }
+            startCalculator();
         }
     }
 
-    private void calculate() {
-        while (true) {
-            try {
-                String formula = inputView.inputFormula();
+    private void startCalculator() {
+        outputView.printMenu();
+        try {
+            Menu menu = Menu.of(inputView.selectMenu());
+            outputView.printEmptySpace();
 
-                Result result = calculator.calculate(new PostOrderFormula(formula));
-
-                Record record = Record.createRecord(formula, result);
-                repository.store(record);
-
-                outputView.printResult(new ResultDTO(result));
-                return;
-            } catch (Exception e) {
-                System.out.println(e.getMessage() + "\n");
-            }
+            process(menu);
+        } catch (Exception e) {
+            System.out.println(e.getMessage() + "\n");
         }
+    }
+
+    private void process(Menu menu) {
+        if (menu.isPrint()) {
+            printRecords();
+            return;
+        }
+        calculate();
+    }
+
+    private void printRecords() {
+        int dataSize = repository.getRecordsSize();
+        List<Record> records = new ArrayList<>();
+        for (long i = 1; i <= dataSize; ++i) {
+            records.add(repository.findById(i));
+        }
+
+        outputView.printRecords(new RecordsDTO(records));
+    }
+
+    private void calculate() {
+        String formula = inputView.inputFormula();
+
+        Result result = calculator.calculate(new PostOrderFormula(formula));
+
+        saveRecord(Record.createRecord(formula, result));
+
+        outputView.printResult(new ResultDTO(result));
+    }
+
+    private void saveRecord(Record record) {
+        repository.store(record);
     }
 
 }
