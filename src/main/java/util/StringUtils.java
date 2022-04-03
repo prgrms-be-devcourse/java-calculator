e0f5d05d4f8758055990e728eeb8390e762c63a7
dbslzld15 <dbslzld15@naver.com>
2022-04-03 14:25:32 +0900

    파싱을 담당하는 유틸 클래스 구현

---
+++ src/main/java/util/StringUtils.java
@@ -0,0 +1,9 @@
+package util;
+
+public class StringUtils {
+    private static final String BLANK = " ";
+
+    public static String[] splitByBlank(String inputExpression){
+        return inputExpression.split(BLANK);
+    }
+}
