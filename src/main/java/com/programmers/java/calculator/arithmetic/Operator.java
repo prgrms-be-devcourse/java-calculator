d575d4e92554873f112c2e7b3925619c18a575a6
Yoonoh <xkakak142@naver.com>
2022-04-03 23:34:21 +0900

    remove: 미사용 주석 코드 제거

--- src/main/java/com/programmers/java/calculator/arithmetic/Operator.java
+++ src/main/java/com/programmers/java/calculator/arithmetic/Operator.java
@@ -8,37 +8,6 @@ import java.util.Stack;
 public class Operator {
     private final Validator validator = new Validator();
 
-//    public String getResult(List<String> postfix) {
-//        Stack<String> stack = new Stack<>();
-//
-//        for (int i = 0; i < postfix.size(); i++) {
-//            stack.push(postfix.get(i));
-//            if (validator.isOperator(postfix.get(i))){
-//                stack.pop();
-//                Double n2 = Double.parseDouble(stack.pop()); // 역순으로 뽑아냄
-//                Double n1 = Double.parseDouble(stack.pop());
-//
-//                String calculationResult = switch (postfix.get(i)){
-//                    case "+" -> Double.toString(n1 + n2);
-//                    case "-" -> Double.toString(n1 - n2);
-//                    case "*" -> Double.toString(n1 * n2);
-//                    case "/" -> Double.toString(n1 / n2);
-//                    default -> "";
-//                };
-//                stack.push(calculationResult);
-//            }
-//        }
-//
-//        String res = stack.get(0);
-//        if (validator.isDecimal(res)) // 실수
-//            return res;
-//
-//        if (res.contains("."))
-//            return res.substring(0, res.indexOf(".")); // 정수인데 . 포함된 경우
-//
-//        return res; // 그냥 정수
-//    }
-
     public String getResult(List<String> postfix) {
         Deque<String> stack = new ArrayDeque<>();
 
--- src/main/java/com/programmers/java/calculator/arithmetic/Parser.java
+++ src/main/java/com/programmers/java/calculator/arithmetic/Parser.java
@@ -5,31 +5,6 @@ import java.util.*;
 public class Parser {
     private final Validator validator = new Validator();
 
-//    public ArrayList<String> parseToToken(String expression) {
-//        String[] result = expression.split("\\s+");
-//        Stack<String> stack = new Stack<>();
-//        ArrayList<String> token = new ArrayList<>();
-//
-//        for (int i = 0; i < result.length; i++) {
-//            if (validator.isOperator(result[i])) {
-//                if (stack.isEmpty())
-//                    stack.push(result[i]);
-//                else {
-//                    if (validator.getPriority(stack.peek()) >= validator.getPriority(result[i]))
-//                        token.add(stack.pop());
-//                    stack.push(result[i]);
-//                }
-//            } else
-//                token.add(result[i]);
-//        }
-//
-//        while (!stack.isEmpty()){
-//            token.add(stack.pop());
-//        }
-//
-//        return token;
-//    }
-
     public List<String> parseToToken(String expression) {
         String[] element = splitBySpace(expression);
         Deque<String> stack = new ArrayDeque<>();
