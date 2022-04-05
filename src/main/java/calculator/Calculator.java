d75cdcd4e6fbcaf63c454e2dced561df29771e0d
blessing333 <>
2022-04-05 22:37:35 +0900

    CalculationProcessor 클래스 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,86 +1,55 @@
 package calculator;
 
-import calculator.model.expression.Expression;
-import calculator.model.expression.ExpressionFactory;
-import calculator.module.arithmetic.ArithmeticModule;
-import calculator.module.history.CalculationHistory;
 import calculator.module.history.CalculationHistoryManager;
 import calculator.module.ui.UserInterface;
-import calculator.module.validator.ExpressionValidator;
-import calculator.module.validator.exception.InvalidExpressionException;
+
 /**
-*
-* Calculator 설명
-* 문자열 수식을 입력받아 계산해주는 클래스
- *
+ * Calculator 설명
+ * 문자열 수식을 입력받아 계산해주는 클래스
+ * <p>
  * 기능
  * 1. 개산 내역 조회
  *    - 현재까지 계산한 모든 내역을 확인 가능  -> CalculationHistoryManager 클래스에 위임
  * 2. 문자열 수식 계산
- *    - 원시 문자열로 구성된 수식이 유효한 수식인지 검증 -> ExpressionValidator 클래스에 위임
- *    - 검증된 문자열 수식 계산 -> ArithmeticModule 클래스에 위임
-**/
+ *    - 문자열 수식을 입력받아 계산 -> CalculationProcessor 클래스에 위임
+ **/
 
 public class Calculator {
-    private static final String INVALID_COMMAND_OPTION_MESSAGE ="잘못된 명령입니다.";
-    private static final String CALCULATION_GUILD_MESSAGE ="계산 수식을 입력하세요. 연산자(괄호 포함)와 숫자는 반드시 공백으로 구분해주세요.";
-
-    private final CalculationHistoryManager calculationHistoryManager;
-    private final ArithmeticModule arithmeticModule;
-    private final ExpressionValidator expressionValidator;
+    private static final String INVALID_COMMAND_OPTION_MESSAGE = "잘못된 명령입니다.";
     private final UserInterface userInterface;
-    private final ExpressionFactory expressionFactory;
+    private final CalculationHistoryManager historyManager;
+    private final CalculationProcessor calculationProcessor;
 
-    public Calculator(CalculationHistoryManager calculationHistoryManager,
-                      ArithmeticModule arithmeticModule,
-                      ExpressionValidator expressionValidator,
-                      UserInterface userInterface,
-                      ExpressionFactory expressionFactory){
-        this.calculationHistoryManager = calculationHistoryManager;
-        this.arithmeticModule = arithmeticModule;
-        this.expressionValidator = expressionValidator;
+    public Calculator(UserInterface userInterface,
+                      CalculationHistoryManager historyManager,
+                      CalculationProcessor calculationProcessor) {
         this.userInterface = userInterface;
-        this.expressionFactory = expressionFactory;
+        this.historyManager = historyManager;
+        this.calculationProcessor = calculationProcessor;
     }
 
-    public void run(){
+    public void run() {
         boolean isUserSelectQuit = false;
         String userInput;
-        while(!isUserSelectQuit){
+        while (!isUserSelectQuit) {
             userInterface.showMenu();
             userInput = userInterface.inputString();
             CommandOption command = CommandOption.createCommandOption(userInput);
-            switch (command){
-                case SHOW_HISTORY :
-                    calculationHistoryManager.printAllCalculationHistory();
+            switch (command) {
+                case SHOW_HISTORY:
+                    historyManager.printAllCalculationHistory();
                     break;
-                case CALCULATION :
-                    startCalculateProcess();
+                case CALCULATION:
+                    calculationProcessor.startCalculateProcess();
                     break;
-                case QUIT :
+                case QUIT:
                     isUserSelectQuit = true;
                     break;
-                default :
+                default:
                     userInterface.printMessage(INVALID_COMMAND_OPTION_MESSAGE);
                     break;
             }
         }
     }
-
-    private void startCalculateProcess(){
-        userInterface.printMessage(CALCULATION_GUILD_MESSAGE);
-        String userInput = userInterface.inputString();
-        try{
-            Expression expression = expressionFactory.createExpression(userInput);
-            expressionValidator.validateExpression(expression);
-            Double calculationResult = arithmeticModule.calculate(expression);
-            CalculationHistory newHistory = new CalculationHistory(userInput,calculationResult);
-            calculationHistoryManager.saveCalculationResultToHistory(userInput,newHistory);
-            userInterface.printMessage(calculationResult.toString());
-        }catch (InvalidExpressionException e) {
-            userInterface.printMessage(e.getMessage());
-        }
-    }
-
 }
 
--- src/main/java/calculator/config/DependencyConfigurer.java
+++ src/main/java/calculator/config/DependencyConfigurer.java
@@ -1,5 +1,6 @@
 package calculator.config;
 
+import calculator.CalculationProcessor;
 import calculator.Calculator;
 import calculator.model.expression.ExpressionFactory;
 import calculator.model.expression.TokenTypeChecker;
@@ -15,45 +16,52 @@ import calculator.module.validator.CommonExpressionValidator;
 import calculator.module.validator.ExpressionValidator;
 
 /**
-* DependencyConfigurer 설명
-*   각 클래스들의 의존성을 설정하고 설정된 의존성에 맞추어 객체를 생성해주는 클래스
-**/
+ * DependencyConfigurer 설명
+ * 각 클래스들의 의존성을 설정하고 설정된 의존성에 맞추어 객체를 생성해주는 클래스
+ **/
 
 public class DependencyConfigurer {
-    public Calculator createCalculatorWithDependency(){
-        return new Calculator(createCalculationHistoryManagerWithDependency(),
-                                createArithmeticModuleWithDependency(),
-                                createExpressionValidatorWithDependency(),
-                                createUserInterfaceWithDependency(),
-                                createExpressionFactoryWithDependency()
+    public Calculator createCalculatorWithDependency() {
+        return new Calculator(createUserInterfaceWithDependency(),
+                createCalculationHistoryManagerWithDependency(),
+                createCalculationProcessorWithDependency()
         );
     }
 
-    public CalculationHistoryManager createCalculationHistoryManagerWithDependency(){
+    public CalculationHistoryManager createCalculationHistoryManagerWithDependency() {
         return new MemoryCalculationHistoryManager(createUserInterfaceWithDependency());
     }
 
-    public ArithmeticModule createArithmeticModuleWithDependency(){
+    public CalculationProcessor createCalculationProcessorWithDependency() {
+        return new CalculationProcessor(createCalculationHistoryManagerWithDependency(),
+                createArithmeticModuleWithDependency(),
+                createUserInterfaceWithDependency(),
+                createExpressionValidatorWithDependency(),
+                createExpressionFactoryWithDependency()
+        );
+    }
+
+    public ArithmeticModule createArithmeticModuleWithDependency() {
         return new StackArithmeticModule();
     }
 
-    public ExpressionValidator createExpressionValidatorWithDependency(){
+    public ExpressionValidator createExpressionValidatorWithDependency() {
         return new CommonExpressionValidator();
     }
 
-    public UserInterface createUserInterfaceWithDependency(){
+    public UserInterface createUserInterfaceWithDependency() {
         return new ConsoleUserInterface();
     }
 
-    public OperatorFactory createOperatorFactoryWithDependency(){
+    public OperatorFactory createOperatorFactoryWithDependency() {
         return new OperatorFactoryImpl();
     }
 
-    public ExpressionFactory createExpressionFactoryWithDependency(){
+    public ExpressionFactory createExpressionFactoryWithDependency() {
         return new ExpressionFactory(createTokenClassifierWithDependency());
     }
 
-    public TokenTypeChecker createTokenClassifierWithDependency(){
+    public TokenTypeChecker createTokenClassifierWithDependency() {
         return new TokenTypeChecker(createOperatorFactoryWithDependency());
     }
 }
