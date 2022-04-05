8df37e3ecbcfa3958303d071eac449ca19e5ce3e
ichoi <ichoi@student.42seoul.kr>
2022-04-06 01:55:01 +0900

    feat:종료 기능을 추가하라

--- app/src/main/java/com/programmers/java/calculator/engine/Calculator.java
+++ app/src/main/java/com/programmers/java/calculator/engine/Calculator.java
@@ -6,14 +6,13 @@ import com.programmers.java.calculator.engine.repository.LogRepository;
 import com.programmers.java.calculator.engine.repository.Repository;
 import lombok.AllArgsConstructor;
 
-import java.util.Optional;
-
 @AllArgsConstructor
 public class Calculator implements Runnable{
     private Console input;
     private Console output;
     private Repository repository = new LogRepository();
 
+    static final String exit = "0";
     static final String printLog = "1";
     static final String calculate = "2";
 
@@ -31,39 +30,38 @@ public class Calculator implements Runnable{
             String choice = (input.input("선택 : "));
 
             switch (choice) {
-                case printLog -> repository.output();
+                case exit -> {
+                    return;
+                }
+                case printLog -> repository.printLog();
                 case calculate -> checkValidate(input, output, repository);
                 default -> output.inputError();
             }
         }
     }
 
-    private static void checkValidate(Console input, Console output, Repository repository) {
+    private void checkValidate(Console input, Console output, Repository repository) {
         {
             String inputString = input.input("\n");
-            Optional<Arithmetic> arithmetic = new Validation().checkValid(inputString);
-            if (arithmetic.isEmpty()) {
+            Arithmetic arithmetic = new Validation().tokenize(inputString);
+            if (arithmetic.getArithmetic().isEmpty()) {
                 output.inputError();
             } else {
-                runCalculate(output, arithmetic.get(), repository, inputString);
+                runCalculate(output, arithmetic, repository, inputString);
             }
         }
     }
 
-    private static void runCalculate(Console output, Arithmetic arithmetic, Repository repository, String inputString) {
+    private void runCalculate(Console output, Arithmetic arithmetic, Repository repository, String inputString) {
         Calculation calculation = new Calculation();
         Arithmetic postfix = calculation.toPostfix(arithmetic);
-        Optional<Double> result = calculation.doCalculation(postfix);
+        double result = calculation.doCalculation(postfix);
 
-        if (result.isEmpty()) {
-            output.calcError();
-        } else {
-            output.outputCalculationResult(result.get());
-            repository.save(arithmeticToString(inputString, result.get()));
-        }
+        output.outputCalculationResult(result);
+        repository.save(arithmeticToString(inputString, result));
     }
 
-    private static String arithmeticToString(String arithmetic, Double result) {
+    private String arithmeticToString(String arithmetic, double result) {
         return arithmetic + " = " + result;
     }
 }
