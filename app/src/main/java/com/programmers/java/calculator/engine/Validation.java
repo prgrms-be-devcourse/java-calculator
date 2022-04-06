2bb5c91c259fa54e8f207bde18f8da779da16fc7
ichoi <ichoi@student.42seoul.kr>
2022-04-06 20:57:31 +0900

    fix:반환값을 수정하라

--- app/src/main/java/com/programmers/java/calculator/engine/Validation.java
+++ app/src/main/java/com/programmers/java/calculator/engine/Validation.java
@@ -8,29 +8,29 @@ import java.util.Arrays;
 
 public class Validation {
     public Arithmetic tokenize(String input) {
-        if (input == null) return (returnEmptyList());
+        if (input == null) return (getEmptyList());
         String[] splitTokens = input.split("\\s");
         boolean checkNumeric = true;
 
         for (String splitToken : splitTokens) {
             if (checkNumeric) {
-                checkNumeric = false;
                 if (!RegularExpression.isNumeric(splitToken)) {
-                    return returnEmptyList();
+                    return getEmptyList();
                 }
+                checkNumeric = false;
             } else {
-                checkNumeric = true;
                 if (!RegularExpression.isOperator(splitToken)) {
-                    return returnEmptyList();
+                    return getEmptyList();
                 }
+                checkNumeric = true;
             }
         }
-        if (checkNumeric) returnEmptyList();
+        if (checkNumeric) return getEmptyList();
 
         return new Arithmetic(new ArrayList<>(Arrays.asList(splitTokens)));
     }
 
-    private Arithmetic returnEmptyList() {
+    private Arithmetic getEmptyList() {
         return new Arithmetic(new ArrayList<>());
     }
 }
