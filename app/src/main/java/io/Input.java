cc1bd1a5428367da4da551c699386fc14f1342d1
wisehero <godjw96@gmail.com>
2022-04-07 23:55:19 +0900

    # Console Input, Output 생성

---
+++ app/src/main/java/io/Input.java
@@ -0,0 +1,9 @@
+package io;
+
+public interface Input {
+
+    String showOption();
+
+    String inputForCalculate();
+
+}
---
+++ app/src/main/java/io/Output.java
@@ -0,0 +1,12 @@
+package io;
+
+import model.Expression;
+
+import java.awt.*;
+import java.util.List;
+
+public interface Output {
+
+    void showHistory(List<Expression> list);
+
+}
