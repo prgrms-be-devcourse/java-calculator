1e9c1b69c2087ae46f108de963273956e255cd99
HyoungUkJJang <guddnr1451@gmail.com>
2022-04-10 17:30:05 +0900

    refactor : 계산기 컨트롤러 메소드 참조 이름 수정 및 상수제거

--- app/src/main/java/com/programmers/cal/logic/CalculatorController.java
+++ app/src/main/java/com/programmers/cal/logic/CalculatorController.java
@@ -7,27 +7,24 @@ import java.util.ArrayList;
 
 public class CalculatorController {
 
-    private static final int LOOK_UP_BTN = 1;
-    private static final int CALCULATOR_BTN = 2;
-    private static final int EXIT_NUMBER_BTN = 3;
-
     private CalculatorStore calculatorStore = new CalculatorStore(new ArrayList<>());
 
     public void start() {
 
         while (true) {
-            switch (InputView.preparation()) {
-                case LOOK_UP_BTN:
-                    OutPutView.calculatorHistory(calculatorStore.selectAll());
+            OutPutView.showsTheMenu();
+
+            switch (InputView.selectTheMenu()) {
+                case LOOKUP:
+                    OutPutView.showsTheCalculationHistory(calculatorStore.selectAll());
                     break;
-                case CALCULATOR_BTN:
+                case CALCULATE:
                     String formula = InputView.inputTheFormula();
-                    double result = CalculatorLogic
-                            .Calculation(formula);
+                    double result = CalculatorLogic.calculateTheSum(formula);
                     calculatorStore.save(new Formula(formula, result));
-                    OutPutView.calculatorResult(result);
+                    OutPutView.showsTheCalculationResult(result);
                     break;
-                case EXIT_NUMBER_BTN:
+                case EXIT:
                     return ;
             }
         }
