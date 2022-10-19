a91626943069fc2ec9c483373e2c523bd2bd04b8
preferkim <shineecard5@gmail.com>
2022-10-19 20:19:31 +0900

    initial commit

---
+++ src/test/java/ConfigTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConfigTest {
+    @Test
+    void assertjTest() {
+        assertThat(1).isEqualTo(1);
+    }
+}
