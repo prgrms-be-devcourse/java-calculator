c4b92ccd5c849ceb207fa5570a4b46f239300961
hongbin <fusis1@naver.com>
2022-10-20 22:00:33 +0900

    feat: 계산기 개발

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+
+        var text = scanner.nextLine();
+
+        System.out.println(text);
+    }
+}
