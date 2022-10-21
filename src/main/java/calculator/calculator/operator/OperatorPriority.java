4fa8b5bd3c5d12b5428667647543956012f6124f
hyunseo <goatsfish@gmail.com>
2022-10-21 23:55:40 +0900

    feat : 연산자 중요도 기능 추가

---
+++ src/main/java/calculator/calculator/operator/OperatorPriority.java
@@ -0,0 +1,45 @@
+package calculator.calculator.operator;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public enum OperatorPriority {
+
+    PLUS_PRIORITY("+", 1),
+    MINUS_PRIORITY("-", 1),
+    MULTIPLY_PRIORITY("*", 2),
+    DIVIDE_PRIORITY("/", 2);
+
+    private final String operator;
+    private final Integer priority;
+
+    OperatorPriority(String operator, Integer priority) {
+        this.operator = operator;
+        this.priority = priority;
+    }
+
+    public static OperatorPriority findOperator(String inputOperator) {
+        return findOptionalOperator(inputOperator)
+                .orElseThrow(() -> new NullPointerException());
+    }
+
+    public static boolean isOperator(String inputOperator) {
+        return findOptionalOperator(inputOperator).isPresent();
+    }
+
+    public boolean isSameOrMoreImportantThan(String inputOperator) {
+        return priority >= findPriority(inputOperator);
+    }
+
+    private Integer findPriority(String inputOperator) {
+        return findOptionalOperator(inputOperator)
+                .orElseThrow(() -> new NullPointerException())
+                .priority;
+    }
+
+    private static Optional<OperatorPriority> findOptionalOperator(String inputOperator) {
+        return Arrays.stream(OperatorPriority.values())
+                .filter(operator -> operator.operator.equals(inputOperator))
+                .findFirst();
+    }
+}
