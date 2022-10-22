adee2d67ab68ebdaeb0e1788d52e2686662e1929
hyunseo <goatsfish@gmail.com>
2022-10-22 15:54:24 +0900

    refactor : 후위 표기법 파싱 메서드 리팩터링

--- src/main/java/calculator/calculator/notation/parser/NotationPostfixParser.java
+++ src/main/java/calculator/calculator/notation/parser/NotationPostfixParser.java
@@ -1,45 +1,60 @@
 package calculator.calculator.notation.parser;
 
-import calculator.calculator.operator.Operators;
-
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.List;
 
-import static calculator.calculator.operator.OperatorPriority.*;
-import static calculator.calculator.operator.Operators.*;
+import static calculator.calculator.operator.Operators.isLeftSameOrMoreImportantThan;
 import static calculator.calculator.operator.Operators.isOperator;
 
 public class NotationPostfixParser implements NotationParser {
 
+    private final Deque<String> operators = new ArrayDeque<>();
+
     @Override
     public List<String> parseFrom(List<String> formulas) {
-        Deque<String> operators = new ArrayDeque<>();
-        List<String> notation = new ArrayList<>();
+        initParser();
 
+        List<String> notation = new ArrayList<>();
         formulas.forEach(formula -> {
-            if (isOperator(formula)) {
-                if (checkPopAll(operators, formula)) {
-                    addOperatorsToNotation(operators, notation);
-                }
-                operators.add(formula);
-            }
-            if (!isOperator(formula)) {
-                notation.add(formula);
-            }
+            handleOperator(notation, formula);
+            handleOperand(notation, formula);
         });
 
         return notation;
     }
 
-    private static void addOperatorsToNotation(Deque<String> operators, List<String> notation) {
+    private void initParser() {
+        operators.clear();
+    }
+
+    private void handleOperand(List<String> notation, String formula) {
+        if (!isOperator(formula)) {
+            notation.add(formula);
+        }
+    }
+
+    private void handleOperator(List<String> notation, String formula) {
+        if (isOperator(formula)) {
+            addOperationsByComparingPriority(notation, formula);
+            operators.add(formula);
+        }
+    }
+
+    private void addOperationsByComparingPriority(List<String> notation, String formula) {
+        if (checkPopAll(formula)) {
+            addOperatorsToNotation(notation);
+        }
+    }
+
+    private void addOperatorsToNotation(List<String> notation) {
         while (!operators.isEmpty()) {
             notation.add(operators.pollLast());
         }
     }
 
-    private static boolean checkPopAll(Deque<String> operators, String operator) {
+    private boolean checkPopAll(String operator) {
         return !operators.isEmpty()
                 && isLeftSameOrMoreImportantThan(operators.peekLast(), operator);
     }
