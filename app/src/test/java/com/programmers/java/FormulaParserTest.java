de60f32bdad1444ea059f79e9194c4e26362909b
권성준 <tjdwns1865@naver.com>
2022-10-22 17:15:56 +0900

    test: 중위표기법을 후위표기법으로 변환하는 테스트
    괄호가 없을 시, 괄호가 있을 시 잘 변환되는지 확인하고자한다.

---
+++ app/src/test/java/com/programmers/java/FormulaParserTest.java
@@ -0,0 +1,19 @@
+package com.programmers.java;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class FormulaParserTest {
+    @Test
+    void changeInfixToPostfix() {
+        FormulaParser parser = new FormulaParser();
+        String infixFormula1 = "1+2*3";
+        String infixFormula2 = "(1+2)/3";
+
+        String postfixFormula1 = parser.changeInfixToPostfix(infixFormula1);
+        String postfixFormula2 = parser.changeInfixToPostfix(infixFormula2);
+
+        Assertions.assertEquals(postfixFormula1, "123*+");
+        Assertions.assertEquals(postfixFormula2, "12+3/");
+    }
+}
