12db45863ad18e1fa32b1c25b30f0d6ed9be2fb4
fortune00 <sinyoungbok99@gmail.com>
2022-10-24 01:54:19 +0900

    test: 여러 연산식에 따른 계산기 테스트

---
+++ app/src/test/java/calculator/Fixture.java
@@ -0,0 +1,13 @@
+package calculator;
+
+public class Fixture {
+    public static String EXPRESSION_div_0 = "2 / 0";
+    public static String EXPRESSION_add_min = "1 + 1 - 5";
+    public static int EXPRESSION_add_min_answer = -3;
+    public static String EXPRESSION_div_mul = "2 * 3 / 4";
+    public static int EXPRESSION_div_mul_answer = 1;
+    public static String EXPRESSION_add_div = "2 + 4 / 4";
+    public static int EXPRESSION_add_div_answer = 3;
+    public static String EXPRESSION_bracket = "2 * (3 + 5)";
+    public static int EXPRESSION_bracket_answer = 16;
+}
