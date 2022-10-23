070a01cc33d0d22020151696176265a9a78bf991
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-10-23 23:16:54 +0900

    test : 문자열 Parse 기능 테스트

---
+++ src/test/java/calculator/parse/StringParseTest.java
@@ -0,0 +1,45 @@
+package calculator.parse;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+class StringParseTest {
+
+    StringParse stringParse = new StringParse();
+
+    @Test
+    @DisplayName("문자열 공백 제거")
+    void test1() {
+
+        //given
+        String origin = "1 + 2  /  3 -4";
+        String expect = "1+2/3-4";
+
+        //when
+        String parseStr = stringParse.parse(origin);
+
+        //then
+        Assertions.assertEquals(expect, parseStr);
+
+    }
+
+    @Test
+    @DisplayName("문자열 split")
+    void test2() {
+
+        //given
+        String origin = "1+2/3-4";
+        List<String> expect = Arrays.asList("1", "+", "2", "/", "3", "-", "4");
+
+        //when
+        List<String> savedList = stringParse.toList(origin);
+
+        //then
+        Assertions.assertEquals(expect, savedList);
+    }
+}
