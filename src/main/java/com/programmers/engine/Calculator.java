b639ed092711ff93b27f7ef64765c8dc9aec4478
Bosub Kim <39071638+boompatron@users.noreply.github.com>
2022-10-23 13:15:00 +0900

    DB 조회 기능 및 DB에 데이터 넣기 기능 추가 DB는 LinkedList<String>으로 구현
    한 이유? -> 데이터에 순차적으로 접근해야 해서 LinkedList가 속도가 빠를 것 같아서

--- src/main/java/com/programmers/engine/Calculator.java
+++ src/main/java/com/programmers/engine/Calculator.java
@@ -20,13 +20,9 @@ public class Calculator implements Runnable{
         while(true){
             int state = Integer.parseInt(input.input("1. 조회\n2. 계산\n"));
 
-            if (state == 1) {
-                dataBase.showAll();
-                System.out.println("DB 조회 선택");
-            } //  DB 조회
+            if (state == 1) dataBase.showAll(); //  DB 조회
             else if (state == 2){
                 formula.setContent(input.input(""));  // 실질적인 계산
-                System.out.println("계산 선택");
                 System.out.println(formula.getContent());
             }
             else break;
--- src/main/java/com/programmers/engine/model/DataBase.java
+++ src/main/java/com/programmers/engine/model/DataBase.java
@@ -1,6 +1,12 @@
 package com.programmers.engine.model;
 
+import java.util.LinkedList;
+
 public class DataBase {
+    private LinkedList<String> data;
     public void showAll() {
+        if (data.isEmpty()) System.out.println("저장된 데이터가 없습니다");
+        else                data.forEach(System.out::println);
     }
+    public void addData(String s){data.add(s);}
 }
