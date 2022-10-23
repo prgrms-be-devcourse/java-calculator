7d7a0476161756d74e4db93fce367601c8dab377
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-10-23 23:16:21 +0900

    feat : 문자열 Parse 기능

---
+++ src/main/java/calculator/parse/Parse.java
@@ -0,0 +1,12 @@
+package calculator.parse;
+
+import java.util.List;
+
+public interface Parse {
+
+    String parse(String s);
+
+    List<String> toList(String s);
+
+
+}
---
+++ src/main/java/calculator/parse/StringParse.java
@@ -0,0 +1,21 @@
+package calculator.parse;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class StringParse implements Parse {
+
+
+    @Override
+    public String parse(String s) {
+        return s.trim().replace(" ", "");
+    }
+
+    @Override
+    public List<String> toList(String s) {
+        return Arrays.asList(s.split("(?=[+/*-])|(?<=[+/*-])"));
+    }
+
+
+}
