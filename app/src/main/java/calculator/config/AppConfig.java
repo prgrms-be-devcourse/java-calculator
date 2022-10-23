53e83626047b95806ca5e76d2148b44113dfc819
fortune00 <sinyoungbok99@gmail.com>
2022-10-23 23:40:17 +0900

    add: Map을 기반으로 하는 레포지토리 생성 및 반영

--- app/src/main/java/calculator/config/AppConfig.java
+++ app/src/main/java/calculator/config/AppConfig.java
@@ -3,13 +3,13 @@ package calculator.config;
 import calculator.domain.BaseCalculation;
 import calculator.domain.BaseCalculator;
 import calculator.domain.Calculator;
-import calculator.repository.ListCalculatorRepository;
+import calculator.repository.MapCalculatorRepository;
 import calculator.service.BaseCalculatorService;
 import calculator.service.CalculatorService;
 
 public class AppConfig {
 
-    private static final Calculator calculator = new BaseCalculator(new BaseCalculation(), new ListCalculatorRepository());
+    private static final Calculator calculator = new BaseCalculator(new BaseCalculation(), new MapCalculatorRepository());
     private static final CalculatorService calculatorService = new BaseCalculatorService(calculator);
 
     public CalculatorService calculatorService() {
