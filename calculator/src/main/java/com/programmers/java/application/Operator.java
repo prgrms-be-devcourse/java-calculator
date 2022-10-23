679a17a227a365e0f819e68a6f3b284928c01349
dlxortmd987 <dlxortmd987@gmail.com>
2022-10-23 21:40:48 +0900

    feat: Operator implements 구현

--- calculator/src/main/java/com/programmers/java/application/Operator.java
+++ calculator/src/main/java/com/programmers/java/application/Operator.java
@@ -8,25 +8,21 @@ import com.programmers.java.engine.operator.Plus;
 public class Operator implements Plus, Minus, Multiply, Divide {
     @Override
     public double divide(Double lhs, Double rhs) {
-
-        return 0;
+        return lhs/rhs;
     }
 
     @Override
     public double minus(Double lhs, Double rhs) {
-
-        return 0;
+        return lhs-rhs;
     }
 
     @Override
     public double multiply(Double lhs, Double rhs) {
-
-        return 0;
+        return lhs*rhs;
     }
 
     @Override
     public double plus(Double lhs, Double rhs) {
-
-        return 0;
+        return lhs+rhs;
     }
 }
