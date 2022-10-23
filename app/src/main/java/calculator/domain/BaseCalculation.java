16c6eb554f1723f96cdc4b9a05ab291cb5068c03
fortune00 <sinyoungbok99@gmail.com>
2022-10-23 22:35:29 +0900

    feat: 사칙연산 함수 생성

---
+++ app/src/main/java/calculator/domain/BaseCalculation.java
@@ -0,0 +1,23 @@
+package calculator.domain;
+
+public class BaseCalculation implements Calculation {
+    @Override
+    public int addition(int a, int b) {
+        return a + b;
+    }
+
+    @Override
+    public int subtraction(int a, int b) {
+        return a - b;
+    }
+
+    @Override
+    public int multiplication(int a, int b) {
+        return a * b;
+    }
+
+    @Override
+    public int division(int a, int b) {
+        return a / b;
+    }
+}
---
+++ app/src/main/java/calculator/domain/Calculation.java
@@ -0,0 +1,8 @@
+package calculator.domain;
+
+public interface Calculation {
+    int addition(int a, int b);
+    int subtraction(int a, int b);
+    int multiplication(int a, int b);
+    int division(int a, int b);
+}
