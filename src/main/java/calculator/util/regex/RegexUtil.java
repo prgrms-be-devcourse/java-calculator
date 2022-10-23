42389f06f297fe3a475939431c8647fa059318cd
hyunseo <goatsfish@gmail.com>
2022-10-23 11:31:43 +0900

    feat : 연산식 소수점 판별 정규표현식 기능 추가

--- src/main/java/calculator/util/regex/RegexUtil.java
+++ src/main/java/calculator/util/regex/RegexUtil.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class RegexUtil {
 
-    public static final Pattern REGEX_FORMULA_WORD = Pattern.compile("[0-9]|[+\\-*/]");
+    public static final Pattern REGEX_FORMULA_WORD = Pattern.compile("[0-9]|[+\\-*/]|[.]");
 
     public static boolean checkWrong(Pattern pattern, String value) {
         return !pattern.matcher(value).find();
