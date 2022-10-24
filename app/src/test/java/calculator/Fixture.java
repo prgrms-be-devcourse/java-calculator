7887035feb3d0a6216a878250ac936be5f37bfce
fortune00 <sinyoungbok99@gmail.com>
2022-10-24 22:40:39 +0900

    test: fixture 이름 수정

--- app/src/test/java/calculator/Fixture.java
+++ app/src/test/java/calculator/Fixture.java
@@ -1,13 +1,13 @@
 package calculator;
 
 public class Fixture {
-    public static String EXPRESSION_div_0 = "2 / 0";
-    public static String EXPRESSION_add_min = "1 + 1 - 5";
-    public static int EXPRESSION_add_min_answer = -3;
-    public static String EXPRESSION_div_mul = "2 * 3 / 4";
-    public static int EXPRESSION_div_mul_answer = 1;
-    public static String EXPRESSION_add_div = "2 + 4 / 4";
-    public static int EXPRESSION_add_div_answer = 3;
-    public static String EXPRESSION_bracket = "2 * (3 + 5)";
-    public static int EXPRESSION_bracket_answer = 16;
+    public static String EXP_OF_DIV_BY_0 = "2 / 0";
+    public static String EXP_OF_ADD_N_MIN = "1 + 1 - 5";
+    public static int ANSWER_OF_ADD_N_MIN = -3;
+    public static String EXP_OF_DIV_N_MUL = "2 * 3 / 4";
+    public static int ANSWER_OF_DIV_N_MUL = 1;
+    public static String EXP_OF_ADD_N_DIV = "2 + 4 / 4";
+    public static int ANSWER_OF_ADD_N_DIV = 3;
+    public static String EXP_OF_BRACKET = "2 * (3 + 5)";
+    public static int ANSWER_OF_BRACKET = 16;
 }
