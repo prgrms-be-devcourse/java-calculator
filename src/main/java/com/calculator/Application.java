6e4335871cba00dde15029e811fd254ac4a0a062
yeezze <yeeeze22@gmail.com>
2022-10-24 16:24:35 +0900

    first commit
    gradle 파일 구조 생성

---
+++ src/main/java/com/calculator/Application.java
@@ -0,0 +1,10 @@
+package com.calculator;
+
+import com.calculator.common.Calculator;
+
+public class Application {
+
+    public static void main(String[] args) {
+        new Calculator().run();
+    }
+}
