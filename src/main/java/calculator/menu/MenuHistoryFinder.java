88299dcaf1e5db56e0a801ff860987aa40eb5f17
hyunseo <goatsfish@gmail.com>
2022-10-24 12:54:05 +0900

    fix : 이력 저장소 의존 변경

--- src/main/java/calculator/menu/MenuHistoryFinder.java
+++ src/main/java/calculator/menu/MenuHistoryFinder.java
@@ -1,11 +1,12 @@
 package calculator.menu;
 
 import calculator.calculator.history.CalculationHistory;
+import calculator.calculator.history.History;
 import calculator.view.output.MenuHistoryOutput;
 
 public class MenuHistoryFinder implements Menu {
 
-    private final CalculationHistory calculationHistory;
+    private final History calculationHistory;
     private final MenuHistoryOutput output;
 
     public MenuHistoryFinder() {
