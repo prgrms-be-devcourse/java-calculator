8a766f577ded4485455d2154d049da48ce8ee1bf
hyunseo <goatsfish@gmail.com>
2022-10-24 10:48:59 +0900

    text : 후위 표기식 연산 테스트

---
+++ src/test/java/calculator/calculator/notation/calculation/NotationPostfixCalculationTest.java
@@ -0,0 +1,97 @@
+package calculator.calculator.notation.calculation;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static calculator.exception.NotationException.NOTATION_POSTFIX_NULL_EXCEPTION;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+class NotationPostfixCalculationTest {
+
+    private final NotationPostfixCalculation notationPostfixCalculation = new NotationPostfixCalculation();
+
+    @ParameterizedTest(name = "[{index}] : formula = {0}, expect = {1}")
+    @MethodSource("whenCalculatePostfixNotationThenSuccessDummy")
+    @DisplayName("후위 표기식 연산 성공 테스트")
+    void whenCalculatePostfixNotationThenSuccessTest(List<String> notation, double expect) {
+        double result = notationPostfixCalculation.calculate(notation).doubleValue();
+        assertThat(result).isEqualTo(expect);
+    }
+
+    @ParameterizedTest(name = "[{index}] : notation = {0}")
+    @MethodSource("whenCalculatePostfixNotationThenExceptionDummy")
+    @DisplayName("후위 표기법 연산 오류 예외처리 테스트")
+    void whenCalculatePostfixNotationThenExceptionTest(List<String> notation) {
+        assertThatExceptionOfType(NullPointerException.class)
+                .isThrownBy(() -> notationPostfixCalculation.calculate(notation))
+                .withMessageMatching(NOTATION_POSTFIX_NULL_EXCEPTION.message);
+    }
+
+    static Stream<Arguments> whenCalculatePostfixNotationThenSuccessDummy() {
+        return Stream.of(
+                Arguments.arguments(List.of("0", "0", "+"), 0),
+                Arguments.arguments(List.of("1", "2", "+"), 3),
+                Arguments.arguments(List.of("1", "2", "-"), -1),
+                Arguments.arguments(List.of("1", "2", "*"), 2),
+                Arguments.arguments(List.of("1", "2", "+", "3", "+", "4", "+", "5", "+"), 15.0),
+                Arguments.arguments(List.of("1", "2", "+", "3", "+", "4", "+", "5", "-"), 5.0),
+                Arguments.arguments(List.of("1", "2", "+", "3", "+", "4", "5", "*", "+"), 26.0),
+                Arguments.arguments(List.of("1", "2", "+", "3", "+", "4", "5", "/", "+"), 6.8),
+                Arguments.arguments(List.of("1", "2", "+", "3", "+", "4", "-", "5", "+"), 7.0),
+                Arguments.arguments(List.of("1", "2", "+", "3", "4", "*", "+", "5", "+"), 20.0),
+                Arguments.arguments(List.of("1", "2", "+", "3", "4", "/", "+", "5", "+"), 8.75),
+                Arguments.arguments(List.of("1", "2", "+", "3", "-", "4", "+", "5", "+"), 9.0),
+                Arguments.arguments(List.of("1", "2", "3", "*", "+", "4", "+", "5", "+"), 16.0),
+                Arguments.arguments(List.of("1", "2", "3", "/", "+", "4", "+", "5", "+"), 10.666666666666668),
+                Arguments.arguments(List.of("1", "2", "-", "3", "+", "4", "+", "5", "+"), 11.0),
+                Arguments.arguments(List.of("1", "2", "*", "3", "+", "4", "+", "5", "+"), 14.0),
+                Arguments.arguments(List.of("1", "2", "/", "3", "+", "4", "+", "5", "+"), 12.5),
+                Arguments.arguments(List.of("1", "2", "+", "3", "+", "4", "-", "5", "-"), -3.0),
+                Arguments.arguments(List.of("1", "2", "+", "3", "+", "4", "5", "*", "-"), -14.0),
+                Arguments.arguments(List.of("1", "2", "+", "3", "+", "4", "5", "/", "-"), 5.2),
+                Arguments.arguments(List.of("1", "2", "+", "3", "4", "*", "+", "5", "-"), 10.0),
+                Arguments.arguments(List.of("1", "2", "+", "3", "4", "*", "+", "5", "/"), 3.0),
+                Arguments.arguments(List.of("1", "2", "+", "3", "4", "*", "+", "5", "*"), 75.0),
+                Arguments.arguments(List.of("1", "2", "+", "3", "4", "/", "+", "5", "-"), -1.25),
+                Arguments.arguments(List.of("1", "2", "+", "3", "4", "/", "+", "5", "*"), 18.75),
+                Arguments.arguments(List.of("1", "2", "+", "3", "4", "/", "+", "5", "/"), 0.75)
+        );
+    }
+
+    static Stream<Arguments> whenCalculatePostfixNotationThenExceptionDummy() {
+        return Stream.of(
+                Arguments.arguments(List.of("0", "+"), 0),
+                Arguments.arguments(List.of("1", "+"), 3),
+                Arguments.arguments(List.of("1", "-"), -1),
+                Arguments.arguments(List.of("1", "*"), 2),
+                Arguments.arguments(List.of("1", "2", "+", "3", "+", "4", "+", "*")),
+                Arguments.arguments(List.of("1", "2", "+", "3", "+", "4", "+", "*")),
+                Arguments.arguments(List.of("1", "2", "+", "3", "+", "4", "*", "*")),
+                Arguments.arguments(List.of("1", "2", "+", "3", "+", "4", "*", "/")),
+                Arguments.arguments(List.of("1", "2", "+", "3", "+", "4", "-", "*")),
+                Arguments.arguments(List.of("1", "2", "+", "3", "4", "*", "+", "*")),
+                Arguments.arguments(List.of("1", "2", "+", "3", "4", "/", "+", "*")),
+                Arguments.arguments(List.of("1", "2", "+", "3", "-", "4", "+", "*")),
+                Arguments.arguments(List.of("1", "2", "3", "*", "+", "4", "+", "*")),
+                Arguments.arguments(List.of("1", "2", "3", "/", "+", "4", "+", "*")),
+                Arguments.arguments(List.of("1", "2", "-", "3", "+", "4", "+", "*")),
+                Arguments.arguments(List.of("1", "2", "*", "3", "+", "4", "+", "*")),
+                Arguments.arguments(List.of("1", "2", "/", "3", "+", "4", "+", "*")),
+                Arguments.arguments(List.of("1", "2", "+", "3", "+", "4", "-", "*")),
+                Arguments.arguments(List.of("1", "2", "+", "3", "+", "4", "*", "*")),
+                Arguments.arguments(List.of("1", "2", "+", "3", "+", "4", "*", "/")),
+                Arguments.arguments(List.of("1", "2", "+", "3", "4", "*", "+", "*")),
+                Arguments.arguments(List.of("1", "2", "+", "3", "4", "*", "+", "*")),
+                Arguments.arguments(List.of("1", "2", "+", "3", "4", "*", "+", "*")),
+                Arguments.arguments(List.of("1", "2", "+", "3", "4", "/", "+", "*")),
+                Arguments.arguments(List.of("1", "2", "+", "3", "4", "/", "+", "*")),
+                Arguments.arguments(List.of("1", "2", "+", "3", "4", "/", "+", "*"))
+        );
+    }
+}
