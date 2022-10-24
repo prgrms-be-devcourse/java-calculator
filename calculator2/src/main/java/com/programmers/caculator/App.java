3455b715f5df7aebf21c5719e989ad73efe593e4
김소현 <thgus345@naver.com>
2022-10-25 02:40:43 +0900

    프로젝트 초기 세팅

---
+++ calculator2/src/main/java/com/programmers/caculator/App.java
@@ -0,0 +1,7 @@
+package com.programmers.caculator;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ calculator2/src/main/java/com/programmers/caculator/engine/io/Output.java
@@ -0,0 +1,6 @@
+package com.programmers.caculator.engine.io;
+
+public interface Output {
+
+    void showHistory();
+}
