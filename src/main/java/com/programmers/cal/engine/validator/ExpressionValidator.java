c214d85009d031f95e5d9980791f918a9816390a
ttaehee <ttaehee1105@gmail.com>
2022-10-25 17:56:01 +0900

    fix : validate 기능 정규식 수정

--- src/main/java/com/programmers/cal/engine/validator/ExpressionValidator.java
+++ src/main/java/com/programmers/cal/engine/validator/ExpressionValidator.java
@@ -3,9 +3,12 @@ package com.programmers.cal.engine.validator;
 import java.util.regex.Pattern;
 
 public class ExpressionValidator implements Validator {
-    private static final Pattern pattern = Pattern.compile("^(-?[0-9]*[+\\-*/])+(-?[0-9]*)");
+
+    private static final Pattern pattern = Pattern.compile("^(-?[0-9]+[+\\-*/])+(-?[0-9]+)");
+    
     @Override
     public boolean isExpression(String inputString) {
-        return pattern.matcher(inputString.trim()).matches();
+        String trimString = inputString.replaceAll("\\p{Z}","");
+        return pattern.matcher(trimString).matches();
     }
 }
