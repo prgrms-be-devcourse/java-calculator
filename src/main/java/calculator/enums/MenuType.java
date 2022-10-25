dc4b6b6ade0de13506d49fa5ee559be2f92cf51c
hwankim123 <hwankim1941@gmail.com>
2022-10-25 23:57:32 +0900

    [CAL-08] fix: 1 입력시 조회, 2 입력시 계산을 실행하도록 버그 수정

--- src/main/java/calculator/enums/MenuType.java
+++ src/main/java/calculator/enums/MenuType.java
@@ -1,8 +1,9 @@
 package calculator.enums;
 
 public enum MenuType {
-  CALCULATE("1"),
-  HISTORY("2"),
+
+  HISTORY("1"),
+  CALCULATE("2"),
   //메뉴에 없는 숫자 입력시 EXIT 상태가 되는데, EXIT의 다른 값을 -1로 설정해놓았습니다.
   //enum에 정의된 EXIT 상태와 실제 종료 조건이 다른 것을 고치고 싶습니다.
   EXIT("-1");
