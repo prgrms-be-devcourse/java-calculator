99285bd6ac3fb749b7838fbe20c5ce0a642b82ee
preferkim <shineecard5@gmail.com>
2022-10-25 15:40:07 +0900

    chore: 불필요한 주석 삭제

--- src/main/java/calculator/engine/controller/Controller.java
+++ src/main/java/calculator/engine/controller/Controller.java
@@ -4,13 +4,14 @@ import calculator.application.io.Input;
 import calculator.application.io.Output;
 import calculator.application.io.enums.SelectOption;
 import calculator.application.model.UserSelection;
+import calculator.engine.calculator.Calculator;
+import calculator.engine.model.CalculationResult;
 import calculator.engine.model.Expression;
 
 public class Controller implements Runnable{
     private final Input input;
     private final Output output;
     private final Calculator calculator;
-//    private final History history;
 
     public Controller(Calculator calculator, Input input, Output output) {
         this.calculator = calculator;
@@ -21,29 +22,12 @@ public class Controller implements Runnable{
     @Override
     public void run() {
         while(true) {
-            // get user selection
             UserSelection selection = input.getUserSelection(SelectOption.getLiterals());
 
-            // if selection is `calculate`, request expression to customer
-            // then calculate expression & record result to history
             if (selection.isEqualTo(SelectOption.CALCULATE)) {
-                Expression expression = input.getExpression();
-//                CalculationResult result = calculator.calculate(expression);
-//                hisotry.record(result);
+                Expression infix = input.getExpression();
             }
 
-            // if selection is `QUERY`, request repository to show history
-            // prompt all calculation records
-//            if (selection.isEqualTo(SelectOption.QUERY)) {
-//
-//            }
-
-            // if selection is `exit`,
-            // prompt exit message end & end loop
-//            if (selection.isEqualTo(SelectOption.EXIT)) {
-//                // output.prompt(exitMessage);
-//                break;
-//            }
         }
     }
 }
