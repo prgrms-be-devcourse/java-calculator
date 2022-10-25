7f2a452729ec45efb85a3f8c97461761f5128e80
권주성 <99165624+JoosungKwon@users.noreply.github.com>
2022-10-26 01:27:28 +0900

    프로젝트를 생성하다

---
+++ app/src/test/java/com/programmers/kwonjoosung/java/calculator/AppTest.java
@@ -0,0 +1,11 @@
+package com.programmers.kwonjoosung.java.calculator;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test void appHasAGreeting() {
+        App classUnderTest = new App();
+        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    }
+}
