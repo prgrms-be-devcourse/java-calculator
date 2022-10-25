eb4f664f3d44da87c7bf5e8efab2c7b970022625
preferkim <shineecard5@gmail.com>
2022-10-25 16:01:37 +0900

    feat: 연산자, 연산자 우선순위 객체 구현

--- src/main/java/calculator/engine/calculator/ArithmeticOperator.java
+++ src/main/java/calculator/engine/calculator/ArithmeticOperator.java
@@ -1,5 +1,16 @@
 package calculator.engine.calculator;
 
 public enum ArithmeticOperator {
+    ADDITION("+", OperatorPriority.SECOND),
+    SUBTRACTION("-", OperatorPriority.SECOND),
+    MULTIPLICATION("*", OperatorPriority.SECOND),
+    DIVISION("/", OperatorPriority.SECOND);
 
+    private final String symbol;
+    private final OperatorPriority priority;
+
+    ArithmeticOperator(String symbol, OperatorPriority priority) {
+        this.symbol = symbol;
+        this.priority = priority;
+    }
 }
