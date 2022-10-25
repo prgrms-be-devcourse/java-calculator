6eedd94be9a109f39aa3e1474a586faabdda5879
cloudwi <cloudwi@naver.com>
2022-10-25 14:17:33 +0900

    Menu.enum을 구현하라

---
+++ src/main/java/org/programmers/java/calculator/model/Menu.java
@@ -0,0 +1,47 @@
+package org.programmers.java.calculator.model;
+
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public enum Menu {
+
+    RECORD("Record",1),
+    CALCULATE("Calculate",2),
+    ERROR("Error",-1);
+
+    private static final int BAD_REQUEST = -1;
+
+    private final String label;
+    private final int code;
+
+    private String getLabel() {
+        return this.label;
+    }
+
+    private int getCode() {
+        return this.code;
+    }
+
+    Menu(String label, int code) {
+        this.label = label;
+        this.code = code;
+    }
+
+    private static final Map<Integer, Menu> BY_CODE =
+            Stream.of(values()).collect(Collectors.toMap(Menu::getCode, Function.identity()));
+
+    public static Menu selectMenu(String input) {
+        int code = toCode(input);
+        return BY_CODE.get(code);
+    }
+
+    private static int toCode(String num) {
+        try {
+            return Integer.parseInt(num);
+        } catch (NumberFormatException e) {
+            return BAD_REQUEST;
+        }
+    }
+}
