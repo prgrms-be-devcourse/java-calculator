56d6b7484989df755f336fd26ebcdb6219583b51
권성준 <tjdwns1865@naver.com>
2022-10-25 19:15:49 +0900

    test: 계산식이 올바른 경우 검증

--- app/src/test/java/com/programmers/java/ValidateTest.java
+++ app/src/test/java/com/programmers/java/ValidateTest.java
@@ -1,6 +1,6 @@
 package com.programmers.java;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,19 +12,31 @@ class ValidateTest {
 	Validator validator = new Validator();
 
 	@Test
-	void formulaInputValidate() {
-		String[] formula = new String[9];
-		formula[0] = "(1*3)?5"; // 연산자, 숫자. 괄호가 아닌 다른 문자가 들어옴
-		formula[1] = "AAAbbb"; // 연산자, 숫자. 괄호가 아닌 다른 문자가 들어옴
-		formula[2] = ""; // 공백
-		formula[3] = "1*3+((1-3)"; // 괄호 짝이 안맞음
-		formula[4] = "*1"; // 첫 문자에 들어올 수 없는 것이 입력됨
-		formula[5] = ")-1"; // 첫 문자에 들어올 수 없는 것이 입력됨
-		formula[6] = "1++34-5"; // 연산자와 숫자의 순서가 잘못됨
-		formula[7] = "1+3+4-"; // 마지막 문자 잘못됨
-		formula[8] = "(1+3+4)3"; // 마지막 문자 잘못됨
+	void validateWrongFormula() {
+		ArrayList<String> formula = new ArrayList<>();
+		formula.add("(1*3)?5"); // 연산자, 숫자. 괄호가 아닌 다른 문자가 들어옴
+		formula.add("AAAbbb"); // 연산자, 숫자. 괄호가 아닌 다른 문자가 들어옴
+		formula.add(""); // 공백
+		formula.add("1*3+((1-3)"); // 괄호 짝이 안맞음
+		formula.add("*1"); // 첫 문자에 들어올 수 없는 것이 입력됨
+		formula.add(")-1"); // 첫 문자에 들어올 수 없는 것이 입력됨
+		formula.add("1++34-5"); // 가운데 부분 순서가 잘못됨
+		formula.add("1+)(3+5)(");
+		formula.add("1+3+4-"); // 마지막 문자 잘못됨
+		formula.add("(1+3+4)3"); // 마지막 문자 잘못됨
 
-		Arrays.stream(formula)
+		formula.stream()
 			.forEach(i -> Assertions.assertThrows(FormulaInputException.class, () -> validator.validateFormula(i)));
+	}
+
+	@Test
+	void validateCorrectFormula() {
+		ArrayList<String> formula = new ArrayList<>();
+		formula.add("1*(2+3)");
+		formula.add("((2+3))*3+3");
+		formula.add("1*(3+4)/2");
+
+		formula.stream()
+			.forEach(i -> Assertions.assertDoesNotThrow(() -> validator.validateFormula(i)));
 	}
 }
