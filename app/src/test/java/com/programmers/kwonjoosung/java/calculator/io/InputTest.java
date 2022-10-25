82cef4f7e1998b6a033fc2e229ec76851736ce1e
권주성 <99165624+JoosungKwon@users.noreply.github.com>
2022-10-26 01:51:47 +0900

    입출력을 구현하다
    * I/O Interface에 구현체 Console 클래스 생성
    * I/O 각각의 테스트 코드
    * (질문) test하기 위해서 인터페이스에 메서드를 추가해도 되는지 궁금합니다.

---
+++ app/src/test/java/com/programmers/kwonjoosung/java/calculator/io/InputTest.java
@@ -0,0 +1,51 @@
+package com.programmers.kwonjoosung.java.calculator.io;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class InputTest {
+    static Input input;
+    public static InputStream generateUserInput(String input) {
+        return new ByteArrayInputStream(input.getBytes());
+    }
+
+    @BeforeAll
+    @DisplayName("입력")
+    static void startInput(){
+        input = new Console();
+    }
+    @Test
+    @DisplayName("메뉴 입력 테스트")
+    void inputMenuTest(){
+        // given
+        InputStream in = generateUserInput("2\n");
+        System.setIn(in);
+        Scanner scanner = new Scanner(System.in);
+        input.setScanner(scanner);
+        int menu;
+        // when
+        menu = input.inputMenu();
+        //then
+        assertEquals(2,menu);
+    }
+    @Test
+    @DisplayName("식 입력 테스트")
+    void inputExpressionTest(){
+        //given
+        InputStream in = generateUserInput("1 + 1\n");
+        System.setIn(in);
+        Scanner scanner = new Scanner(System.in);
+        input.setScanner(scanner);
+        //when
+        String expression = input.inputExpression();
+        //then
+        assertEquals("1 + 1",expression);
+    }
+}
---
+++ app/src/test/java/com/programmers/kwonjoosung/java/calculator/io/OutputTest.java
@@ -0,0 +1,89 @@
+package com.programmers.kwonjoosung.java.calculator.io;
+
+import org.junit.jupiter.api.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OutputTest {
+    static Output output;
+    @BeforeAll
+    @DisplayName("입력")
+    static void startInput(){
+        output = new Console();
+    }
+    static ByteArrayOutputStream outputStream ;
+    final PrintStream standardOut = System.out;
+
+    @BeforeEach
+    void setUp() {
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
+    @AfterEach
+    void tearDown() {
+        System.setOut(standardOut);
+    }
+    @Test
+    @DisplayName("출력 테스트")
+    void printlnTest(){
+        // given
+        String result = "출력";
+        // when
+        output.println(result);
+        // then
+        assertEquals(result, outputStream.toString().trim());
+    }
+    @Test
+    @DisplayName("메뉴 출력 테스트")
+    void printMenuTest(){
+        // given
+        String result = "1. 조회\r\n2. 계산";
+        // when
+        output.printMenu();
+        // then
+        assertEquals(result, outputStream.toString().trim());
+    }
+    @Test
+    @DisplayName("종료 출력 테스트")
+    void printExitTest(){
+        // given
+        String result = "프로그램을 종료합니다.";
+        //when
+        output.printExit();
+        // then
+        assertEquals(result, outputStream.toString().trim());
+    }
+    @Test
+    @DisplayName("에러 출력 테스트")
+    void printError(){
+        // given
+        String result = "오류 발생!\r\n오류";
+        // when
+        output.printError("오류");
+        // then
+        assertEquals(result, outputStream.toString().trim());
+    }
+    @Test
+    @DisplayName("메뉴 에러 출력 테스트")
+    void printMenuError(){
+        // given
+        String result = "1 ~ 2번 메뉴 중 하나를 선택해주세요(종료는 99)";
+        // when
+        output.printMenuError();
+        // then
+        assertEquals(result, outputStream.toString().trim());
+    }
+    @Test
+    @DisplayName("조회 값 없음 출력 테스트")
+    void printNullError(){
+        // given
+        String result = "조회할 데이터가 없습니다.";
+        // when
+        output.printNullError();
+        // then
+        assertEquals(result, outputStream.toString().trim());
+    }
+}
