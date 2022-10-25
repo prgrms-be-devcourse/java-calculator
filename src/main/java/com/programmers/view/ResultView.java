11e7e9022103d9f596443c4d3b9c6cb8c5549fbb
kivv00ng <rldnd2637@naver.com>
2022-10-25 22:09:54 +0900

    resultView 수정

--- src/main/java/com/programmers/view/ResultView.java
+++ src/main/java/com/programmers/view/ResultView.java
@@ -4,18 +4,19 @@ import java.util.Map;
 
 public class ResultView {
     private static final String MIDDLEMAN = " = ";
-    private static StringBuilder stringBuilder = new StringBuilder();
-
+    
     public static void calculateResult(int result) {
         System.out.println(result + "\n");
     }
 
     public static void inquire(Map<String, Integer> results) {
+        StringBuilder stringBuilder = new StringBuilder();
         for (String input : results.keySet()) {
             stringBuilder.append(input + MIDDLEMAN + results.get(input) + "\n");
         }
 
         System.out.println(stringBuilder.toString());
+
     }
 
 }
