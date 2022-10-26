6a2a300752be77eade364046a00c20b57babdef1
preferkim <shineecard5@gmail.com>
2022-10-26 18:49:20 +0900

    chore: 주석 삭제

--- src/main/java/calculator/application/io/Parser.java
+++ src/main/java/calculator/application/io/Parser.java
@@ -8,6 +8,6 @@ import java.util.List;
 public class Parser {
     public static List<String> toList(String literal) {
         String[] characters = literal.split(Characters.BLANK.toString());
-        return Arrays.asList(characters); // TODO: 띄어쓰기 유무, 개수에 상관없이 split 할 수 있게 수정
+        return Arrays.asList(characters);
     }
 }
