c0c4506218e4c146d1b9ca92c60c2307348f9574
Kim Changgyu <kimc980106@naver.com>
2022-10-27 00:03:48 +0900

    feat: 예외처리 클래스 구현
    1. MenuException.java : Menu 관련 Exception
    - 주어진 입력(1, 2, 3) 외 다른 입력
    2. OperatorException.java : Operator 관련 Exception
    - 주어진 입력(+, -, *, /) 외 다른 입력
    - 나누는 수가 0인 입력

---
+++ src/main/java/com/programmers/java/engine/exception/MenuException.java
@@ -0,0 +1,7 @@
+package com.programmers.java.engine.exception;
+
+public class MenuException extends RuntimeException {
+    public MenuException(String message) {
+        super(message);
+    }
+}
