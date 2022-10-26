69cca60df13b7e9ea59fdf8c7e91a0c8d68e6380
bufflect <bndgc@naver.com>
2022-10-26 20:25:53 +0900

    FEAT : console 실행기 구현

--- app/src/main/java/com/programmers/java/App.java
+++ app/src/main/java/com/programmers/java/App.java
@@ -1,7 +1,26 @@
 package com.programmers.java;
 
+import com.programmers.java.calculator.PostfixCalculator;
+import com.programmers.java.controller.Console;
+import com.programmers.java.repository.InMemoryMapRepository;
+import com.programmers.java.validator.FormValidator;
+import com.programmers.java.validator.InputValidator;
+import com.programmers.java.validator.MathExpressionValidator;
+import com.programmers.java.validator.ParenthesesValidator;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        List<InputValidator> validatorList = new ArrayList<>();
+        validatorList.add(new FormValidator());
+        validatorList.add(new MathExpressionValidator());
+        validatorList.add(new ParenthesesValidator());
+
+        new Console(
+                validatorList,
+                new PostfixCalculator(),
+                new InMemoryMapRepository()).run();
     }
 }
