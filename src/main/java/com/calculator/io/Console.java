6d2edd81fcbbacb58b2b899da2501b7c2991cc86
yeezze <yeeeze22@gmail.com>
2022-10-27 01:49:53 +0900

    refactor: 콘솔 공백 라인 정리

--- src/main/java/com/calculator/io/Console.java
+++ src/main/java/com/calculator/io/Console.java
@@ -6,20 +6,22 @@ public class Console implements Input, Output{
 
     @Override
     public int inputType() throws IOException {
-        System.out.println("1. 조회\n 2. 계산\n 3. 끝");
-        System.out.println("\n 선택 : ");
-        return Integer.parseInt(br.readLine());
+        System.out.println("\n1. 조회\n2. 계산\n3. 끝");
+        System.out.print("\n선택 : ");
+
+        String input = br.readLine();
+        System.out.println();
+        return Integer.parseInt(input);
     }
 
     @Override
     public String inputNum() throws IOException {
         String input = br.readLine();
-        System.out.println(input);
         return input;
     }
 
     @Override
     public void output(double result) {
-        System.out.println(result);
+        System.out.println(String.valueOf(result).replaceAll(".0$", ""));
     }
 }
