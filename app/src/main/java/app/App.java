99f17ffd70e0fc08f574c03c001976001b559e2b
Kong <rhdtn311@naver.com>
2022-10-26 16:25:23 +0900

    chore : 앱 실행 시 필요한 의존관계 설정

--- app/src/main/java/app/App.java
+++ app/src/main/java/app/App.java
@@ -1,8 +1,14 @@
 package app;
 
+import app.calculator.BasicPostfixMaker;
+import app.calculator.Calculator;
+import app.io.Console;
+import app.storage.MapStorage;
+import app.validator.InputValidator;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("init");
+        new Calculator(new Console(), new Console(), new MapStorage(), new BasicPostfixMaker(),new InputValidator()).run();
     }
 }
