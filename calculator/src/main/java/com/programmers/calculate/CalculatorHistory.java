dcd69df627b218cd4a152de379c9deb0b86ef31b
김소현 <thgus345@naver.com>
2022-10-27 17:52:51 +0900

    Map 변수명 수정 및 findAll() 메소드 객체 전달 기능으로 변경

--- calculator/src/main/java/com/programmers/calculate/CalculatorHistory.java
+++ calculator/src/main/java/com/programmers/calculate/CalculatorHistory.java
@@ -1,21 +1,22 @@
 package com.programmers.calculate;
 
+import com.programmers.calculate.engine.io.Output;
 import com.programmers.calculate.engine.model.History;
 
 import java.util.*;
 
 public class CalculatorHistory implements History {
-    private final Map<Long, String> historyMap = new HashMap<>();
+    private final Map<Long, String> history = new HashMap<>();
     private Long key = 0L;
+    private Console console;
 
     @Override
     public void save(String expression) {
-        historyMap.put(++key, expression);
+        history.put(++key, expression);
     }
 
     @Override
-    public void findAll() {
-        Collection<String> history = new Collection<>(new ArrayList<>(historyMap.values()));
-        history.forEach(System.out::println);
+    public void findAll(Output output) {
+        output.printMapValues(history);
     }
 }
