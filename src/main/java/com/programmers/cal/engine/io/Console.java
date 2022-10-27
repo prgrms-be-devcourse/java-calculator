3e4b16ee00828d8b68f9e8e73fa22684d8f690c1
ttaehee <ttaehee1105@gmail.com>
2022-10-27 23:40:32 +0900

    refactor : 출력메시지 Enum으로 관리

--- src/main/java/com/programmers/cal/engine/io/Console.java
+++ src/main/java/com/programmers/cal/engine/io/Console.java
@@ -4,13 +4,6 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Console implements Input, Output {
-
-    private static final String MENU_MESSAGE = "\n번호를 입력하세요\n1. 조회\n2. 계산\n3. 종료\n선택 : ";
-    private static final String WRONG_ORDER_MESSAGE = "잘못된 입력입니다.";
-    private static final String EXIT_MESSAGE = "종료되었습니다.";
-    private static final String ZERO_MESSAGE = "0으로 나눌 수 없습니다.";
-    private static final String RECORD_MESSAGE = "데이터가 없습니다.";
-
     private static final Scanner scanner = new Scanner(System.in);
 
     @Override
@@ -20,18 +13,17 @@ public class Console implements Input, Output {
 
     @Override
     public void requestInput() {
-        System.out.println(MENU_MESSAGE);
+        System.out.println(Message.MENU_MESSAGE);
     }
 
     @Override
     public void printWrongOrder() {
-        System.out.println(WRONG_ORDER_MESSAGE);
+        System.out.println(Message.WRONG_ORDER_MESSAGE);
     }
 
     @Override
     public void printRecord(List<String> recordList) {
-        recordList.stream()
-                .forEach(System.out::println);
+        recordList.forEach(System.out::println);
     }
 
     @Override
@@ -41,16 +33,16 @@ public class Console implements Input, Output {
 
     @Override
     public void printExit() {
-        System.out.println(EXIT_MESSAGE);
+        System.out.println(Message.EXIT_MESSAGE);
     }
 
     @Override
     public void printZeroDivision() {
-        System.out.println(ZERO_MESSAGE);
+        System.out.println(Message.ZERO_MESSAGE);
     }
 
     @Override
     public void printNoRecord() {
-        System.out.println(RECORD_MESSAGE);
+        System.out.println(Message.RECORD_MESSAGE);
     }
 }
---
+++ src/main/java/com/programmers/cal/engine/io/Message.java
@@ -0,0 +1,24 @@
+package com.programmers.cal.engine.io;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+public enum Message {
+    MENU_MESSAGE("\n번호를 입력하세요\n1. 조회\n2. 계산\n3. 종료\n선택 : "),
+    WRONG_ORDER_MESSAGE("잘못된 입력입니다."),
+    EXIT_MESSAGE("종료되었습니다."),
+    ZERO_MESSAGE("0으로 나눌 수 없습니다."),
+    RECORD_MESSAGE("데이터가 없습니다.");
+
+    private final String message;
+
+    Message(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}
