7d2507f5624872fdae2fdb2d3145184fd95a26d6
goseungwon <knmy0925@gmail.com>
2022-10-27 19:50:19 +0900

    [Feat] 계산 결과 저장 후 보여주기

---
+++ src/main/java/com/programmers/calculator/engine/storage/HashMapStorage.java
@@ -0,0 +1,27 @@
+package com.programmers.calculator.engine.storage;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapStorage implements Storage{
+    private final Map<Integer, String> calculationResult = new HashMap<>();
+    private int mapIndex = 1;
+
+    @Override
+    public void save(String result) {
+        calculationResult.put(mapIndex++, result);
+    }
+
+    @Override
+    public void findAll() {
+        System.out.println("################## 계산 내역 #################");
+        if (mapIndex == 1) {
+            System.out.println("계산 내역이 없습니다.");
+            return ;
+        }
+        calculationResult
+                .forEach((k, v) -> System.out.println(k + ". " + v));
+        System.out.println("###########################################");
+
+    }
+}
---
+++ src/main/java/com/programmers/calculator/engine/storage/Storage.java
@@ -0,0 +1,6 @@
+package com.programmers.calculator.engine.storage;
+
+public interface Storage {
+    void save(String s);
+    void findAll();
+}
