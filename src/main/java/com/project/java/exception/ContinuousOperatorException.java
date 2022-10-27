b505e8031530e798bdabeb86486827988fec73db
ybkim-dev <beanybigggy@gmail.com>
2022-10-27 19:51:27 +0900

    refactor : 에러 출력 기능 삭제

--- src/main/java/com/project/java/exception/ContinuousOperatorException.java
+++ src/main/java/com/project/java/exception/ContinuousOperatorException.java
@@ -1,8 +1,11 @@
 package com.project.java.exception;
 
-public class ContinuousOperatorException extends Exception{
-    public ContinuousOperatorException() {}
-    public ContinuousOperatorException(String message) {
-        System.out.println(message);
+public class ContinuousOperatorException extends Exception {
+    public ContinuousOperatorException() {
     }
+
+    public ContinuousOperatorException(String message) {
+        super(message);
+    }
+
 }
