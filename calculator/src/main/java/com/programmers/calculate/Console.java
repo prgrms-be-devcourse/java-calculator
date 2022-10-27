009caf179af7d71e72e32739cfc2dc23f80e9291
김소현 <thgus345@naver.com>
2022-10-27 17:55:59 +0900

    입력값 타입 변경 및 계산 이력 출력 메소드 추가

--- calculator/src/main/java/com/programmers/calculate/Console.java
+++ calculator/src/main/java/com/programmers/calculate/Console.java
@@ -3,16 +3,19 @@ package com.programmers.calculate;
 import com.programmers.calculate.engine.io.Input;
 import com.programmers.calculate.engine.io.Output;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Console implements Input, Output {
     private final Scanner scanner = new Scanner(System.in);
 
     @Override
-    public int selectNumber() {
+    public String selectNumber() {
         System.out.println("1. 조회\n2. 계산\n3. 종료");
         System.out.print("선택: ");
-        return Integer.parseInt(scanner.nextLine());
+        return scanner.nextLine();
     }
 
     @Override
@@ -24,5 +27,11 @@ public class Console implements Input, Output {
     @Override
     public void valueError() {
         System.out.println("잘못된 입력 값입니다.");
+    }
+
+    @Override
+    public void printMapValues(Map<Long, String> map) {
+        List<String> list = new ArrayList<>(map.values());
+        list.forEach(System.out::println);
     }
 }
