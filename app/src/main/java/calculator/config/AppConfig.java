714a142b43cbe043cdf3d4bf77572287f16970c2
fortune00 <sinyoungbok99@gmail.com>
2022-10-28 16:36:38 +0900

    refactor: 서비스 인터페이스 삭제
    - 구현체 이름을 CalculatorService로 변경

--- app/src/main/java/calculator/config/AppConfig.java
+++ app/src/main/java/calculator/config/AppConfig.java
@@ -3,13 +3,12 @@ package calculator.config;
 import calculator.domain.BaseCalculator;
 import calculator.domain.Calculator;
 import calculator.repository.MapCalculatorRepository;
-import calculator.service.BaseCalculatorService;
 import calculator.service.CalculatorService;
 
 public class AppConfig {
 
     private static final Calculator calculator = new BaseCalculator(new MapCalculatorRepository());
-    private static final CalculatorService calculatorService = new BaseCalculatorService(calculator);
+    private static final CalculatorService calculatorService = new CalculatorService(calculator);
 
     public CalculatorService calculatorService() {
         return calculatorService;
