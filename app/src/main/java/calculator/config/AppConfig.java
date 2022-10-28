08773fcc79996033970368f559654e3edc2277e1
fortune00 <sinyoungbok99@gmail.com>
2022-10-28 16:32:49 +0900

    refactor: 사칙연산을 Enum으로 구현
    - Calculation을 Operator로 파일 이름 변경
    - Calculator에서는 getOperator 메소드로 Enum에 접근하여 연산을 진행

--- app/src/main/java/calculator/config/AppConfig.java
+++ app/src/main/java/calculator/config/AppConfig.java
@@ -1,6 +1,5 @@
 package calculator.config;
 
-import calculator.domain.BaseCalculation;
 import calculator.domain.BaseCalculator;
 import calculator.domain.Calculator;
 import calculator.repository.MapCalculatorRepository;
@@ -9,7 +8,7 @@ import calculator.service.CalculatorService;
 
 public class AppConfig {
 
-    private static final Calculator calculator = new BaseCalculator(new BaseCalculation(), new MapCalculatorRepository());
+    private static final Calculator calculator = new BaseCalculator(new MapCalculatorRepository());
     private static final CalculatorService calculatorService = new BaseCalculatorService(calculator);
 
     public CalculatorService calculatorService() {
--- app/src/test/java/calculator/domain/BaseCalculatorTest.java
+++ app/src/test/java/calculator/domain/BaseCalculatorTest.java
@@ -2,61 +2,46 @@ package calculator.domain;
 
 import calculator.repository.CalculatorRepository;
 import calculator.repository.MapCalculatorRepository;
+
 import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 import static calculator.fixture.Fixture.*;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class BaseCalculatorTest {
-
-    Calculation calculation = mock(BaseCalculation.class);
     CalculatorRepository calculatorRepository = mock(MapCalculatorRepository.class);
-    Calculator calculator = new BaseCalculator(calculation, calculatorRepository);
+    Calculator calculator = new BaseCalculator(calculatorRepository);
 
     @Test
-    void calculate_더하기랑빼기(){
-        when(calculation.addition(1, 1)).thenReturn(2);
-        when(calculation.subtraction(2, 5)).thenReturn(-3);
-
+    void calculate_더하기랑빼기() {
         int answer = calculator.calculate(EXP_OF_ADD_N_MIN);
         assertThat(answer).isEqualTo(ANSWER_OF_ADD_N_MIN);
     }
 
     @Test
-    void calculate_0으로나누는경우(){
-        when(calculation.division(2, 0)).thenThrow(ArithmeticException.class);
-
+    void calculate_0으로나누는경우() {
         Assertions.assertThrows(ArithmeticException.class, () -> {
             calculator.calculate(EXP_OF_DIV_BY_0);
         });
     }
 
     @Test
-    void calculate_곱셈과나눗셈(){
-        when(calculation.multiplication(2, 3)).thenReturn(6);
-        when(calculation.division(6, 4)).thenReturn(1);
-
+    void calculate_곱셈과나눗셈() {
         int answer = calculator.calculate(EXP_OF_DIV_N_MUL);
         assertThat(answer).isEqualTo(ANSWER_OF_DIV_N_MUL);
     }
 
     @Test
-    void calculate_더하기와나눗셈(){
-        when(calculation.addition(2, 1)).thenReturn(3);
-        when(calculation.division(4, 4)).thenReturn(1);
-
+    void calculate_더하기와나눗셈() {
         int answer = calculator.calculate(EXP_OF_ADD_N_DIV);
         assertThat(answer).isEqualTo(ANSWER_OF_ADD_N_DIV);
     }
 
     @Test
-    void calculate_괄호(){
-        when(calculation.addition(3, 5)).thenReturn(8);
-        when(calculation.multiplication(2, 8)).thenReturn(16);
-
+    void calculate_괄호() {
         int answer = calculator.calculate(EXP_OF_BRACKET);
         assertThat(answer).isEqualTo(ANSWER_OF_BRACKET);
     }
