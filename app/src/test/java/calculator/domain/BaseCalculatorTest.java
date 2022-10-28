b49273e69b3afbe4a3fd7c9d6ce44bbe42abd40f
fortune00 <sinyoungbok99@gmail.com>
2022-10-28 18:38:49 +0900

    refactor: fixture를 테스트 코드 내부로 이동

--- app/src/test/java/calculator/domain/BaseCalculatorTest.java
+++ app/src/test/java/calculator/domain/BaseCalculatorTest.java
@@ -8,7 +8,6 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
-import static calculator.fixture.Fixture.*;
 import static org.mockito.Mockito.mock;
 
 public class BaseCalculatorTest {
@@ -17,12 +16,16 @@ public class BaseCalculatorTest {
 
     @Test
     void calculate_더하기랑빼기() {
+        String EXP_OF_ADD_N_MIN = "1 + 1 - 5";
+        String ANSWER_OF_ADD_N_MIN = "-3";
+
         String answer = calculator.calculate(EXP_OF_ADD_N_MIN);
         assertThat(answer).isEqualTo(ANSWER_OF_ADD_N_MIN);
     }
 
     @Test
     void calculate_0으로나누는경우() {
+        String EXP_OF_DIV_BY_0 = "2 / 0";
         Assertions.assertThrows(ArithmeticException.class, () -> {
             calculator.calculate(EXP_OF_DIV_BY_0);
         });
@@ -30,18 +33,27 @@ public class BaseCalculatorTest {
 
     @Test
     void calculate_곱셈과나눗셈() {
+        String EXP_OF_DIV_N_MUL = "2 * 3 / 4";
+        String ANSWER_OF_DIV_N_MUL = "1";
+
         String answer = calculator.calculate(EXP_OF_DIV_N_MUL);
         assertThat(answer).isEqualTo(ANSWER_OF_DIV_N_MUL);
     }
 
     @Test
     void calculate_더하기와나눗셈() {
+        String EXP_OF_ADD_N_DIV = "2 + 4 / 4";
+        String ANSWER_OF_ADD_N_DIV = "3";
+
         String answer = calculator.calculate(EXP_OF_ADD_N_DIV);
         assertThat(answer).isEqualTo(ANSWER_OF_ADD_N_DIV);
     }
 
     @Test
     void calculate_괄호() {
+        String EXP_OF_BRACKET = "2 * (3 + 5)";
+        String ANSWER_OF_BRACKET = "16";
+
         String answer = calculator.calculate(EXP_OF_BRACKET);
         assertThat(answer).isEqualTo(ANSWER_OF_BRACKET);
     }
