9a4dbab7fa85a3268d3440d6c95901647d316ea1
ysk <kim206gh@naver.com>
2022-10-29 18:57:02 +0900

    fix : ConsoleView.validateSeletected 예외 처리 추가
    Integer.parseInt시 발생하는 NumberFormatException을 IllegalArgumentException 으로 전환하여 메시지와 함께 던져 run() 메소드에서 에러메시지 출력하도록 수정

--- src/main/java/com/programmers/calculator/view/ConsoleView.java
+++ src/main/java/com/programmers/calculator/view/ConsoleView.java
@@ -6,7 +6,7 @@ import com.programmers.calculator.controller.io.ConsoleResponse;
 import java.util.Map;
 import java.util.Scanner;
 
-public class ConsoleView  {
+public class ConsoleView {
 
     private final Scanner scanner;
 
@@ -15,16 +15,16 @@ public class ConsoleView  {
     public ConsoleView(ConsoleController controller, Scanner console) {
         this.scanner = console;
         mapping = Map.of(
-                Command.INQUIRY,
-                () -> write(controller.inquiry()),
-                Command.CALCULATION,
-                () -> {
-                    ConsoleRequest request = read();
-                    write(controller.calculate(request));
-                },
-                Command.EXIT, () -> {
-                    throw new RuntimeException("프로그램을 종료합니다.");
-                }
+            Command.INQUIRY,
+            () -> write(controller.inquiry()),
+            Command.CALCULATION,
+            () -> {
+                ConsoleRequest request = read();
+                write(controller.calculate(request));
+            },
+            Command.EXIT, () -> {
+                throw new RuntimeException("프로그램을 종료합니다.");
+            }
         );
     }
 
@@ -34,7 +34,7 @@ public class ConsoleView  {
         System.out.println("2. 계산");
 
         String selectedInput = read().getExpression();
-        validateSelected(selectedInput);
+        validateSelectedText(selectedInput);
 
         System.out.println("선택 : " + selectedInput);
 
@@ -53,12 +53,16 @@ public class ConsoleView  {
         System.out.println();
     }
 
-    private void validateSelected(String select) {
-        int selectNumber = Integer.parseInt(select);
-
-        if (!Command.canCreate(selectNumber)) {
-            throw new IllegalArgumentException("입력값이 잘못되었습니다.");
+    private void validateSelectedText(String select) {
+        try {
+            int selectNumber = Integer.parseInt(select);
+            if (!Command.canCreate(selectNumber)) {
+                throw new IllegalArgumentException("입력값 범위가 잘못되었습니다.");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("입력값이 잘못되었습니다. 숫자여야 합니다.", e);
         }
+
     }
 
 }
