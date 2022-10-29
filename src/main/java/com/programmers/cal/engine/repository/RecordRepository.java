14930dd5f35c507489811c22683b50ebc8896784
ttaehee <ttaehee1105@gmail.com>
2022-10-29 22:50:21 +0900

    refactor : 저장기능 가독성위해 코드 수정

--- src/main/java/com/programmers/cal/engine/repository/RecordRepository.java
+++ src/main/java/com/programmers/cal/engine/repository/RecordRepository.java
@@ -1,34 +1,27 @@
 package com.programmers.cal.engine.repository;
 
-import com.programmers.cal.engine.model.Answer;
 import com.programmers.cal.engine.model.Equation;
 import com.programmers.cal.engine.model.InputData;
 import com.programmers.cal.engine.model.Record;
 
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class RecordRepository implements Repository {
 
-    private static final Map<InputData, Answer> map = new LinkedHashMap<>();
+    private static final Map<InputData, Equation> map = new LinkedHashMap<>();
 
     @Override
     public void save(Equation equation) {
-        map.put(equation.getInputData(), equation.getAnswer());
+        map.put(equation.getInputData(), equation);
     }
 
     @Override
     public Record findAll() {
-        List<Equation> recordList = new ArrayList<>();
-
-        map.forEach((key, value) -> {
-            recordList.add(Equation.builder()
-                    .inputData(key)
-                    .answer(value)
-                    .build());
-        });
+        List<Equation> recordList = map.values().stream()
+                .collect(Collectors.toList());
 
         return Record.builder()
                 .equations(recordList)
