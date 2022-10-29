72018898b95457e1e6608c4f889aa2fa4a9df222
fortune00 <sinyoungbok99@gmail.com>
2022-10-29 17:24:04 +0900

    feat: Repo에 연산식을 통해 연산 결과를 찾아내는 함수 추가 및 서비스 코드에 반영
    - 항상 연산 하지 않고, 이전에 기록된 해당 연산식과 결과가 레포지토리에 있다면 해당 연산 결과를 반환함
    - ListRepo는 삭제함

--- app/src/main/java/calculator/repository/CalculatorRepository.java
+++ app/src/main/java/calculator/repository/CalculatorRepository.java
@@ -3,8 +3,10 @@ package calculator.repository;
 import calculator.domain.CalculationResult;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CalculatorRepository {
     void save(CalculationResult result);
     List<String> getAll();
+    Optional<String> getResultFromExpression(String expression);
 }
