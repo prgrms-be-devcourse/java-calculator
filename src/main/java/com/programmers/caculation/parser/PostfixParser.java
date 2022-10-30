6ebb4665fdf91c9337f9d520070bed9d38a3b808
박세준 <tpwns6693@gmail.com>
2022-10-30 13:22:16 +0900

    refactor : PostfixParser의 depth 조절 내부 로직 추상화를 통한 depth 조절 -함수 명이 너무 길어지는것 같은데 괜찮나요?

--- src/main/java/com/programmers/caculation/parser/PostfixParser.java
+++ src/main/java/com/programmers/caculation/parser/PostfixParser.java
@@ -12,29 +12,42 @@ public class PostfixParser implements Parser {
         Stack<Character> operatorStack = new Stack<>();
         List<NumberAndOperator> parseResult = new ArrayList<>();
         for (NumberAndOperator o : numOrOp) {
-            if (o.isOperator()) {
-                Character myOperator = o.getOperator();
-                try {
-                    Character stackTop = operatorStack.peek();
-                    int myPriority = Operator.getPriority(myOperator);
-                    int stackTopPriority = Operator.getPriority(stackTop);
-                    if (myPriority < stackTopPriority) {
-                        operatorStack.push(myOperator);
-                    } else {
-                        parseResult.add(new NumberAndOperator(operatorStack.pop()));
-                        operatorStack.push(myOperator);
-                    }
-                } catch (EmptyStackException e) {
-                    operatorStack.push(myOperator);
-                }
-
-            } else if (o.isNumber()) {
-                parseResult.add(o);
-            }
+            addMemberToResultConsiderPriority(operatorStack, parseResult, o);
         }
+        addOperatorToResultThatRemainsInOperatorStack(operatorStack, parseResult);
+        return new ParseResponseDto(parseResult);
+    }
+
+    private void addMemberToResultConsiderPriority(Stack<Character> operatorStack, List<NumberAndOperator> parseResult, NumberAndOperator o) {
+        if (o.isOperator()) {
+            addOperatorToResultConsiderPriorityUsingStack(operatorStack, parseResult, o.getOperator());
+
+        } else if (o.isNumber()) {
+            addNumberToResult(parseResult, o.getNumber());
+        }
+    }
+
+    private void addOperatorToResultConsiderPriorityUsingStack(Stack<Character> operatorStack, List<NumberAndOperator> parseResult, Character myOperator) {
+        try {
+            Character stackTop = operatorStack.peek();
+            int myPriority = Operator.getPriority(myOperator);
+            int stackTopPriority = Operator.getPriority(stackTop);
+            if (myPriority < stackTopPriority) {
+                operatorStack.push(myOperator);
+            } else {
+                parseResult.add(new NumberAndOperator(operatorStack.pop()));
+                operatorStack.push(myOperator);
+            }
+        } catch (EmptyStackException e) {
+            operatorStack.push(myOperator);
+        }
+    }
+    private void addNumberToResult(List<NumberAndOperator> parseResult, Double myNumber){
+        parseResult.add(new NumberAndOperator(myNumber));
+    }
+    private void addOperatorToResultThatRemainsInOperatorStack(Stack<Character> operatorStack, List<NumberAndOperator> parseResult) {
         while (operatorStack.size() != 0) {
             parseResult.add(new NumberAndOperator(operatorStack.pop()));
         }
-        return new ParseResponseDto(parseResult);
     }
 }
