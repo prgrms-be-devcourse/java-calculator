0db14c31bcb5d89957ad815e732d5bc01fcac9a4
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2022-10-31 09:58:58 +0900

    ModeExcuter 클래스명을 CommandPasade 클래스명으로 변경하다.

--- app/src/main/java/com/programmers/calculator/CommandPasade.java
+++ app/src/main/java/com/programmers/calculator/CommandPasade.java
@@ -5,18 +5,18 @@ import com.programmers.calculator.io.Console;
 import com.programmers.calculator.processor.Calculator;
 import com.programmers.calculator.storage.OperationStorage;
 
-public class ModeExecuter {
-    public void excute(OperationStorage operationStorage, Calculator calculator, Console console, int mode) {
-        switch (mode) {
-            case 1:
+public class CommandPasade {
+    public void excute(OperationStorage operationStorage, Calculator calculator, Console console, int command) {
+        switch (command) {
+            case 1: // 주어진 연산 수행
                 Operation operation = calculator.excute(console.inputStr());
                 operationStorage.save(operation);
                 System.out.println(operation);
-            case 2:
+            case 2: // 이전 모든 연산 결과 출력
                 if (operationStorage.isEmpty()){
                     console.printNoOperation();
                 } else {
-                    console.printHistory(operationStorage);
+                    console.printLastAllOperation(operationStorage);
                 }
         }
     }
