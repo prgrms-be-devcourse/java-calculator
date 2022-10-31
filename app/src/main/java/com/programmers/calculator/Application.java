53d51c5a22bd867522576a8f6d0f9029e38791b9
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2022-10-31 09:58:09 +0900

    Main 클래스를 삭제하고 Application 클래스에 이를 구현하다.

--- app/src/main/java/com/programmers/calculator/Application.java
+++ app/src/main/java/com/programmers/calculator/Application.java
@@ -2,10 +2,45 @@ package com.programmers.calculator;
 
 import com.programmers.calculator.io.Console;
 import com.programmers.calculator.processor.Calculator;
+import com.programmers.calculator.processor.Validator;
 import com.programmers.calculator.storage.OperationMemoryStorage;
+import com.programmers.calculator.storage.OperationStorage;
+
+import java.util.regex.Pattern;
 
 public class Application {
+    private final OperationStorage operationStorage;
+    private final Console console;
+    private final Calculator calculator;
+    private final CommandPasade commandPasade;
+
+    public Application() {
+        this.operationStorage = new OperationMemoryStorage();
+        this.console = new Console();
+        this.calculator = new Calculator();
+        this.commandPasade = new CommandPasade();
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                int mode = inputMode();
+                commandPasade.excute(operationStorage, calculator, console, mode);
+            } catch(Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public int inputMode() {
+        String inputModeStr = console.inputStr();
+        if (!Pattern.matches(Validator.NUMBER_REGEX, inputModeStr))
+            throw new IllegalArgumentException("잘못된 기능을 입력하셨습니다.");
+
+        return Integer.parseInt(inputModeStr);
+    }
+
     public static void main(String[] args) {
-        new Main(new OperationMemoryStorage(), new Console(), new Calculator(), new ModeExecuter()).run();
+        new Application().run();
     }
 }
