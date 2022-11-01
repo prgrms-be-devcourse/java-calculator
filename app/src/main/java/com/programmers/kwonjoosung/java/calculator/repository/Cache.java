a2889a9373b5d71b07af988cc7c4a97323e64890
권주성 <99165624+JoosungKwon@users.noreply.github.com>
2022-11-02 01:57:41 +0900

    캐시 기능을 제거하다

--- app/src/main/java/com/programmers/kwonjoosung/java/calculator/repository/Cache.java
+++
@@ -1,9 +0,0 @@
-package com.programmers.kwonjoosung.java.calculator.repository;
-
-import java.util.Optional;
-
-public interface Cache {
-    void add(String[] expression, String result);
-
-    Optional<String> getResult(String[] expression);
-}
