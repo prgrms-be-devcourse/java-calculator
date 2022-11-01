19c9f62fe167e1356f6e28a050e3586986591ce9
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2022-11-02 02:48:10 +0900

    chore : 주석 제거

--- app/src/main/java/app/calculator/Answer.java
+++ app/src/main/java/app/calculator/Answer.java
@@ -1,9 +1,5 @@
 package app.calculator;
 
-/* 계산 결과를 반환할 타입을 정의한 클래스입니다.
-올바른 값을 반환한다면 correctAnswer에, 올바르지 않은 값을 반환한다면 abnormalAnswer에
-값을 가지는 클래스를 생성합니다.
- */
 public class Answer {
 
     private final Integer correctAnswer;
