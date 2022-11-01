c3d95119d0c5bec156e7487e85a3c529af536fd8
Tina <starandpoem143@gmail.com>
2022-11-02 02:30:43 +0900

    [refact] 접근제어자 private으로 변경

--- app/src/main/java/com/programmers/devcourse/calculator/Operator.java
+++ app/src/main/java/com/programmers/devcourse/calculator/Operator.java
@@ -9,9 +9,9 @@ public enum Operator {
     MUL("*", (n1, n2) -> n1 * n2,2),
     DIV("/", (n1, n2) -> n1 / n2,2);
 
-    public String operatorStr;
-    private BinaryOperator<Integer> expression;
-    public int precedence;
+    private final String operatorStr;
+    private final BinaryOperator<Integer> expression;
+    private final int precedence;
     Operator(String operatorStr, BinaryOperator<Integer> expression, int precedence) {
         this.operatorStr = operatorStr;
         this.expression = expression;
@@ -20,5 +20,13 @@ public enum Operator {
 
     public int calculate(Integer num1, Integer num2) {
         return this.expression.apply(num1, num2);
+    }
+
+    public String getOperatorStr() {
+        return operatorStr;
+    }
+
+    public int getPrecedence() {
+        return precedence;
     }
 }
