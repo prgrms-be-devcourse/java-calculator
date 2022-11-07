206fd5362df11511d7f7a5d4dd32ec0cab2a1cbf
Kim Changgyu <kimc980106@naver.com>
2022-11-07 17:08:27 +0900

    Refactor: CalculatorController와 Menu의 책임 변경
    기존: 입력에 대한 실행 메소드를 CalculatorController에서 제어
    변경: 입력에 대한 실행 메소드를 Menu에서 제어 및 실행

--- src/main/java/com/programmers/java/engine/CalculatorController.java
+++ src/main/java/com/programmers/java/engine/CalculatorController.java
@@ -2,7 +2,6 @@ package com.programmers.java.engine;
 
 import com.programmers.java.engine.calculator.Calculator;
 import com.programmers.java.engine.io.IOController;
-import com.programmers.java.engine.model.Expression;
 import com.programmers.java.engine.repository.HistoryRepository;
 import com.programmers.java.engine.util.Menu;
 import com.programmers.java.engine.util.translator.Translator;
@@ -12,6 +11,7 @@ public class CalculatorController implements Runnable {
     private final Calculator calculator;
     private final Translator translator;
     private final HistoryRepository historyRepository;
+    private boolean isExit = false;
 
     public CalculatorController(IOController ioController, Calculator calculator, Translator translator, HistoryRepository historyRepository) {
         this.ioController = ioController;
@@ -22,45 +22,33 @@ public class CalculatorController implements Runnable {
 
     @Override
     public void run() {
-        boolean isExit = false;
         while (!isExit) {
             try {
-                ioController.print(Menu.GUIDE.get());
-                switch (Menu.findMenu(ioController.read())) {
-                    case LOOKUP:
-                        lookup();
-                        break;
-                    case COMPUTE:
-                        calculate();
-                        break;
-                    case EXIT:
-                        ioController.print("프로그램이 종료됩니다...\n");
-                        isExit = true;
-                        break;
-                }
-
-                if (isExit) break;
+                Menu.GUIDE.run(this);
+                Menu.findMenu(ioController.read()).run(this);
             } catch (Exception e) {
                 ioController.print(e.getMessage());
             }
         }
     }
 
-
-    private void lookup() {
-        if (historyRepository.size() == 0) {
-            ioController.print("저장된 내역이 없습니다.\n");
-        }
-
-        historyRepository.load().stream()
-                .forEach(expression -> ioController.print(expression + "\n"));
+    public IOController getIoController() {
+        return ioController;
     }
 
-    private void calculate() {
-        Expression expression = new Expression(ioController.read());
-        Number result = calculator.calculate(translator.translate(expression));
-        ioController.print(result + "\n");
-        expression.add(" = " + result);
-        historyRepository.save(expression);
+    public Calculator getCalculator() {
+        return calculator;
+    }
+
+    public Translator getTranslator() {
+        return translator;
+    }
+
+    public HistoryRepository getHistoryRepository() {
+        return historyRepository;
+    }
+
+    public void setExit(boolean exit) {
+        isExit = exit;
     }
 }
--- src/main/java/com/programmers/java/engine/util/Menu.java
+++ src/main/java/com/programmers/java/engine/util/Menu.java
@@ -1,14 +1,46 @@
 package com.programmers.java.engine.util;
 
+import com.programmers.java.engine.CalculatorController;
 import com.programmers.java.engine.exception.MenuException;
+import com.programmers.java.engine.model.Expression;
 
 import java.util.Arrays;
 
 public enum Menu {
-    LOOKUP("1"),
-    COMPUTE("2"),
-    EXIT("3"),
-    GUIDE("\n1. 조회\n2. 계산\n3. 종료\n\n선택 : ");
+    LOOKUP("1") {
+        @Override
+        public void run(CalculatorController controller) {
+            if (controller.getHistoryRepository().size() == 0) {
+                controller.getIoController().print("저장된 내역이 없습니다.\n");
+            }
+
+            controller.getHistoryRepository().load().stream()
+                    .forEach(expression -> controller.getIoController().print(expression + "\n"));
+        }
+    },
+    COMPUTE("2") {
+        @Override
+        public void run(CalculatorController controller) {
+            Expression expression = new Expression(controller.getIoController().read());
+            Number result = controller.getCalculator().calculate(controller.getTranslator().translate(expression));
+            controller.getIoController().print(result + "\n");
+            expression.add(" = " + result);
+            controller.getHistoryRepository().save(expression);
+        }
+    },
+    EXIT("3") {
+        @Override
+        public void run(CalculatorController controller) {
+            controller.getIoController().print("프로그램이 종료됩니다...\n");
+            controller.setExit(true);
+        }
+    },
+    GUIDE("\n1. 조회\n2. 계산\n3. 종료\n\n선택 : ") {
+        @Override
+        public void run(CalculatorController controller) {
+            controller.getIoController().print(this.get());
+        }
+    };
 
     private final String select;
 
@@ -26,4 +58,6 @@ public enum Menu {
     public String get() {
         return this.select;
     }
+
+    public abstract void run(CalculatorController controller);
 }
