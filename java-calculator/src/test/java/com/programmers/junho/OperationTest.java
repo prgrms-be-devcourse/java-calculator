a152c5f661eebdb82219357dfc3cb3c0d8c71b82
juno-junho <ssmm0205@naver.com>
2023-06-02 16:45:29 +0900

    test: setup 메서드로 중복 코드 제거 refactor

--- java-calculator/src/test/java/com/programmers/junho/OperationTest.java
+++ java-calculator/src/test/java/com/programmers/junho/OperationTest.java
@@ -1,35 +1,38 @@
 package com.programmers.junho;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class OperationTest {
 
+    private Operation operation;
+    @BeforeEach
+    void setUp() {
+        this.operation = new Operation();
+    }
+
     @Test
     void add() {
-        Operation operation = new Operation();
         int result = operation.add(1, 2);
         assertThat(result).isEqualTo(3);
     }
 
     @Test
     void subtract() {
-        Operation operation = new Operation();
         int result = operation.subtract(1, 2);
         assertThat(result).isEqualTo(-1);
     }
 
     @Test
     void multiply() {
-        Operation operation = new Operation();
         int result = operation.multiply(2, 4);
         assertThat(result).isEqualTo(8);
     }
 
     @Test
     void divide() {
-        Operation operation = new Operation();
         int result = operation.divide(4, 2);
         assertThat(result).isEqualTo(2);
     }
