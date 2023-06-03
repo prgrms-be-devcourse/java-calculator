ca5baa66302a03ed40b2c1f5fa03b17e421c6120
player-geun <player_geun@naver.com>
2023-06-04 00:07:36 +0900

    feat : 프로젝트 초기화

---
+++ src/main/java/com/javacalculator/JavaCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.javacalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javacalculator/JavaCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
