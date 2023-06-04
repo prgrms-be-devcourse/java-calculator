dfc8e1197379023aba1ccc32920e49bc39f83978
HyuckJuneHong <jjj8337721@naver.com>
2023-06-05 03:22:53 +0900

    feat : 계산기 저장소 저장 및 조회 구현 / ID 구현

---
+++ src/main/java/domain/IdGenerator.java
@@ -0,0 +1,23 @@
+package domain;
+
+public class IdGenerator {
+
+    private static IdGenerator instance;
+
+    private long idCounter;
+
+    private IdGenerator() {
+
+        idCounter = 0;
+    }
+
+    public static IdGenerator getInstance() {
+
+        return instance == null ? instance = new IdGenerator() : instance;
+    }
+
+    public synchronized long generateId() {
+        return idCounter++;
+    }
+}
+
--- src/main/java/domain/repository/CalculationRepository.java
+++ src/main/java/domain/repository/CalculationRepository.java
@@ -1,11 +1,25 @@
 package domain.repository;
 
+import domain.IdGenerator;
 import domain.model.Calculator;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class CalculationRepository {
 
-    Map<Long, Calculator> repository = new HashMap<>();
+    private Map<Long, Calculator> repository = new HashMap<>();
+
+    private IdGenerator idGenerator = IdGenerator.getInstance();
+
+    public void save(Calculator calculator) {
+
+        Long key = idGenerator.generateId();
+
+        repository.put(key, calculator);
+    }
+
+    public List<Calculator> findAll() {
+
+        return new ArrayList<>(repository.values());
+    }
 }
