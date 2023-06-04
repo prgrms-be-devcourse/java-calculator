ffe72520b827e8024e969943bb2f900e854d05d5
PHJ <legowww@gmail.com>
2023-06-04 14:08:53 +0900

    feat/view: 메뉴 선택 구현
    종료 기능을 3번 메뉴로 추가적으로 구현했다.

---
+++ src/main/java/exception/ErrorMessage.java
@@ -0,0 +1,6 @@
+package exception;
+
+public class ErrorMessage {
+    public static final String INVALID_SELECT_NUMBER = "1~3 사이의 숫자만 입력할 수 있습니다.";
+    public static final String INVALID_CHAR = "문자가 아닌 숫자를 입력하시오.";
+}
