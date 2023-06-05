bddde89c8400872d57133f3cb2347b194187defa
bombo-dev <bombo96@chungwoon.ac.kr>
2023-06-05 13:13:48 +0900

    refactor : 메뉴 입력 값 반환 로직 간단화

--- src/main/java/com/programmers/calculator/MenuType.java
+++ src/main/java/com/programmers/calculator/MenuType.java
@@ -5,31 +5,19 @@ import com.programmers.exception.WrongInputMenuException;
 import java.util.Arrays;
 
 public enum MenuType {
-    HISTORY(1), CALCULATE(2), EXIT(3);
+    HISTORY("1"), CALCULATE("2"), EXIT("3");
 
-    private final int menuNumber;
+    private final String menuNumber;
 
-    MenuType(int menuNumber) {
+    MenuType(String menuNumber) {
         this.menuNumber = menuNumber;
     }
 
     public static MenuType findMenuType(String inputNumber) {
 
-        int inputMenuNumber = convertMenuNumber(inputNumber);
-
         return Arrays.stream(MenuType.values())
-                .filter(menuType -> menuType.menuNumber == inputMenuNumber)
+                .filter(menuType -> menuType.menuNumber.equals(inputNumber))
                 .findAny()
                 .orElseThrow(() -> new WrongInputMenuException("잘못된 입력 메뉴가 들어왔습니다. 1, 2, 3 만 가능합니다."));
-    }
-
-    private static int convertMenuNumber(String inputNumber) {
-        int convertInputNumber;
-        try {
-            convertInputNumber = Integer.parseInt(inputNumber);
-        } catch(NumberFormatException e) {
-            throw new WrongInputMenuException("잘못된 입력 메뉴가 들어왔습니다. 1, 2, 3 만 가능합니다.");
-        }
-        return convertInputNumber;
     }
 }
