ca19f4051229a048ddc38793b852c00ec3922a7b
doyeon <kimsk3113@naver.com>
2023-06-05 18:04:45 +0900

    feat: 계산기 엔진(계산 로직이 담겨있는 캡슐화된 클래스) 모듈, 테스트 추가

---
+++ app/src/main/java/java/calculator/common/engine/CalculatorEngine.java
@@ -0,0 +1,55 @@
+package java.calculator.common.engine;
+
+import java.calculator.common.operator.Operator;
+import java.calculator.common.utils.Tokenizer;
+import java.calculator.common.utils.Number;
+import java.util.Stack;
+
+public class CalculatorEngine {
+    private final Stack<Integer> numberStack = new Stack<>();
+    private final Stack<Operator> operatorStack = new Stack<>();
+
+    public CalculatorEngine(String rawExpression) {
+        Tokenizer tokenizer = new Tokenizer(rawExpression);
+        parse(tokenizer.getTokens());
+    }
+
+    private void parse(String[] tokens) {
+        for (String token : tokens) {
+            Number number = new Number(token);
+            if (number.isValid()) {
+                numberStack.push(number.toInt());
+                continue;
+            }
+            handleOperator(Operator.of(token));
+        }
+
+        finalizeCalculation();
+    }
+
+    private void handleOperator(Operator operator) {
+        while (!operatorStack.isEmpty() && !operator.isHighPriority(operatorStack.peek())) {
+            calculateTop();
+        }
+
+        operatorStack.push(operator);
+    }
+
+    private void finalizeCalculation() {
+        while (!operatorStack.isEmpty()) {
+            calculateTop();
+        }
+    }
+
+    private void calculateTop() {
+        int number2 = numberStack.pop();
+        int number1 = numberStack.pop();
+        Operator operator = operatorStack.pop();
+
+        numberStack.push(operator.calculate(number1, number2));
+    }
+
+    public int evaluate() {
+        return numberStack.pop();
+    }
+}
