16398c586cf78e9f22ce2d7849d63177237efff4
jun <alap_u@naver.com>
2023-06-05 21:23:04 +0900

    feat 우선순위를 구분한다.
    *,/를 우선순위 +,-를 후순위 연산자 역할을 한다.

--- src/test/java/calulator/domain/OperationTest.java
+++ src/test/java/calulator/domain/OperationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
@@ -23,6 +24,20 @@ class OperationTest {
         assertThatThrownBy(() -> Operation.operator("=", 2, 0))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("사용할 수 없는 연산자입니다.");
+    }
+
+    @DisplayName("*,/ 는 우선 연산자 역할을 한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"*", "/"})
+    void isPriorityTest(String operator) {
+        assertThat(Operation.isPriority(operator)).isTrue();
+    }
+
+    @DisplayName("+,- 는 후순위 연산자 역할을 한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"+", "-"})
+    void isNonePriorityTest(String operator) {
+        assertThat(Operation.isNonePriority(operator)).isTrue();
     }
 
 }
