247002d0eb5ce498695d8ee5170ddd6777b6037e
BeommoKoo <rnqjaah1536@gmail.com>
2023-06-05 12:00:33 +0900

    calculator test 코드 간결화

--- src/test/CalculatorTest.java
+++ src/test/CalculatorTest.java
@@ -6,36 +6,32 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
 
     String command;
-    String[] commandArr;
-    int result;
+
     @DisplayName("calculate함수는 띄어쓰기로 파싱된 commandArr를 계산하는 함수.")
     @Test
     void calculateTest() {
         // case 테스트마다 commandArr, result 갱신해주는거 불편하다.
         // 매크로로 못 선언하나 ?
-        Calculator calculator = Calculator.getInstance();
         // 1. * + / -
         command = "5 * 2 + 64 / 32 - 7";
-        commandArr = command.split(" ");
-        result = calculator.calculate(commandArr);
-        assertEquals(5, result);
+        assertEquals(5, input(command));
 
         // 2. / * + /
         command = "15 / 5 * 2 + 8 / 2";
-        commandArr = command.split(" ");
-        result = calculator.calculate(commandArr);
-        assertEquals(10, result);
+        assertEquals(10, input(command));
 
         // 3. + / - *
         command = "2 + 12 / 6 - 3 * 7";
-        commandArr = command.split(" ");
-        result = calculator.calculate(commandArr);
-        assertEquals(-17, result);
+        assertEquals(-17, input(command));
 
         // 4. - * + /
         command = "7 - 25 * 3 + 20 / 10";
-        commandArr = command.split(" ");
-        result = calculator.calculate(commandArr);
-        assertEquals(-66, result);
+        assertEquals(-66, input(command));
+    }
+
+    int input(String command) {
+        Calculator calculator = Calculator.getInstance();
+        String[] commandArr = command.split(" ");
+        return calculator.calculate(commandArr);
     }
 }
