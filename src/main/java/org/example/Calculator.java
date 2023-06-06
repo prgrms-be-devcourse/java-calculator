fb32c3fa07514f54431a05ddb9fb2e6f17dc68d3
박은지 <qkrdmswl1018@naver.com>
2023-06-06 15:18:05 +0900

    feat: 계산 기능 구현

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,6 +1,19 @@
 package org.example;
 
 public class Calculator {
-  public static void main(String[] args) {
+  public static double plus(double number1, double number2) {
+    return number1 + number2;
+  }
+
+  public static double minus(double number1, double number2) {
+    return number1 - number2;
+  }
+
+  public static double multiply(double number1, double number2) {
+    return number1 * number2;
+  }
+
+  public static double divide(double number1, double number2) {
+    return number1 / number2;
   }
 }
