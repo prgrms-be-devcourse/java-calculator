bebf57af8fbd579997d06da8404b1e4a99b3894c
jun <alap_u@naver.com>
2023-06-06 14:07:23 +0900

    refactor 수식을 입력받을때 정규식을 통해 유효성 검사를 진행함

--- src/main/java/calulator/view/InputView.java
+++ src/main/java/calulator/view/InputView.java
@@ -3,22 +3,30 @@ package calulator.view;
 import calulator.domain.Menu;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class InputView {
 
     private static final String SELECT_MESSAGE = "선택 : ";
     private static final String BLANK_REGEX = "//s";
     private static final String NONE_SPACE_REGEX = "";
+    private static final String EXPRESSION_REGEX = "^-?\\d+([+\\-*/]-?\\d+)*$";
+    private static final String INVALID_EXPRESSION_MESSAGE = "올바른 수식이 아닙니다.";
     private final Scanner scanner = new Scanner(System.in);
 
     public Menu inputMenu() {
         System.out.println(Menu.menuInfo());
-        System.out.printf(SELECT_MESSAGE);
+        System.out.print(SELECT_MESSAGE);
         return Menu.findMenu(inputLineWithoutBlank());
     }
 
     public String inputExpression() {
-        return inputLineWithoutBlank();
+        String expression = inputLineWithoutBlank();
+        Pattern pattern = Pattern.compile(EXPRESSION_REGEX);
+        if (pattern.matcher(expression).matches()) {
+            return expression;
+        }
+        throw new IllegalArgumentException(INVALID_EXPRESSION_MESSAGE);
     }
 
     private String inputLineWithoutBlank() {
