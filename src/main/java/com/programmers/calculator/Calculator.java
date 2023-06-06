4b7af5e03943e791c17b66dd299ed1496d768f53
bombo-dev <bombo96@chungwoon.ac.kr>
2023-06-06 12:49:13 +0900

    fix : 잘못된 메뉴 선택 시 프로그램이 정상적으로 동작하지 않는 오류 수정

--- src/main/java/com/programmers/calculator/Calculator.java
+++ src/main/java/com/programmers/calculator/Calculator.java
@@ -1,16 +1,34 @@
 package com.programmers.calculator;
 
+import com.programmers.converter.ExpressionConverter;
+import com.programmers.converter.InfixToPostfixConverter;
+import com.programmers.exception.WrongInputExpressionException;
 import com.programmers.exception.WrongInputMenuException;
 import com.programmers.io.Console;
 
+import java.util.Collections;
+import java.util.List;
+
 public class Calculator {
+    ExpressionConverter expressionConverter;
     boolean power = true;
+
+    public Calculator() {
+        expressionConverter = new InfixToPostfixConverter();
+    }
+
     public void run() {
         while(power) {
             Console.printMenu();
             MenuType menu = makeMenuType(Console.inputMenuNumber());
 
+            if(menu == null)
+                continue;
+
             switch(menu){
+                case CALCULATE:
+                    String expression = Console.inputExpression();
+                    break;
                 case EXIT:
                     power = false;
                     Console.printExit();
