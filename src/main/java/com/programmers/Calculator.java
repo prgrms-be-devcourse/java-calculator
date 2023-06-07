c11659712f4406dfec9d1ac7b6a2fbc2fb1f1abe
Sunny <rosa7536@naver.com>
2023-06-07 22:10:29 +0900

    FEAT : JavaCalculator 작성

---
+++ src/main/java/com/programmers/Calculator.java
@@ -0,0 +1,4 @@
+package com.programmers;
+
+public class Calculator {
+}
--- src/main/java/com/programmers/JavaCalculator.java
+++ src/main/java/com/programmers/JavaCalculator.java
@@ -2,6 +2,28 @@ package com.programmers;
 
 import com.programmers.model.Input;
 import com.programmers.model.Output;
+import com.programmers.model.Result;
 
 public class JavaCalculator {
+    private final Input input;
+    private final Output output;
+    private final Result result;
+
+    public JavaCalculator(Input input, Output output, Result result) {
+        this.input = input;
+        this.output = output;
+        this.result = result;
+    }
+
+    public void run() {
+        boolean isExcutable = true;
+        while (isExcutable) {
+            output.showMenu();
+            switch (input.selectMenu()) {
+                case 1 -> result.readAllResults();
+                case 3 -> isExcutable = false;
+                default -> System.out.println("다시 선택해주세요!");
+            }
+        }
+    }
 }
