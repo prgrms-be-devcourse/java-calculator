ffe8c824312f0a075f9c3d47c2e32eb1fe5c39a2
Sunny <rosa7536@naver.com>
2023-06-07 23:06:49 +0900

    FEAT : 자바 계산기 실행 부분 완성

--- src/main/java/com/programmers/JavaCalculator.java
+++ src/main/java/com/programmers/JavaCalculator.java
@@ -8,11 +8,13 @@ public class JavaCalculator {
     private final Input input;
     private final Output output;
     private final Result result;
+    private final BasicCalculator bc;
 
-    public JavaCalculator(Input input, Output output, Result result) {
+    public JavaCalculator(Input input, Output output, Result result, BasicCalculator bc) {
         this.input = input;
         this.output = output;
         this.result = result;
+        this.bc = bc;
     }
 
     public void run() {
@@ -21,6 +23,7 @@ public class JavaCalculator {
             output.showMenu();
             switch (input.selectMenu()) {
                 case 1 -> result.readAllResults();
+                case 2 -> result.save(bc.doCalculate(input.getExpression()));
                 case 3 -> isExcutable = false;
                 default -> System.out.println("다시 선택해주세요!");
             }
