5216d3b8acbd9f65bc3f51f852396ee481970e8b
박은지 <qkrdmswl1018@naver.com>
2023-06-07 16:36:12 +0900

    feat: 전체적인 계산기 동작 구현

--- src/main/java/org/example/CalculatorController.java
+++ src/main/java/org/example/CalculatorController.java
@@ -1,17 +1,36 @@
 package org.example;
 
+import org.example.history.Formula;
+import org.example.history.History;
 import org.example.io.Input;
 import org.example.io.Output;
 import org.example.calculator.Calculator;
 
-public class CalculatorController {
-  Input input;
-  Output output;
-  Calculator calculator;
+import java.io.IOException;
 
-  public CalculatorController(Input input, Output output, Calculator calculator) {
-    this.input = input;
-    this.output = output;
-    this.calculator = calculator;
+public class CalculatorController {
+  static Input input;
+  static Output output;
+  static Calculator calculator;
+  static History history;
+
+  public static void run() throws IOException {
+    while (true) {
+      output.showMenu();
+
+      switch (input.inputMenu()) {
+        case 1:
+          history.findAll();
+          break;
+        case 2:
+          String formula = input.inputFormula();
+          double result = calculator.calculate(formula);
+          history.save(new Formula(formula, result));
+          output.printResult(result);
+          break;
+        case 3:
+          return ;
+      }
+    }
   }
 }
