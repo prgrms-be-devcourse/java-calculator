995d3a31c832f195d029f5914845f1788e5ae963
hseong3243 <hseong3243@gmail.com>
2023-06-07 22:05:40 +0900

    feat: 후위표기법 변환에 올바르지 않은 순서의 계산식이 주어진 경우에 대한 예외 처리 추가

--- src/test/java/com/programmers/postfixconverter/PostfixConverter.java
+++ src/test/java/com/programmers/postfixconverter/PostfixConverter.java
@@ -7,11 +7,14 @@ public class PostfixConverter {
     public String[] convert(String[] tokenized) {
         ArrayList<String> result = new ArrayList<>();
 
+        boolean numberTurn = true;
         Stack<String> operators = new Stack<>();
         for (String token : tokenized) {
-            if(isNumber(token)) {
+            if(isNumber(token) && numberTurn) {
                 result.add(token);
-            } else {
+
+                numberTurn = false;
+            } else if(isOperator(token) && !numberTurn) {
                 while (!operators.isEmpty()) {
                     if(getPriority(token) > getPriority(operators.peek())) {
                         break;
@@ -20,6 +23,10 @@ public class PostfixConverter {
                     result.add(operators.pop());
                 }
                 operators.push(token);
+
+                numberTurn = true;
+            } else {
+                throw new UnsupportedOperationException("올바른 숫자/연산자를 입력해주세요.");
             }
         }
 
@@ -32,6 +39,10 @@ public class PostfixConverter {
 
     private boolean isNumber(String token) {
         return token.matches("[0-9]+");
+    }
+
+    private boolean isOperator(String token) {
+        return token.matches("[+\\-*/]?");
     }
 
     private int getPriority(String operator) {
--- src/test/java/com/programmers/postfixconverter/PostfixConverterTest.java
+++ src/test/java/com/programmers/postfixconverter/PostfixConverterTest.java
@@ -3,9 +3,10 @@ package com.programmers.postfixconverter;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PostfixConverterTest {
-    private PostfixConverter parser = new PostfixConverter();
+    private PostfixConverter converter = new PostfixConverter();
 
     @Test
     void parsingDataMixedWithPlusAndMinus() {
@@ -13,7 +14,7 @@ public class PostfixConverterTest {
         String[] tokenizedA = {"1", "+", "3", "-", "4"};
 
         //when
-        String[] resultA = parser.convert(tokenizedA);
+        String[] resultA = converter.convert(tokenizedA);
 
         //then
         assertThat(resultA).containsExactly("1", "3", "+", "4", "-");
@@ -26,12 +27,30 @@ public class PostfixConverterTest {
         String[] tokenizedB = {"1", "-", "1", "*", "1", "/", "1"};
 
         //when
-        String[] resultA = parser.convert(tokenizedA);
-        String[] resultB = parser.convert(tokenizedB);
+        String[] resultA = converter.convert(tokenizedA);
+        String[] resultB = converter.convert(tokenizedB);
 
         //then
         assertThat(resultA).containsExactly("1", "1", "*", "1", "1", "/", "+");
         assertThat(resultB).containsExactly("1", "1", "1", "*", "1", "/", "-");
 
+    }
+
+    @Test
+    void convertDataButInvalidOrder_Then_Exception() {
+        //given
+        String[] tokenizedA = {"1", "1", "1"};
+        String[] tokenizedB = {"+", "1", "*", "1", "-"};
+        String[] tokenizedC = {"1", "*", "1", "-", "-"};
+
+        //when
+
+        //then
+        assertThatThrownBy(() -> converter.convert(tokenizedA))
+                .isInstanceOf(UnsupportedOperationException.class);
+        assertThatThrownBy(() -> converter.convert(tokenizedB))
+                .isInstanceOf(UnsupportedOperationException.class);
+        assertThatThrownBy(() -> converter.convert(tokenizedC))
+                .isInstanceOf(UnsupportedOperationException.class);
     }
 }
