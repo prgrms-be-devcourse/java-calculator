0343b02873305b675f5c911ea118ecd800e2fac9
young970 <yyww9@naver.com>
2023-06-08 15:04:58 +0900

    Test: 메모리에 계산 이력 저장 및 조회

---
+++ src/test/java/org/example/repository/MemoryRepositoryTest.java
@@ -0,0 +1,33 @@
+package org.example.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MemoryRepositoryTest {
+    private MemoryRepository memoryRepository;
+
+    @BeforeEach
+    public void setupList(){
+        memoryRepository = new MemoryRepository();
+    }
+
+    @Test
+    public void 계산값_1개_저장_후_조회(){
+        memoryRepository.save("5 + 3 * 2 = 11");
+        String[] result = memoryRepository.findAll();
+        assertThat(result[0]).isEqualTo("5 + 3 * 2 = 11");
+    }
+
+    @Test
+    public void 계산값_2개_저장_후_조회(){
+        memoryRepository.save("5 + 3 * 2 = 11");
+        memoryRepository.save("3 + 5 * 7 = 38");
+        String[] result = memoryRepository.findAll();
+        assertThat(result[0]).isEqualTo("5 + 3 * 2 = 11");
+        assertThat(result[1]).isEqualTo("3 + 5 * 7 = 38");
+    }
+}
