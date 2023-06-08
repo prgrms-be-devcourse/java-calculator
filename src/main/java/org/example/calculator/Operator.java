4f6acb6f1d0e731d21b00ed3b61787d8d78a51a1
박은지 <qkrdmswl1018@naver.com>
2023-06-08 21:15:37 +0900

    feat(Operator): 사용되지 않는 메소드 정리

--- src/main/java/org/example/calculator/Operator.java
+++ src/main/java/org/example/calculator/Operator.java
@@ -1,6 +1,5 @@
 package org.example.calculator;
 
-import java.util.Arrays;
 import java.util.function.BiFunction;
 
 public enum Operator {
@@ -23,12 +22,5 @@ public enum Operator {
 
   public double calculate(double num1, double num2) {
     return formula.apply(num1, num2);
-  }
-
-  public Operator checkOperator(String operator) {
-    return Arrays.stream(values())
-            .filter(op -> op.getOperator().equals(operator))
-            .findFirst()
-            .orElseThrow(() -> new RuntimeException("해당 연산자는 지원하지 않습니다."));
   }
 }
