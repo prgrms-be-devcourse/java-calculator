c2986acf9b576d7af7e1b63e3551731174438850
ysng_ysng <yeasung67@gmail.com>
2023-06-08 17:28:09 +0900

    feat: Viewer의 출력 메소드 추가
    정해진 형식으로 결과를 출력하기 위해 Viewer의 출력 메소드를 추가했습니다.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,13 +7,14 @@ public class Main {
         while (true) {
             viewer.printInfoMessage();
             CommandType command = inputReader.readUserCommand();
+            viewer.printNewLine();
             if (command.equals(CommandType.HISTORY_COMMAND)) {
                 System.out.println("아직 구현 안함");
             }
             if (command.equals(CommandType.CALCULATE_COMMAND)) {
                 String expression = inputReader.readExpression();
-                int result = calculator.calculate(expression);
-                System.out.println(result);
+                int answer = calculator.calculate(expression);
+                viewer.printAnswer(answer);
             }
         }
     }
