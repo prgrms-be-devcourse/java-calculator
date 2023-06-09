e0c42fa5403c362d805652fefafba67fdb2af447
juno-junho <ssmm0205@naver.com>
2023-06-10 04:05:15 +0900

    refactor(domain): 결과값 추가

--- java-calculator/src/test/java/com/programmers/junho/domain/CalculatorTest.java
+++ java-calculator/src/test/java/com/programmers/junho/domain/CalculatorTest.java
@@ -9,8 +9,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CalculatorTest {
 
     @DisplayName("계산 결과 테스트")
-    @ParameterizedTest(name ="식: {0}, 결과: {1}")
-    @CsvSource(value = {"1 + 2:3", "1 + 2 * 3:7", "3 - 2 * 2:-1"}, delimiter = ':')
+    @ParameterizedTest(name = "식: {0}, 결과: {1}")
+    @CsvSource(value = {"1 + 2:3", "1 + 2 * 3:7", "3 - 2 * 2:-1", "44 / 2 * 8:176", "22 * 8:176", "12 + 44 / 2 * 8 - 10 + 18 / 2 / 3:181"}, delimiter = ':')
     void name(String expression, int calculatedResult) {
         Calculator calculator = new Calculator(expression);
 
