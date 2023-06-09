27c00314410291c4cf2f2c2b1624431d55b2f345
heenahan <mby1997s@gmail.com>
2023-06-09 10:00:56 +0900

    feat : 연산 결과 맵에 저장

---
+++ app/src/main/java/com/programmers/storage/Storage.java
@@ -0,0 +1,20 @@
+package com.programmers.storage;
+
+import com.programmers.expression.Expression;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Storage {
+	
+	static Map<String, Integer> map = new ConcurrentHashMap<>();
+	
+	public void save(Expression expression, Integer result) {
+		map.put(expression.toString(), result);
+	}
+	
+	public Map<String, Integer> findAll() {
+		return map;
+	}
+	
+}
