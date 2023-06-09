eb3763d9f9dd3061cf8ecff17d5c8fe59d7f22a4
juno-junho <ssmm0205@naver.com>
2023-06-10 02:40:30 +0900

    feat(domain): 중위 연산을 후위 연산으로 바꾸는 기능 구현

--- java-calculator/src/main/java/com/programmers/junho/domain/Expression.java
+++ java-calculator/src/main/java/com/programmers/junho/domain/Expression.java
@@ -1,5 +1,6 @@
 package com.programmers.junho.domain;
 
+import java.util.Stack;
 import java.util.regex.Pattern;
 
 public class Expression {
@@ -16,4 +17,38 @@ public class Expression {
             throw new IllegalArgumentException("잘못된 형식의 식을 입력하셨습니다.");
         }
     }
+
+    public String getPostfixExpression() {
+        String expression = removeSpace();
+
+        StringBuilder postfixExpression = new StringBuilder();
+
+        Stack<ArithmeticOperators> stack = new Stack<>();
+
+        String[] tokens = expression.split("");
+        for (String token : tokens) {
+            // 펴연산자 바로 출력
+            if (!ArithmeticOperators.isOperator(token)) {
+                postfixExpression.append(token);
+                continue;
+            }
+            // 연산자면
+            ArithmeticOperators operator = ArithmeticOperators.convertTokenToOperator(token);
+            while (!stack.isEmpty() && stack.peek().getPriority() >= operator.getPriority()) {
+                postfixExpression.append(stack.pop().getOperator());
+            }
+            if (stack.isEmpty() || stack.peek().getPriority() < operator.getPriority()) {
+                stack.push(operator);
+            }
+        }
+        while (!stack.isEmpty()) {
+            postfixExpression.append(stack.pop().getOperator());
+        }
+        return postfixExpression.toString();
+    }
+
+    private String removeSpace() {
+        return this.expression.replace(" ", "");
+    }
 }
+
