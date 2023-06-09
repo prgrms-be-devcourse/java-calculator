839015af891dfebd74982423cd987bc52ad1a4ae
Yebin Lee <lyb2325@gmail.com>
2023-06-10 08:03:12 +0900

    [add]: Constant 상수 추가

--- src/main/java/calculator/global/InputConstants.java
+++ src/main/java/calculator/global/InputConstants.java
@@ -1,7 +1,7 @@
 package calculator.global;
 
 public class InputConstants {
-    public static final String CHOICE_PROMPT = "선택 : ";
+    public static final String CHOICE_PROMPT = "\n선택 : ";
     public static final int FIRST_INDEX = 0;
     public static final int MENU_INPUT_LENGTH = 1;
     public static final char REQUEST_VIEW_CALCULATION_RESULT = '1';
@@ -9,4 +9,6 @@ public class InputConstants {
 
     public static final String OPERATOR_REGEX = "[-*/+]";
     public static final String OPERAND_REGEX = "^\\d+$";
+
+    public static final int MINIMUM_OPS = 3;
 }
---
+++ src/main/java/calculator/global/Priority.java
@@ -0,0 +1,6 @@
+package calculator.global;
+
+public class Priority {
+    public static final Integer LOW = 0;
+    public static final Integer HIGH = 1;
+}
