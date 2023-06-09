87aa8627bbb2c146ea8f86ad696568a5e1c3032c
wonu606 <wonu606@gmail.com>
2023-06-10 00:59:46 +0900

    test: 입출력 구현체 테스트 작성

---
+++ calculator/src/test/java/com/wonu606/io/ConsolePrinterTest.java
@@ -0,0 +1,38 @@
+package com.wonu606.io;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ConsolePrinterTest {
+
+    @Test
+    @DisplayName("정수 출력")
+    void testIntegerPrint() {
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        Print printer;
+        int number = -1;
+        printer.print(number);
+
+        assertThat(Integer.toString(number)).isEqualTo(out.toString().trim());
+    }
+
+    @Test
+    @DisplayName("문자열 출력")
+    void testStringPrint() {
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        Print printer;
+        String str = "Hello World";
+        printer.print(str);
+
+        assertThat(str).isEqualTo(out.toString().trim());
+    }
+}
