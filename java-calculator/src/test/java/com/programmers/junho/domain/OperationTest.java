5034d14f45147c173e653744c4d9462f51c483d2
juno-junho <ssmm0205@naver.com>
2023-06-10 01:41:34 +0900

    test(domain): 사칙연산에 대한 테스트 리펙토링

--- java-calculator/src/test/java/com/programmers/junho/domain/OperationTest.java
+++ java-calculator/src/test/java/com/programmers/junho/domain/OperationTest.java
@@ -1,39 +1,33 @@
 package com.programmers.junho.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.programmers.junho.domain.ArithmeticOperators.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class OperationTest {
 
-    private Operation operation;
-    @BeforeEach
-    void setUp() {
-        this.operation = new Operation(1);
-    }
-
     @Test
     void add() {
-        int result = operation.add(1, 2);
+        double result = ADDITION.apply(1, 2);
         assertThat(result).isEqualTo(3);
     }
 
     @Test
     void subtract() {
-        int result = operation.subtract(1, 2);
+        double result = SUBTRACTION.apply(1, 2);
         assertThat(result).isEqualTo(-1);
     }
 
     @Test
     void multiply() {
-        int result = operation.multiply(2, 4);
+        double result = MULTIPLICATION.apply(2, 4);
         assertThat(result).isEqualTo(8);
     }
 
     @Test
     void divide() {
-        int result = operation.divide(4, 2);
+        double result = DIVISION.apply(4, 2);
         assertThat(result).isEqualTo(2);
     }
 }
