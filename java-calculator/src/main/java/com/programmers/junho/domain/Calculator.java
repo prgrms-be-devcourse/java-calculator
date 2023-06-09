a61066528a20bb2cca0dc8a55d50d0cfc634271a
juno-junho <ssmm0205@naver.com>
2023-06-10 03:24:51 +0900

    refactor(domain): Calculator 클래스 refactor

--- java-calculator/src/main/java/com/programmers/junho/domain/Calculator.java
+++ java-calculator/src/main/java/com/programmers/junho/domain/Calculator.java
@@ -16,18 +16,20 @@ public class Calculator {
         Stack<Integer> stack = new Stack<>();
 
         for (String token : tokens) {
-            // 숫자면 일단 스택에 넣는다.
-            if (!ArithmeticOperators.isOperator(token)) {
-                stack.push(Integer.parseInt(token));
-                continue;
-            }
-            // 문자면 두번 pop해서 연산해서 다시 스택에 넣는다
-            ArithmeticOperators operator = ArithmeticOperators.convertTokenToOperator(token);
-            Integer second = stack.pop();
-            Integer first = stack.pop();
-            stack.push(operator.apply(first, second));
+            extracted(stack, token);
         }
         // 스택 결과값
         return stack.pop();
+    }
+
+    private void extracted(Stack<Integer> stack, String token) {
+        if (ArithmeticOperators.isNumber(token)) {
+            stack.push(Integer.parseInt(token));
+            return;
+        }
+        ArithmeticOperators operator = ArithmeticOperators.convertTokenToOperator(token);
+        Integer second = stack.pop();
+        Integer first = stack.pop();
+        stack.push(operator.apply(first, second));
     }
 }
