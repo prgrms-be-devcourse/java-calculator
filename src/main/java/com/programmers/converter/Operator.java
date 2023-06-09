15ba0150b83aa571e72cf3df48cd5f7ac9100362
bombo-dev <bombo96@chungwoon.ac.kr>
2023-06-09 09:24:28 +0900

    refactor : Map을 이용한 Enum 탐색 최적화, 개별 연산 분류

--- src/main/java/com/programmers/converter/Operator.java
+++ src/main/java/com/programmers/converter/Operator.java
@@ -6,18 +6,32 @@ import com.programmers.exception.WrongOperationException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
 import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public enum Operator {
-    ADD("+", 1, BigDecimal::add),
-    SUBTRACT( "-", 1, BigDecimal::subtract),
-    MULTIPLY( "*",2, BigDecimal::multiply),
-    DIVIDE( "/", 2, (n1, n2) -> n1.divide(n2, 30, RoundingMode.HALF_EVEN)),
-    OPEN_PARENTHESES("(", 0, (n1, n2) -> BigDecimal.valueOf(0));
+    ADD("+", 2, BigDecimal::add),
+    SUBTRACT("-", 2, BigDecimal::subtract),
+    MULTIPLY("*", 1, BigDecimal::multiply),
+    DIVIDE("/", 1, (n1, n2) -> {
+        if (n2.doubleValue() == 0) {
+            throw new DividedByZeroException("0으로 나눌 수 없습니다.");
+        }
+        return n1.divide(n2, 30, RoundingMode.HALF_EVEN);
+    }),
+    OPEN_PARENTHESES("(", 3, (n1, n2) -> BigDecimal.valueOf(0));
+
+    private static final Map<String, Operator> OPERATOR_MAP =
+            Collections.unmodifiableMap(Arrays.stream(values())
+                    .collect(Collectors.toMap(Operator::getSymbol, Function.identity())));
 
     private final String symbol;
     private final int priority;
     private final BiFunction<BigDecimal, BigDecimal, BigDecimal> function;
+
 
     Operator(String symbol, int priority, BiFunction<BigDecimal, BigDecimal, BigDecimal> function) {
         this.symbol = symbol;
@@ -26,15 +40,14 @@ public enum Operator {
     }
 
     public static boolean isOperator(String symbol) {
-        return Arrays.stream(Operator.values())
-                .anyMatch(operator -> operator.symbol.equals(symbol));
+        return OPERATOR_MAP.containsKey(symbol);
     }
 
     public static Operator getOperation(String symbol) {
-        return Arrays.stream(Operator.values())
-                .filter(operator -> operator.symbol.equals(symbol))
-                .findFirst()
-                .orElseThrow(() -> new WrongOperationException("잘못된 연산자가 들어왔습니다." + symbol));
+        if (OPERATOR_MAP.containsKey(symbol))
+            return OPERATOR_MAP.get(symbol);
+
+        throw new WrongOperationException("잘모된 연산자가 들어왔습니다." + symbol);
     }
 
     public int compareTo(int priority) {
@@ -54,9 +67,6 @@ public enum Operator {
     }
 
     public BigDecimal operation(BigDecimal n1, BigDecimal n2) {
-        if(this == DIVIDE && n2.doubleValue() == 0) {
-            throw new DividedByZeroException("0으로 나눌 수 없습니다.");
-        }
         return function.apply(n1, n2);
     }
 
