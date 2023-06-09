bba7d3bef6c0c5c2cd745268d986e143194759a1
juno-junho <ssmm0205@naver.com>
2023-06-10 05:42:05 +0900

    feat(view): BufferedReaderInputView 생성 및 적용

--- java-calculator/src/main/java/com/programmers/junho/Application.java
+++ java-calculator/src/main/java/com/programmers/junho/Application.java
@@ -1,14 +1,11 @@
 package com.programmers.junho;
 
 import com.programmers.junho.controller.CalculatorController;
-import com.programmers.junho.view.InputView;
-import com.programmers.junho.view.OutputView;
-import com.programmers.junho.view.PrintStreamOutputView;
-import com.programmers.junho.view.ScannerInputView;
+import com.programmers.junho.view.*;
 
 public class Application {
     public static void main(String[] args) {
-        InputView inputView = new ScannerInputView();
+        InputView inputView = new BufferedReaderInputView();
         OutputView outputView = new PrintStreamOutputView();
 
         CalculatorController calculator = new CalculatorController(inputView, outputView);
