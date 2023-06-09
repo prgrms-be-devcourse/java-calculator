b0839b65fe486dce2889891fbbed3e1a3445ed45
Younghoon Kim <the_basic_@naver.com>
2023-06-09 18:01:24 +0900

    refactor: 실행 메인부 클래스명 변경

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,8 @@
 import display.Display;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
 
-public class Main {
+public class App {
     public static void main(String[] args) throws IOException {
         Display display = new Display();
         display.run();
