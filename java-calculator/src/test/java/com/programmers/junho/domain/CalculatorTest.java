cab396ba1b1e37a6ec0f8012794c2da2fd51d4c4
juno-junho <ssmm0205@naver.com>
2023-06-10 03:04:35 +0900

    test(domain): 식에서 계산 결과 테스트 작성

---
+++ java-calculator/src/test/java/com/programmers/junho/domain/CalculatorTest.java
@@ -0,0 +1,21 @@
+package com.programmers.junho.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CalculatorTest {
+
+    @DisplayName("계산 결과 테스트")
+    @ParameterizedTest(name ="식: {0}, 결과: {1}")
+    @CsvSource(value = {"1 + 2:3", "1 + 2 * 3:7", "3 - 2 * 2:-1"}, delimiter = ':')
+    void name(String expression, int calculatedResult) {
+        Calculator calculator = new Calculator(expression);
+
+        int actual = calculator.calculate();
+
+        assertThat(actual).isEqualTo(calculatedResult);
+    }
+}
