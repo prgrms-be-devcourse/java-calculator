daaabb9199ed9478f5dd3432095d8089a76c1a81
juno-junho <ssmm0205@naver.com>
2023-06-09 20:14:19 +0900

    feat : controller를 실행하는 application 생성

---
+++ java-calculator/src/main/java/com/programmers/junho/Application.java
@@ -0,0 +1,10 @@
+package com.programmers.junho;
+
+import com.programmers.junho.controller.CalculatorController;
+
+public class Application {
+    public static void main(String[] args) {
+        CalculatorController calculator = new CalculatorController();
+        calculator.run();
+    }
+}
