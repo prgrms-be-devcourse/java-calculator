af32682b07f6e0f727bb21a26b4b11bf7ffb27f3
ymkim97 <rladudaud97@naver.com>
2023-06-10 03:06:32 +0900

    선택 메뉴 출력 기능

--- src/main/java/com/devcourse/java/calculator/Console.java
+++ src/main/java/com/devcourse/java/calculator/Console.java
@@ -4,15 +4,30 @@ import com.devcourse.java.calculator.constant.MenuConstant;
 import com.devcourse.java.calculator.io.Input;
 import com.devcourse.java.calculator.io.Output;
 
+import java.util.Scanner;
+
 public class Console implements Input, Output {
+
+    private final Scanner scanner = new Scanner(System.in);
 
     @Override
     public void printCommandMenu() {
+        System.out.println(MenuConstant.PRINT_COMMAND_FIRST_HISTORY);
+        System.out.println(MenuConstant.PRINT_COMMAND_SECOND_CALCULATE);
+        System.out.println(MenuConstant.PRINT_COMMAND_THIRD_EXIT);
+        System.out.println();
         System.out.print(MenuConstant.REQUEST_COMMAND_INT);
     }
 
     @Override
     public int getCommand() {
+
+        try {
+            int command = scanner.nextInt();
+        } catch (RuntimeException e) {
+
+        }
+
         return 0;
     }
 }
