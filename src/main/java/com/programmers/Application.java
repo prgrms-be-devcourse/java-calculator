82960052ff16adc33d5dbf996463d3c15327daa1
weonest <geonhee33@naver.com>
2023-06-10 21:58:10 +0900

    refactor: Application 의존성 처리

--- src/main/java/com/programmers/Application.java
+++ src/main/java/com/programmers/Application.java
@@ -3,21 +3,33 @@ package com.programmers;
 import com.programmers.engine.Calculator;
 import com.programmers.engine.PostfixCalculator;
 import com.programmers.io.Console;
+import com.programmers.repository.CalculatorHistory;
+import com.programmers.validator.InputValidator;
 import com.programmers.validator.Validator;
 
 public class Application {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
+        Calculator calculator = new Calculator(
+                consoleInstance(),
+                consoleInstance(),
+                postfixCalculatorInstance(),
+                calculatorHistoryInstance()
+        );
         calculator.run();
 
     }
 
-    public Console getConsole() {
+    private static Console consoleInstance() {
         return new Console();
     }
 
-    public PostfixCalculator getPostfixCalculator() {
+    private static PostfixCalculator postfixCalculatorInstance() {
         return new PostfixCalculator();
+    }
+
+
+    private static CalculatorHistory calculatorHistoryInstance() {
+        return new CalculatorHistory();
     }
 
 }
