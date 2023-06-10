dd891534ba61acd96a8f065e827378781751f18e
heenahan <mby1997s@gmail.com>
2023-06-11 00:24:55 +0900

    feat : 유효하지 않은 입력값은 예외 던짐

--- app/src/main/java/com/programmers/expression/Expression.java
+++ app/src/main/java/com/programmers/expression/Expression.java
@@ -1,6 +1,5 @@
 package com.programmers.expression;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,12 +9,17 @@ public class Expression {
 	private List<String> expression;
 	
 	public Expression(String expression) {
+		isValid(expression);
 		this.expression = Arrays.stream(expression.split(" "))
 			                  .collect(Collectors.toList());
 	}
 	
 	public List<String> getExpression() {
 		return expression;
+	}
+	
+	public void isValid(String expression) {
+		if (!expression.matches("([0-9] [+|-|*|/] )+[0-9]")) throw new IllegalArgumentException();
 	}
 	
 	public String toString() {
