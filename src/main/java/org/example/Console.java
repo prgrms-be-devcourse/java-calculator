a23441b5c33eea80399f5c5bc08d14f1080f810a
Juwoong <wwong7247@gmail.com>
2023-06-10 23:00:30 +0900

    feat : 입출력을 BufferedReader로 변경
    * 추가적인 문제로 Scanner 사용 시, 모드 선택 값을 받고  연산입력값을 받을 떄, 입력 값을 받지 않고, 그냥 넘어감 
    * gradle-wrapper버전변경, BufferedReader 사용 동  이것저것 변경하여 문제 현상이 발생하지 않지만, 원인을 다시 확인해야함

--- src/main/java/org/example/Console.java
+++ src/main/java/org/example/Console.java
@@ -1,30 +1,27 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Console {
-
-    Scanner scanner = new Scanner(System.in);
-
-    public int  inputSelectMode(){
+    private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public int  inputSelectMode() throws IOException {
         System.out.println("1. 조회");
         System.out.println("2. 계산");
-        return  scanner.nextInt();
+        int mode = Integer.parseInt(br.readLine());
+        return mode;
     }
 
-    public String inputExpression(){
-        return scanner.nextLine();
+    public String inputExpression() throws IOException{
+        System.out.println("값을 입력하세요!");
+        String inputExpression = br.readLine();
+        return inputExpression;
     }
-
 
     public void outputSelectResult(int mode){
         System.out.printf("선택 : %d%n", mode);
     }
-
-
-
-
-
-
 
 }
