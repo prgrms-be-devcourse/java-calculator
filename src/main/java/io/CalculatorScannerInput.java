fb90a6dc1ea34d861ba94362cd0bea60e9911e35
beombu <beombu13@gmail.com>
2023-06-11 04:16:19 +0900

    Refactor : input에서 validation 분리

--- src/main/java/io/CalculatorScannerInput.java
+++ src/main/java/io/CalculatorScannerInput.java
@@ -1,38 +1,25 @@
 package io;
 
-import exception.ExceptionMessage;
+import validation.InputValidation;
+import validation.InputValidationImpl;
 
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class CalculatorScannerInput implements CalculatorInput{
-    private static final Pattern BUTTON_PATTERN = Pattern.compile("^[1-2]$");
-    private static final Pattern EXPRESSION_PATTERN = Pattern.compile("^[0-9]+(\\s[+\\-*/]\\s[0-9]+)+$");
-    private static Scanner sc = new Scanner(System.in);
+    private static final Scanner sc = new Scanner(System.in);
+    private static final InputValidation validation = new InputValidationImpl();
 
     @Override
     public int inputButton() {
         String input = sc.nextLine();
-        validateButton(input);
+        validation.validateButton(input);
         return Integer.parseInt(input);
-    }
-
-    private void validateButton(String input) {
-        if (!BUTTON_PATTERN.matcher(input).matches()) {
-            throw new IllegalArgumentException(ExceptionMessage.INPUT_BUTTON_NOT_RANGE.getMessage());
-        }
     }
 
     @Override
     public String inputExpression() {
-        String input = sc.nextLine();
-        validateExpression(input);
-        return input;
-    }
-
-    private void validateExpression(String input) {
-        if (!EXPRESSION_PATTERN.matcher(input).matches()) {
-            throw new IllegalArgumentException(ExceptionMessage.INVALID_NOT_EXPRESSION.getMessage());
-        }
+        String expression = sc.nextLine();
+        validation.validateExpression(expression);
+        return expression;
     }
 }
---
+++ src/main/java/validation/InputValidation.java
@@ -0,0 +1,6 @@
+package validation;
+
+public interface InputValidation {
+    boolean validateButton(String input);
+    boolean validateExpression(String expression);
+}
