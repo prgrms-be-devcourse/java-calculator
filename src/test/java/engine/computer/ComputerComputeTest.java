2d344f987ea43e6f275059bb31639b7e6bed8709
SY97P <onetuks@naver.com>
2023-06-10 20:02:40 +0900

    chore: gradle 한글 인코딩 설정

--- src/test/java/engine/computer/ComputerComputeTest.java
+++ src/test/java/engine/computer/ComputerComputeTest.java
@@ -1,8 +1,11 @@
 package engine.computer;
 
 import com.devcourse.engine.computer.Computer;
+import com.devcourse.engine.exception.InvalidInputException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.Callable;
 
 public class ComputerComputeTest {
 
@@ -18,6 +21,20 @@ public class ComputerComputeTest {
         Computer computer = new Computer();
         String expression = "1 +2 * 5- 8/4";
         Assertions.assertEquals(9.0, computer.compute(computer.convert(computer.validate(expression))));
+    }
+
+    @Test
+    void computeTest3() {
+        Computer computer = new Computer();
+        String expression = "1 +";
+        Assertions.assertThrows(InvalidInputException.class, () -> computer.compute(computer.convert(computer.validate(expression))));
+    }
+
+    @Test
+    void computeTest4() {
+        Computer computer =  new Computer();
+        String expression = "\n";
+        Assertions.assertThrows(InvalidInputException.class, () -> computer.compute(computer.convert(computer.validate(expression))));
     }
 
 }
