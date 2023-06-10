4468d89808c3d7b9658daa52312b52c43ae44a3b
byeolhaha <j35635aa@gmail.com>
2023-06-10 23:38:51 +0900

    다시 초기화

--- src/main/java/com/programmers/java/Main.java
+++ src/main/java/com/programmers/java/Main.java
@@ -9,30 +9,7 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-
-
-        System.out.println("1.조회");
-        System.out.println("2.계산");
-
-        System.out.print("선택:");
-        int option = Integer.parseInt(br.readLine());
-        if(option ==1){
-            SearchService searchService = new SearchService();
-            searchService.searchResult();
-        }else{
-            br = new BufferedReader(new InputStreamReader(System.in));
-            String input = br.readLine();
-
-            CalculateService calculateService = new CalculateService();
-
-            String answer = calculateService.calculator(input);
-
-            System.out.println(answer);
-        }
-
+        
     }
 
 }
