e54ea0cc5645f4503ebe57dc8132a379b27ec744
Yiseull <omjl5123@catholic.ac.kr>
2023-06-11 15:31:40 +0900

    test: 메뉴 선택 정상/예외 테스트 추가

---
+++ src/test/java/com/programmers/java/calculator/MenuSelectionTest.java
@@ -0,0 +1,30 @@
+package com.programmers.java.calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class MenuSelectionTest {
+
+    @Test
+    @DisplayName("메뉴 선택 정상 케이스")
+    void correctMenuSelection() {
+        MenuType selecedtMenuType1 = MenuType.of("1");
+        MenuType selecedtMenuType2 = MenuType.of("2");
+        MenuType selecedtMenuType3 = MenuType.of("3");
+
+        assertThat(selecedtMenuType1).isEqualTo(MenuType.HISTORY);
+        assertThat(selecedtMenuType2).isEqualTo(MenuType.CALCULATE);
+        assertThat(selecedtMenuType3).isEqualTo(MenuType.END);
+    }
+
+    @Test
+    @DisplayName("메뉴 선택 예외 케이스")
+    void wrongMenuSelection() {
+        assertThatThrownBy(() -> MenuType.of("4"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("메뉴를 찾을 수 없습니다.");
+    }
+}
