8c7203bb87558edef6839e8ac1e43c6eb87805df
kylekim2123 <kyle2123@naver.com>
2023-06-12 03:03:56 +0900

    refactor: try-catch를 MainController에서 App으로 이동 - 단순 예외 문구 프린트 용이므로 최상단 App에서 예외처리 - 정상과 예외 상황 모두 종료 메시지 출력 - MainController의 인덴트 깊이가 2에서 1로 감소

--- app/src/main/java/calculator/App.java
+++ app/src/main/java/calculator/App.java
@@ -1,10 +1,19 @@
 package calculator;
 
+import static calculator.view.OutputView.printWithLineBreak;
+import static calculator.view.OutputView.showQuitMessage;
+
 import calculator.controller.MainController;
 
 public class App {
 
     public static void main(String[] args) {
-        new MainController().run();
+        try {
+            new MainController().run();
+        } catch (Exception e) {
+            printWithLineBreak(e.getMessage());
+        } finally {
+            showQuitMessage();
+        }
     }
 }
--- app/src/main/java/calculator/controller/MainController.java
+++ app/src/main/java/calculator/controller/MainController.java
@@ -3,8 +3,6 @@ package calculator.controller;
 import static calculator.entity.Menu.getSelectedMenu;
 import static calculator.entity.Menu.isQuit;
 import static calculator.view.InputView.inputMenuNumber;
-import static calculator.view.OutputView.printWithLineBreak;
-import static calculator.view.OutputView.showQuitMessage;
 
 import calculator.entity.Menu;
 import java.util.Optional;
@@ -15,14 +13,8 @@ public class MainController {
         Optional<Menu> selectedMenu = Optional.empty();
 
         while (!isQuit(selectedMenu)) {
-            try {
-                int menuNumber = inputMenuNumber();
-                selectedMenu = getSelectedMenu(menuNumber);
-            } catch (Exception e) {
-                printWithLineBreak(e.getMessage());
-            }
+            int menuNumber = inputMenuNumber();
+            selectedMenu = getSelectedMenu(menuNumber);
         }
-
-        showQuitMessage();
     }
 }
