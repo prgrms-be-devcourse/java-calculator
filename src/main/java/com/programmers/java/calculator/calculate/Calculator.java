c95dd3fcce72e7eabacca8f327808c76b80068e2
byeolhaha <j35635aa@gmail.com>
2023-06-12 02:09:00 +0900

    feat : 우선순위 고려 없이 계산기능 구현

--- src/main/java/com/programmers/java/calculator/calculate/Calculator.java
+++ src/main/java/com/programmers/java/calculator/calculate/Calculator.java
@@ -1,11 +1,57 @@
 package com.programmers.java.calculator.calculate;
 
+import com.programmers.java.repository.ResultRepository;
+import com.programmers.java.util.ExpressionTokenizer;
+
+import java.util.List;
+import java.util.Stack;
+
 public class Calculator {
 
-    // 후휘표기식으로 바꾸는 객채를 호출하고
+
+    private static ResultRepository resultRepository = new ResultRepository();
+    private static ExpressionTokenizer expressionTokenizer = new ExpressionTokenizer();
+
 
     //계산의 역할을 한다.
+    public double getResult(String expression) {
 
 
+        List<String> tokenList = expressionTokenizer.expressionSplit(expression);
 
+        Stack<Double> tempStack = new Stack<>();
+        int index = 0;
+
+        while (index < tokenList.size()) {
+            String token = tokenList.get(index++);
+            switch (token) {
+                case "+": {
+                    int nextOperand = Integer.parseInt(tokenList.get(index++));
+                    tempStack.add(tempStack.pop() + nextOperand);
+                    break;
+                }
+                case "-": {
+                    int nextOperand = Integer.parseInt(tokenList.get(index++));
+                    tempStack.add(tempStack.pop() - nextOperand);
+                    break;
+                }
+                case "/": {
+                    int nextOperand = Integer.parseInt(tokenList.get(index++));
+                    tempStack.add(tempStack.pop() / nextOperand);
+                    break;
+                }
+                case "*": {
+                    int nextOperand = Integer.parseInt(tokenList.get(index++));
+                    tempStack.add(tempStack.pop() * nextOperand);
+                    break;
+                }
+                default:
+                    tempStack.add(Double.parseDouble(token));
+            }
+        }
+
+        double result = tempStack.pop();
+        resultRepository.save(expression, Double.toString(result));
+        return result;
+    }
 }
--- src/main/java/com/programmers/java/calculator/calculate/PostfixGenerator.java
+++
@@ -1,8 +0,0 @@
-package com.programmers.java.calculator.calculate;
-
-public class PostfixGenerator {
-
-    //후위표기식으로 바꾸는 역할을 담당한다.
-    //Calculotor에게 후위표기식을 전달한다.
-
-}
