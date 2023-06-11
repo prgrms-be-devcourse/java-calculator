45abcdcf4eb0bc1563d8c92687884fc18a2399ec
Younghoon Kim <the_basic_@naver.com>
2023-06-11 17:46:00 +0900

    feat: Validator 기능 추가

--- src/main/java/validation/Validator.java
+++ src/main/java/validation/Validator.java
@@ -1,11 +1,18 @@
 package validation;
 
+import java.util.Stack;
+
 public class Validator {
     private static final String numberPattern = "^[0-9]";
     private static final String operatorPattern = "^[-+*/]";
+    private static final String exprSequence = "^([0-9][-+*/])+[0-9]$";
 
     public static boolean isNumber(String getString) {
         return getString.matches(numberPattern);
+    }
+
+    public static boolean isNotNumber(String getString) {
+        return !isNumber(getString);
     }
 
     public static boolean isOperator(String getString) {
@@ -18,5 +25,31 @@ public class Validator {
 
     public static boolean isCloseBrackets(String getString) {
         return getString.equals(")");
+    }
+
+    public static boolean checkBrackets(String getExpr) {
+        Stack<String> bracketStack = new Stack<>();
+        String[] exprArray = getExpr.split("");
+
+        for (String elem : exprArray) {
+            if (isOpenBrackets(elem)) {
+                bracketStack.push(elem);
+            } else if (isCloseBrackets(elem)) {
+                if (bracketStack.isEmpty()) {
+                    return false;
+                }
+                bracketStack.pop();
+            }
+        }
+        return bracketStack.isEmpty();
+    }
+
+    public static boolean exprSequenceCheck(String getExpr) {
+        getExpr = getExpr.replace("(", "").replace(")", "");
+        return getExpr.matches(exprSequence);
+    }
+
+    public static boolean isRightExpr(String getExpr) {
+        return checkBrackets(getExpr) && exprSequenceCheck(getExpr);
     }
 }
