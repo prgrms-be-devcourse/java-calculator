5437c7446f788a075510da25528b1836071d11a3
charlesuu <tiger0192004@gmail.com>
2023-06-11 16:29:25 +0900

    refactor: Calculator 클래스 싱글턴으로 변경

--- src/main/java/org/example/domain/Calculator.java
+++ src/main/java/org/example/domain/Calculator.java
@@ -1,10 +1,24 @@
 package org.example.domain;
 
 public class Calculator {
+    private static Calculator instance;
 
-    private ExpressionConvertor expressionConvertor = new ExpressionConvertor();
-    private Processor processor = new Processor();
-    private Memory memory = new Memory();
+    private ExpressionConvertor expressionConvertor;
+    private Processor processor;
+    private Memory memory;
+
+    private Calculator() {
+        this.expressionConvertor = new ExpressionConvertor();
+        this.processor = new Processor();
+        this.memory = new Memory();
+    }
+
+    public static Calculator getInstance() {
+        if (instance == null) {
+            instance = new Calculator();
+        }
+        return instance;
+    }
 
     public Integer run(String expression) {
         Integer result = processor.calculate(expressionConvertor.convertToPostfix(expression));
