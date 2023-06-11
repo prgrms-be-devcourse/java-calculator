146fb4e5c5bc1951d02bd4c9ae9599df84821dfd
hi-june <wnsgur735@gmail.com>
2023-06-11 17:50:31 +0900

    feat: 사용자의 입력을 처리하는 기능

--- app/src/main/java/com/programmers/io/IOConsole.java
+++ app/src/main/java/com/programmers/io/IOConsole.java
@@ -8,6 +8,7 @@ public class IOConsole {
     // 사용자에게 보여주는 메뉴 메시지
     private static final String HISTORY_OPTION_MESSAGE = "1. 조회";
     private static final String CALCULATE_OPTION_MESSAGE = "2. 계산";
+    private static final String USER_CHOICE_MESSAGE = "선택 : ";
     /**
      * 계산기의 선택 메뉴를 출력하는 함수
      */
@@ -15,5 +16,19 @@ public class IOConsole {
         System.out.println(HISTORY_OPTION_MESSAGE);
         System.out.println(CALCULATE_OPTION_MESSAGE);
         System.out.println();
+    }
+
+    /**
+     * 사용자의 선택을 받아 반환하는 함수
+     *
+     * @return
+     * @throws IOException
+     */
+    public String getOption() throws IOException {
+        System.out.print(USER_CHOICE_MESSAGE);
+        String expression = br.readLine();
+        System.out.println();
+
+        return expression;
     }
 }
