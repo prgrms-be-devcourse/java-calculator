8d55ffcc110cc27668dad1a88db5d1b33f90d7dc
pushedrumex <pushedrumex@naver.com>
2023-06-11 15:11:40 +0900

    refactor: Console -> ConsoleInput, ConsoleOutput 분리

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,10 @@
+import io.Input;
+import io.Output;
+
 public class Application {
     public static void main(String[] args) {
-        Console console = new Console();
-        new Calculator(console, console).run();
+        Output output = new ConsoleOutput();
+        Input input = new ConsoleInput();
+        new Calculator(input, output).run();
     }
 }
--- src/main/java/ConsoleOutput.java
+++ src/main/java/ConsoleOutput.java
@@ -1,26 +1,10 @@
-import io.Input;
 import io.Output;
 import model.Result;
 
 import java.util.List;
-import java.util.Scanner;
 import java.util.stream.Collectors;
 
-public class Console implements Input, Output {
-
-    private final Scanner scanner = new Scanner(System.in);
-
-    @Override
-    public String read() {
-        return scanner.nextLine();
-    }
-
-    @Override
-    public String readWithPrompt(String prompt) {
-        System.out.print(prompt);
-        return scanner.nextLine();
-    }
-
+public class ConsoleOutput implements Output {
     @Override
     public void inputError(String message) {
         System.out.println("\n" + message + "\n");
@@ -30,7 +14,7 @@ public class Console implements Input, Output {
     public void results(List<Result> results) {
         String output = results.stream()
                 .map(result -> result.getProblem() + " = " + result.getAnswer())
-                        .collect(Collectors.joining("\n"));
+                .collect(Collectors.joining("\n"));
         System.out.println("\n" + output + "\n");
     }
 
