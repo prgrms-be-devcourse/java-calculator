1707ed34510d46afa374e319e0830003346f277a
박은지 <qkrdmswl1018@naver.com>
2023-06-11 13:17:33 +0900

    test: 테스트 코드 수정

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,37 +1,36 @@
 
-import org.assertj.core.api.Assertions;
 import org.example.calculator.Operator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
-public class CalculatorTest {
-
+class CalculatorTest {
   @Test
   @DisplayName("더하기")
   void plus() {
-    Assertions.assertThat(Operator.PLUS.calculate(6,2)).isEqualTo(8);
-    Assertions.assertThat(Operator.PLUS.calculate(6,2)).isEqualTo(9);
+    assertThat(Operator.PLUS.calculate(6, 2)).isEqualTo(8);
+    assertThat(Operator.PLUS.calculate(6, 2)).isEqualTo(9);
   }
 
   @Test
   @DisplayName("빼기")
   void minus() {
-    Assertions.assertThat(Operator.MINUS.calculate(6,2)).isEqualTo(4);
-    Assertions.assertThat(Operator.MINUS.calculate(6,2)).isEqualTo(5);
+    assertThat(Operator.MINUS.calculate(6, 2)).isEqualTo(4);
+    assertThat(Operator.MINUS.calculate(6, 2)).isEqualTo(5);
   }
 
   @Test
   @DisplayName("곱하기")
   void multiply() {
-    Assertions.assertThat(Operator.MULTI.calculate(6,2)).isEqualTo(12);
-    Assertions.assertThat(Operator.MULTI.calculate(6,2)).isEqualTo(13);
+    assertThat(Operator.MULTI.calculate(6, 2)).isEqualTo(12);
+    assertThat(Operator.MULTI.calculate(6, 2)).isEqualTo(13);
   }
 
   @Test
   @DisplayName("나누기")
   void divide() {
-    Assertions.assertThat(Operator.DIV.calculate(6,2)).isEqualTo(3);
-    Assertions.assertThat(Operator.DIV.calculate(6,2)).isEqualTo(4);
+    assertThat(Operator.DIV.calculate(6, 2)).isEqualTo(3);
+    assertThat(Operator.DIV.calculate(6, 2)).isEqualTo(4);
   }
 }
