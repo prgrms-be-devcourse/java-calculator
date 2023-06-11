5a6deed857818821dc5f64fe64678eba05d9cb6f
SW-H <hsw08@naver.com>
2023-06-12 06:00:37 +0900

    feat : 예외 처리 문구

---
+++ src/main/java/co/programmers/exception/ExceptionMessage.java
@@ -0,0 +1,11 @@
+package co.programmers.exception;
+
+public class ExceptionMessage {
+
+    public static final String DIVIDED_BY_ZERO = "0으로 나눌 수 없습니다.";
+    public static final String INVALID_SYMBOL = "잘못된 연산 기호입니다.";
+    public static final String INVALID_INPUT = "잘못된 입력입니다";
+
+    private ExceptionMessage() {
+    }
+}
