df8112bad47a1b61f3f4acb8d70ca6048e3e5cf6
hi-june <wnsgur735@gmail.com>
2023-06-11 17:57:38 +0900

    feat: 사용자가 입력한 표현식을 저장하는 기능

--- app/src/main/java/com/programmers/memory/HistoryMemory.java
+++ app/src/main/java/com/programmers/memory/HistoryMemory.java
@@ -10,4 +10,15 @@ public class HistoryMemory {
         this.historyMemoryMap = new HashMap<>();
         this.orderNumber = 0;
     }
+
+    /**
+     * 사용자가 입력한 중위 표현식을 저장하는 메소드
+     *
+     * @param inputExpression 사용자가 입력한 중위 표현식
+     * @param result 계산 결과
+     */
+    public void saveHistory(String inputExpression, double result) {
+        String formatExpression = String.format("%s = %.2f", inputExpression, result);
+        historyMemoryMap.put(++orderNumber, formatExpression);
+    }
 }
