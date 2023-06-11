5c1d91add4d2354358cb814164e1ff8019243b72
charlesuu <tiger0192004@gmail.com>
2023-06-11 18:52:37 +0900

    feat: main메서드 구현

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,17 @@
 package org.example;
 
+import org.example.controller.CalculatorSystem;
 import org.example.domain.Calculator;
-
-import java.util.*;
+import org.example.view.CalculatorView;
 
 public class Main {
     public static void main(String[] args) {
-        Calculator calc = Calculator.getInstance();
-        System.out.println(calc.run("(50+50)*15"));
-        System.out.println(calc.run("1+2*3+(4+2)/2"));
+        CalculatorSystem calculatorSystem = new CalculatorSystem();
+
+        try {
+            calculatorSystem.run();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
