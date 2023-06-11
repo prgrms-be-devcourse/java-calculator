411b5628ddb77b62bc884fe2dd482489d45f1c07
ysng_ysng <yeasung67@gmail.com>
2023-06-11 17:20:00 +0900

    fix: 우선순위 연산 버그 픽스
    우선순위를 비교하는 부분에서 동등 조건을 넣지 않아 버그가 발생해서 고쳤습니다.

--- src/main/java/model/Calculator.java
+++ src/main/java/model/Calculator.java
@@ -26,7 +26,7 @@ public class Calculator {
                 OperatorType currentOperator = OperatorType.getOperator(String.valueOf(component));
                 OperatorType peekOperator = operator.peekOperator();
 
-                if (peekOperator.getPrecedence() > currentOperator.getPrecedence()) {
+                if (peekOperator.getPrecedence() >= currentOperator.getPrecedence()) {
                     int num2 = operand.pop();
                     int num1 = operand.pop();
                     int result = peekOperator.applyCalculate(num1, num2);
@@ -49,10 +49,4 @@ public class Calculator {
     }
 }
 
-// 연산자면 에러와 예외의 차이
-// 에러는 메모리 부족,
-// 터지거나 예상치 못한 것
-// 예외는 비즈니스 서비스에서 지원하지 않는 값이 들어온것,
-// 예기치 못한 이상한 ? -> 프로그램 수행에 영향
-// 비즈니스 로직에 관한 예외
-// ex) 너무 큰 숫자일 경우 -> 같은 예외처리 중복으로 했는지도 생각해보기
+
