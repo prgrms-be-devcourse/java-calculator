4aabf7e5dfb9fc7b862679e3349311e2c56e57c0
박은지 <qkrdmswl1018@naver.com>
2023-06-11 13:36:25 +0900

    fix: 수식 게산 오류 수정

--- src/main/java/org/example/calculator/Operator.java
+++ src/main/java/org/example/calculator/Operator.java
@@ -4,9 +4,9 @@ import java.util.function.BiFunction;
 
 public enum Operator {
   PLUS("+", (num1, num2) -> num1 + num2),
-  MINUS("-", (num1, num2) -> num1 - num2),
+  MINUS("-", (num1, num2) -> num2 - num1),
   MULTI("*", (num1, num2) -> num1 * num2),
-  DIV("/", (num1, num2) -> num1 / num2);
+  DIV("/", (num1, num2) -> num2 / num1);
 
   private final String type;
   private final BiFunction<Double, Double, Double> formula;
