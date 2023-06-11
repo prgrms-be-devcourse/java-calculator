ff04d98506a364947446a7620ba60c1157396788
byeolhaha <j35635aa@gmail.com>
2023-06-11 15:06:10 +0900

    feat : 정규식을 이용해 연산자와 피연산자 분리 구현 환료

--- src/main/java/com/programmers/java/util/ExpressionTokenizer.java
+++ src/main/java/com/programmers/java/util/ExpressionTokenizer.java
@@ -1,8 +1,23 @@
 package com.programmers.java.util;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ExpressionTokenizer {
 
+    private static final String REGEXP = "\\d+|[-+*/]";
     // 연산자와 피연사자를 정규식을 이용해서 분리하는 역할을 담당한다.
+    public List<String> expressionSplit(String expression){
 
+        List<String> result = new ArrayList<>();
+        Pattern pattern = Pattern.compile(REGEXP);
+        Matcher matcher = pattern.matcher(expression);
 
+        while(matcher.find()){
+            result.add(matcher.group());
+        }
+        return result;
+    }
 }
