3ae07427e654833acb2e8861bef3904bcc67950a
ysng_ysng <yeasung67@gmail.com>
2023-06-12 16:04:15 +0900

    refactor: Calculator 리팩토링 : 인덴트 줄이기, 변수명 변경
    인덴트를 줄이기 위해 메소드 추출을 해봤습니다. 처음에는 switch를 통해 가독성을 향상 시킬까 생각해봤는데, 그렇게 될 경우
    새로운 클래스가 필요하게 되어 일단은 메소드추출로 바꿨습니다. 그리고 변수명을 의미있게 바꿔보았습니다.

--- src/main/java/model/Calculator.java
+++ src/main/java/model/Calculator.java
@@ -8,26 +8,35 @@ public class Calculator {
     }
 
     public String calculate(String expression) throws RuntimeException {
-        for (String expComponent : expression.split("")) {
-            if (Character.isDigit(expComponent.charAt(0))) {
-                operand.push(Integer.parseInt(expComponent));
+        for (String expressionComponent : expression.split("")) {
+            if (Character.isDigit(expressionComponent.charAt(0))) {
+                operand.push(Integer.parseInt(expressionComponent));
                 continue;
             }
-
-            Operator currentOperator = Operator.getOperator(expComponent);
-            if (!Operator.isEmpty()) {
-                Operator peekOperator = Operator.peek();
-                if (peekOperator.getPrecedence() >= currentOperator.getPrecedence()) {
-                    int rightNumber = operand.pop();
-                    int leftNumber = operand.pop();
-                    int result = peekOperator.applyCalculate(leftNumber, rightNumber);
-                    operand.push(result);
-                    Operator.pop();
-                }
-            }
-            Operator.push(currentOperator);
+            calculateIfOperatorNotEmpty(expressionComponent);
         }
+        return makeFinalCalculation();
+    }
 
+    private void calculateIfOperatorNotEmpty(String expressionComponent) {
+        Operator currentOperator = Operator.getOperator(expressionComponent);
+        if (!Operator.isEmpty()) {
+            calculateByPrecedence(currentOperator);
+        }
+        Operator.push(currentOperator);
+    }
+
+    private void calculateByPrecedence(Operator currentOperator) {
+        Operator peekOperator = Operator.peek();
+        if (peekOperator.getPrecedence() >= currentOperator.getPrecedence()) {
+            int rightNumber = operand.pop();
+            int leftNumber = operand.pop();
+            operand.push(peekOperator.applyCalculate(leftNumber, rightNumber));
+            Operator.pop();
+        }
+    }
+
+    private String makeFinalCalculation() {
         Operator currentOperator = Operator.pop();
         int rightNumber = operand.pop();
         int leftNumber = operand.pop();
