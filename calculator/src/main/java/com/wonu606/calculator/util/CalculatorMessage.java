8a836099bc30086968c6aa9cbe7af3464ecf2330
wonu606 <wonu606@gmail.com>
2023-06-13 01:50:38 +0900

    feat: 연산자 구현

--- calculator/src/main/java/com/wonu606/calculator/util/CalculatorMessage.java
+++ calculator/src/main/java/com/wonu606/calculator/util/CalculatorMessage.java
@@ -2,7 +2,8 @@ package com.wonu606.calculator.util;
 
 public enum CalculatorMessage {
     INVALID_ORDER("잘못된 순번입니다."),
-    INVALID_INPUT("잘못된 입력입니다.");
+    INVALID_INPUT("잘못된 입력입니다."),
+    NOT_DIVISIBLE_BY_ZERO("0으로 나눌 수 없습니다.");
 
     public final String message;
 
