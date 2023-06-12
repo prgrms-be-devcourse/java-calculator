b6b8c00c562c368fdec36afad1ecde31f6867673
Sey0 <onetuks@naver.com>
2023-06-12 18:48:33 +0900

    refactor: Calculator 클래스 들여쓰기 조절

--- src/main/java/com/devcourse/engine/Calculator.java
+++ src/main/java/com/devcourse/engine/Calculator.java
@@ -3,10 +3,10 @@ package com.devcourse.engine;
 import com.devcourse.engine.computer.Computer;
 import com.devcourse.engine.io.Input;
 import com.devcourse.engine.io.Output;
-import com.devcourse.engine.model.Menu;
 import com.devcourse.engine.exception.InvalidInputException;
 import com.devcourse.engine.historian.Historian;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -15,10 +15,10 @@ import static com.devcourse.engine.exception.InvalidInputException.NO_HISTORY;
 
 public class Calculator implements Runnable {
 
-    private Computer computer;
-    private Historian historian;
-    private Input input;
-    private Output output;
+    private final Computer computer;
+    private final Historian historian;
+    private final Input input;
+    private final Output output;
 
     public Calculator(
             Computer computer,
@@ -33,43 +33,46 @@ public class Calculator implements Runnable {
 
     @Override
     public void run() {
+        label:
         while (true) {
             String menu = input.inputMenu();
 
             try {
+                validMenuCheck(menu);
 
-                if (menu.length() != 1)
-                    throw new InvalidInputException("올바른 메뉴를 선택해주세요.");
-
-                if (menu.equals(Menu.EXIT.getMenuOrdinal())) {
-
-                    output.endGame();
-                    break;
-
-                } else if (menu.equals(Menu.HISTORY.getMenuOrdinal())) {
-
-                    if (historian.getLastIndex() < 1) {
-                        output.printError("표시할 이력이 없습니다.");
-                        continue;
-                    }
-
-                    IntStream.rangeClosed(1, historian.getLastIndex())
-                            .forEach(i -> output.showHistory(historian.getHistory(i)));
-                    output.showHistory("");
-
-                } else if (menu.equals(Menu.COMPUTE.getMenuOrdinal())) {
-
-                    String userInput = input.inputExpression();
-                    List<String> infixExpression = computer.validate(userInput);
-                    List<String> postfixExpression = computer.convert(infixExpression);
-                    double result = computer.compute(postfixExpression);
-
-                    historian.saveHistory(infixExpression, result);
-                    output.showResult(result);
-
-                } else {
-                    throw new InvalidInputException("올바른 메뉴를 선택해주세요.");
+                switch (menu) {
+                    case "0":
+                        output.endGame();
+                        break label;
+                    case "1":
+                        checkHasHistory();
+                        printHistory();
+                        break;
+                    case "2":
+                        calculate();
+                        break;
                 }
+            } catch (InvalidInputException e) {
+                output.printError(e.getMessage());
+            }
+        }
+    }
+
+    private void calculate() {
+        String userInput = input.inputExpression();
+        List<String> infixExpression = computer.validate(userInput);
+        List<String> postfixExpression = computer.convert(infixExpression);
+        double result = computer.compute(postfixExpression);
+
+        historian.saveHistory(infixExpression, result);
+        output.showResult(result);
+    }
+
+    private void printHistory() {
+        IntStream.rangeClosed(1, historian.getLastIndex())
+                .forEach(i -> output.showHistory(historian.getHistory(i)));
+        output.showHistory("");
+    }
 
     private void checkHasHistory() {
         if (historian.getLastIndex() < 1)
