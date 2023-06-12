d39771431b3b1c1c66b653c93e02a4bb1b667e50
hongxeob <sjun2918@naver.com>
2023-06-12 16:02:12 +0900

    feat : Calculator 클래스 구현 (계산 기능)

---
+++ src/main/java/com/programmers/model/Calculator.java
@@ -0,0 +1,37 @@
+package com.programmers.model;
+
+import com.programmers.util.Operators;
+import com.programmers.util.PostfixConverter;
+import com.programmers.util.StringUtil;
+
+import java.util.List;
+import java.util.Stack;
+
+public class Calculator {
+
+    private final String formula;
+
+    public Calculator(String formula) {
+        this.formula = formula;
+    }
+
+    public long calculate(String formula) throws IllegalArgumentException, ArithmeticException {
+        List<String> postfix = PostfixConverter.convert(formula);
+        return calculatePostfixFormula(postfix);
+    }
+
+    private long calculatePostfixFormula(List<String> formula) {
+        Stack<Long> stack = new Stack<>();
+        for (String numOrOperator : formula) {
+            if (StringUtil.isNumber(numOrOperator)) {
+                stack.push(StringUtil.convertStringToLong(numOrOperator));
+            } else {
+                Long num2 = stack.pop();
+                Long num1 = stack.pop();
+                stack.push(Operators.calculate(numOrOperator, num1, num2));
+            }
+        }
+        return stack.pop();
+    }
+
+}
