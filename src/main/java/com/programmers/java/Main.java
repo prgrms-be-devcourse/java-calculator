029fd5461bd7129c15835f11aae8824358022d87
byeolhaha <j35635aa@gmail.com>
2023-06-12 09:45:15 +0900

    feat: main메서드에서 계산기 콘솔 불러오기 구현

--- src/main/java/com/programmers/java/Main.java
+++ src/main/java/com/programmers/java/Main.java
@@ -1,9 +1,14 @@
 package com.programmers.java;
 
+import com.programmers.java.calculator.Controller;
+import com.programmers.java.calculator.calculate.Calculator;
+import com.programmers.java.calculator.search.Searcher;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-
-        //계산기 Controller 호출
+    public static void main(String[] args) throws IOException {
+        Controller controller = new Controller();
+        controller.console(new Calculator(), new Searcher());
     }
-
 }
