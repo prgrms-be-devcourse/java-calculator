ecf45dc37a84c9f378ed0da7db6bc118ddcfd58c
hongxeob <sjun2918@naver.com>
2023-06-12 13:56:51 +0900

    feat : Priority 클래스 구현 (우선 순위)

---
+++ src/main/java/com/programmers/util/Priority.java
@@ -0,0 +1,26 @@
+package com.programmers.util;
+
+import com.programmers.error.exception.InvalidOperatorInputException;
+
+import java.util.Arrays;
+
+public enum Priority {
+    HIGH(2, new String[]{"/", "*"}), LOW(1, new String[]{"+", "-"});
+
+    private final int priority;
+    private final String[] operators;
+
+    Priority(int priority, String[] operators) {
+        this.priority = priority;
+        this.operators = operators;
+    }
+
+    public static int getPriority(String operator) {
+        return Arrays.stream(Priority.values())
+                .filter(symbol -> Arrays.asList(symbol.operators).contains(operator))
+                .findFirst()
+                .orElseThrow(() -> new InvalidOperatorInputException("잘못된 수식입니다. +, -, *, / 연산만 가능합니다."))
+                .priority;
+    }
+}
+
