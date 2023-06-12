3c233b1aad2a4d7a7f3f714282c289406b7bbe90
byeolhaha <j35635aa@gmail.com>
2023-06-13 01:31:39 +0900

    fix : .idea 폴더 제거

--- src/test/java/com/programmers/java/calculator/calculate/CalculatorTest.java
+++ src/test/java/com/programmers/java/calculator/calculate/CalculatorTest.java
@@ -1,4 +1,15 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.programmers.java.calculator.calculate;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class CalculatorTest {
-  
+
+    @Test
+    void getResult() {
+        Calculator calculator = new Calculator();
+        double result = calculator.getResult("2+3");
+        assertThat(result).isEqualTo(5);
+    }
 }
--- src/test/java/com/programmers/java/view/InputTest.java
+++ src/test/java/com/programmers/java/view/InputTest.java
@@ -1,4 +1,27 @@
+package com.programmers.java.view;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.StringReader;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class InputTest {
-  
+
+    Input input = new Input();
+    @Test
+    void enterMenu() {
+        String input = "1";  // 여기에 테스트에 사용할 입력을 작성합니다.
+        BufferedReader reader = new BufferedReader(new StringReader(input));
+         // 테스트 대상 객체에 BufferedReader를 주입합니다.
+
+
+
+
+    }
+
+    @Test
+    void enterExpression() {
+    }
 }
