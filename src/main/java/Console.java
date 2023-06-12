6a1527252e1a21093fa5a6148434d3a57a2f98e8
jay-so <merry9504@naver.com>
2023-06-13 02:39:12 +0900

    fix: ConsoleMenu 추가

--- src/main/java/Console.java
+++ src/main/java/Console.java
@@ -1,10 +1,17 @@
 import calculator.engine.io.Input;
 import calculator.engine.io.Output;
 
+import java.util.Scanner;
+
 public class Console implements Input, Output {
+
+    private final Scanner scanner = new Scanner(System.in);
+
     @Override
     public String input(String s) {
-        return null;
+        ConsoleMenu();
+        System.out.println(s);
+        return scanner.nextLine();
     }
     @Override
     public String inputError() {
@@ -13,5 +20,10 @@ public class Console implements Input, Output {
     @Override
     public String outputError() {
         return "프로그램 오류가 발생하였습니다.";
+    }
+    public void ConsoleMenu() {
+        System.out.println("1.조회");
+        System.out.println("2.계산");
+        System.out.println("선택: ");
     }
 }
