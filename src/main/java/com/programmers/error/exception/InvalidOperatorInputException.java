0caa0d2728a8cb126d1b11f0be8645b758e08172
hongxeob <sjun2918@naver.com>
2023-06-12 16:32:00 +0900

    chore : 커스텀 예외 패키지 및 클래스 삭제

--- src/main/java/com/programmers/error/exception/InvalidOperatorInputException.java
+++
@@ -1,8 +0,0 @@
-package com.programmers.error.exception;
-
-public class InvalidOperatorInputException extends RuntimeException {
-    public InvalidOperatorInputException(String msg) {
-        super(msg);
-
-    }
-}
