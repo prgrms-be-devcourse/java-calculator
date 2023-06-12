e55608fc539c8ca34fbd612667838fbf4731f5ac
byeolhaha <j35635aa@gmail.com>
2023-06-13 00:03:39 +0900

    add : Output에계산기 시작, 종료 출력 메서드 추가

--- src/main/java/com/programmers/java/view/Output.java
+++ src/main/java/com/programmers/java/view/Output.java
@@ -1,14 +1,23 @@
 package com.programmers.java.view;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class Output {
+
+    public void viewStartConsole() {
+        System.out.println("1.조회");
+        System.out.println("2.계산");
+    }
+
+    public void viewEndConsole() {
+        System.out.println("계산기를 종료합니다.");
+    }
 
     public void viewCalculateResult(double result) {
         System.out.println(result);
     }
 
-    public void viewSearchResult(ArrayList<String> calcList) {
+    public void viewSearchResult(List<String> calcList) {
         for (String calc : calcList) {
             System.out.println(calc);
         }
