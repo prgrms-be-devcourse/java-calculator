c38e9bbe011823c8e7c5c4363b412cdaece8e685
byeolhaha <j35635aa@gmail.com>
2023-06-12 22:58:35 +0900

    fix: 정규식 상수명 수정 및 Controller,Searcher 삭제

--- src/main/java/com/programmers/java/util/ExpressionTokenizer.java
+++ src/main/java/com/programmers/java/util/ExpressionTokenizer.java
@@ -7,13 +7,12 @@ import java.util.regex.Pattern;
 
 public class ExpressionTokenizer {
 
-    private static final String REGEXP = "\\d+|[-+*/]";
+    private static final String OPERATOR_REGEXP = "\\d+|[-+*/]";
 
-    // 연산자와 피연사자를 정규식을 이용해서 분리하는 역할을 담당한다.
     public List<String> expressionSplit(String expression) {
 
         List<String> result = new ArrayList<>();
-        Pattern pattern = Pattern.compile(REGEXP);
+        Pattern pattern = Pattern.compile(OPERATOR_REGEXP);
         Matcher matcher = pattern.matcher(expression);
 
         while (matcher.find()) {
