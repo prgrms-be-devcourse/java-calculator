61d550a93faeb072dfe6c43b06a8a084b8aec864
beombu <beombu13@gmail.com>
2023-06-12 17:31:45 +0900

    style : typo

--- src/main/java/io/CalculatorPrintOutput.java
+++ src/main/java/io/CalculatorPrintOutput.java
@@ -1,6 +1,6 @@
 package io;
 
-public class CalculatorPrintOutput implements CalculatorOutput{
+public class CalculatorPrintOutput implements CalculatorOutput {
     private static final String BUTTON_INIT_MESSAGE = "1. 조회\n2. 계산\n\n선택 : ";
     private static final String RECORD_MESSAGE = "%s = %d";
 
