0af079fe195ad566d0e1bc085d33f5c874407b28
byeolhaha <j35635aa@gmail.com>
2023-06-13 00:06:18 +0900

    fix : Input에 존재했던 Output과 Button 책임 분리

--- src/main/java/com/programmers/java/view/Input.java
+++ src/main/java/com/programmers/java/view/Input.java
@@ -11,23 +11,9 @@ public class Input {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
+        Button selected = Button.findByValue(input);
 
-        try {
-            switch (input) {
-                case "1":
-                    button = Button.SEARCH;
-                    break;
-                case "2":
-                    button = Button.CALCULATE;
-                    break;
-                default:
-                    throw new IllegalArgumentException("올바른 입력이 아닙니다.");
-            }
-        } catch (Exception e) {
-            System.out.println("계산기를 종료합니다.");
-            System.exit(0);
-        }
-        return button;
+        return selected;
     }
 
     public String enterExpression() throws IOException {
