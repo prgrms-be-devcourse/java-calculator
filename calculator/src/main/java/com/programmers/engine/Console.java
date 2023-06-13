13794566900ccfdc9d8e56e8d056e54675cdddcc
yumyeonghan <mgtmh991013@gmail.com>
2023-06-13 22:30:40 +0900

    feat: Console, 예외 발생 구현
    - calculationCommandValidation(), 계산식에 잘못된 문자가 있을 경우 예외 발생
    - selectionNumberValidation(), 1 또는 2 이외의 문자를 입력하면 예외 발생

--- calculator/src/main/java/com/programmers/engine/Console.java
+++ calculator/src/main/java/com/programmers/engine/Console.java
@@ -2,22 +2,27 @@ package com.programmers.engine;
 
 import com.programmers.engine.io.Input;
 import com.programmers.engine.io.Output;
+import com.programmers.engine.util.Confirmation;
 
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Console implements Input, Output {
     private final Scanner scanner = new Scanner(System.in);
 
     @Override
     public String input() {
-        return scanner.nextLine();
+        String calculationCommand = scanner.nextLine();
+        calculationCommandValidation(calculationCommand);
+        return calculationCommand;
     }
 
     @Override
     public String choose() {
         System.out.print("선택 : ");
         String selectionNumber = scanner.nextLine();
+        selectionNumberValidation(selectionNumber);
         System.out.println();
         return selectionNumber;
     }
@@ -39,5 +44,21 @@ public class Console implements Input, Output {
     public void printHistory(List<String> history) {
         history.forEach(System.out::println);
         System.out.println();
+    }
+
+    private static void calculationCommandValidation(String calculationCommand) {
+        if (Pattern.compile(" ")
+                .splitAsStream(calculationCommand)
+                .filter(e -> !(Confirmation.isOperator(e) || Confirmation.isOperand(e)))
+                .findAny()
+                .isPresent()) {
+            throw new IllegalArgumentException("잘못된 계산식 입니다.");
+        }
+    }
+
+    private static void selectionNumberValidation(String selectionNumber) {
+        if (!(selectionNumber.equals("1") || selectionNumber.equals("2"))) {
+            throw new IllegalArgumentException("1 또는 2 를 입력해 주세요");
+        }
     }
 }
