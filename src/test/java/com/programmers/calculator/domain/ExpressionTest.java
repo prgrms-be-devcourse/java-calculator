42a1743ac1c37d931bb44478237960db7d3584a5
kmebin <blowhuiee@gmail.com>
2023-06-14 01:12:59 +0900

    test: 수식 포맷팅 및 0으로 나누는 경우 예외 테스트

--- src/test/java/com/programmers/calculator/domain/ExpressionTest.java
+++ src/test/java/com/programmers/calculator/domain/ExpressionTest.java
@@ -9,14 +9,37 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ExpressionTest {
     @Test
+    @DisplayName("수식 포맷팅에 성공한다.")
+    void 수식_포맷팅() {
+        // given
+        String expression = "2 +   30/3 ";
+        // when
+        Expression result = new Expression(expression);
+        // then
+        assertThat(result.getExpression()).isEqualTo("2 + 30 / 3");
+    }
+
+    @Test
     @DisplayName("잘못된 수식인 경우 예외가 발생한다.")
     void 잘못된_수식_형식_예외() {
         // given
-        String expression = "2 + -8/";
+        String expression = "2 + - 8 / ";
         // when
         InvalidExpressionException e = assertThrows(InvalidExpressionException.class,
                 () -> new Expression(expression));
         // then
         assertThat(e.getMessage()).isEqualTo("올바르지 않은 수식입니다.");
+    }
+
+    @Test
+    @DisplayName("0으로 나누는 경우 예외가 발생한다.")
+    void 나누기_0_예외() {
+        // given
+        String expression = "2 + 8 / 0";
+        // when
+        InvalidExpressionException e = assertThrows(InvalidExpressionException.class,
+                () -> new Expression(expression));
+        // then
+        assertThat(e.getMessage()).isEqualTo("0으로 나눌 수 없습니다.");
     }
 }
