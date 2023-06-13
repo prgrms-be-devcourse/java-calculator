9f97efbb6377b27e215dc11a1c79afbebcf1dfea
kmebin <blowhuiee@gmail.com>
2023-06-13 17:27:05 +0900

    test: 후위 표기식 계산 테스트

---
+++ src/test/java/com/programmers/CalculatorTest.java
@@ -0,0 +1,27 @@
+package com.programmers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CalculatorTest {
+    Calculator calculator;
+
+    @BeforeEach
+    void beforeEach() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    @DisplayName("후위 표기식 계산")
+    void 후위_표기식_계산() {
+        // given
+        String postfixExpression = "324*+93/-";
+        // when
+        int result = calculator.calculate(postfixExpression);
+        // then
+        assertThat(result).isEqualTo(8);
+    }
+}
--- src/test/java/com/programmers/util/PostfixConverterTest.java
+++ src/test/java/com/programmers/util/PostfixConverterTest.java
@@ -8,8 +8,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class PostfixConverterTest {
     @Test
-    @DisplayName("후위 표기법으로 변환")
-    void 후위_표기법으로_변환() {
+    @DisplayName("후위 표기식으로 변환")
+    void 후위_표기식으로_변환() {
         // given
         String expression = "3 + 2*4 -9  /3";
         // when
