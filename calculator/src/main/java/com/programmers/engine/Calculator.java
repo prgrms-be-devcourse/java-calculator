d06255fb015d11b6b6254a964342f0cd21547e2f
yumyeonghan <mgtmh991013@gmail.com>
2023-06-13 13:26:38 +0900

    feat: Calculator 클래스 생성 및 run() 구현
    - 계산기 애플리케이션이 실제 실행(run)되는 클래스

---
+++ calculator/src/main/java/com/programmers/engine/Calculator.java
@@ -0,0 +1,34 @@
+package com.programmers.engine;
+
+import com.programmers.engine.io.Input;
+import com.programmers.engine.io.Output;
+import com.programmers.engine.model.Operation;
+import com.programmers.engine.model.Storage;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Calculator implements Runnable{
+    public static final String CALCULATION = "2";
+    public static final String HISTORY = "1";
+
+    private final Input input;
+    private final Output output;
+    private final Storage storage;
+
+    @Override
+    public void run() {
+            while(true) {
+                output.printStart();
+                String selectionResult = input.choose();
+                if (selectionResult.equals(CALCULATION)) {
+                    output.printResult(Operation.calculate(input.input()));
+                    continue;
+                }
+                if (selectionResult.equals(HISTORY)) {
+                    output.printHistory(storage.findHistory());
+                    continue;
+                }
+                break;
+            }
+    }
+}
---
+++ calculator/src/main/java/com/programmers/engine/model/Storage.java
@@ -0,0 +1,7 @@
+package com.programmers.engine.model;
+
+import java.util.List;
+
+public interface Storage {
+    List<String> findHistory();
+}
