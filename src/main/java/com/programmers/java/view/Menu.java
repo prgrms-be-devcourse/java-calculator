1f2c1ee018dbecc511f9e17f0bef9f9d46957b5d
byeolhaha <j35635aa@gmail.com>
2023-06-13 20:26:24 +0900

    fix : findByValue 메서드 리턴형 Optional로 변경 및 Output에서 Enum 활용 출력을 위해 toString() override

--- src/main/java/com/programmers/java/view/Menu.java
+++ src/main/java/com/programmers/java/view/Menu.java
@@ -3,22 +3,22 @@ package com.programmers.java.view;
 import java.util.Arrays;
 import java.util.Optional;
 
-public enum Button {
+public enum Menu {
     SEARCH("1"), CALCULATE("2");
     private final String value;
 
-    Button(String value) {
+    Menu(String value) {
         this.value = value;
     }
 
-    public static Optional<Button> findByValue(String selected) {
-        return Arrays.stream(Button.values())
+    public static Optional<Menu> findByValue(String selected) {
+        return Arrays.stream(Menu.values())
                 .filter(v -> v.value.equals(selected))
                 .findFirst();
     }
 
     @Override
     public String toString() {
-        return value+". " + name();
+        return value + ". " + name();
     }
 }
