e7590a7e55bed18cd6f5e5328e2c8e81791b2aa0
hyee0715 <hyee0715@naver.com>
2023-06-13 22:03:43 +0900

    test: 조회, 계산, 종료 기능 테스트 코드 작성

---
+++ src/test/java/com/programmers/domain/CalculatorTest.java
@@ -0,0 +1,93 @@
+package com.programmers.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.NoSuchElementException;
+
+class CalculatorTest {
+
+    private final Calculator calculator = new Calculator();
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    private final InputStream inputStream = System.in;
+    private final PrintStream printStream = System.out;
+
+    @BeforeEach
+    public void before() {
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @AfterEach
+    public void after() {
+        System.setOut(printStream);
+        System.setIn(inputStream);
+    }
+
+    @Test
+    public void 계산한다() {
+        String[] inputs = { "2", "1 + 9 * 5", "2", "4 / 6 - 3", "2", "4 * 2 + 6", "2", "2 * 9 - 5", "3" };
+        System.setIn(new ByteArrayInputStream(String.join("\n", inputs).getBytes()));
+
+        try {
+            calculator.run();
+            String output = outputStream.toString();
+
+            assertTrue(output.contains("1. 조회"));
+            assertTrue(output.contains("2. 계산"));
+            assertTrue(output.contains("3. 종료"));
+            assertTrue(output.contains("선택 : "));
+
+            assertTrue(output.contains("46"));
+            assertTrue(output.contains("-3"));
+            assertTrue(output.contains("14"));
+            assertTrue(output.contains("13"));
+        } catch (NoSuchElementException ignore) {
+        }
+    }
+
+    @Test
+    public void 조회한다() {
+        String[] inputs = { "2", "1 + 9 * 5", "1", "3" };
+        System.setIn(new ByteArrayInputStream(String.join("\n", inputs).getBytes()));
+
+        try {
+            calculator.run();
+            String output = outputStream.toString();
+
+            assertTrue(output.contains("1. 조회"));
+            assertTrue(output.contains("2. 계산"));
+            assertTrue(output.contains("3. 종료"));
+            assertTrue(output.contains("선택 : "));
+
+            assertTrue(output.contains("1 + 9 * 5 = 46"));
+
+        } catch (NoSuchElementException ignore) {
+        }
+    }
+
+    @Test
+    public void 종료한다() {
+        String[] inputs = { "3" };
+        System.setIn(new ByteArrayInputStream(String.join("\n", inputs).getBytes()));
+
+        try {
+            calculator.run();
+            String output = outputStream.toString();
+
+            assertTrue(output.contains("1. 조회"));
+            assertTrue(output.contains("2. 계산"));
+            assertTrue(output.contains("3. 종료"));
+            assertTrue(output.contains("선택 : "));
+            assertTrue(output.contains("계산기 프로그램을 종료합니다."));
+
+        } catch (NoSuchElementException ignore) {
+        }
+    }
+}
