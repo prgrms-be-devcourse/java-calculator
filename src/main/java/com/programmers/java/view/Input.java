37922715f813cd27d364638fc4d4cd1044a21c6c
byeolhaha <j35635aa@gmail.com>
2023-06-13 20:28:55 +0900

    fix : enterMenu() 리턴형Optional로 변경

--- src/main/java/com/programmers/java/view/Input.java
+++ src/main/java/com/programmers/java/view/Input.java
@@ -3,22 +3,20 @@ package com.programmers.java.view;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Optional;
 
 public class Input {
-    public Button enterMenu() throws IOException {
-
-        Button button = null;
-
+    public Optional<Menu> enterMenu() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
-        Button selected = Button.findByValue(input);
 
-        return selected;
+        return Menu.findByValue(input);
     }
 
     public String enterExpression() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String expression = br.readLine();
+
         return expression;
     }
 }
