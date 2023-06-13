b2b75662b3d0e14c645f1ddfee00f0dd2c414e6a
Yebin Lee <lyb2325@gmail.com>
2023-06-13 20:08:28 +0900

    [add]: AtomicInteger을 통해 멀티 스레드 환경에서의 Map의 id값 동등성 보장 및 관리

--- src/main/java/calculator/repository/CalculationRepository.java
+++ src/main/java/calculator/repository/CalculationRepository.java
@@ -4,12 +4,20 @@ import calculator.model.CalculationResult;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CalculationRepository {
-    private final static Map<Integer, CalculationResult> results = new ConcurrentHashMap<>();
+    private static Map<Integer, CalculationResult> results;
+    private static AtomicInteger idCounter;
+
+
+    public CalculationRepository() {
+        results = new ConcurrentHashMap<>();
+        idCounter = new AtomicInteger(0);
+    }
 
     public void save(CalculationResult result) {
-        results.put(results.size() + 1, result);
+        results.put(idCounter.getAndIncrement(), result);
     }
 
     public Map<Integer, CalculationResult> findAll() {
