f102bc99f0abdb21800bbfef4cc8f950570acef8
kmebin <blowhuiee@gmail.com>
2023-06-13 17:08:26 +0900

    test: 후위 표기식으로 변환 테스트

---
+++ src/test/java/com/programmers/util/PostfixConverterTest.java
@@ -0,0 +1,20 @@
+package com.programmers.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.programmers.util.PostfixConverter.convert;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PostfixConverterTest {
+    @Test
+    @DisplayName("후위 표기법으로 변환")
+    void 후위_표기법으로_변환() {
+        // given
+        String expression = "3 + 2*4 -9  /3";
+        // when
+        String result = convert(expression);
+        // then
+        assertThat(result).isEqualTo("324*+93/-");
+    }
+}
