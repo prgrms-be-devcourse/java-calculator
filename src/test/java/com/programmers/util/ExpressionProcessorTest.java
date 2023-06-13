f6084e295b0439fa1d84be4803d18f960c6ac5d7
hyee0715 <hyee0715@naver.com>
2023-06-13 20:48:10 +0900

    test: 계산식 결과 저장 기능 테스트 코드 작성

--- src/test/java/com/programmers/util/ExpressionProcessorTest.java
+++ src/test/java/com/programmers/util/ExpressionProcessorTest.java
@@ -2,6 +2,9 @@ package com.programmers.util;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ExpressionProcessorTest {
@@ -16,6 +19,20 @@ class ExpressionProcessorTest {
 
         //when
         String result = expressionProcessor.removeSpace(input);
+
+        //then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void 계산식에_공백을_추가하여_재배열한다() {
+        //given
+        List<String> expression = Arrays.asList("5", "+", "2", "+", "34", "*", "23", "/", "2", "-", "3");
+        int expressionResult = 395;
+        String expected = "5 + 2 + 34 * 23 / 2 - 3 = 395";
+
+        //when
+        String result = expressionProcessor.rearrangeExpression(expression, expressionResult);
 
         //then
         assertThat(result).isEqualTo(expected);
