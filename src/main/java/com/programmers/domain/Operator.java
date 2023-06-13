a44dbb6cc6b57d80f86c4189842399bb9d47da2e
hyee0715 <hyee0715@naver.com>
2023-06-13 20:29:45 +0900

    feat: 후위표기식 계산 기능 구현

--- src/main/java/com/programmers/domain/Operator.java
+++ src/main/java/com/programmers/domain/Operator.java
@@ -5,17 +5,26 @@ import java.util.Objects;
 import java.util.function.BiFunction;
 
 public enum Operator {
-    ADDITION("+", 1),
-    SUBTRACTION("-", 1),
-    MULTIPLICATION("*", 2),
-    DIVISION("/", 2);
+    ADDITION("+", (n1, n2) -> n1 + n2, 1),
+    SUBTRACTION("-", (n1, n2) -> n1 - n2, 1),
+    MULTIPLICATION("*", (n1, n2) -> n1 * n2, 2),
+    DIVISION("/", (n1, n2) -> n1 / n2, 2);
 
     private final String operator;
+    private final BiFunction<Integer, Integer, Integer> expression;
     private final int importance;
 
-    Operator(String operator, int importance) {
+    Operator(String operator, BiFunction<Integer, Integer, Integer> expression, int importance) {
         this.operator = operator;
+        this.expression = expression;
         this.importance = importance;
+    }
+
+    public static Operator findOperator(String input) {
+        return Arrays.stream(Operator.values())
+                .filter(op -> Objects.equals(op.operator, input))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("연산자를 찾을 수 없습니다. = " + input));
     }
 
     public static int findImportance(String operator) {
@@ -24,5 +33,9 @@ public enum Operator {
                 .map(op -> op.importance)
                 .findAny()
                 .orElseThrow(() -> new IllegalArgumentException("우선순위를 찾을 수 없습니다. = " + operator));
+    }
+
+    public int calculate(int num1, int num2) {
+        return this.expression.apply(num1, num2);
     }
 }
--- src/main/java/com/programmers/service/CalculatorService.java
+++ src/main/java/com/programmers/service/CalculatorService.java
@@ -1,11 +1,15 @@
 package com.programmers.service;
 
 import com.programmers.domain.ExpressionValidator;
+import com.programmers.domain.Operator;
 import com.programmers.domain.PostFixConverter;
 import com.programmers.domain.SelectionValidator;
 import com.programmers.io.Console;
 
 import java.util.List;
+import java.util.Stack;
+
+import static com.programmers.domain.Operator.findOperator;
 
 public class CalculatorService {
 
@@ -45,8 +49,34 @@ public class CalculatorService {
         List<String> expression = getValidatedExpression();
         List<String> postfix = postfixConverter.convertInfixToPostfix(expression);
 
-        for (String s : postfix) {
-            System.out.print(s + " ");
+        int result = calculatePostfix(postfix);
+        System.out.println(result + "\n");
+    }
+
+    public int calculatePostfix(List<String> postfix) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < postfix.size(); i++) {
+            String token = postfix.get(i);
+
+            if (postfixConverter.isNumeric(token)) {
+                int num = Integer.parseInt(token);
+                stack.push(num);
+                continue;
+            }
+
+            int result = operate(stack, token);
+            stack.push(result);
         }
+
+        return stack.pop();
+    }
+
+    public int operate(Stack<Integer> stack, String token) {
+        int num2 = stack.pop();
+        int num1 = stack.pop();
+
+        Operator operator = findOperator(token);
+        return operator.calculate(num1, num2);
     }
 }
