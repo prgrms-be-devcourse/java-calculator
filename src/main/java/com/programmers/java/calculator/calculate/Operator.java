ad449c6f42bdc0b27c8fa857b40bb6cc1f2f6018
byeolhaha <j35635aa@gmail.com>
2023-06-14 01:41:51 +0900

    add : 연산자에 관한 Enum 추가 및 계산 결과 저장 객체 이름 변경

---
+++ src/main/java/com/programmers/java/calculator/calculate/Operator.java
@@ -0,0 +1,31 @@
+package com.programmers.java.calculator.calculate;
+
+import java.util.Arrays;
+import java.util.function.BiFunction;
+
+public enum Operator {
+    PLUS("+", (preOperand, nextOperand) -> preOperand + nextOperand),
+    MINUS("-", (preOperand, nextOperand) -> preOperand - nextOperand),
+    MULTIPLE("*", (preOperand, nextOperand) -> preOperand * nextOperand),
+    DIVIDE("/", (preOperand, nextOperand) -> preOperand / nextOperand);
+
+    private String operator;
+    private BiFunction<Double, Double, Double> calculateLamda;
+
+    Operator(String operator, BiFunction<Double, Double, Double> calculateLamda) {
+        this.operator = operator;
+        this.calculateLamda = calculateLamda;
+    }
+
+    public double calculate(double preOperand, double nextOperand) {
+        return calculateLamda.apply(preOperand, nextOperand);
+    }
+
+    public static Operator findByOperator(String operator) {
+
+        return Arrays.stream(Operator.values())
+                .filter(opt -> opt.operator.equals(operator))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("연산자가 아닙니다."));
+    }
+}
