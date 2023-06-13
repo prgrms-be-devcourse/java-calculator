0bed482344d5028db561c0c3b25e6a88f5da7bf1
beombu <beombu13@gmail.com>
2023-06-13 23:23:16 +0900

    Feat : 컨트롤러 스켈래톤 코드 추가

--- src/main/java/CalculatorApplication.java
+++ src/main/java/CalculatorApplication.java
@@ -3,12 +3,18 @@ import io.CalculatorInput;
 import io.CalculatorOutput;
 import io.CalculatorPrintOutput;
 import io.CalculatorScannerInput;
+import model.calculation.Calculation;
+import model.calculation.CalculationImpl;
+import model.converter.Converter;
+import model.converter.PostfixConverter;
 
 public class CalculatorApplication {
     public static void main(String[] args) {
         CalculatorInput input = new CalculatorScannerInput();
         CalculatorOutput output = new CalculatorPrintOutput();
+        Converter converter = new PostfixConverter();
+        Calculation calculation = new CalculationImpl();
 
-        new CalculatorController(input, output).run();
+        new CalculatorController(input, output, converter, calculation).run();
     }
 }
--- src/main/java/controller/CalculatorController.java
+++ src/main/java/controller/CalculatorController.java
@@ -2,20 +2,43 @@ package controller;
 
 import io.CalculatorInput;
 import io.CalculatorOutput;
+import model.calculation.Calculation;
+import model.converter.Converter;
+import model.vo.Menu;
 
 public class CalculatorController implements Runnable {
     private final CalculatorInput input;
     private final CalculatorOutput output;
+    private final Converter converter;
+    private final Calculation calculation;
 
-    public CalculatorController(CalculatorInput input, CalculatorOutput output) {
+    public CalculatorController(CalculatorInput input, CalculatorOutput output, Converter converter, Calculation calculation) {
         this.input = input;
         this.output = output;
+        this.converter = converter;
+        this.calculation = calculation;
     }
 
     @Override
     public void run() {
         while (true) {
-            //no switch
+            output.printMenuMessage();
+            selectMenu(input.menuInput());
         }
+    }
+
+    private void selectMenu(Menu menu) {
+        switch (menu.getMenu()) {
+            case 1 -> runRecordLogic();
+            case 2 -> runCalculationLogic();
+        }
+    }
+
+    private void runRecordLogic() {
+
+    }
+
+    private void runCalculationLogic() {
+
     }
 }
