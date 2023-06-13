39eb058a793f9b04aebe1e4f1b6316a04a204b24
hongxeob <sjun2918@naver.com>
2023-06-13 21:19:20 +0900

    refactor : try catch문 제거 후 정규식 사용, 인자명 명확하게 변경

--- src/main/java/com/programmers/util/StringUtil.java
+++ src/main/java/com/programmers/util/StringUtil.java
@@ -1,16 +1,13 @@
 package com.programmers.util;
 
 public class StringUtil {
-    public static boolean isNumber(String unCheckedNumber) {
-        try {
-            convertStringToLong(unCheckedNumber);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
+    private static final String NUMBER_REGEX = "-?\\d+(\\.\\d+)?";
+
+    public static boolean isNumber(String unCheckedToken) {
+        return unCheckedToken.matches(NUMBER_REGEX);
     }
 
-    public static long convertStringToLong(String unCheckedNumOrOperator) {
-        return Long.parseLong(unCheckedNumOrOperator);
+    public static long convertStringToLong(String token) {
+        return Long.parseLong(token);
     }
 }
