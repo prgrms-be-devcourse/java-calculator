74abb5d325e9f2a48a60508d0be15402a9edc363
kylekim2123 <kyle2123@naver.com>
2023-06-13 23:30:14 +0900

    refactor: App에서 MainController, Calculator 인스턴스를 변수로 할당 - 가독성 측면에서 변수로 할당하여 run() 메서드 실행

--- app/src/main/java/calculator/App.java
+++ app/src/main/java/calculator/App.java
@@ -9,8 +9,11 @@ import calculator.service.Calculator;
 public class App {
 
     public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        MainController mainController = new MainController(calculator);
+
         try {
-            new MainController(new Calculator()).run();
+            mainController.run();
         } catch (Exception e) {
             printWithLineBreak(e.getMessage());
         } finally {
