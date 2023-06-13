c4ebf968465167142fdd26ab3a9e9cc40202b032
hyee0715 <hyee0715@naver.com>
2023-06-13 16:50:12 +0900

    test: 메뉴 번호를 입력받는 기능 테스트 코드 작성

--- src/test/java/com/programmers/io/ConsoleTest.java
+++ src/test/java/com/programmers/io/ConsoleTest.java
@@ -1,9 +1,16 @@
 package com.programmers.io;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,19 +20,49 @@ class ConsoleTest {
     private static final String MENU_MESSAGE = "1. 조회\n2. 계산\n3. 종료\n";
     private static final String MENU_SELECTION_MESSAGE = "선택 : ";
 
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    private final InputStream inputStream = System.in;
+    private final PrintStream printStream = System.out;
+
     Console console = new Console();
+
+    @BeforeEach
+    public void before() {
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @AfterEach
+    public void after() {
+        System.setOut(printStream);
+        System.setIn(inputStream);
+    }
 
     @Test
     void 메뉴를_출력한다() {
         //given
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outputStream));
-
         //when
         console.printMenu();
 
         //then
         assertThat(outputStream.toString()).contains(MENU_MESSAGE);
         assertThat(outputStream.toString()).contains(MENU_SELECTION_MESSAGE);
+    }
+
+    @ValueSource(strings = {"1", "2", "3"})
+    @ParameterizedTest
+    void 메뉴_번호를_입력받는다(String input) {
+        //given
+        //when
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        //then
+        try {
+            console.getSelection();
+
+            assertThat(outputStream.toString()).contains(MENU_MESSAGE);
+            assertThat(outputStream.toString()).contains(MENU_SELECTION_MESSAGE);
+            assertThat(outputStream.toString()).contains(input);
+        } catch (NoSuchElementException ignore) {
+        }
     }
 }
