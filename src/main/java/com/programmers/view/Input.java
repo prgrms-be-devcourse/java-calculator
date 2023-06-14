f67c247d18eccfaba7bdaac072cf7fa59562f93c
KimMinheee <99alsgmlalsl@naver.com>
2023-06-14 15:43:51 +0900

    Feat : inputMenu() 함수 재귀 호출 및 사용 함수 수정 - while, inputSuccessful을 사용하여 catch문에서 재귀호출 되지 않도록 코드 수정 - 2가지 함수 호출을 하나에 정리한 함수(Validator.validateMenu)를 호출하는 것으로 코드 변경

--- src/main/java/com/programmers/view/Input.java
+++ src/main/java/com/programmers/view/Input.java
@@ -1,5 +1,6 @@
 package com.programmers.view;
 
+import com.programmers.error.CalculatorException;
 import com.programmers.util.Validator;
 
 import java.util.Scanner;
@@ -9,29 +10,27 @@ import java.util.Scanner;
  */
 
 public class Input {
-    private static final String NOTINTEGERMESSAGE = "\n정수가 아닙니다. 메뉴를 다시 입력해주세요\n선택 : ";
     private static final String NOTMENUMESSAGE = "없는 메뉴 입니다. 메뉴를 다시 입력해주세요\n선택 : ";
     private static final String NOTVALIDARITHMETICEXPRESSION = "계산식이 옳지 않습니다. 식을 다시 입력해주세요\n";
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static int inputMenu() {
-        int menuNum;
+        int menuNum = 0;
+        boolean inputSuccessful = false;
 
-        try {
-            String menuString = removeWhiteSpace(scanner.nextLine());
-            menuNum = Validator.checkInteger(menuString);
-        } catch (NumberFormatException e) {
-            System.out.print(NOTINTEGERMESSAGE);
-            return inputMenu(); //반복
+        while(!inputSuccessful){
+            try {
+                String menuString = removeWhiteSpace(scanner.nextLine());
+                menuNum = Validator.validateMenu(menuString);
+
+                inputSuccessful = true;
+            } catch (CalculatorException e) {
+                System.out.println("다시 입력하세요.");
+            }
         }
 
         System.out.println(); //공백
-
-        if (!Validator.checkValidMenu(menuNum)) {
-            System.out.print(NOTMENUMESSAGE);
-            inputMenu(); //반복
-        }
 
         return menuNum;
     }
