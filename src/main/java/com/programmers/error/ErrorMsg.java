f7036b3a8db846e9398e8984679895f99c5f875c
hongxeob <sjun2918@naver.com>
2023-06-14 15:36:17 +0900

    refactor : final을 붙여 각 메시지를 상수로 선언

--- src/main/java/com/programmers/error/ErrorMsg.java
+++ src/main/java/com/programmers/error/ErrorMsg.java
@@ -4,9 +4,9 @@ public final class ErrorMsg {
     private ErrorMsg() {
     }
 
-    public static String INVALID_MENU_SELECTION = "메뉴를 잘못 선택하셨습니다. 메뉴는 1,2,3만 가능합니다.";
-    public static String ENTER_ZERO_FOR_DIVISION = "0으로는 나눌 수 없습니다. 0이 아닌 다른 숫자를 사용해 주세요.";
-    public static String INVALID_OPERATOR_INPUT = "잘못된 수식입니다. +, -, *, / 연산만 가능합니다.";
-    public static String NO_CALCULATION_RECORDS = "계산 기록이 없습니다. 먼저 계산 결과를 얻은 후 사용하세요.";
-    public static String OPERAND_IS_NOT_A_NUMBER = "피연산자가 숫자가 아닙니다. 숫자를 입력해 주세요.";
+    public final static String INVALID_MENU_SELECTION = "메뉴를 잘못 선택하셨습니다. 메뉴는 1,2,3만 가능합니다.";
+    public final static String ENTER_ZERO_FOR_DIVISION = "0으로는 나눌 수 없습니다. 0이 아닌 다른 숫자를 사용해 주세요.";
+    public final static String INVALID_OPERATOR_INPUT = "잘못된 수식입니다. +, -, *, / 연산만 가능합니다.";
+    public final static String NO_CALCULATION_RECORDS = "계산 기록이 없습니다. 먼저 계산 결과를 얻은 후 사용하세요.";
+    public final static String OPERAND_IS_NOT_A_NUMBER = "피연산자가 숫자가 아닙니다. 숫자를 입력해 주세요.";
 }
