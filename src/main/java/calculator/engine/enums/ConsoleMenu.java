1172864d24a544dc6dce3f9dcf273a6c1c0343ba
jay-so <merry9504@naver.com>
2023-06-14 19:50:59 +0900

    fix: enum 클래스 수정

--- src/main/java/calculator/engine/enums/ConsoleMenu.java
+++ src/main/java/calculator/engine/enums/ConsoleMenu.java
@@ -1,5 +1,11 @@
 package calculator.engine.enums;
 
 public enum ConsoleMenu {
-    ONE,TWO
+    ONE(1),TWO(2);
+
+    private final int MenuNumber;
+
+    ConsoleMenu(int menuNumber) {
+        MenuNumber = menuNumber;
+    }
 }
--- src/main/java/calculator/engine/enums/Operators.java
+++ src/main/java/calculator/engine/enums/Operators.java
@@ -1,4 +1,10 @@
 package calculator.engine.enums;
 public enum Operators {
-    PLUS,MINUS,DIVISION,MULTIPLICATION
+    PLUS('+'),MINUS('-'),DIVISION('/'),MULTIPLICATION('*');
+
+    private final char operator;
+
+    Operators(char operator) {
+        this.operator = operator;
+    }
 }
