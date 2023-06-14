ddf36179c295c510a3fee6b962d3a918fcf9cae6
hongxeob <sjun2918@naver.com>
2023-06-14 14:06:43 +0900

    refactor : println() 추가, 에러 메세지 수정

--- src/main/java/com/programmers/view/Writer.java
+++ src/main/java/com/programmers/view/Writer.java
@@ -9,17 +9,18 @@ public class Writer implements Output {
     @Override
     public void print(String message) {
         System.out.println(message);
-        System.out.println();
     }
 
     @Override
     public void print(long result) {
+        System.out.println();
         System.out.println(result);
         System.out.println();
     }
 
     @Override
     public void print(List<CalculationResult> record) {
+        System.out.println();
         record.forEach(System.out::println);
         System.out.println();
     }
