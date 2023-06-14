36c7c8e836fe6c19cbd5cb483105b20aafb9bfa4
jay-so <merry9504@naver.com>
2023-06-15 03:13:33 +0900

    fix: enum 클래스 수정

--- src/main/java/calculator/engine/enums/ConsoleMenu.java
+++ src/main/java/calculator/engine/enums/ConsoleMenu.java
@@ -1,10 +1,8 @@
 package calculator.engine.enums;
 
 public enum ConsoleMenu {
-    ONE(1),TWO(2);
-
-    private final int MenuNumber;
-
+   ONE(1),TWO(2);
+    public final int MenuNumber;
     ConsoleMenu(int menuNumber) {
         MenuNumber = menuNumber;
     }
