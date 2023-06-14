fbef879186068af8c098fa60e193d4bc8c044619
jay-so <merry9504@naver.com>
2023-06-15 03:29:16 +0900

    feat: 후위 표기법 & 정규화 구현

--- src/main/java/calculator/engine/model/Operator.java
+++ src/main/java/calculator/engine/model/Operator.java
@@ -2,33 +2,30 @@ package calculator.engine.model;
 
 import calculator.engine.enums.Operators;
 
-public class Operator {
+class Operator {
     public int operate(int num1, int num2, Operators operators){
         switch (operators){
             case PLUS:
-                return PlusCalcul(num1,num2);
+                return add(num1,num2);
             case MINUS:
-                return MinusCalcul(num1,num2);
+                return substract(num1,num2);
             case MULTIPLICATION:
-                return MultiCalcul(num1,num2);
+                return multiply(num1,num2);
             case DIVISION:
-                return DiviCalcul(num1,num2);
+                return divide(num1,num2);
         }
         return 0;
     }
-
-    private int PlusCalcul(int num1,int num2) {
+    private int add(int num1,int num2) {
         return num1 + num2;
     }
-
-    private int MinusCalcul(int num1,int num2) {
+    private int substract(int num1,int num2) {
         return num1 - num2;
     }
-
-    private int DiviCalcul(int num1, int num2){
+    private int divide(int num1, int num2){
         return num1 / num2;
     }
-    private int MultiCalcul(int num1,int num2){
+    private int multiply(int num1,int num2){
         return num1 * num2;
     }
 }
--- src/main/java/calculator/engine/model/PostfixCalculator.java
+++ src/main/java/calculator/engine/model/PostfixCalculator.java
@@ -1,4 +1,41 @@
 package calculator.engine.model;
 
+import java.util.List;
+import java.util.Stack;
+
 public class PostfixCalculator {
+    public int calculate(List<String> postfixExpression) {
+        Stack<Integer> operandStack = new Stack<>();
+
+        for (String operand : postfixExpression) {
+            if (operand.matches("^?\\d*$")) {
+                operandStack.push(Integer.parseInt(operand));
+            } else {
+                int operand2 = operandStack.pop();
+                int operand1 = operandStack.pop();
+                int result = 0;
+
+                switch (operand) {
+                    case "+":
+                        result = operand1 + operand2;
+                        break;
+                    case "-":
+                        result = operand1 - operand2;
+                        break;
+                    case "*":
+                        result = operand1 * operand2;
+                        break;
+                    case "/":
+                        if (operand2 == 0) {
+                            throw new ArithmeticException("Divide by zero!");
+                        }
+                        result = operand1 / operand2;
+                        break;
+                }
+
+                operandStack.push(result);
+            }
+        }
+        return operandStack.pop();
+    }
 }
