25bca05f43a6b3d84f8d1ea80aa3a401e1399a2e
2jie0516 <2jie0516>
2023-06-14 13:35:14 +0900

    refactor : 사용하지 않는 코드 제거

--- sub_cal/sub_cal/src/main/java/model/History.java
+++ sub_cal/sub_cal/src/main/java/model/History.java
@@ -8,19 +8,13 @@ import java.util.Stack;
 public class History {
     // 계산 내역을 저장하는 리스트
     private List<String> log = new ArrayList<>();
-    public boolean isEmpty() {
-        return log.isEmpty();
-    }
-    // 외부에서 계산 내역을 조회할려고 하면 실행합니다
+
     public List<String> getHistory() {
         return log;
     }
 
     // 저장된 계산 내역 개수를 반환합니다
-    public Integer getHistoryLen() {
-        return log.size();
-    }
-    // 계산 내역을 추가합니다
+
     public void addHistory(String inputString,int result){
         StringBuilder sb = new StringBuilder();
         log.add(sb.append(inputString).append(" = ").append(result).toString());
