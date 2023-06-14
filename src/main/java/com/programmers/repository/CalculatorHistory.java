7d039e984b9d9b775724828a3a818a2bc5265ff2
weonest <geonhee33@naver.com>
2023-06-14 21:47:26 +0900

    refactor: 결과 반환 및 출력 리팩토링

--- src/main/java/com/programmers/repository/CalculatorHistory.java
+++ src/main/java/com/programmers/repository/CalculatorHistory.java
@@ -1,28 +1,19 @@
 package com.programmers.repository;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class CalculatorHistory {
 
     private final Map<String, Double> storage = new LinkedHashMap<>();
-    private final StringBuilder sb = new StringBuilder();
 
     public void save(String formula, double answer) {
         storage.put(formula, answer);
     }
 
-    public String findAll() {
-        
-        // 콘솔로 역할을 주자
-        if (storage.isEmpty()) {
-            return "조회할 기록이 없습니다.";
-        }
-
-        storage.keySet().forEach(key -> {
-            double answer = storage.get(key);
-            sb.append(key).append(" = ").append(answer).append("\n");
-        });
-        return sb.toString();
+    public Map<String, Double> findAll() {
+        Map<String, Double> map = new LinkedHashMap<>(storage);
+        return map;
     }
 }
