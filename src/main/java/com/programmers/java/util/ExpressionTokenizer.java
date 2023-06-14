fd3739c0054e00a95b1bf1859a01e0c943d6eee8
byeolhaha <j35635aa@gmail.com>
2023-06-14 11:18:45 +0900

    fix : find() 대신  Stream을 사용하여 일치하는 패턴 찾기

--- src/main/java/com/programmers/java/util/ExpressionTokenizer.java
+++ src/main/java/com/programmers/java/util/ExpressionTokenizer.java
@@ -1,24 +1,18 @@
 package com.programmers.java.util;
 
-import java.util.ArrayList;
+import java.util.regex.*;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class ExpressionTokenizer {
-
     private static final String EXPRESSION_REGEXP = "\\d+|[-+*/]";
 
     public List<String> expressionSplit(String expression) {
-        List<String> result = new ArrayList<>();
         Pattern pattern = Pattern.compile(EXPRESSION_REGEXP);
         Matcher matcher = pattern.matcher(expression);
 
-        //stream이용해서
-        while (matcher.find()) {
-            result.add(matcher.group());
-        }
-
-        return result;
+        return matcher.results()
+                .map(MatchResult::group)
+                .collect(Collectors.toList());
     }
 }
