7e031c47f55ae239ebefcd04de172d040c1a7034
baejunil <bjo6300@naver.com>
2023-06-15 05:42:19 +0900

    fix : 인덱스 동기화 기능 삭제
    함수 실행 범위를 생각해보니 같은 클래스 내에서 사용되어서 동기화를 생각하지 않아도 될 것 같다.

--- calculator/src/main/java/com/programmers/engine/model/History.java
+++ calculator/src/main/java/com/programmers/engine/model/History.java
@@ -2,15 +2,14 @@ package com.programmers.engine.model;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class History {
 
   private final Map<Integer, String> history = new HashMap<>();
-  AtomicInteger index = new AtomicInteger();
+  private int index = 0;
 
   public void save(String formula, double answer) {
-    history.put(index.addAndGet(1), formula + " = " + answer);
+    history.put(++index, formula + " = " + answer);
   }
 
   public void showHistory() {
