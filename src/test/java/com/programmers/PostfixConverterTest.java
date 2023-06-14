8824513cf1f9d54b13b18c95ad748c43dd2dd9fb
hongxeob <sjun2918@naver.com>
2023-06-14 21:39:15 +0900

    refactor : PostfixConverter 테스트 코드 수정

--- src/test/java/com/programmers/PostfixConverterTest.java
+++ src/test/java/com/programmers/PostfixConverterTest.java
@@ -1,5 +1,6 @@
 package com.programmers;
 
+import com.programmers.core.Converter;
 import com.programmers.core.PostfixConverter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -14,12 +15,13 @@ public class PostfixConverterTest {
     @Test
     @DisplayName("후위 표기식 변환 성공 테스트")
     void postfixFormulaConversionSuccessful() throws Exception {
+        Converter postfixConverter = new PostfixConverter();
         //given
         String formula1 = "1 + 2 * 3";
         String formula2 = "3 + 5 / 1 + 2 / 2";
         //when
-        List<String> postfixFormula1 = PostfixConverter.convert(formula1);
-        List<String> postfixFormula2 = PostfixConverter.convert(formula2);
+        List<String> postfixFormula1 = postfixConverter.convert(formula1);
+        List<String> postfixFormula2 = postfixConverter.convert(formula2);
         //then
         assertThat(postfixFormula1).containsExactly("1", "2", "3", "*", "+");
         assertThat(postfixFormula2).containsExactly("3", "5", "1", "/", "+", "2", "2", "/", "+");
@@ -28,9 +30,11 @@ public class PostfixConverterTest {
     @Test
     @DisplayName("후위 표기식 변환 실패 테스트")
     void postfixFormulaConversionFailed() throws Exception {
+        Converter postfixConverter = new PostfixConverter();
+
         String formula = "1 + 2 * 3 + a ";
 
-        Assertions.assertThrows(IllegalArgumentException.class, () -> PostfixConverter.convert(formula));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> postfixConverter.convert(formula));
     }
 }
 
