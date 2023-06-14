d31597e7cd805ba8083501129397cae86b599d0e
KimJaeUng <78838534+kju2405@users.noreply.github.com>
2023-06-14 14:53:01 +0900

    refactor: 식을 입력받고 결과값을 반환하는 기능만 수행, 나머지 기능은 내부적으로 감춤

---
+++ calculator/src/main/java/org/example/Calculate/Calculate.java
@@ -0,0 +1,7 @@
+package org.example.Calculate;
+
+import java.util.Stack;
+
+public interface Calculate {
+    int calculate(Stack<String> expressionStack);
+}
---
+++ calculator/src/main/java/org/example/Calculate/CalculateImpl.java
@@ -0,0 +1,17 @@
+package org.example.Calculate;
+
+import java.util.Stack;
+
+public class CalculateImpl implements Calculate {
+    CalOrder calOrder;
+
+    public CalculateImpl(CalOrder calOrder) {
+        this.calOrder = calOrder;
+    }
+
+    public int calculate(Stack<String> expressionStack) {
+        calOrder.setStack(expressionStack);
+        calOrder.calMultiplyDivide();
+        return calOrder.calPlusMinus();
+    }
+}
