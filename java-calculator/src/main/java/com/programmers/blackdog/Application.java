95d0f50c4ed66c7e74e7960c5eddbb57183bc1dd
juno-junho <ssmm0205@naver.com>
2023-06-15 20:55:12 +0900

    refactor(application): application 주입 수정

--- java-calculator/src/main/java/com/programmers/blackdog/Application.java
+++ java-calculator/src/main/java/com/programmers/blackdog/Application.java
@@ -1,17 +1,15 @@
 package com.programmers.blackdog;
 
 import com.programmers.blackdog.controller.CalculatorController;
-import com.programmers.blackdog.view.InputView;
-import com.programmers.blackdog.view.OutputView;
-import com.programmers.blackdog.view.PrintStreamOutputView;
-import com.programmers.blackdog.view.ScannerInputView;
+import com.programmers.blackdog.view.*;
 
 public class Application {
     public static void main(String[] args) {
         InputView inputView = new ScannerInputView();
         OutputView outputView = new PrintStreamOutputView();
+        Console console = new Console(inputView, outputView);
 
-        CalculatorController calculator = new CalculatorController(inputView, outputView);
+        CalculatorController calculator = new CalculatorController(console);
         calculator.run();
     }
 }
