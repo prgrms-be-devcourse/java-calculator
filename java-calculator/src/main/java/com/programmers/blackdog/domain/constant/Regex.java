500eddffe9f02a4daddfe17dd98fede1f9229757
juno-junho <ssmm0205@naver.com>
2023-06-15 20:48:07 +0900

    refactor(domain): REGEX 상수 클래스 인스턴스화 및 상속 막는 기능 구현

--- java-calculator/src/main/java/com/programmers/blackdog/domain/constant/Regex.java
+++ java-calculator/src/main/java/com/programmers/blackdog/domain/constant/Regex.java
@@ -2,7 +2,13 @@ package com.programmers.blackdog.domain.constant;
 
 import com.programmers.blackdog.domain.RegexGenerator;
 
-public class Regex {
+public final class Regex {
+
+    private Regex(){
+        throw new AssertionError("인스턴스화 할 수 없습니다!");
+    }
+
+
     public static final String BLANK = " ";
     public static final String DELIMITER;
     public static final String REGEX;
