7fda6a3e76d1bf3ab5634aa9610b40915d26fdd2
jae_yoon(mac) <wlwhsvkdlxh@gmail.com>
2023-06-15 14:31:45 +0900

    refactor: Regex 방어코드 리팩토링

--- src/main/java/com/programmers/calculator/constant/Regex.java
+++ src/main/java/com/programmers/calculator/constant/Regex.java
@@ -2,7 +2,12 @@ package com.programmers.calculator.constant;
 
 import java.util.regex.Pattern;
 
-public class Regex {
+public final class Regex {
+
+    private Regex() {
+        throw new RuntimeException("Regex를 생성하지 마세요.");
+    }
+
     public static final String regex = "\\d+|[-+*/]";
     public static final Pattern pattern = Pattern.compile(regex);
 }
