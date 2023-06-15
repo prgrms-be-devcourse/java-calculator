7fe46d5f2fc788ca73857900d4b7d57b39dc5c0a
SW-H <hsw08@naver.com>
2023-06-15 23:31:04 +0900

    feat : 없는 메뉴 선택시 처리 방법 수정

--- src/main/java/co/programmers/domain/UserMenu.java
+++ src/main/java/co/programmers/domain/UserMenu.java
@@ -2,10 +2,8 @@ package co.programmers.domain;
 
 import java.util.Arrays;
 
-import co.programmers.exception.ExceptionMessage;
-
 public enum UserMenu {
-	INQUIRY(1), CALCULATE(2), TERMINATE(3), ERROR(4);
+	INQUIRY(1), CALCULATE(2), TERMINATE(3), ERROR(-9999);
 
 	private final Integer menu;
 
@@ -17,6 +15,6 @@ public enum UserMenu {
 		return Arrays.stream(values())
 				.filter(menuNum -> menuNum.menu.equals(input))
 				.findFirst()
-				.orElseThrow(() -> new IllegalArgumentException(ExceptionMessage.INVALID_INPUT));
+				.orElse(ERROR);
 	}
 }
