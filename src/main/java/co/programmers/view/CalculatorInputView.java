9b345812829a3cec96b6182e52c179c893f2b969
SW-H <hsw08@naver.com>
2023-06-15 14:39:51 +0900

    feat : CalculatorInputView 내에서 출력 함수 호출 삭제

--- src/main/java/co/programmers/view/CalculatorInputView.java
+++ src/main/java/co/programmers/view/CalculatorInputView.java
@@ -8,14 +8,12 @@ public class CalculatorInputView implements InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public Integer inputUserMenu() {
-        CalculatorOutputView.printMenuChoiceGuide();
         Integer userInput = SCANNER.nextInt();
         SCANNER.nextLine();
         return userInput;
     }
 
     public Expression inputExpression() throws ArithmeticException {
-        CalculatorOutputView.printCalculationGuide();
         return new Expression(SCANNER.nextLine());
     }
 }
