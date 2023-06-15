94f6a0221c20793038f52fce0c170fdfcb1e6033
jae_yoon(mac) <wlwhsvkdlxh@gmail.com>
2023-06-16 00:27:50 +0900

    refactor: parser 삭제

--- src/main/java/com/programmers/calculator/domain/component/Parser.java
+++
@@ -1,9 +0,0 @@
-package com.programmers.calculator.domain.component;
-
-import com.programmers.calculator.domain.vo.Expression;
-
-import java.util.List;
-
-public interface Parser {
-    public List<String> parseToTokens(Expression expression);
-}
