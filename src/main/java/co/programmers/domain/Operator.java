6d43a1389e3fca7a83fdefcc0a4dc72d474e2b36
SW-H <hsw08@naver.com>
2023-06-15 14:41:59 +0900

    feat : 계산 인자 Integer -> Double (나눗셈)

--- src/main/java/co/programmers/domain/Operator.java
+++ src/main/java/co/programmers/domain/Operator.java
@@ -13,21 +13,19 @@ public enum Operator {
             throw new ArithmeticException(ExceptionMessage.DIVIDED_BY_ZERO);
         }
         return operand1 / operand2;
-    }),
-    OPENED_PARENTHESIS("(", 3, (operand1, operand2) -> 0),
-    CLOSED_PARENTHESIS(")", 3, (operand1, operand2) -> 0);
+    });
 
     private final String symbol;
     private final int priority;
-    private final BiFunction<Integer, Integer, Integer> operation;
+    private final BiFunction<Double, Double, Double> operation;
 
-    private Operator(String symbol, int priority, BiFunction<Integer, Integer, Integer> operation) {
+    private Operator(String symbol, int priority, BiFunction<Double, Double, Double> operation) {
         this.symbol = symbol;
         this.priority = priority;
         this.operation = operation;
     }
 
-    public static Integer calculate(String operator, Integer operand1, Integer operand2) {
+    public static Double calculate(String operator, Double operand1, Double operand2) {
         return getSymbol(operator).operation.apply(operand1, operand2);
     }
 
