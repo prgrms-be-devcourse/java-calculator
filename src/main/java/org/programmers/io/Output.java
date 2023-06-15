6537a0f2e6e158f2bdac4a9a3d93ba3d155f7e49
kys0411 <32180846@dankook.ac.kr>
2023-06-15 23:32:24 +0900

    add: ConsoleOutput의 추상체인 Output interface 추가

---
+++ src/main/java/org/programmers/io/Output.java
@@ -0,0 +1,17 @@
+package org.programmers.io;
+
+
+import org.programmers.expression.ExpressionResult;
+
+import java.util.Map;
+
+public interface Output {
+
+    void printConsole();
+
+    void printAnswer(double answer);
+
+    void printError();
+
+    void printHistory(Map<Long, ExpressionResult> history);
+}
