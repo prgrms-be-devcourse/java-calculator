610619a6a37024b6f40c68d3f9aaa444bcd4cec2
Curry4182 <kangbk4182@naver.com>
2023-06-15 19:52:43 +0900

    refactor: 구현 하지 않는 메서드 null 대신 빈 값을 반환하도록 수정

--- src/main/java/calcproject/engine/CalcExpressionTokenizer.java
+++ src/main/java/calcproject/engine/CalcExpressionTokenizer.java
@@ -1,10 +1,12 @@
 package calcproject.engine;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class CalcExpressionTokenizer {
 	public List<String> tokenizeExpression(String expression) {
-
-		return null;
+		List<String> list = new ArrayList<>();
+		return list;
 	}
 }
