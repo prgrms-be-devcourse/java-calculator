cf23519bd1f9fbc47bf72132cb1aba62c31419ff
kys0411 <32180846@dankook.ac.kr>
2023-06-15 20:43:51 +0900

    feat: 사용자로부터 입력을 받는 input 기능 추가

---
+++ src/main/java/org/programmers/io/ConsoleInput.java
@@ -0,0 +1,26 @@
+package org.programmers.io;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ConsoleInput {
+    private final Scanner scanner;
+
+    public ConsoleInput() {
+        scanner = new Scanner(System.in);
+    }
+
+    public int inputNumber() {
+        try {
+            return scanner.nextInt();
+        } catch (InputMismatchException e) {
+            scanner.nextLine();
+            return 0;
+        }
+    }
+
+    public String inputExpression() {
+        scanner.nextLine();
+        return scanner.nextLine();
+    }
+}
