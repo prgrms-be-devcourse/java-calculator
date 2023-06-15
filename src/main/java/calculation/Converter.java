2cc308cc61bfd904c76de2c1336becc65be0bfcb
Younghoon Kim <the_basic_@naver.com>
2023-06-15 20:02:50 +0900

    refactor: 패키지명 변경 및 static 메소드 분리, 메소드 타입 변경

--- src/main/java/calculation/Converter.java
+++
@@ -1,9 +0,0 @@
-package calculation;
-
-public interface Converter {
-    String convert();
-
-    static String removeWhiteSpace(String targetExpr) {
-        return targetExpr.replace(" ", "");
-    }
-}
