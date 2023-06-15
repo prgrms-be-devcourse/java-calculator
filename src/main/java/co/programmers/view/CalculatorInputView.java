a3901a5be61ee4342f6d7ce32d4f2d13d8411a3e
SW-H <hsw08@naver.com>
2023-06-15 14:05:03 +0900

    feat : 출력 관련 내용 CalculatorInputView -> CalculatorOutputView 이동

--- src/main/java/co/programmers/view/CalculatorInputView.java
+++ src/main/java/co/programmers/view/CalculatorInputView.java
@@ -1,58 +1,21 @@
 package co.programmers.view;
 
-import co.programmers.exception.ExceptionMessage;
-import co.programmers.domain.UserMenu;
-import java.util.Arrays;
+import co.programmers.domain.Expression;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class CalculatorInputView implements InputView {
 
     private static final Scanner SCANNER = new Scanner(System.in);
-    private static final Set<String> USER_MENU =
-            Arrays.stream(UserMenu.values())
-                    .map(UserMenu::getValue)
-                    .collect(Collectors.toSet());
 
-    public CalculatorInputView() {
-    }
-
-    public String inputUserMenu() {
-        printMenuChoiceGuide();
-        String userInput = SCANNER.next();
+    public Integer inputUserMenu() {
+        CalculatorOutputView.printMenuChoiceGuide();
+        Integer userInput = SCANNER.nextInt();
         SCANNER.nextLine();
-        try {
-            validateUserMenuChoice(String.valueOf(userInput));
-        } catch (IllegalArgumentException illegalArgumentException) {
-            System.out.println(illegalArgumentException.getMessage());
-            return UserMenu.TERMINATE.getValue();
-        }
         return userInput;
     }
 
-    public String inputExpression() {
-        printCalculationGuide();
-        String expression = SCANNER.nextLine();
-        return expression;
-    }
-
-    private void printCalculationGuide() {
-        System.out.println("1 + 2 * 3와 같은 형식으로 계산하고자 하는 식을 입력하세요.");
-        System.out.print("> ");
-    }
-
-    private void printMenuChoiceGuide() {
-        System.out.println("\n[다음 중 원하시는 항목을 숫자로 입력하세요]");
-        System.out.println("1. 조회");
-        System.out.println("2. 계산");
-        System.out.println("3. 종료");
-        System.out.print("> 선택 :  ");
-    }
-
-    private void validateUserMenuChoice(String userInput) throws IllegalArgumentException {
-        if (!USER_MENU.contains(userInput)) {
-            throw new IllegalArgumentException(ExceptionMessage.INVALID_INPUT);
-        }
+    public Expression inputExpression() throws ArithmeticException {
+        CalculatorOutputView.printCalculationGuide();
+        return new Expression(SCANNER.nextLine());
     }
 }
--- src/main/java/co/programmers/view/CalculatorOutputView.java
+++ src/main/java/co/programmers/view/CalculatorOutputView.java
@@ -2,7 +2,20 @@ package co.programmers.view;
 
 public class CalculatorOutputView implements OutputView {
 
-    public void print(String content) {
+    public static void printCalculationGuide() {
+        System.out.println("1 + 2 * 3와 같은 형식으로 계산하고자 하는 식을 입력하세요.");
+        System.out.print("> ");
+    }
+
+    public static void printMenuChoiceGuide() {
+        System.out.println("\n[다음 중 원하시는 항목을 숫자로 입력하세요]");
+        System.out.println("1. 조회");
+        System.out.println("2. 계산");
+        System.out.println("3. 종료");
+        System.out.print("> 선택 :  ");
+    }
+
+    public void printCalculationRes(Integer content) {
         System.out.println(">> 결과 : " + content);
     }
 }
