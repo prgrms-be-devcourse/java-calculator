0ac007e62562a06f644fd15afcd3b96993c3feef
SW-H <hsw08@naver.com>
2023-06-15 20:24:30 +0900

    feat : 연산자 우선 순위 계산

--- src/main/java/co/programmers/domain/Operator.java
+++ src/main/java/co/programmers/domain/Operator.java
@@ -1,55 +1,51 @@
 package co.programmers.domain;
 
-import co.programmers.exception.ExceptionMessage;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.function.BiFunction;
 
+import co.programmers.exception.ExceptionMessage;
+
 public enum Operator {
-    ADDITION("+", 2, (operand1, operand2) -> operand1 + operand2),
-    SUBTRACTION("-", 2, (operand1, operand2) -> operand1 - operand2),
-    MULTIPLICATION("*", 1, (operand1, operand2) -> operand1 * operand2),
-    DIVISION("/", 1, (operand1, operand2) -> {
-        if (operand2 == 0) {
-            throw new ArithmeticException(ExceptionMessage.DIVIDED_BY_ZERO);
-        }
-        return operand1 / operand2;
-    });
+	ADDITION("+", 2, (operand1, operand2) -> operand1 + operand2),
+	SUBTRACTION("-", 2, (operand1, operand2) -> operand1 - operand2),
+	MULTIPLICATION("*", 1, (operand1, operand2) -> operand1 * operand2),
+	DIVISION("/", 1, (operand1, operand2) -> {
+		if (operand2 == 0) {
+			throw new ArithmeticException(ExceptionMessage.DIVIDED_BY_ZERO);
+		}
+		return operand1 / operand2;
+	});
 
-    private final String symbol;
-    private final int priority;
-    private final BiFunction<Double, Double, Double> operation;
+	private final String symbol;
+	private final int priority;
+	private final BiFunction<Double, Double, Double> operation;
 
-    private Operator(String symbol, int priority, BiFunction<Double, Double, Double> operation) {
-        this.symbol = symbol;
-        this.priority = priority;
-        this.operation = operation;
-    }
+	private Operator(String symbol, int priority, BiFunction<Double, Double, Double> operation) {
+		this.symbol = symbol;
+		this.priority = priority;
+		this.operation = operation;
+	}
 
-    public static Double calculate(String operator, Double operand1, Double operand2) {
-        return getSymbol(operator).operation.apply(operand1, operand2);
-    }
+	public static Double calculate(String operator, Double operand1, Double operand2) {
+		return getSymbol(operator).operation.apply(operand1, operand2);
+	}
 
-    public static Operator getSymbol(String operator) {
-        return Arrays.stream(values())
-                .filter(o -> o.symbol.equals(operator))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException(ExceptionMessage.INVALID_INPUT));
-    }
+	public static Operator getSymbol(Object operator) {
+		return Arrays.stream(values())
+				.filter(o -> o.symbol.equals(operator))
+				.findFirst()
+				.orElseThrow(() -> new IllegalArgumentException(ExceptionMessage.INVALID_INPUT));
+	}
 
-    public static boolean isOperator(String symbol) {
-        return !Arrays.stream(values())
-                .filter(o -> o.symbol.equals(symbol))
-                .findAny()
-                .isEmpty();
-    }
+	public static void decideCalculationOrder(List<String> operators) {
+		Collections.sort(operators, (operator1, operator2) ->
+				Integer.compare(getSymbol(operator1).getPriority(), getSymbol(operator2).getPriority())
+		);
+	}
 
-    public static boolean hasLowerPrecedence(String current, String toCompare) {
-        Operator operator1 = getSymbol(current);
-        Operator operator2 = getSymbol(toCompare);
-        return (operator1.getPriority() >= operator2.getPriority());
-    }
-
-    public int getPriority() {
-        return priority;
-    }
+	public int getPriority() {
+		return priority;
+	}
 }
