c37cdad7c86c16f3a73c81e9ce67e1ea2a965c5a
SW-H <hsw08@naver.com>
2023-06-15 20:23:11 +0900

    feat : 수식 내의 공백 제거, split

--- src/main/java/co/programmers/domain/Expression.java
+++ src/main/java/co/programmers/domain/Expression.java
@@ -1,33 +1,39 @@
 package co.programmers.domain;
 
-import co.programmers.exception.ExceptionMessage;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import co.programmers.exception.ExceptionMessage;
+
 public class Expression {
 
-    private static final Pattern EXPRESSION_FORMAT = Pattern.compile(
-            "(\\d+\\s[\\+\\-\\*\\/]\\s)+\\d+"
-    );
-    private String expression;
+	private static final Pattern EXPRESSION_FORMAT = Pattern.compile("(\\d+\\s[\\+\\-\\*\\/]\\s)+\\d+");
+	private static final String DELIMITER = " ";
+	private String expression;
 
-    public Expression(String expression) {
-        this.expression = expression;
-        if (!validate()) {
-            throw new ArithmeticException(ExceptionMessage.INVALID_EXPRESSION);
-        }
-    }
+	public Expression(String expression) {
+		this.expression = expression;
+		if (!validate()) {
+			throw new ArithmeticException(ExceptionMessage.INVALID_EXPRESSION);
+		}
+	}
 
-    private boolean validate() {
-        Matcher matcher = EXPRESSION_FORMAT.matcher(expression);
-        return matcher.matches();
-    }
+	private boolean validate() {
+		Matcher matcher = EXPRESSION_FORMAT.matcher(expression);
+		return matcher.matches();
+	}
 
-    public String getExpression() {
-        return expression;
-    }
+	public List<String> split() {
+		return new ArrayList<>(List.of(expression.split(DELIMITER)));
+	}
 
-    public String[] split(String delimiter) {
-        return expression.split(delimiter);
-    }
+	public List<String> split(String delimiter) {
+		return new ArrayList<>(List.of(expression.split(delimiter)));
+	}
+
+	public void eliminateWhiteSpace() {
+		expression = expression.replaceAll("\\s", "");
+	}
 }
