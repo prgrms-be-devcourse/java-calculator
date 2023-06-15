ddc919cdf5d10de1fcdcd8f1ccbca203829a3390
SW-H <hsw08@naver.com>
2023-06-15 14:38:45 +0900

    feat : 함수 매핑 HashMap 삭제

--- src/main/java/co/programmers/domain/CalculatorApp.java
+++ src/main/java/co/programmers/domain/CalculatorApp.java
@@ -1,66 +1,53 @@
 package co.programmers.domain;
 
-import static co.programmers.domain.UserMenu.CALCULATE;
-import static co.programmers.domain.UserMenu.INQUIRY;
-import static co.programmers.domain.UserMenu.TERMINATE;
-
-import co.programmers.view.CalculatorInputView;
 import co.programmers.view.CalculatorOutputView;
 import co.programmers.view.InputView;
 import co.programmers.view.OutputView;
-import java.util.HashMap;
-import java.util.Map;
 
 public class CalculatorApp {
 
-    private final Map<Integer, Runnable> menuToCommandMapper = new HashMap<>();
-    private InputView inputView;
-    private OutputView outputView;
-
-    private CalculatorApp() {
-        menuToCommandMapper.put(INQUIRY.getMenu(), this::executeInquiryMenu);
-        menuToCommandMapper.put(CALCULATE.getMenu(), this::executeCalculationMenu);
-        menuToCommandMapper.put(TERMINATE.getMenu(), () -> {
-        });
-    }
+    private final InputView inputView;
+    private final OutputView outputView;
 
     public CalculatorApp(InputView inputView, OutputView outputView) {
-        this();
         this.inputView = inputView;
         this.outputView = outputView;
-    }
-
-    public static void main(String[] args) {
-        InputView inputView = new CalculatorInputView();
-        OutputView outputView = new CalculatorOutputView();
-        CalculatorApp calculatorApp = new CalculatorApp(inputView, outputView);
-        try {
-            calculatorApp.run();
-        } catch (IllegalArgumentException illegalArgumentException) {
-            System.out.println(illegalArgumentException.getMessage());
-        }
     }
 
     public void run() {
         UserMenu userMenu;
         do {
+            CalculatorOutputView.printMenuChoiceGuide();
             userMenu = UserMenu.get(inputView.inputUserMenu());
-            Runnable command = menuToCommandMapper.get(userMenu.getMenu());
-            command.run();
-        } while (userMenu != TERMINATE);
+            executeSelectedMenu(userMenu);
+        } while (userMenu != UserMenu.TERMINATE);
     }
 
-    public void executeInquiryMenu() {
+    private void executeSelectedMenu(UserMenu userMenu) {
+        switch (userMenu) {
+            case INQUIRY:
+                inquiry();
+                break;
+            case CALCULATE:
+                calculate();
+                break;
+            case TERMINATE:
+                break;
+        }
+    }
+
+    public void inquiry() {
         //TODO
     }
 
-    public void executeCalculationMenu() {
+    public void calculate() {
         try {
+            CalculatorOutputView.printCalculationGuide();
             Expression expression = inputView.inputExpression();
-            Calculator calculator = new Calculator(" ", expression.getExpression());
-            Integer output = calculator.calculate();
-            outputView.printCalculationRes(output);
-        } catch (ArithmeticException | IllegalArgumentException arithmeticException) {
+            Calculation calculator = new Calculation(expression);
+            Double output = calculator.calculate();
+            outputView.printCalculationResult(output);
+        } catch (ArithmeticException arithmeticException) {
             System.out.println(arithmeticException.getMessage());
         }
     }
