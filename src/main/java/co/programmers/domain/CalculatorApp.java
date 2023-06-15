a3544d812a776d042a193932335b2657663788dd
SW-H <hsw08@naver.com>
2023-06-15 23:27:13 +0900

    feat : 없는 메뉴선택시 처리 추가

--- src/main/java/co/programmers/domain/CalculatorApp.java
+++ src/main/java/co/programmers/domain/CalculatorApp.java
@@ -1,54 +1,58 @@
 package co.programmers.domain;
 
+import co.programmers.exception.ExceptionMessage;
 import co.programmers.view.CalculatorOutputView;
 import co.programmers.view.InputView;
 import co.programmers.view.OutputView;
 
 public class CalculatorApp {
 
-    private final InputView inputView;
-    private final OutputView outputView;
+	private final InputView inputView;
+	private final OutputView outputView;
 
-    public CalculatorApp(InputView inputView, OutputView outputView) {
-        this.inputView = inputView;
-        this.outputView = outputView;
-    }
+	public CalculatorApp(InputView inputView, OutputView outputView) {
+		this.inputView = inputView;
+		this.outputView = outputView;
+	}
 
-    public void run() {
-        UserMenu userMenu;
-        do {
-            CalculatorOutputView.printMenuChoiceGuide();
-            userMenu = UserMenu.get(inputView.inputUserMenu());
-            executeSelectedMenu(userMenu);
-        } while (userMenu != UserMenu.TERMINATE);
-    }
+	public void run() {
+		UserMenu userMenu;
+		do {
+			CalculatorOutputView.printMenuChoiceGuide();
+			userMenu = UserMenu.get(inputView.inputUserMenu());
+			executeSelectedMenu(userMenu);
+		} while (userMenu != UserMenu.TERMINATE);
+	}
 
-    private void executeSelectedMenu(UserMenu userMenu) {
-        switch (userMenu) {
-            case INQUIRY:
-                inquiry();
-                break;
-            case CALCULATE:
-                calculate();
-                break;
-            case TERMINATE:
-                break;
-        }
-    }
+	private void executeSelectedMenu(UserMenu userMenu) {
+		switch (userMenu) {
+			case INQUIRY:
+				inquiry();
+				break;
+			case CALCULATE:
+				calculate();
+				break;
+			case TERMINATE:
+				break;
+			default:
+				outputView.printMessage(ExceptionMessage.INVALID_INPUT);
+				break;
+		}
+	}
 
-    public void inquiry() {
-        //TODO
-    }
+	public void inquiry() {
+		//TODO
+	}
 
-    public void calculate() {
-        try {
-            CalculatorOutputView.printCalculationGuide();
-            Expression expression = inputView.inputExpression();
-            Calculation calculator = new Calculation(expression);
-            Double output = calculator.calculate();
-            outputView.printCalculationResult(output);
-        } catch (ArithmeticException arithmeticException) {
-            System.out.println(arithmeticException.getMessage());
-        }
-    }
+	public void calculate() {
+		try {
+			CalculatorOutputView.printCalculationGuide();
+			Expression expression = inputView.inputExpression();
+			Calculation calculator = new Calculation(expression);
+			Double output = calculator.calculate();
+			outputView.printCalculationResult(output);
+		} catch (ArithmeticException arithmeticException) {
+			System.out.println(arithmeticException.getMessage());
+		}
+	}
 }
