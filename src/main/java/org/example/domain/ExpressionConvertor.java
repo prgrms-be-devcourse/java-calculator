db0b40faf52106ce9e2dce66dc38dda1338dc290
charlesuu <tiger0192004@gmail.com>
2023-06-15 17:05:43 +0900

    fix: 계산식이 괄호로 시작할 때 발생하는 버그 수정

--- src/main/java/org/example/domain/ExpressionConvertor.java
+++ src/main/java/org/example/domain/ExpressionConvertor.java
@@ -5,13 +5,7 @@ import java.util.Stack;
 
 public class ExpressionConvertor {
     public String[] convertToPostfix(String infix) {
-        boolean isBracket = false;
-
-        if (infix.charAt(0) == '(') {
-            isBracket = true;
-        }
-
-        infix = trimSpaces(infix);
+        infix = removeSpaces(infix);
         String[] str = splitBySpaces(infix);
 
         ArrayList<String> sb = new ArrayList<>();
@@ -20,21 +14,19 @@ public class ExpressionConvertor {
         for (int i = 0; i < str.length; i++) {
             String now = str[i];
 
-            switch (now){
+            switch (now) {
                 case "+":
                 case "-":
                 case "*":
                 case "/":
+                case "(":
                     while (!stack.isEmpty() && priority(stack.peek()) >= priority(now)) {
                         sb.add(stack.pop());
                     }
                     stack.push(now);
                     break;
-                case "(":
-                    stack.push(now);
-                    break;
                 case ")":
-                    while(!stack.isEmpty() && !stack.peek().equals("(")){
+                    while (!stack.isEmpty() && !stack.peek().equals("(")) {
                         sb.add(stack.pop());
                     }
                     stack.pop();
@@ -48,28 +40,25 @@ public class ExpressionConvertor {
             sb.add(stack.pop());
         }
 
-        if(isBracket) {
-            sb.remove(0);}
-
         String[] result = new String[sb.size()];
 
-        for(int i = 0; i < sb.size(); i++) {
-            result[i]=sb.get(i);
+        for (int i = 0; i < sb.size(); i++) {
+            result[i] = sb.get(i);
         }
 
         return result;
     }
 
-    private String trimSpaces(String infix) {
-        infix = infix.replace("(", " ( ")
+    private String removeSpaces(String infix) {
+        infix = infix.replace(" ", "")
+                .replace("(", " ( ")
                 .replace(")", " ) ")
                 .replace("+", " + ")
                 .replace("-", " - ")
                 .replace("/", " / ")
                 .replace("*", " * ")
                 .replace("  ", " ")
-                .replace("  ", " ");
-
+                .trim();
         return infix;
     }
 
