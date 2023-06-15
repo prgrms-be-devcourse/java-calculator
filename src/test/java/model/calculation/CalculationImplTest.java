2ad3a201e935f591595eda4bd9f127f604f7bc8b
beombu <beombu13@gmail.com>
2023-06-16 03:16:32 +0900

    Test : 후위 연산 테스트

--- src/test/java/model/calculation/CalculationImplTest.java
+++ src/test/java/model/calculation/CalculationImplTest.java
@@ -9,14 +9,44 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 
 class CalculationImplTest {
+    private final CalculationImpl calculation = new CalculationImpl();
 
     @Test
     @DisplayName("후위연산 테스트")
-    void calculate() {
+    void calculate1() {
         //given
-        final Calculation calculation = new CalculationImpl();
         final CalculationResult calculationResult = new CalculationResult(8);
         final List<String> postfixExpression = List.of("3", "2", "4", "*", "+", "9", "3", "/", "-");
+
+        //when
+        CalculationResult result = calculation.calculate(postfixExpression);
+
+        //then
+        assertThat(result.getCalculationResult())
+                .isEqualTo(calculationResult.getCalculationResult());
+    }
+
+    @Test
+    @DisplayName("후위연산 테스트")
+    void calculate2() {
+        //given
+        final CalculationResult calculationResult = new CalculationResult(2);
+        final List<String> postfixExpression = List.of("5", "2", "*", "8", "-");
+
+        //when
+        CalculationResult result = calculation.calculate(postfixExpression);
+
+        //then
+        assertThat(result.getCalculationResult())
+                .isEqualTo(calculationResult.getCalculationResult());
+    }
+
+    @Test
+    @DisplayName("후위연산 테스트")
+    void calculate3() {
+        //given
+        final CalculationResult calculationResult = new CalculationResult(10);
+        final List<String> postfixExpression = List.of("7", "3", "/", "2", "4", "*", "+");
 
         //when
         CalculationResult result = calculation.calculate(postfixExpression);
