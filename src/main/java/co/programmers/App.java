84540a2181b3a2d06ea7b59521314e68b3ea6c0c
SW-H <hsw08@naver.com>
2023-06-15 20:16:00 +0900

    feat : main 함수

--- src/main/java/co/programmers/App.java
+++ src/main/java/co/programmers/App.java
@@ -8,10 +8,10 @@ import co.programmers.view.OutputView;
 
 public class App {
 
-    public static void main(String[] args) {
-        InputView inputView = new CalculatorInputView();
-        OutputView outputView = new CalculatorOutputView();
-        CalculatorApp calculatorApp = new CalculatorApp(inputView, outputView);
-        calculatorApp.run();
-    }
+	public static void main(String[] args) {
+		InputView inputView = new CalculatorInputView();
+		OutputView outputView = new CalculatorOutputView();
+		CalculatorApp calculatorApp = new CalculatorApp(inputView, outputView);
+		calculatorApp.run();
+	}
 }
