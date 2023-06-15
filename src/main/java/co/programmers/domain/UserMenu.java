8b4ec3a21b1451bf7e036d228d96d6306c8e5761
SW-H <hsw08@naver.com>
2023-06-15 22:51:42 +0900

    refactor : 미사용 메소드 삭제

--- src/main/java/co/programmers/domain/UserMenu.java
+++ src/main/java/co/programmers/domain/UserMenu.java
@@ -1,25 +1,22 @@
 package co.programmers.domain;
 
-import co.programmers.exception.ExceptionMessage;
 import java.util.Arrays;
 
+import co.programmers.exception.ExceptionMessage;
+
 public enum UserMenu {
-    INQUIRY(1), CALCULATE(2), TERMINATE(3), ERROR(4);
+	INQUIRY(1), CALCULATE(2), TERMINATE(3), ERROR(4);
 
-    private final Integer menu;
+	private final Integer menu;
 
-    private UserMenu(Integer value) {
-        this.menu = value;
-    }
+	private UserMenu(Integer value) {
+		this.menu = value;
+	}
 
-    public static UserMenu get(Integer input) {
-        return Arrays.stream(values())
-                .filter(menuNum -> menuNum.menu.equals(input))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException(ExceptionMessage.INVALID_INPUT));
-    }
-
-    public Integer getMenu() {
-        return menu;
-    }
+	public static UserMenu get(Integer input) {
+		return Arrays.stream(values())
+				.filter(menuNum -> menuNum.menu.equals(input))
+				.findFirst()
+				.orElseThrow(() -> new IllegalArgumentException(ExceptionMessage.INVALID_INPUT));
+	}
 }
