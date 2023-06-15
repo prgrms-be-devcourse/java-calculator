60442851072086a4d21bcf258b949e0f2bd13d6a
kys0411 <32180846@dankook.ac.kr>
2023-06-15 23:15:44 +0900

    feat: 연산자인지 숫자인지 검사하는 메서드 추가

---
+++ src/main/java/org/programmers/expression/ExpressionValidator.java
@@ -0,0 +1,17 @@
+package org.programmers.expression;
+
+import org.programmers.constant.Operator;
+
+public class ExpressionValidator {
+
+    public boolean isOperator(String symbol) {
+        return Operator.find(symbol).isPresent();
+    }
+
+    public boolean isNumber(String number) {
+        if (number.length() == 1) {
+            return number.charAt(0) >= '0' && number.charAt(0) <= '9';
+        }
+        return false;
+    }
+}
