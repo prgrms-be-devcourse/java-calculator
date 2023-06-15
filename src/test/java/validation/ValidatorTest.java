5dcd2aa1bfb7b4cd836bcc6d72919417800117a6
Younghoon Kim <the_basic_@naver.com>
2023-06-15 20:15:53 +0900

    refactor: @ParameterizedTest 반영 및 올바른 식 검증

--- src/test/java/validation/ValidatorTest.java
+++ src/test/java/validation/ValidatorTest.java
@@ -2,56 +2,40 @@ package validation;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ValidatorTest {
 
-    @Test
-    @DisplayName("괄호의 쌍 테스트")
-    void bracketsPairTest() {
-        String expr = "(3+4)*2";
-        boolean result = Validator.checkBrackets(expr);
-        assertThat(result).isTrue();
-
-        expr = "(3+4)*2)";
-        result = Validator.isRightExpr(expr);
-        assertThat(result).isFalse();
-
-        expr = "3+2*(2+6";
-        result = Validator.isRightExpr(expr);
-        assertThat(result).isFalse();
+    @ParameterizedTest
+    @DisplayName("올바르지 않은 괄호의 쌍 검증 테스트")
+    @ValueSource(strings = {"((3+4)*2", "(3+4)*2)", "3+2*(2+6", "3)+4*2"})
+    void bracketsPairWrongTest(String expression) {
+        assertThat(Validator.checkBrackets(expression)).isFalse();
     }
 
-    @Test
+    @ParameterizedTest
+    @DisplayName("올바른 괄호의 쌍 검증 테스트")
+    @ValueSource(strings = {"(3+4)*2", "3+(4*2)", "(3+2*2+6)", "((3+4)*2)"})
+    void bracketsPairTest(String expression) {
+        assertThat(Validator.checkBrackets(expression)).isTrue();
+    }
+
+    @ParameterizedTest
     @DisplayName("올바르지 않은 계산식 테스트")
-    void wrongExprTest() {
-        String expr = "1+";
-        boolean result = Validator.isRightExpr(expr);
-        assertThat(result).isFalse();
-
-        expr = "+1";
-        result = Validator.isRightExpr(expr);
-        assertThat(result).isFalse();
-
-        expr = "1++";
-        result = Validator.isRightExpr(expr);
-        assertThat(result).isFalse();
-
-        expr = "*";
-        result = Validator.isRightExpr(expr);
-        assertThat(result).isFalse();
-
-        expr = "((3+4)";
-        result = Validator.isRightExpr(expr);
-        assertThat(result).isFalse();
-
-        expr = "(3+4))";
-        result = Validator.isRightExpr(expr);
-        assertThat(result).isFalse();
-
-        expr = "3/45+-";
-        result = Validator.isRightExpr(expr);
-        assertThat(result).isFalse();
+    @ValueSource(strings = {"1+", "+1", "1++", "*", "((3+4)", "(3+4))", "3/45+-"})
+    void wrongExpressionWrongTest(String expression) {
+        assertThat(Validator.isRightExpression(expression)).isFalse();
     }
+
+    @ParameterizedTest
+    @DisplayName("올바른 계산식 테스트")
+    @ValueSource(strings = {"1+7", "11+1", "121+23", "87*17", "(31+4)", "(3*4)+4", "3+(4*3)"})
+    void wrongExpressionTest(String expression) {
+        assertThat(Validator.isRightExpression(expression)).isTrue();
+    }
+
+
 }
