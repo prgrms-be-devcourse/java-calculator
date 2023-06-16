83776a249bcb025d6cfe61113b7fa719e644a55a
SY97P <onetuks@naver.com>
2023-06-16 17:03:52 +0900

    test: Computer 테스트 코드 수정

--- src/test/java/engine/computer/ComputerTest.java
+++ src/test/java/engine/computer/ComputerTest.java
@@ -1,108 +1,33 @@
 package engine.computer;
 
+import com.devcourse.engine.model.accepter.Accepter;
 import com.devcourse.engine.model.computer.Computer;
 import com.devcourse.engine.model.converter.Converter;
 import com.devcourse.engine.model.exception.InvalidInputException;
 import com.devcourse.engine.model.validator.Validator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
 
 public class ComputerTest {
 
-    @Test
-    void computeTest1() {
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1+1,2.0", "1 + 2 * 5 -8/4,9.0", " 1,1.0",
+            "2. * 3,6.0", "2.*3,6", "1+2*(10-8)/4,2",
+            "1.2*2,2.4", "(1+2*(10-8))/4,1.25"
+    })
+    void computerTest(String input, double expected) {
+        List<String> infixExperssions = new Accepter().accept(input);
+        List<String> postfixExpressions = new Converter().convert(infixExperssions);
         Computer computer = new Computer();
-        String expression = "1+1";
-        Assertions.assertEquals(
-                2.0,
-                computer.compute(
-                        new Converter()
-                                .convert(
-                                        new Validator().
-                                                validate(expression)
-                                )
-                )
-        );
-    }
 
-    @Test
-    void computeTest2() {
-        Computer computer = new Computer();
-        String expression = "1 +2 * 5- 8/4";
-        Assertions.assertEquals(
-                9.0,
-                computer.compute(
-                        new Converter()
-                                .convert(
-                                        new Validator().
-                                                validate(expression)
-                                )
-                )
-        );
-    }
+        double result = computer.compute(postfixExpressions);
 
-    @Test
-    void computeTest3() {
-        Computer computer = new Computer();
-        String expression = "1 +";
-        Assertions.assertThrows(
-                InvalidInputException.class,
-                () -> computer.compute(
-                        new Converter()
-                                .convert(
-                                        new Validator().
-                                                validate(expression)
-                                )
-                )
-        );
-    }
-
-    @Test
-    void computeTest4() {
-        Computer computer = new Computer();
-        String expression = "\n";
-        Assertions.assertThrows(
-                InvalidInputException.class,
-                () -> computer.compute(
-                        new Converter()
-                                .convert(
-                                        new Validator().
-                                                validate(expression)
-                                )
-                )
-        );
-    }
-
-    @Test
-    void computeTest5() {
-        Computer computer = new Computer();
-        String expression = "(1+2*(10-8))/4";
-        Assertions.assertEquals(
-                1.25,
-                computer.compute(
-                        new Converter()
-                                .convert(
-                                        new Validator().
-                                                validate(expression)
-                                )
-                )
-        );
-    }
-
-    @Test
-    void computeTest6() {
-        Computer computer = new Computer();
-        String expression = "1+2*(10-8)/4";
-        Assertions.assertEquals(
-                2.0,
-                computer.compute(
-                        new Converter()
-                                .convert(
-                                        new Validator().
-                                                validate(expression)
-                                )
-                )
-        );
+        Assertions.assertEquals(expected, result);
     }
 
 }
