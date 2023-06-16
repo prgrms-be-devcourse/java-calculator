2b86871da9138c03f7710f86ead8e1812215a9f5
byeolhaha <j35635aa@gmail.com>
2023-06-16 11:19:04 +0900

    add : 계산 기능에 대한 test 코드 추가

--- src/test/java/com/programmers/java/calculator/calculate/CalculatorTest.java
+++ src/test/java/com/programmers/java/calculator/calculate/CalculatorTest.java
@@ -1,15 +1,47 @@
 package com.programmers.java.calculator.calculate;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    private Calculator calculator;
+
+    @BeforeEach
+    public void setUp() {
+        calculator = new Calculator();
+    }
+
     @Test
-    void getResult() {
-        Calculator calculator = new Calculator();
-        double result = calculator.getResult("2+3");
-        assertThat(result).isEqualTo(5);
+    void calculatePlus() {
+        double result = calculator.calculate("3 5 PLUS");
+        Assertions.assertEquals(8.0, result);
+    }
+
+    @Test
+    void calculateMinus() {
+        double result = calculator.calculate("3 5 MINUS");
+        Assertions.assertEquals(-2.0, result);
+    }
+
+    @Test
+    void calculateDivide() {
+        double result = calculator.calculate("8 4 DIVIDE");
+        Assertions.assertEquals(2, result);
+    }
+
+    @Test
+    void calculateMultiple() {
+        double result = calculator.calculate("3 5 MULTIPLE");
+        Assertions.assertEquals(15, result);
+    }
+
+    @Test
+    void calculate() {
+        double result = calculator.calculate("3 5 3 MULTIPLE PLUS");
+        Assertions.assertEquals(18, result);
     }
 }
