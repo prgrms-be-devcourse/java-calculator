abeb4ac6efb36d0494adf5b3299ae2cf4f074daa
ysng_ysng <yeasung67@gmail.com>
2023-06-16 16:24:29 +0900

    feat: CommandTest, HistoryStorageTest, OperatorTest를 구현했습니다.
    예외케이스와 성공케이스에 관한 테스트를 구현했습니다. 제 수준에 가능한 부분까지만 검증했습니다..!

--- src/test/java/view/CommandTest.java
+++ src/test/java/view/CommandTest.java
@@ -1,21 +1,22 @@
 package view;
 
+import exception.NoSuchCommandException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.NoSuchElementException;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class ViewMessageTest {
+class CommandTest {
     @ParameterizedTest
     @ValueSource(strings = {"", "     ", "\n", "qwer", "!@#$%"})
     @DisplayName("숫자가 아닌 입력이 들어온 경우")
     void getExceptionIfNoNumberInput(String noValidCommand) {
+        // given
+
         // when, then
-        assertThatThrownBy(() -> ViewMessage.getViewMessage(noValidCommand))
+        assertThatThrownBy(() -> Command.resolveCommand(noValidCommand))
                 .isInstanceOf(NumberFormatException.class);
     }
 
@@ -24,19 +25,21 @@ class ViewMessageTest {
     @DisplayName("유효하지 않은 숫자 입력이 들어올 경우 ")
     void getExceptionIfNoValidNumberInput(String noValidCommand) {
         // when, then
-        assertThatThrownBy(() -> ViewMessage.getViewMessage(noValidCommand))
-                .isInstanceOf(NoSuchElementException.class);
+        assertThatThrownBy(() -> Command.resolveCommand(noValidCommand))
+                .isInstanceOf(NoSuchCommandException.class);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"1", "2"})
     @DisplayName("정상적인 명령이 입력된 경우")
     void getValidViewMessageIfValidNumberInput(String validCommand) {
+        // given
+
         // when
-        ViewMessage viewMessage = ViewMessage.getViewMessage(validCommand);
+        Command viewMessage = Command.resolveCommand(validCommand);
         int commandNumber = Integer.parseInt(validCommand);
         // then
-        assertThat(viewMessage).isInstanceOf(ViewMessage.class);
+        assertThat(viewMessage).isInstanceOf(Command.class);
         assertThat(viewMessage.getCommandNumber()).isEqualTo(commandNumber);
     }
 }
