d923d2bb351bb5e82f2c8a473549ec76c6ba5e55
hongxeob <sjun2918@naver.com>
2023-06-16 12:42:07 +0900

    test : 클래스 삭제

--- src/main/java/com/programmers/core/calculator/AbstractCalculator.java
+++
@@ -1,8 +0,0 @@
-package com.programmers.core.calculator;
-
-import com.programmers.core.data.CalculationRequest;
-import com.programmers.core.data.CalculationResult;
-
-public interface AbstractCalculator {
-    CalculationResult calculate(CalculationRequest request);
-}
