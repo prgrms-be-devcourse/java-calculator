1eed9781a5d84e0cf6b47244c0bec8463e8b197c
byeolhaha <j35635aa@gmail.com>
2023-06-16 11:20:33 +0900

    add : 후위표기식 객체에 대한 test 코드 추가

---
+++ src/test/java/com/programmers/java/calculator/calculate/PostfixConverterTest.java
@@ -0,0 +1,50 @@
+package com.programmers.java.calculator.calculate;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostfixConverterTest {
+
+    private PostfixConverter postfixConverter;
+
+    @BeforeEach
+    public void setUp() {
+        postfixConverter = new PostfixConverter();
+    }
+
+    @Test
+    void postfixConvertPlus() {
+        List<String> tokenList = Arrays.asList(new String[]{"3", "+", "5"});
+        String postfixExpression = postfixConverter.postfixConvert(tokenList);
+        Assertions.assertEquals("3 5 PLUS ", postfixExpression);
+    }
+
+    @Test
+    void postfixConvertMultiple() {
+        List<String> tokenList = Arrays.asList(new String[]{"3", "-", "5"});
+        String postfixExpression = postfixConverter.postfixConvert(tokenList);
+        Assertions.assertEquals("3 5 MINUS ", postfixExpression);
+    }
+
+    @Test
+    void postfixConvertMix() {
+        List<String> tokenList = Arrays.asList(new String[]{"3", "+", "5", "*", "4"});
+        String postfixExpression = postfixConverter.postfixConvert(tokenList);
+        Assertions.assertEquals("3 5 4 MULTIPLE PLUS ", postfixExpression);
+    }
+
+    @Test
+    void postfixConvertException() {
+        List<String> tokenList = Arrays.asList("3", "!", "5", "*", "4");
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            postfixConverter.postfixConvert(tokenList);
+        });
+    }
+}
