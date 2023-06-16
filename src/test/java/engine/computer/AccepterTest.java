e5118d6dc56aadb90a54226c6505666733f81170
SY97P <onetuks@naver.com>
2023-06-16 16:45:57 +0900

    test: Accepter 테스트 코드 추가

--- src/test/java/engine/computer/AccepterTest.java
+++ src/test/java/engine/computer/AccepterTest.java
@@ -1,2 +1,24 @@
-package engine.computer;public class AccepterTest {
+package engine.computer;
+
+import com.devcourse.engine.model.accepter.Accepter;
+import com.devcourse.engine.model.validator.Validator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class AccepterTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "1+1,3", "1 + 2 * 5 -8/4,9", " 1,1",
+            "2. * 3,3", "2.*3,3", "1+2*(10-8)/4,11",
+            "1.2*2,3", "(1+2*(10-8))/4,13"
+    })
+    void acceptTest(String input, int expected) {
+        Accepter accepter = new Accepter();
+        int result = accepter.accept(input).size();
+        Assertions.assertEquals(expected, result);
+    }
+
 }
