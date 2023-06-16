e827477296786feebcb9015fd3f01713ccba54fe
yumyeonghan <mgtmh991013@gmail.com>
2023-06-16 15:20:59 +0900

    test: 계산기 테스트

---
+++ src/test/java/com/programmers/engine/model/OperationTest.java
@@ -0,0 +1,14 @@
+package com.programmers.engine.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class OperationTest {
+
+    @Test
+    void 입력된_수식_계산() {
+        String input = "1 + 2 * 3 - 4 / 2";
+        Integer result = Operation.calculate(input);
+        Assertions.assertThat(result).isEqualTo(5);
+    }
+}
