ce555f25643c6e24caf8471e0f6535265cedae21
beombu <beombu13@gmail.com>
2023-06-16 14:54:38 +0900

    Fix : Conflict 코드 수정

--- src/main/java/CalculatorApplication.java
+++ src/main/java/CalculatorApplication.java
@@ -3,29 +3,21 @@ import io.CalculatorInput;
 import io.CalculatorOutput;
 import io.CalculatorPrintOutput;
 import io.CalculatorScannerInput;
-<<<<<<< HEAD
-=======
 import model.calculation.Calculation;
 import model.calculation.CalculationImpl;
 import model.converter.Converter;
 import model.converter.PostfixConverter;
 import model.repository.CalculatorRepository;
 import model.repository.MemoryCalculatorRepository;
->>>>>>> 580d76c (Feat : 계산기 저장소 스캘레톤 코드)
 
 public class CalculatorApplication {
     public static void main(String[] args) {
         CalculatorInput input = new CalculatorScannerInput();
         CalculatorOutput output = new CalculatorPrintOutput();
-<<<<<<< HEAD
-
-        new CalculatorController(input, output).run();
-=======
         Converter converter = new PostfixConverter();
         Calculation calculation = new CalculationImpl();
         CalculatorRepository repository = new MemoryCalculatorRepository();
 
         new CalculatorController(input, output, converter, calculation, repository).runProgram();
->>>>>>> 580d76c (Feat : 계산기 저장소 스캘레톤 코드)
     }
 }
