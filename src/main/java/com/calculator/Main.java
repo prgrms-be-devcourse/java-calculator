241af2f64ea156b4edf34bae89e7ddffc05e8bdb
Na Yeongkyeong <102007066+na-yk@users.noreply.github.com>
2023-06-16 14:15:22 +0900

    chore: Initial setting

---
+++ src/main/java/com/calculator/Main.java
@@ -0,0 +1,11 @@
+package com.calculator;
+
+import com.calculator.calculation.Calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.run();
+
+    }
+}
