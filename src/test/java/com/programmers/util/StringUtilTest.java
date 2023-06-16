b860458475eb7d6f077de5797bab7d847faa8dce
hongxeob <sjun2918@naver.com>
2023-06-16 13:48:37 +0900

    test : StringUtil 테스트

---
+++ src/test/java/com/programmers/util/StringUtilTest.java
@@ -0,0 +1,25 @@
+package com.programmers.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class StringUtilTest {
+    @Test
+    @DisplayName("숫자인지 확인")
+    void testValidNumber() throws Exception {
+        assertTrue(StringUtil.isNumber("123"));
+        assertTrue(StringUtil.isNumber("0"));
+    }
+
+    @Test
+    @DisplayName("숫자외 형식")
+    void testInvalidNumber() throws Exception {
+        assertFalse(StringUtil.isNumber("asb"));
+        assertFalse(StringUtil.isNumber("!@#$"));
+        assertFalse(StringUtil.isNumber("-"));
+        assertFalse(StringUtil.isNumber("."));
+    }
+}
