96353c3116005e38db7fc9a7f31d2a4fcf6f1d3f
hyee0715 <hyee0715@naver.com>
2023-06-17 11:29:30 +0900

    test: Operator 테스트 코드 내용 수정

--- src/test/java/com/programmers/domain/OperatorTest.java
+++ src/test/java/com/programmers/domain/OperatorTest.java
@@ -7,36 +7,32 @@ import org.junit.jupiter.api.Test;
 class OperatorTest {
 
     @Test
-    void 입력된_값에_맞는_연산자_enum을_찾는다() {
+    void 입력된_값에_맞는_연산자를_찾는다() {
         //given
         //when
         Operator operator = Operator.findOperator("+");
 
         //then
-        assertThat(operator.name()).isEqualTo("ADDITION");
+        assertThat(operator).isEqualTo(Operator.ADDITION);
     }
 
     @Test
     void 입력된_연산자의_우선순위를_찾는다() {
         //given
-        int expected = 2;
-
         //when
         int result = Operator.findImportance("*");
 
         //then
-        assertThat(result).isEqualTo(expected);
+        assertThat(result).isEqualTo(2);
     }
 
     @Test
     void 연산자에_맞는_계산을_수행한다() {
         //given
-        int expected = 20;
-
         //when
         int result = Operator.MULTIPLICATION.calculate(2, 10);
 
         //then
-        assertThat(result).isEqualTo(expected);
+        assertThat(result).isEqualTo(20);
     }
 }
