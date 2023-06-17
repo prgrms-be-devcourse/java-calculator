8984cd5264b3da8b1d75f57524b519a7f771b9f7
heenahan <mby1997s@gmail.com>
2023-06-17 23:21:04 +0900

    feat : 저장소 List로 자료구조 변경

--- app/src/main/java/com/programmers/storage/Storage.java
+++ app/src/main/java/com/programmers/storage/Storage.java
@@ -1,28 +1,26 @@
 package com.programmers.storage;
 
-import com.programmers.util.Formula;
+import com.programmers.calculator.Formula;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 public class Storage {
 	
-	private final static Map<LocalDateTime, String> map = new ConcurrentHashMap<>();
+	private final static List<Formula> list = new ArrayList<>();
 	
-	public void save(Formula formula, Integer result) {
-		map.put(LocalDateTime.now(), formula.toString(result));
+	public void save(Formula formula) {
+		list.add(formula);
 	}
 	
 	public List<String> findAll() {
-		if (map.size() == 0) return null;
-		return map.keySet().stream()
-				       .sorted()
-				       .map(date -> map.get(date))
-				       .collect(Collectors.toList());
-		
+		if (list.size() == 0) {
+			return null;
+		}
+		return list.stream()
+			       .map(Formula::toString)
+			       .collect(Collectors.toList());
 	}
 	
 }
