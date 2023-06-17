8ec3a90c13d026c3f7498fd22e6744e8a58285d6
heenahan <mby1997s@gmail.com>
2023-06-17 23:28:25 +0900

    refactor : 서비스 레이어 리팩토링

--- app/src/main/java/com/programmers/service/CalculatorService.java
+++ app/src/main/java/com/programmers/service/CalculatorService.java
@@ -1,26 +1,22 @@
 package com.programmers.service;
 
-import com.programmers.calculator.Calculator;
 import com.programmers.storage.Storage;
-import com.programmers.util.Formula;
+import com.programmers.calculator.Formula;
 
 import java.util.List;
 
 public class CalculatorService {
 	
-	private final Calculator calculator;
 	private final Storage storage;
 	
 	public CalculatorService() {
-		this.calculator = new Calculator();
 		this.storage = new Storage();
 	}
 	
 	public Integer calculate(String stringOfFormula) {
 		Formula formula = new Formula(stringOfFormula);
-		List<String> postfix = calculator.changeInfixToPostfix(formula.getInfixFormula());
-		Integer result = calculator.calculatePostfix(postfix);
-		storage.save(formula, result);
+		Integer result = formula.calcualteFormula();
+		storage.save(formula);
 		return result;
 	}
 	
