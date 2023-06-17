e3f4c216d7ce16c7da51d48a38e05096588dcf53
hyee0715 <hyee0715@naver.com>
2023-06-17 11:43:23 +0900

    refactor: Console 내용 수정

--- src/main/java/com/programmers/io/Console.java
+++ src/main/java/com/programmers/io/Console.java
@@ -1,7 +1,11 @@
 package com.programmers.io;
 
+import com.programmers.domain.CalculatorRepository;
+import com.programmers.domain.Expression;
+import com.programmers.domain.Menu;
 import com.programmers.util.ExpressionProcessor;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Console implements Output, Input {
@@ -11,7 +15,7 @@ public class Console implements Output, Input {
     private static final String TERMINATION_MESSAGE = "계산기 프로그램을 종료합니다.";
 
     private final Scanner scanner = new Scanner(System.in);
-    private final ExpressionProcessor expressionProcessor = new ExpressionProcessor();
+    private final CalculatorRepository calculatorRepository = new CalculatorRepository();
 
     @Override
     public void printMenu() {
@@ -30,12 +34,51 @@ public class Console implements Output, Input {
     }
 
     public String getExpressionSpaceRemoved() {
+        System.out.print("\n");
         String expression = readInput();
-        return expressionProcessor.removeSpace(expression);
+        return removeSpace(expression);
+    }
+
+    public String removeSpace(String input) {
+        return input.trim().replace(" ", "");
     }
 
     @Override
     public void printTermination() {
         System.out.println("\n" + TERMINATION_MESSAGE);
+    }
+
+    public Menu getValidatedMenuSelection() {
+        String selection = getSelection();
+
+        try {
+            return Menu.findMenu(selection);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getValidatedMenuSelection();
+        }
+    }
+
+    public List<String> getValidatedExpression() {
+        String input = getExpressionSpaceRemoved();
+
+        try {
+            Expression expression = new Expression(input);
+            return expression.getValidatedTokens();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return getValidatedExpression();
+        }
+    }
+
+    public void printResult(int result) {
+        System.out.println(result + "\n");
+    }
+
+    public void getResults() {
+        System.out.println();
+        List<String> all = calculatorRepository.findAll();
+        all.forEach(System.out::println);
+        System.out.println();
     }
 }
