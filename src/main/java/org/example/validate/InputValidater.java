82a44357cee60dd0dfe6e9c6c7883afe2faa0e3b
charlesuu <tiger0192004@gmail.com>
2023-06-17 22:08:57 +0900

    refactor: REGEX 사용시 Pattern객체를 캐싱할 수 있도록 변경

--- src/main/java/org/example/validate/InputValidater.java
+++ src/main/java/org/example/validate/InputValidater.java
@@ -1,14 +1,15 @@
 package org.example.validate;
 
+import java.util.regex.Pattern;
+
 public class InputValidater {
     private static final String ERROR_MESSAGE_NOT_PROPER_COMMAND = "[ERROR] 숫자 1, 2 또는 3을 입력해주세요.";
+    private static final Pattern PROPER_COMMAND_REGEX = Pattern.compile("[123]");
 
-    public void validateExpression(String expression) {//추후 검증 로직 추가
-    }
 
     public void validateCommand(final String command) {
-        final String REGEX = "[123]";
-        if(!command.matches(REGEX)) {
+
+        if(!PROPER_COMMAND_REGEX.matcher(command).matches()) {
             throw new IllegalArgumentException(ERROR_MESSAGE_NOT_PROPER_COMMAND);
         }
     }
