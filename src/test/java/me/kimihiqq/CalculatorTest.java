d1abcc8c91768af9b6d64c7a5859f2d1b72c212d
kimihiqq <kimihiqq@gmail.com>
2023-06-26 15:41:40 +0900

    test: CalculatorTest 수정

--- src/test/java/me/kimihiqq/CalculatorTest.java
+++ src/test/java/me/kimihiqq/CalculatorTest.java
@@ -3,53 +3,44 @@ package me.kimihiqq;
 import me.kimihiqq.io.Input;
 import me.kimihiqq.io.Output;
 import me.kimihiqq.model.History;
-import org.junit.jupiter.api.DisplayName;
+import me.kimihiqq.options.Option;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 public class CalculatorTest {
+    private Input mockInput;
+    private Output mockOutput;
+    private History mockHistory;
+    private Calculator calculator;
 
-    @DisplayName("계산기 정상 계산 테스트")
-    @Test
-    public void testCalculate() {
-        History history = new History();
-        Input mockInput = Mockito.mock(Input.class);
-        Output mockOutput = Mockito.mock(Output.class);
-
-        when(mockInput.nextLine()).thenReturn("-1 + 2 * 3");
-
-        Calculator calculator = new Calculator(mockInput, mockOutput, history);
-        calculator.calculate();
-
-        String expectedHistory = "-1 + 2 * 3 = 5";
-        Map<Integer, String> allHistory = history.getAll();
-        assertTrue(allHistory.containsValue(expectedHistory));
+    @BeforeEach
+    public void setup() {
+        mockInput = Mockito.mock(Input.class);
+        mockOutput = Mockito.mock(Output.class);
+        mockHistory = Mockito.mock(History.class);
+        calculator = new Calculator(mockInput, mockOutput, mockHistory);
     }
 
-    @DisplayName("계산기 이력 조회 테스트")
     @Test
-    public void testList() {
-        History history = new History();
-        Input mockInput = Mockito.mock(Input.class);
-        Output mockOutput = Mockito.mock(Output.class);
+    public void testCalculationFlow() {
+        // Arrange
+        when(mockInput.nextLine("선택 : ")).thenReturn(Option.CALCULATE.getValue(), Option.CALCULATE.getValue(), Option.EXIT.getValue());
+        when(mockInput.nextLine()).thenReturn("-1 + 34.3 / 3 * 5.2", "13.3 - 2.5 * 5.9 / 1.1");
 
-        when(mockInput.nextLine()).thenReturn("5 + 6", "4 * 2");
+        // Act
+        calculator.run();
 
-        Calculator calculator = new Calculator(mockInput, mockOutput, history);
-        calculator.calculate();
-        calculator.calculate();
+        // Assert
+        verify(mockOutput, times(1)).printResult("58.453333333333326");
+        verify(mockHistory, times(1)).add("-1 + 34.3 / 3 * 5.2 = 58.453333333333326");
 
-        calculator.list();
+        calculator.run();
 
-        Map<Integer, String> allHistory = history.getAll();
-        String expectedHistory1 = "5 + 6 = 11";
-        String expectedHistory2 = "4 * 2 = 8";
-        assertTrue(allHistory.containsValue(expectedHistory1));
-        assertTrue(allHistory.containsValue(expectedHistory2));
+        verify(mockOutput, times(1)).printResult("-0.10909090909090757");
+        verify(mockHistory, times(1)).add("13.3 - 2.5 * 5.9 / 1.1 = -0.10909090909090757");
     }
 }
